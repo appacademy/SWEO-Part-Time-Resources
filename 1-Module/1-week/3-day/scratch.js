5e71f524edf3da85a5ea7e2a7cf79450fd8436f0
Mylo James <mjames@appacademy.io>
2021-09-15 19:58:21 -0500

    day3e

--- 1-Module/1-week/3-day/scratch.js
+++ 1-Module/1-week/3-day/scratch.js
@@ -1,4 +1,94 @@
 /* EAST */
+// console.log('tacos');
+// console.log("tacos");
+// console.log("What's up!");
+// // console.log('What's up!') what??
+// console.log('He said, "Hello"');
+// console.log('He said, "What\'s up?');
+
+// Length - property (a part of a data type that isn't a function)
+// Length starts at 1... more later
+// console.log("pizza".length);
+// console.log("5 _.!asdf".length);
+
+// Indexing
+// 'dogs'
+// // 0 - d, 1 - o, 2 - g, 3 - s
+// 'dogs'.length // 4
+
+// let animal = 'elephant';
+
+// // console.log the letter h;
+// console.log(animal[4]);
+
+// //console.log the letter t;
+// console.log(animal[animal.length - 1]);
+// console.log(animal[-1]); // not in JS :'(
+
+// indexOf - method (a part of a data type that is a function)
+// let game = "Dota 2 is cool, dude";
+// let o = ",";
+
+// console.log(game.indexOf("t"));
+// console.log(game.indexOf("x"));
+// console.log(game.indexOf("o"));
+// console.log(game.indexOf(" co"));
+// console.log(game.indexOf("d"));
+// console.log(game.indexOf(o))
+
+// Concat - join
+// let drink = 'coffee';
+// let flavor = 'pumpkin';
+
+// let fallDrink = flavor + " " + drink;
+
+// console.log(fallDrink);
+
+// let numOfCoffee = 3 + 1 + drink
+// console.log(numOfCoffee);
+
+// // String.toLowerCase()
+
+// Functions!
+function aName (optionalParams) {
+  // may have a return value
+}
+
+// // declare a function called two that console.logs the number two
+// function two () {
+//    console.log(2);
+//    return "Here is a string";
+// }
+
+// //Invoking functions
+// console.log(two());
+
+// declare a function called divide that intakes two numbers and returns the first
+// number divied by the second number
+
+// function divide (num1, num2) {
+//   return num1 / num2;
+// }
+
+// console.log(divide(4,2)); // 2 let num1 = 4, let num2 = 2
+// console.log(divide(6,2)); // 3
+
+// function test(num1, num2) {
+//   console.log(num1, num2)
+//   return num1 + num2
+// }
+
+// console.log(test(1))
+
+// too few args makes undefined
+// too many args we don't care
+
+
+
+
+
+
+
 
 
 /* WEST */
