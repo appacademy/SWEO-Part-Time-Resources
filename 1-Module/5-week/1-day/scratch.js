604819ff8e760139a0700acdf3888e594aaba821
William Vincent <william@william-vincent.dev>
2021-10-11 19:59:57 -0400

    scratch file

---
+++ 1-Module/5-week/1-day/scratch.js
@@ -0,0 +1,10 @@
+// let firstName = "Doctor";
+// let lastName = "Who";
+
+// let greeting = `Hello, ${firstName} ${lastName}!`;
+
+// let greeting = "Hello, " + firstName + " " + lastName + "!"
+// console.log(greeting);
+
+// let sentence = `5 + 5 = ${5 + 5}`;
+// console.log(sentence);
