53543552ecb593a1b1b18c8ea15f4607eb3c7382
mylo-james <mylo.james114@gmail.com>
2021-11-13 09:42:02 -0600

    changed rest to array

--- 2-Module/3-week/5-day/scratch.js
+++ 2-Module/3-week/5-day/scratch.js
@@ -1,4 +1,4 @@
-const sum = (...nums) => {
+const sum = (nums) => {
   const sums = [0];
   for (let i = 0; i < nums.length; i++) {
     let num = nums[i];
@@ -7,15 +7,15 @@ const sum = (...nums) => {
   return sums[sums.length - 1];
 }
 
-const sum = (...nums) => {
+const sum = (nums) => {
   return nums.reduce((sum, num)=> sum + num);
 }
 
-const sum = (...nums) => {
+const sum = (nums) => {
   for (let i = 1; i < nums.length; i++) {
     nums[i] = nums[i - 1] + nums[i];
   }
   return nums[nums.length - 1];
 }
 
-console.log(sum(1,2,3,4,5))
+console.log(sum([1,2,3,4,5]))
