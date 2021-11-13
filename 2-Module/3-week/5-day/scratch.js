0998cc953de9136b7d460b4364865a38d635f9c1
mylo-james <mylo.james114@gmail.com>
2021-11-13 09:35:40 -0600

    m2w3d5

---
+++ 2-Module/3-week/5-day/scratch.js
@@ -0,0 +1,21 @@
+const sum = (...nums) => {
+  const sums = [0];
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i];
+    sums.push(sums[sums.length - 1] + num)
+  }
+  return sums[sums.length - 1];
+}
+
+const sum = (...nums) => {
+  return nums.reduce((sum, num)=> sum + num);
+}
+
+const sum = (...nums) => {
+  for (let i = 1; i < nums.length; i++) {
+    nums[i] = nums[i - 1] + nums[i];
+  }
+  return nums[nums.length - 1];
+}
+
+console.log(sum(1,2,3,4,5))
