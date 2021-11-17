8457efc621c990cc05d2bb622b4f85bdf3e44cc0
mitchellreiss <mitchellreiss@gmail.com>
2021-11-16 16:44:01 -0800

    Add scratch starting code

---
+++ 2-Module/4-week/2-day/scratch.js
@@ -0,0 +1,21 @@
+class Queue {
+  constructor() {
+    this.store = new Array()
+  }
+
+  size() {
+    // returns the length of the queue
+  }
+
+  enqueue(ele) {
+    //adds ele to end of queue
+  }
+
+  dequeue() {
+    //remove the first element
+  }
+
+  peek() {
+    //return the first element
+  }
+}
