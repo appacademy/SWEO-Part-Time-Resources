1c0e7bc697704de0cbebbc2751816b071db248b0
Philip Ling <66927690+philling83@users.noreply.github.com>
2021-12-14 00:02:05 -0500

    Update scratch.js

---
+++ 1-Module/5-week/1-day/scratch.js
@@ -0,0 +1,14 @@
+let firstName = "Philip";
+let lastName = "Ling";
+
+let middleName = (str) => {
+    return str;
+}
+
+console.log("Hello " + firstName + " " + lastName);
+console.log(`Hello ${firstName} ${lastName}`);
+console.log(`I got ${5+1+4}/10 on the test.`);
+console.log(`I have a pet ${false ? "dog" : "cat"}`);
+console.log(`My middle name is ${middleName("John")}`);
+console.log(`Sky is blue.\nGrass is green`);
+console.log(`Tom 'said', "Good job on the test!"`);
