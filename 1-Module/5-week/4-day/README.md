2c540322ad8d15ba1f7339f0b09aa78a0fd81f11
tombetthauser <tombetthauser@gmail.com>
2021-12-16 14:42:56 -0800

    Add M1W5D4 recursion review notes

---
+++ 1-Module/5-week/4-day/README.md
@@ -0,0 +1,22 @@
+# M1W5D4
+
+## PRACTICE DAY
+
+### Default Parameters Demo (30 min)
+
+* exponent example with default squaring
+* show stack frame comments example
+* mdn page breakdown
+* stack frames diary / persistent array example
+
+### Solving Problems Recursively Project pt.3 (2 hours)
+
+* fibonacci
+* factorial
+* flatten
+* is sorted
+
+### Problem Set Walk-through (30 min)
+
+* multiple solutions for fibonacci
+* default parameter with flatten
---
+++ 1-Module/5-week/4-day/scratch.js
@@ -0,0 +1,133 @@
+// Default Parameters
+
+function exponent(num, power) {
+  // 
+}
+// exponent(2, 3); <--- stack frame notes
+
+
+console.log(exponent(2, 0)); // 1
+console.log(exponent(2, 1)); // 2
+
+console.log(exponent(3, 2)); // 9
+console.log(exponent(5, 5)); // 3125
+
+console.log(exponent(2, 2)); // 4
+console.log(exponent(2, 3)); // 8
+console.log(exponent(2, 4)); // 16
+console.log(exponent(2, 5)); // 32
+console.log(exponent(2, 6)); // 64
+
+console.log(exponent(2)); // 4
+console.log(exponent(3)); // 9
+
+
+/***********************************************************************
+Write a recursive function called `fibonacci` that takes an integer, `n`,
+and returns the `n`th number in the Fibonacci sequence.
+
+Not familiar with the Fibonacci sequence? Beginning with 0 and 1, we add the two
+previous numbers in the sequence together to form the next one:
+
+0, 1, 1, 2, 3, 5, 8, etc....
+
+We count Fibonacci numbers beginning with the first 1. Take a look at the
+examples below if you're unsure where to start!
+
+Examples:
+
+fibonacci(1); // 1
+fibonacci(2); // 1
+fibonacci(3); // 2
+fibonacci(4); // 3
+fibonacci(10); // 55
+***********************************************************************/
+
+function fibonacci(n) {
+  // Your code here
+}
+
+// console.log(fibonacci(1)); // 1
+// console.log(fibonacci(2)); // 1
+// console.log(fibonacci(3)); // 2
+// console.log(fibonacci(4)); // 3
+// console.log(fibonacci(10)); // 55
+
+
+/***********************************************************************
+Write a recursive function called `factorial` that takes an integer, `num`,
+and returns the factorial of `num`. Assume the value of `num` is greater 
+than or equal to 1.
+
+A factorial is the number get when multiplying a number by itself minus one
+all the way down to 1 (but not 0)! We represent them with an exclamation
+point, also sometimes called a "bang" in programming.
+
+5! = 5 x 4 x 3 x 2 x 1 = 120
+
+Examples:
+
+factorial(1); // 1
+factorial(3); // 6
+factorial(5); // 120
+***********************************************************************/
+
+function factorial(num) {
+  // Your code here
+}
+
+// console.log(factorial(1)); // 1
+// console.log(factorial(3)); // 6
+// console.log(factorial(5)); // 120
+
+
+
+
+
+
+
+
+
+
+/***********************************************************************
+Write a recursive function called `flatten` that takes a single array with
+any number of nested arrays and returns and array with all the nested
+contents on one level.
+
+Examples:
+
+flatten([]); // []
+flatten([1, 2]); // [1, 2]
+flatten([1, [2, [3]]]); // [1, 2, 3]
+***********************************************************************/
+
+function flatten(arr) {
+  // Your code here
+}
+
+// console.log(flatten([])); // []
+// console.log(flatten([1, 2])); // [1, 2]
+// console.log(flatten([1, [2, [3]]])); // [1, 2, 3]
+
+
+/***********************************************************************
+Write a recursive solution called `isSorted` to determine if the input array
+is sorted in ascending order.
+
+Examples:
+
+isSorted([1, 2, 3, 4, 5]); // true
+isSorted([1, 2, 4, 3, 5]); // false
+isSorted([2, 4, 6, 7, 8]); // true
+isSorted([5, 4, 3, 2, 1]); // false
+***********************************************************************/
+
+
+function isSorted(arr) {
+  // Your code here
+}
+
+// console.log(isSorted([1, 2, 3, 4, 5])); // true
+// console.log(isSorted([1, 2, 4, 3, 5])); // false
+// console.log(isSorted([2, 4, 6, 7, 8])); // true
+// console.log(isSorted([5, 4, 3, 2, 1])); // false
