0448fc426bf580768db7e49756a86bd635a91adc
baylend123 <baylendoss12@gmail.com>
2022-02-21 20:55:07 -0600

    week 6 day 1

--- 1-Module/6-week/1-day/scratch.js
+++ 1-Module/6-week/1-day/scratch.js
@@ -1,35 +1,34 @@
-// Example of non async code 
-// const slowCode = () => {
-//     console.log('I just called an api')
 
-//     let i = 0;
-//     console.log('loading.....')
-//     while( i < 10000000000){
-//         i++
-        
-//     }
-//     if(i === 10000000000) {
-//         console.log('My api call done')
-//     }
-//     console.log('I am continuing to run')
 
-//     console.log('code complete')
 
-// }           
-const fastCode = () => {
-    console.log('I just called an api')
-
-    let i = 0;
+const slowCode =  () => {
+    console.log(
+        'Before my code'
+    )
+    // let i = 0
+    console.log('Loading...')
     setTimeout(() => {
-        console.log('Api Complete')
+        console.log('Executing code')
     }, 0)
-    
-    console.log('I am continuing to run')
-
-    console.log('code complete')
-
-}           
+    // while(i < 10000000000){
+    //     i++
+    // }
+    // if(i === 10000000000){
+    //     console.log('I have finished')
+    // }
+    console.log('Done')
+}
 
 // slowCode()
+
+const fastCode = () => {
+    console.log(
+        'one')
+    console.log('2')
+    setInterval(() => {
+        console.log('Complex code')
+    }, 1000)
+    console.log('4')
+}
+
 fastCode()
-//more stuff
