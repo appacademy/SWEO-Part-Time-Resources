6921de28ed00bb7be6de8521eb6563f1af38c0ca
Zaviar Brown <brown.zaviar@gmail.com>
2022-03-15 18:13:40 -0700

    3/15 lecture notes

---
+++ 1-Module/1-week/2-day/scratch.js
@@ -0,0 +1,167 @@
+// This is a comment!
+/*
+These are comments
+whoohoo
+*/
+
+/*
+Number
+console.log(100);
+console.log(-100);
+console.log(100.5 + 0.5);
+console.log("String")
+
+string + number = string
+string * / - number = number
+*/
+
+// console.log(7 % 2); // this number is odd, so = 1
+// console.log(8 % 2); // this number is even, so = 0
+// console.log(3 % 100); // 3
+// console.log(26 % 5); // 1
+// console.log(-1 * 50);
+// console.log((5 + 2) * 7);
+
+// console.log('100' * 5);
+// console.log('100' + 1);
+// console.log("butter" + "fly")
+// console.log("100" * "2" + "hello")
+// console.log("hello" - " world") // NaN
+// console.log("hello" * " world") // NaN
+// console.log("hello" / " world") // NaN
+
+// string + number = string
+// string * / - number = number
+
+// console.log(5);
+// console.log('Hello');
+// console.log(true && false);
+// console.log((true && false) || !true);
+// console.log(!false)
+// console.log(aefe)
+// console.log(false || aefe); //errors
+// console.log(true || aefe); //does not error - short circuit
+
+// console.log(100 > 1000)
+// console.log("Z" < "a") // true
+// console.log("z" < "a") // false
+
+// console.log((5 + 20) < 20) // false
+// console.log(5 + 20 >= 30) // false
+
+// Single = is the ASSIGNMENT operator
+// == Loosely Equal
+// === STRICTLY Equal
+// console.log(5 + 1 = 6)
+
+// console.log(5 === 5)
+// console.log(5 === Number("5"))
+// console.log(5 === "5")
+// console.log(5 == "5")
+
+// console.log(5 !== '5'); // true
+// console.log('abc' < 'xyz'); // true
+// console.log(5 !== '5' && 'abc' < 'xyz'); // true
+// console.log([] === 0) // false
+
+// let firstName = "Zaviar"
+// console.log(firstName)
+
+// let firstName;
+// let firstNameAgain;
+// console.log(firstName);
+// console.log(firstNameAgain);
+
+// firstName = 'Zaviar';
+// firstNameAgain = ' Zach';
+// console.log(firstName);
+// console.log(firstNameAgain);
+// console.log(firstName + firstNameAgain)
+
+// console.log(num);
+// let num = 5;
+// console.log(num);
+
+//? Naming conventions
+
+// let firstName;
+// firstName = 'Zaviar';
+
+// let aNum = 5;
+// let firstFavorite = "Dog"
+// let firstInputFavoriteAnimal = "Dog"
+// let first_input_fav = "my fav thing"
+// console.log(first_input_fav)
+// console.log("Hello", firstName)
+// console.log("This = 10", aNum + 5)
+
+// let num = 5;
+// num++
+// num += 2
+// num = num + 4
+// num + 1000000
+// console.log(num)
+
+// let num = 5;
+// console.log(num);
+// num--;
+// console.log(num);
+// num -= 2;
+// console.log(num);
+// num = num - 4;
+// console.log(num);
+// num - 1000000;
+// console.log(num);
+
+// let name = "Zaviar"
+// name + "5"
+// console.log(name) // Just prints Zaviar
+
+// let num = 6;
+// console.log(num);
+// num++
+// console.log(num);
+
+// console.log(num); // 6
+// console.log(num++); // 6
+// console.log(num); // 7
+// console.log(++num); // 8
+// console.log(num); // 8
+
+// console.log('___________________');
+// let aNewNum = ++num;
+// console.log(aNewNum); // 9
+// console.log(num); // 9
+// console.log('___________________');
+// let aNewNum = num++;
+// console.log(aNewNum); // 8
+// console.log(num); // 9
+// aNewNum++
+// aNewNum++
+// aNewNum++
+// aNewNum++
+
+// console.log(aNewNum);
+// console.log(num);
+
+// let num = 5;
+// num += 1
+// console.log(num)
+// num -= 1
+// console.log(num)
+// num *= 5
+// console.log(num)
+// num /= 5
+// console.log(num)
+
+// let num = 31;
+// let isPositive = num > 0;
+// let isEven = num % 2 === 0;
+// console.log(isPositive && isEven);
+
+// console.log(5 + 5 === 10)
+// console.log("5" + "5" === "55")
+
+// console.log("three" / 3 === NaN)
+// let myNotNum = NaN
+// console.log(myNotNum === NaN)
