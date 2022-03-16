77bab997c23115b465afb4c163ed0d434a37d9ba
Zaviar Brown <brown.zaviar@gmail.com>
2022-03-16 14:29:57 -0700

    3/15 lecture notes

---
+++ 1-Module/1-week/2-day/scratch.js
@@ -0,0 +1,177 @@
+// This is a comment
+// console.log("This runs still") // this is a comment too
+
+/*
+Block
+Comments
+Are 
+Great
+*/
+
+// console.log(5);
+// console.log("Hello");
+// console.log("5");
+
+// console.log(100);
+// console.log(100.5);
+// console.log(-100.5);
+
+// console.log(5 + 5);
+// console.log(5 - 5);
+// console.log(5 / 5);
+// console.log(5 * 5);
+
+// + Changes type to string
+// console.log("100" + 5);
+// console.log('100' + '100');
+
+// - * / Changes type to number
+// console.log(5 - "1");
+// console.log("100" * 5);
+// console.log("100" / 5);
+// console.log('100' - '100');
+// console.log('100' * '100');
+// console.log('100' / '100');
+
+// console.log(10 % 2);
+// console.log(11 % 2);
+// console.log(10 % 11);
+
+// console.log(5 + 2 * 6);
+// console.log(5 + (2 * 6));
+// console.log((5 + 2) * 6);
+
+// console.log(5)
+// console.log("Hello")
+// console.log(!true)
+// console.log(!false)
+// console.log(true)
+// console.log(false)
+// console.log(true && false)
+// console.log(true || false && true)
+// console.log(true && false || true)
+
+// console.log(true && false || ( true && true || !false || true)) // true
+
+// console.log(true || (false && false && false && false))
+
+// console.log(true || aefaefef) // does not error, short circuits
+// console.log("Hello")
+
+// console.log((true && false) || !true);
+
+// console.log(5 < 10);
+// console.log(5 > 10);
+// console.log('a' < 'z'); // true - a before z
+// console.log('!' < 'A'); // false - capital before lowercase
+// console.log("A" < "a"); // true - capital before lowercase
+
+// console.log(5 <= 10)
+// console.log(5 >= 10)
+// console.log(5 >= 5)
+// console.log(5 <= 5)
+
+// = is the ASSIGNMENT operator
+// == is Loosely equal
+// === is STRICTLY equal
+
+// console.log(5 === 5)
+// console.log(5 == 5)
+// console.log(5 === "5")
+// console.log(5 == "5")
+// console.log(5 === Number("5"))
+// console.log(5 === 5)
+// console.log(5 === 5)
+
+// console.log(5 !== Number('5'));
+// console.log(5 !== '5');
+// console.log([] == 0)
+// console.log("5" + 5)
+// console.log(5 - "Hello" === 5 - "Hello") // false
+// console.log(NaN === NaN) // false
+// console.log(NaN == NaN)
+// console.log('abj' < "abc")
+
+// let number = 5;
+// let firstName = 'Zaviar';
+// let lastName = 'Brown';
+// let firstChoiceFavoriteAnimal = 'Dog';
+// let first_choice_favorite_animal = 'Snake';
+// let favoriteAnimal = 'Dog';
+// console.log(number);
+// console.log(firstName);
+// console.log(firstChoiceFavoriteAnimal);
+
+// Variable declaration
+// let num;
+
+// Variable assignment
+// num = 5
+
+// Variable initialization
+// let num = 5;
+// console.log(num)
+// num = num + 1
+// num += 1
+// num++
+// console.log(num)
+// console.log(num++)
+
+// let number = ++num
+// console.log(number)
+// console.log(num)
+// console.log(++num)
+// console.log(num)
+
+// num -= 1
+// console.log(num)
+// num * 5
+// num *= 5
+// num * 5
+// num = num ** 5
+// console.log(num)
+
+// let name = "Zaviar"
+// let otherName = "Zach"
+// console.log(name + otherName)
+// console.log(name, otherName, num)
+
+// let myVariable = true;
+// let otherVariable = 5 > 100;
+
+// console.log(myVariable && otherVariable)
+// console.log(myVariable || otherVariable)
+
+// let myNum = 3 % 6 - 4 / 2 + (5 * 3)
+//  console.log(myNum)
+
+// let num = 5;
+// console.log(num) // 5
+// num++
+// console.log(num) // 6
+// num--
+// console.log(num) // 5
+// let num = 5;
+// console.log(num) // 5
+// ++num
+// console.log(num) // 6
+// --num
+// console.log(num) // 5
+// let number = 10;
+// console.log(num)
+
+
+// num = num + number;
+// console.log(num)
+// num++;
+// console.log(num)
+// ++num;
+// console.log(num)
+// num += number;
+// console.log(num)
+// num++
+// let newNum = ++num + 5
+// let newNum = num
+// let newNum = num++ + 5
+// console.log(newNum)
+// console.log(num)
