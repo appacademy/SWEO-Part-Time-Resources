76ca23b6c0618a10eb704838e9ee3cb7c2003dd4
Zaviar Brown <brown.zaviar@gmail.com>
2022-03-16 22:08:55 -0700

    3/16 lecture notes

---
+++ 1-Module/1-week/3-day/scratch.js
@@ -0,0 +1,91 @@
+// console.log('tacos');
+// console.log('tacos');
+// console.log("Let's go to the park");
+// console.log("Let's go to the park");
+// console.log('Zach says, "Hey everyone, Let\'s go to the park"');
+
+//! Length property
+let myString = 'Zaviar';
+let str = 'I want to eat tacos';
+// let str2 = 'Iwanttoeattacos';
+// console.log(myString);
+// console.log(myString.length);
+// console.log(str.length);
+// console.log(str2.length);
+
+// console.log(myString[0]);
+// console.log(myString[myString.length]); // index 6
+// console.log(myString[myString.length - 1]); // index 5
+// console.log('Hello everyone in PST'.length);
+// console.log('Hello everyone in PST'[6]);
+
+//! indexOf
+// console.log(myString.indexOf("a"))
+// console.log(str.indexOf("at t"))
+// console.log(str[str.indexOf("at t")])// these are the same
+// console.log(str[11]) // these are the same
+// let tacos = "tacos tacos tacos tacos tacos tacos tacos tacos"
+// console.log(tacos.lastIndexOf("a"))
+// console.log(tacos.indexOf("acos tacos tacos tacos"))
+
+// console.log(myString + " " + str)
+// //! console.log() can be written on mutliple lines, no difference here.
+// console.log(
+//   myString[0] +
+//     str[3] + // these are the same
+//     myString[2] +
+//     myString[3] +
+//     str[str.indexOf('a')] + // these are the same
+//     myString[5]
+// );
+
+// function theNameOfMyFunction() {
+
+// }
+
+function addFive(num) {
+  return num + 5;
+}
+
+let myVariable = addFive(5);
+
+// console.log(myVariable)
+
+function addSix(num) {
+  console.log(num + 6);
+  return 'Hello';
+  console.log(num + 100);
+}
+
+// let myVar = addSix(4);
+
+// console.log(myVar);
+
+// function printZaviar() {
+//   console.log('Zaviar');
+//   console.log(age);
+// }
+
+// function printZaviar(name) {
+//   console.log('Zaviar');
+
+// }
+
+// function printOutManyThings(str1, str2, num1, num2) {
+//   console.log(str1);
+//   console.log(str2);
+//   console.log(num1);
+//   console.log(num2);
+//   return num2;
+// }
+// printZaviar("Zach");
+
+// let answer = printOutManyThings(5, 'Zach', 102834, 'Zaviar');
+// console.log(answer[0]);
+// printOutManyThings(5, 'Zach', 102834, 'Zaviar');
+// printOutManyThings(500, 'Zaefefach', 102834, 'Zavefaefeiar');
+// printOutManyThings(5, 'Zach', 102834, 'Zaviar');
+// printOutManyThings(5, 'Zach', 102834, 'Zaviar');
+// printOutManyThings(5, 'Zach', 102834, 'Zaviar');
+
+
