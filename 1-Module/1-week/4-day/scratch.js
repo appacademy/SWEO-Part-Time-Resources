fc958dd966e8d11bdeeffc1fd380ad22436c55e0
Zaviar Brown <brown.zaviar@gmail.com>
2022-03-17 23:38:37 -0700

    3/17 lecture notes

---
+++ 1-Module/1-week/4-day/scratch.js
@@ -0,0 +1,114 @@
+// let name = 'Zaviar';
+// let name = 'Zach';
+// console.log(name === "Zaviar")
+// console.log(name === "Zaviar")
+// if (name === 'Zaviar') {
+//   console.log("That's the cohort instructor");
+// } else if (name === 'Zach') {
+//   console.log("That's the traitor who's leaving us");
+// } else {
+// 	console.log("Idk who that is")
+// }
+
+// function myFunc() {
+// let num = 2;
+
+// if (true) {
+//   console.log('Hi');
+// }
+// if (true) {
+//   console.log('Hi');
+// }
+// if (true) {
+//   console.log('Hi');
+// }
+//!
+// if (false) {
+//   console.log('Hi');
+// }
+// else if (true) {
+//   console.log('Hello');
+// }
+// else if (true) {
+//   console.log('heyoo');
+// }
+// return "That's smaller than 4";
+//!
+// else if (num < 4) {
+//   return "That's smaller than 4";
+// }
+
+// else {
+//   return "That's smaller than 4";
+// }
+// }
+
+// console.log(myFunc());
+
+// if (false) {
+// 	console.log("This value was truthy")
+// } else {
+// 	console.log("This value was falsy")
+// }
+
+// let str = 'Zach';
+// let num = 0;
+
+// while (num < str.length) {
+//   console.log(str[num]);
+//   num++;
+//   // console.log(num);
+//   // if (something is true) {
+//   // 	num--
+//   // }
+//   // console.log('------------------------------------------------');
+// }
+
+// // (starting point; when I want to stop; how much to increment)
+
+// for (let i = 0; i < str.length; i++) {
+// 	console.log(str[i])
+// }
+
+function isOdd(num) {
+  // console.log(num);
+  if (num % 2 > 0) {
+    return true;
+  } else {
+    console.log(num % 2);
+    return false;
+  }
+}
+
+// console.log(isOdd(2)); // => false
+// console.log(isOdd(5)); // => true
+// console.log(isOdd(-17)); // => true
+// let arr = [1, 'hello', false, NaN, undefined];
+
+// let arr = [1, 'hello', false, NaN, [1, 2, 3], isOdd, isOdd(5)];
+
+// console.log(arr.indexOf(false));
+// console.log(arr[2]);
+// console.log(arr.length);
+// console.log(arr[arr.length - 1])
+
+// let arr1 = [0, 1, 2]
+// let arr2 = [3, 4, 5, 6]
+
+// console.log(arr1.concat(arr2))
+// console.log(arr2.concat(arr1))
+
+// console.log(arr1)
+// arr1.push(-1)
+// console.log(arr1)
+// arr1.pop()
+// console.log(arr1)
+// arr1.pop()
+// arr1.pop()
+// console.log(arr1)
+// arr1.pop()
+// console.log(arr1)
+// arr1.pop()
+// arr1.pop()
+// arr1.pop()
+// console.log(arr1.length)
