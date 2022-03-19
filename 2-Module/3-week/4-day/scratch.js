bc314e19502bd4dfb1bf3ee9125f538204d4f2ed
baylen doss <baylendoss@baylens-Air.lan>
2022-03-19 10:08:00 -0500

    Day 4

--- 2-Module/3-week/4-day/scratch.js
+++ 2-Module/3-week/4-day/scratch.js
@@ -1,16 +1,16 @@
 
 
-// let myLogFunc = (n) => { // Time Complexity O(log(n)) because I am only looping to half my input.  Less than linear more than constant 
+// let myLogFunc = (n) => { // Time Complex Log O(log(n)) Because I am only looping through half my array, less than O(n) more than O(1)
 //     let arr = []
 //     for(let i = 0; i < n /2; i++){
 //         arr.push(i)
 //     }
 // }
 
-// myNLogNFunc = (n) => { // O(n log(n)) because we have a log loop inside of a linear loop.
+// myNLogNFunc = (n) => { // O(n log(n)) Higher than linear but less than quadratic
 //     let arr = []
-//     for(let i = 0; i < n; i++){
-//         for(let j = 0; j < n /2 ;j++){
+//     for(let i = 0; i < n; i++){ // O(n)
+//         for(let j = 0; j < n /2 ;j++){ // O(log(n))
 //             arr.push(j)
 //         }
 //     }
@@ -41,52 +41,66 @@
 // 18
 // 19
 // 20
-// 21
-// 22
-// 23
-// 24
 
 
+// Binary is base 2 meaning we only have digits, 0 and 1
+// when we run out of digits the left increases and right got back to 0
+// each number of binary is one bit, and 8 bits are a byte 
+// one letter is one byte, 8 bits of information
 // // 0b01101100011
 // // Base 2, because there is only 2 digits
+// Ob in the front to tell people this is binary
 
 // 0 - 0000
-// console.log(parseInt('0000', 2))
 // 1 - 0001
-// console.log(parseInt('0001', 2))
 // 2 - 0010
-// console.log(parseInt('0010', 2))
 // 3 - 0011
-// console.log(parseInt('0011', 2))
 // 4 - 0100
-// console.log(parseInt('0100', 2))
 // 5 - 0101
-// console.log(parseInt('0101', 2))
 // 6 - 0110
-// console.log(parseInt('0110', 2))
 // 7 - 0111
-// console.log(parseInt('0111', 2))
 // 8 - 1000
-// console.log(parseInt('1000', 2))
 // 9 - 1001
-// console.log(parseInt('1001', 2))
 // 10 - 1010
-// console.log(parseInt('1010', 2))
 // 11 - 1011
-// console.log(parseInt('1011', 2))
 // 12 - 1100
-// console.log(parseInt('1100', 2))
 // 13 - 1101
-// console.log(parseInt('1101', 2))
 // 14 - 1110
-// console.log(parseInt('1110', 2))
 // 15 - 1111
-// console.log(parseInt('1111', 2))
 // 16 - 10000
-// console.log(parseInt('10000', 2))
-// console.log(parseInt('00110101010101', 2))
 
-// console.log(0b00110101010101)
+// How do we convert binary to decimal base 10 numbers
+// create a function that takes in a string of binary
+// look at each index of the string starting from the right
+// create a sum variable to keep track of your sum
+// the equation for conversion is base to the power(**) of the index multiplied by the value at that index 
+// return your sum 
+
+
+// let binaryToDec = (string) => {
+//     let output  = 0
+//     let stringLength = string.length - 1
+//     for(let i = 0; i < string.length; i++){
+//         output+= (2** (stringLength -i) * string[i])
+//     }
+//     return output
+// }
+// const binaryToDec = (bin) => {
+//     let val = 1;
+//     let num = 0;
+//     for(let i = bin.length-1; i > 1; i--) {
+//       if(Number(bin[i])) num += val;
+//       val *= 2;
+//     }
+//     return num;
+//   }
+//   // String starts with "0b"
+// // console.log(binaryToDec("0b1010"));
+// // console.log(parseInt('10000', 2))
+// // console.log(binaryToDec('0010101110'))
+
+// console.log(binaryToDec('0b0101010101100111101000101110011010101011101010101011010010'))
+// console.log(parseInt('0101010101100111101000101110011010101011101010101011010010', 2))
 
 // converting from binary to decimal 
 // base to the power of(^) position/index multiplied by the value/number at that position, starting from the right, moving the the left
@@ -128,6 +142,9 @@
 // 15
 // 16
 
+// // Base 16 because we have 16 available digits 
+// // you will see it in the wild with the prefix of 0x
+// // Hex / decimal
 // 0 - 0
 // 1 - 1
 // 2 - 2
@@ -145,7 +162,7 @@
 // 'E' - 14
 // 'F' - 15
 // 10 - 16
-// 11 - 17
+// 11- 17
 // 12 - 18
 // 13 - 19
 // 14 - 20
@@ -158,59 +175,77 @@
 // '1B' - 27
 // '1C' - 28
 // '1D' - 29
-// '1E' - 30
-// '1F' = 31
-// let num = 97
-// let binary = num.toString(2)
-// let hex = num.toString(16)
+// '1E'  -30
+// '1F' - 31
+// 20 - 32
 
-// console.log(String.fromCharCode(num))
-// console.log(String.fromCharCode(binary))
-// console.log(String.fromCharCode('0x61'))
-// console.log(String.fromCharCode(97))
-// console.log(String.fromCharCode('0b1100001'))
-// let num = 3414
 
-// console.log(num.toString(2))
-// console.log(num.toString(36))
-
-// let hexHelper = (char) => {
-//     let numberObj = {
-//         'A': 10,
-//         'B': 11,
-//         'C': 12,
-//         'D': 13,
-//         'E': 14,
-//         'F': 15
+// const hexToDec = (hexCode) => {
+//     let obj = {
+//         "A": 10,
+//         "B": 11,
+//         "C": 12,
+//         "D": 13,
+//         "E": 14,
+//         "F": 15
 //     }
-//     if(Number(char) || Number(char) === 0){
-//         return char
+
+//     let sum = 0
+//     let reverseStr = hexCode.split('').reverse()
+//     for(let i = 0 ; i < reverseStr.length; i++){
+//         let currentVal = reverseStr[i]
+//         if(currentVal === 'x') return sum
+//         let num;
+//         if(Object.keys(obj).includes(currentVal)){
+//             num = 16**i * obj[currentVal]
+//         }else{
+//             num = 16**i * currentVal
+//         }
+//         sum+=num
 //     }
-//     return numberObj[char]
-// }
-
-
-// let hexToDec = (hex) => {
-//     let sum = 0;
-//     let arr = hex.split('').reverse()
-//     arr.forEach((el, i) => {
-//         let num = hexHelper(el)
-//         sum += 16**i * num
-//     });
 //     return sum
 // }
 
-// console.log(hexToDec("A1"))
-// console.log(parseInt('0xA1', 16))
+// console.log(hexToDec('0x1C'))
+
+// console.log(parseInt('1C', 16))
+
+// let num = 1234
+
+// console.log(num.toString(10))
+
+// console.log(String.fromCharCode('0b1000010') + String.fromCharCode('0x41') + String.fromCharCode(89))
+
+// console.log(typeof String.fromCharCode(32)
 
 // Bytes, kilobytes, megabytes, gigabytes, terabytes:
 // 1 byte = 8 bits
+// one binary digit is one bit 
 
-// kilo - thousand
-// mega - million
+// kilo - thousand // 3.2 kilobytes
+// mega - million 
 // giga - billion
 // tera - trillion
 // peta - quadrillion
 // exa - quintillion
 // zetta - sextillion
 // yotta - septillion
+
+
+
+
+
+const nameToBin = (name) => {
+    let binArr = []
+    for(let i = 0 ; i < name.length ; i ++){
+        let letter = name[i]
+        let charCode = letter.charCodeAt()
+        binArr.push('0' + charCode.toString(2))
+    }
+    return binArr.join('')
+}
+
+console.log(nameToBin('Baylen'))
+
+
+
