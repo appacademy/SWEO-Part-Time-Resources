0a4fcfb03dc095748c1cf68eb5ee173f66d074c7
baylen doss <baylendoss@baylens-Air.lan>
2022-03-22 20:35:10 -0500

    Day 2

--- 2-Module/4-week/2-day/scratch.js
+++ 2-Module/4-week/2-day/scratch.js
@@ -1,104 +1,104 @@
-class LinkedListNode {
-    constructor(val) {
-      this.value = val;
-      this.next = null;
-    }
-  }
+// class LinkedListNode {
+//     constructor(val) {
+//       this.value = val;
+//       this.next = null;
+//     }
+//   }
   
-  class LinkedList {
-    constructor() {
-      this.head = null
-      this.length = 0
-    }
+//   class LinkedList {
+//     constructor() {
+//       this.head = null
+//       this.length = 0
+//     }
   
-    addToHead(val) {
-      const newNode = new LinkedListNode(val)
-      newNode.next = this.head
-      this.head = newNode
-      this.length++
-    }
+//     addToHead(val) {
+//       const newNode = new LinkedListNode(val)
+//       newNode.next = this.head
+//       this.head = newNode
+//       this.length++
+//     }
   
-    addToTail(val) {
-      if(!this.head){
-        this.addToHead(val)
-      }else{
-        let newNode = new LinkedListNode(val)
-        let current = this.head
-        while(current){
-          if(current.next === null){
-            current.next = newNode
-            this.length++
-            break
-          }
-          current = current.next
-        }
-      }
-    }
+//     addToTail(val) {
+//       if(!this.head){
+//         this.addToHead(val)
+//       }else{
+//         let newNode = new LinkedListNode(val)
+//         let current = this.head
+//         while(current){
+//           if(current.next === null){
+//             current.next = newNode
+//             this.length++
+//             break
+//           }
+//           current = current.next
+//         }
+//       }
+//     }
 
-    removeHead(){
-        if(this.head){
-            let removed = this.head
-            this.head = this.head.next
-            this.length --
-            return removed
-        }
-    }
+//     removeHead(){
+//         if(this.head){
+//             let removed = this.head
+//             this.head = this.head.next
+//             this.length --
+//             return removed
+//         }
+//     }
 
-    removeTail(){
-        if(this.head == null){
-            return 'The list is empty'
-        }
-        let current = this.head
-        while(current){
-            if(current.next.next === null){
-                let tail = current.next
-                current.next = null
-                this.length --
-                return tail
-            }
-            current = current.next
-        }
-    }
+//     removeTail(){
+//         if(this.head == null){
+//             return 'The list is empty'
+//         }
+//         let current = this.head
+//         while(current){
+//             if(current.next.next === null){
+//                 let tail = current.next
+//                 current.next = null
+//                 this.length --
+//                 return tail
+//             }
+//             current = current.next
+//         }
+//     }
 
-    print() {
-      let current = this.head;
+//     print() {
+//       let current = this.head;
   
-      while (current) {
-        process.stdout.write(`${current.value} -> `);
-        current = current.next;
-      }
+//       while (current) {
+//         process.stdout.write(`${current.value} -> `);
+//         current = current.next;
+//       }
   
-      console.log("NULL");
-    }
-}
+//       console.log("NULL");
+//     }
+// }
 
-class Queue{
-    constructor(){
-        this.myQueue = new LinkedList()
-    }
+// class Queue{
+//     constructor(){
+//         this.myQueue = new LinkedList()
+//     }
 
-    addToQueue(val){
-        this.myQueue.addToTail(val)
-    }
+//     addToQueue(val){
+//         this.myQueue.addToTail(val)
+//     }
 
-    dequeue(){
-       return this.myQueue.removeHead()
-    }
+//     dequeue(){
+//        return this.myQueue.removeHead()
+//     }
 
-    printQueue(){
-        this.myQueue.print()
-    }
-}
-let queue = new Queue()
+//     printQueue(){
+//         this.myQueue.print()
+//     }
+// }
+// let queue = new Queue()
 
-queue.addToQueue(1)
-queue.addToQueue(2)
-queue.addToQueue(3)
-queue.addToQueue(4)
-console.log(queue.dequeue().value)
-console.log(queue.dequeue().value)
-console.log(queue.dequeue().value)
-queue.printQueue()
+// queue.addToQueue(1)
+// queue.addToQueue(2)
+// queue.addToQueue(3)
+// queue.addToQueue(4)
+// console.log(queue.dequeue().value)
+// console.log(queue.dequeue().value)
+// console.log(queue.dequeue().value)
+// queue.printQueue()
 
 // class Stack {
 //     constructor(){
