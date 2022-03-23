01102d385bb8c014cea4350ca2df4320ec17b4f1
AlexBetita <alexbheb25@gmail.com>
2022-03-22 22:51:32 -0700

    updated lecture

---
+++ 1-Module/6-week/Solutions for timeout/practice-for-week-03-timeout-pt2-practice-main/problems/06-dynamic-interval-count.js
@@ -0,0 +1,78 @@
+/***********************************************************************
+Write a function, `dynamicIntervalCount`, that accepts a callback, a delay
+in milliseconds, and an optional amount as arguments. The function should
+set an interval with the given callback and delay. If an amount argument
+is passed, the interval should be cleared after the callback has been
+called 'amount' number of times. If an amount argument is not passed,
+the interval should not be cleared and `dynamicIntervalCount` should instead
+return the Interval object for the interval.
+
+In addition to Mocha, we recommend that you test your code manually using
+node with the examples below.
+
+Examples:
+
+dynamicIntervalCount(function() {
+    console.log('hi');
+}, 500, 3); // prints 'hi' at 500ms intervals a total of 3 times
+
+
+const timeoutObject = dynamicIntervalCount(function() {
+    console.log('hi');
+}, 500); // prints 'hi' at 500ms intervals indefinitely
+
+console.log(timeoutObject); // Timeout { ... }
+***********************************************************************/
+
+// function dynamicIntervalCount(cb, delay, amount) {
+// 	let interval = setInterval(()=>{
+// 		cb();
+// 		if(amount !== undefined){
+// 			amount--
+// 			if(amount === 0){
+// 				clearInterval(interval)
+// 			}
+// 		}
+// 	}, delay)
+
+// 	return interval
+// }
+
+// function dynamicIntervalCount(cb, delay, amount) {
+// 	let interval = setInterval(()=>{
+// 		cb();
+// 		amount--
+// 		if(amount === 0){
+// 			clearInterval(interval)
+// 		}
+
+// 	}, delay)
+
+// 	if(amount === undefined){
+// 		return interval
+// 	}
+// }
+
+function dynamicIntervalCount(cb, delay, amount) {
+	if (amount === undefined) {
+	  let timeOut = setInterval(cb, delay);
+	  return timeOut;
+	} else {
+	  let timeOutTwo = setInterval(() => {
+		cb();
+		amount--;
+
+		if (amount === 0) {
+		  clearInterval(timeOutTwo);
+		}
+	  }, delay);
+	}
+  }
+
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+try {
+  module.exports = dynamicIntervalCount;
+} catch {
+  module.exports = null;
+}
