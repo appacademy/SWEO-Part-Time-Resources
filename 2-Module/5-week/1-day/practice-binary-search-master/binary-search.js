6b01fa9f1df557dd5ee09f77ce9c1b9f415daedf
baylen doss <baylendoss@baylens-Air.lan>
2022-04-05 11:52:40 -0500

    day one

--- 2-Module/5-week/1-day/practice-binary-search-master/binary-search.js
+++ 2-Module/5-week/1-day/practice-binary-search-master/binary-search.js
@@ -1,47 +1,4 @@
 function linearSearch (arr, target) {
-<<<<<<< HEAD
-  // some condition ? return this if true : return this is false
-  return arr.filter((el, i) => el === target ? i: false).length === 1 ? arr.filter((el, i) => el === target ? i: false)[0] : -1
-    // for(let i = 0 ; i < arr.length ; i++){
-    //   if(arr[i] === target){
-    //     return i
-    //   }
-    // }
-    // return -1
-};
-
-function binarySearch(arr, target) {
-
-//   // Set integers pointing to the high and low range of possible indices
-//   let low = 0;
-//   let high = arr.length -1
-//   // While high and low indices do not overlap...
-//   while(low <= high){
-//     // Find the midpoint between high and low indices
-//     let mid = Math.floor((low + high) / 2)    
-//     let compare = arr[mid]
-//     // Compare the target value to the midpoint value
-//     if(compare === target){
-//       // If the target equals the midpoint...
-//         // Return the midpoint index
-//       return mid
-//     }
-//       // If the target is higher than the midpoint...
-//     else if(target > compare){
-//       // Move the low pointer to midpoint + 1
-//       low = mid + 1
-//     }
-//     // If the target is less than the midpoint...
-//     // if(target < compare){
-//       else{
-//       // Move the high pointer to midpoint - 1
-//       high = mid -1
-//     }
-//   }
-//   // Return -1 if the loop exits with overlapping pointers
-//   return -1
-}
-=======
   return arr.indexOf(target)
 
   // for(let i =0; i < arr.length; i++){
@@ -97,6 +54,5 @@ function binarySearch(arr, target) {
 }
 binarySearch(example, 8)
 
->>>>>>> 2022-Jan-W
 
 module.exports = [linearSearch, binarySearch]
