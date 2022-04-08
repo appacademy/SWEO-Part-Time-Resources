8882ebc9a11a13a7ee620102961d2c74abeabf8b
Zaviar Brown <brown.zaviar@gmail.com>
2022-04-08 14:10:54 -0700

    Saturday

---
+++ 1-Module/3-week/5-day/scratch.js
@@ -0,0 +1,80 @@
+// Let's make a .length function
+
+let arr = [1, 2, 3, 4, 5];
+
+let arrayLength = function (array) {};
+
+console.log(arr.length);
+
+
+
+//! Sum forEach
+
+let myForEach = function () {};
+
+/*
+Write a function `sumForEach(arr)` that accepts an array as an arg.
+The function should return the total sum of all values in the array.
+
+Solve this using Array's `forEach()` method.
+
+Examples:
+
+console.log(sumForEach([4, 3, -1, 10])); // 16
+console.log(sumForEach([6, 7, 2])); // 15
+console.log(sumForEach([])); // 0
+*/
+
+//! Tripler Map
+
+let myMap = function () {};
+
+/*
+Write a function `tripler(nums)` that takes in an array as an arg. The function
+should return a new array containing three times every number of the original
+array.
+
+Solve this using Array's `map()` method.
+
+Examples: 
+
+console.log(tripler([2, 7, 4])); // [ 6, 21, 12 ]
+console.log(tripler([-5, 10, 0, 11])); // [ -15, 30, 0, 33 ]
+*/
+
+//! Long Words Filter
+
+let myFilter = function () {};
+
+/*
+
+Write a function `longWords(words)` that takes in an array of words.
+The function should return an array containing only the words that are longer
+than 5 characters.
+
+Solve this using Array's `filter()` method.
+
+Examples:
+
+console.log(longWords(['bike', 'skateboard','scooter', 'moped'])); // [ 'skateboard', 'scooter' ]
+console.log(longWords(['couscous', 'soup', 'ceviche', 'solyanka' ,'taco'])); // [ 'couscous', 'ceviche', 'solyanka' ]
+
+*/
+
+//! Sum Reduce
+
+let myReduce = function () {};
+
+/*
+
+Write a function `sumWithReduce(nums)` that takes in an array of numbers.
+The function should return the total sum of all numbers in the array.
+
+Solve this using Array's `reduce()` method.
+
+Examples:
+
+console.log(sumWithReduce([10, 3, 5, 2])); // 20
+console.log(sumWithReduce([])); // 0
+
+*/
