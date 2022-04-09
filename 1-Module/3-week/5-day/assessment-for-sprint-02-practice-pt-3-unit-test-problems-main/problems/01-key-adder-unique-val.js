06d088c68cd3e9a4af9f779c85b7a606efd68029
Zaviar Brown <brown.zaviar@gmail.com>
2022-04-09 10:06:09 -0700

    Practice assessment added

---
+++ 1-Module/3-week/5-day/assessment-for-sprint-02-practice-pt-3-unit-test-problems-main/problems/01-key-adder-unique-val.js
@@ -0,0 +1,22 @@
+/***********************************************************************
+Write a function `keyAdderUniqueVal(object, key, value)` that accepts an
+object, a string to be set as a key in that object, and a value. When invoked
+the function should assign the key and value arguments as a key value pair in
+the object argument ONLY IF the value is not already a value for a different
+key in the object. Return the mutated object.
+
+See below for examples
+
+let cat = { name: 'Willie', color: 'orange' };
+keyAdderUniqueVal(cat, "toy", "yarn"); // => {name: "Willie", color: "orange", toy: "yarn"}
+keyAdderUniqueVal(cat, "fruit", "orange"); // => {name: "Willie", color: "orange", toy: "yarn"}
+console.log(cat); // { name: "Willie", color: "orange", toy: "yarn" }
+***********************************************************************/
+
+function keyAdderUniqueVal(object, key, value) {
+
+
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = keyAdderUniqueVal;
