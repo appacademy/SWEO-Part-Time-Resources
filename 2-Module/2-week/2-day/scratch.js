10b2aa818c1846a89bca466dceb2b28e45ec5d19
AlexBetita <alexbheb25@gmail.com>
2022-04-12 18:50:32 -0700

    updated lecture

--- 2-Module/2-week/2-day/scratch.js
+++ 2-Module/2-week/2-day/scratch.js
@@ -1,51 +1,107 @@
-class Test {
-  testFunc(name, num) {
-    console.log(name, num)
-    console.log(this === test1);
-  }
+// bind
+// Function.prototype.bind()
+
+
+// returns/creates a new function
+// it does not invoke the function
+
+class Person{
+	constructor(name, age){
+		this.name = name;
+		this.age = age;
+	}
+
+	showContext(){
+		// console.log(this);
+		// console.log(this.name);
+		// console.log(this.age);
+
+		function declaredFunc(){
+			console.log(this)
+		}
+
+		declaredFunc() //
+
+		// does not have the this keyword
+		// does not have the super keyword
+		const arrowFunc = () => {
+			console.log(this)
+		}
+
+		arrowFunc() //
+	}
 }
 
-const test1 = new Test();
+const newPerson = new Person('Alex', 30);
 
-// setTimeout(test1.testFunc.bind(test1, "Mylo", 3), 1000); // wait 1 second, true
+newPerson.showContext();
 
-/* 
-Using this in Function Declaration Syntax
-Could I make `this` equal to test1 in this function?
-*/
-// function someFunc() {
-//   console.log(this);
+// class Animal{
+// 	constructor(name, age){
+// 		this.name = name;
+// 		this.age = age;
+// 	}
+
+// 	bark(){
+// 		console.log(`${this.name} goes arf arf`);
+// 	}
 // }
 
-// someFunc.call(test1); // (undefined) Test {...}
-
-
-
-
-// function greeting(...messages) {
-//   return messages.map((message) => {
-//     return `${this.firstName} says ${message}`;
-//   });
+// const cat = {
+// 	"name" : "Gwendolyn"
 // }
 
-// const derek = {
-//   firstName: 'Derek',
-// };
+// const dog = {
+// 	"name" : "Maxie"
+// }
 
-// const derekMessages = greeting.bind(derek, 'Hello class!');
-// console.log(derekMessages('Goodbye class!'));
+// const newPerson = new Person('Alex', 30);
+// const newAnimal = new Animal('Gwendolyn', 8);
+// //
+// const newFunction = newPerson.showContext;
+// const animalFunc = newAnimal.bark;
 
-class Fruit {
-  constructor(name) {
-    this.name = name;
-  }
+// const newContext = animalFunc.bind(newPerson)
+// newContext()
 
-  eat = () => {
-    console.log(`${this.name} has a bite mark`);
-  };
+// setTimeout(newPerson.showContext.bind(newPerson), 2000)
+// bind(context, arg1, ..., argN)
+// const bindedFunction = newFunction.bind(newPerson)
+
+// bindedFunction()
+
+// const bindedFunction2 = newFunction.bind(cat)
+// bindedFunction2()
+// const bindedFunction3 = newFunction.bind(dog)
+// bindedFunction3()
+
+
+// function number(arg1, arg2, arg3){
+// 	console.log(arg1, arg2, arg3)
+// }
+
+// const bindArg = number.bind(null, 34, 'hello', 'world')
+
+// bindArg()
+
+
+// call and apply
+
+// Function.prototype.call()
+// Function.prototype.apply()
+
+// they invoke the function
+// call(context, ...args)
+// apply(context, [args])
+
+
+// animalFunc.apply(newPerson)
+
+function addAnyNumberOfArgs(){
+	console.log(arguments)
 }
 
-const apple = new Fruit('apple');
-const eat = apple.eat;
-eat();
-setTimeout(eat, 1000);
+
+
+// addAnyNumberOfArgs.call(null, ['Hello', 'World', 'Javascript'])
+// addAnyNumberOfArgs.apply(null, ['Hello', 'World', 'Javascript'], '123')
