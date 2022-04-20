7c870439dacc1b3b1fbbda326c931dc5d19c2465
westrinhkl <trinhpt@uci.edu>
2022-04-19 21:46:38 -0700

    scratch lecture

---
+++ 1-Module/1-week/2-day/scratch.js
@@ -0,0 +1,59 @@
+// CONSOLELOG
+// Print out your favorite food using console.log below:
+console.log('wes', "wings");
+console.log("nelson", "ceviche");
+console.log("ricky", "sea food");
+console.log("Andres", 'Pasta');
+
+console.log("WT-problem1", 'fake-data'); 
+
+
+// Numbers Exercise
+// Write 5 more expressions below:
+console.log("1+5+4-1:", 1 + 5 + 4 - 1);
+console.log("5 / 2 % 2:", 5 / 2 % 2);
+console.log(10 * 100 - 10);
+console.log(10/10);
+
+// BOOLEAN 
+// Write 3 more expressions below:
+console.log(true && true && false);
+console.log(!true && !false || true);
+console.log(!(true || false) && true); //(false && true) && true //false && true // false
+
+
+// Variables Exercise
+// 1. Declare a variable called `firstName` and assign it your first name as a string:
+let firstName = "Wes"; 
+
+// 2. Declare a variable called `lastName` and assign it your last name as a string:
+let lastName = "Trinh";
+
+// 3. Declare a variable called `age`:
+let age;
+
+// 4. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
+
+console.log("Firstname:", firstName);
+console.log("lastName:", lastName);
+console.log("age:", age);
+// 5. Assign the `age` variable to a number:
+
+age = 2400;
+
+// 6. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
+
+firstName = "cool"
+let string = "Firstname: " + firstName
+console.log(string);
+console.log("lastName:", lastName);
+console.log("new assigned age:", age);
+
+console.log("---------------example---------------");
+let x = 7;
+x++; //x = x + 1
+x - 2; //line doesn't update x.
+x *= 3; // x = x * 3
+console.log(x);
