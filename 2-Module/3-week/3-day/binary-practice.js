2dfb8c0ecf7a707e90bdd315609526c17248317c
AlexBetita <alexbheb25@gmail.com>
2022-04-20 22:51:16 -0700

    updated lecture

--- 2-Module/3-week/3-day/binary-practice.js
+++ 2-Module/3-week/3-day/binary-practice.js
@@ -13,9 +13,36 @@ and consult their documentation on how to utilize them:
 /*
 Base 10 to binary
 */
-function deciToBinary() {
+// function deciToBinary(num) {
+// 	return `0b${num.toString(2)}`
+// }
 
+function deciToBinary(n) {
+    let binary = `0b`;
+    let remainders = []
+    let bit = 0;
+    for (let i = 0; n >= 2; i++) {
+        bit = Math.floor(n % 2)
+        remainders.unshift(bit)
+        n /= 2
+    }
+    remainders.unshift(Math.floor(n % 2))
+    binary = binary.concat(remainders.join(""))
+    return binary
 }
+
+// function deciToBinary(n, res = []) {
+// 	if (n === 0) return `0b${res.join("")}`;
+
+// 	let digit = n % 2;
+// 	if (digit === 0) {
+// 	  res.unshift(digit);
+// 	  return deciToBinary(n / 2, res);
+// 	} else {
+// 	  res.unshift(digit);
+// 	  return deciToBinary((n - 1) / 2, res);
+// 	}
+// }
 
 console.log('Base 10 to binary:');
 console.log(deciToBinary(8)); // '0b1000'
@@ -26,7 +53,8 @@ console.log('');
 /*
 Base 10 to hexadecimal
 */
-function deciToHexa() {
+function deciToHexa(num) {
+    return `0x${num.toString(16)}`
 }
 
 console.log('Base 10 to hexadecimal:');
@@ -38,8 +66,11 @@ console.log('');
 /*
 Binary to base 10
 */
-function binaryToDeci() {
-}
+function binaryToDeci(num) {
+	let newNum = num.substr(2);
+	return parseInt(newNum, 2);
+  }
+
 
 console.log('Binary to base 10:');
 console.log(binaryToDeci('0b1010')); // 10
@@ -49,7 +80,9 @@ console.log('');
 /*
 Hexadecimal to base 10
 */
-function hexaToDeci() {
+function hexaToDeci(num) {
+	return parseInt(num, 16);
+// 	return parseInt(num)
 }
 
 console.log('Hexadecimal to base 10:');
@@ -60,9 +93,20 @@ console.log('');
 /*
 Binary to hexadecimal
 */
-function binaryToHexa() {
+// function binaryToHexa(bit) {
+//     let n = binaryToDeci(bit)
+//     return hexaToDeci(n)
+// }
 
+const binaryToHexa = (number) => {
+   return  `0x${parseInt(number.substr(2), 2).toString(16)}`
 }
+
+// function binaryToHexa(number) {
+// 	let newNum = number.substr(2);
+// 	return `0x${parseInt(newNum, 2).toString(16)}`;
+//   }
+
 
 console.log('Binary to hexadecimal:');
 console.log(binaryToHexa('0b1010')); // '0xa'
@@ -72,8 +116,15 @@ console.log('');
 /*
 Hexadecimal to binary
 */
-function hexaToBin() {
+function hexaToBin(num) {
+	return '0b' + parseInt(num, 16).toString(2);
 }
+
+// function hexaToBin(bit) {
+//     let n = hexaToDeci(bit)
+//     return deciToBinary(n)
+// }
+
 
 console.log('Hexadecimal to binary:');
 console.log(hexaToBin('0xa1')); // '0b10100001'
@@ -81,12 +132,13 @@ console.log(hexaToBin('0xff')); // '0b11111111'
 console.log('');
 
 
-// bonus
+// // bonus
 
 /*
 Base 10 to ASCII
 */
-function deciToAscii() {
+function deciToAscii(num) {
+	return String.fromCharCode(num);
 }
 
 console.log('Base 10 to ASCII:');
@@ -99,8 +151,11 @@ console.log('');
 /*
 Hexadecimal to ASCII
 */
-function hexaToAscii() {
+function hexaToAscii(str) {
+	return String.fromCharCode(str);
+	// return String.fromCharCode(parseInt(str));
 }
+
 
 console.log('Hexadecimal to ASCII:');
 console.log(hexaToAscii(`0x41`)); // 'A'
@@ -112,8 +167,12 @@ console.log('');
 /*
 Binary to ASCII
 */
-function binToAscii() {
+function binToAscii(bin) {
+	let str = bin.toString();
+  //   console.log(str);
+	return String.fromCharCode(str);
 }
+
 
 console.log('Binary to ASCII:');
 console.log(binToAscii(0b01000001)); // 'A'
@@ -125,8 +184,10 @@ console.log('');
 /*
 ASCII to decimal
 */
-function asciiToDeci() {
+function asciiToDeci(char) {
+	return char.charCodeAt();
 }
+
 
 console.log('ASCII to decimal:');
 console.log(asciiToDeci('A')); // 65
@@ -135,7 +196,18 @@ console.log(asciiToDeci('a')); // 97
 console.log(asciiToDeci('b')); // 98
 console.log(' ')
 
-function asciiStrToDeci() {
+// function asciiStrToDeci(str) {
+// 	for (let i = 0; i < str.length; i++) {
+// 	  console.log(str.charCodeAt(i));
+// 	}
+// }
+
+function asciiStrToDeci(string) {
+	if (!string.length) return "";
+	console.log(string.charCodeAt(string[0]));
+	return asciiStrToDeci(string.substr(1));
 }
+
+
 
 asciiStrToDeci('abc'); // 97 98 99
