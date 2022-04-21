f6a7150ed1cfc37245a7c962771cc3f6f74cebcb
AlexBetita <alexbheb25@gmail.com>
2022-04-21 14:53:53 -0700

    updated lecture

---
+++ 2-Module/3-week/4-day/bonus_practices/01-big-octupus.js
@@ -0,0 +1,34 @@
+function quadraticBiggestFish(fishes) {
+	// Code goes here ...
+  }
+
+
+  function nlognBiggestFish(fishes) {
+	// Code goes here ...
+  }
+
+
+  function linearBiggestFish(fishes) {
+	// Code goes here ...
+  }
+
+
+  tilesArray = ["up", "right-up", "right", "right-down", "down", "left-down", "left", "left-up"]
+  function slowDance(direction, tilesArray) {
+	// Code goes here ...
+  }
+
+
+  tilesObj = {
+	"up": 0,
+	"right-up": 1,
+	"right": 2,
+	"right-down": 3,
+	"down": 4,
+	"left-down": 5,
+	"left": 6,
+	"left-up": 7
+  }
+  function fastDance(direction, tilesObj) {
+	// Code goes here ...
+  }
---
+++ 2-Module/3-week/4-day/bonus_practices/04-execution-time-differences.js
@@ -0,0 +1,23 @@
+function myMin1a(list) {
+// Code goes here ...
+}
+
+
+function myMin1b(list) {
+// Code goes here ...
+}
+
+
+function myMin2(list) {
+// Code goes here ...
+}
+
+
+function largestContiguousSubsum1(array) {
+// Code goes here ...
+}
+
+
+function largestContiguousSubsum2(array) {
+// Code goes here ...
+}
