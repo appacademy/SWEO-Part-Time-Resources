1419b5b3b829b18f3fe7a3a06f0915914def31c6
westrinhkl <trinhpt@uci.edu>
2022-04-21 21:57:42 -0700

    updated

--- 1-Module/1-week/4-day/lecture-ex.js
+++ 1-Module/1-week/4-day/lecture-ex.js
@@ -33,8 +33,42 @@
 
 // console.log(0 === false); //0 is not strictly equal to false
 
-if (true) { 
-    if (true === 'true'){ //expression of true === 'true' --> false --> false is a falsy value
-        console.log("trueception");
-    }
-}
+// if (true) { 
+//     if (true === 'true'){ //expression of true === 'true' --> false --> false is a falsy value
+//         console.log("trueception");
+//     }
+// }
+
+// ------------------------------arrays---------------------
+// let arr2 = ["zero", false, "Let's go Nets", undefined, [1,2,3]];
+
+// console.log(arr2[0]);
+// console.log(arr2[1]);
+// console.log("value at index 4:", arr2[4]);
+// console.log(arr2[4][1]);
+
+// console.log(arr2.length);
+// console.log(arr2.indexOf(false));
+// console.log(arr2.indexOf("dogs"));
+
+// -----------------concat example-------------------
+
+// let array1 = [1, 2, 3];
+// let array2 = [4, 5, 6];
+// let array3 = [7, 8, 9];
+
+
+// let goodCombine = array1.concat(array2 ,array3, 10)
+
+// console.log(goodCombine) 
+
+// ------------------concat doesn't mutate original array--------------------
+let array1 = [1, 2, 3];
+let array2 = [4, 5, 6];
+let array3 = [7, 8, 9];
+
+
+array1 = array1.concat(array2 ,array3, 10)
+
+// console.log(goodCombine) 
+console.log(array1);
