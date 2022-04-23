a259645309b394725fb6de33a69c366dba79ed4e
baylen doss <baylendoss@baylens-Air.lan>
2022-04-23 11:07:21 -0500

    west

---
+++ 3-Module/1-week/5-day/scratch.js
@@ -0,0 +1,71 @@
+/*
+ css selectors, 
+
+    Type selectors, select by the type of html tag
+
+    div{
+
+    }
+    p{
+
+    }
+    h1{
+
+    }
+
+    Class selectors,  have a period in front of the class name
+    ID selectors have a pound symbol # in front of the ID
+
+    Attribute selectors
+
+    div[id="1"]
+
+    would select <div id="1"></div>
+
+
+    Pseudo selectors,  selectors and : or :: 
+
+    Combinators --  something > tag name will select off of the direct children of something
+    https://developer.mozilla.org/en-US/docs/Learn/CSS/Building_blocks/Selectors#combinators
+
+
+    Css sizes
+
+    Absolute units of measurement 
+    cm	Centimeters	1cm = 37.8px = 25.2/64in -- rarely used
+    mm	Millimeters	1mm = 1/10th of 1cm -- rarely used
+    Q	Quarter-millimeters	1Q = 1/40th of 1cm
+    in	Inches	1in = 2.54cm = 96px -- rarely used
+    pc	Picas	1pc = 1/6th of 1in -- rarely used
+    pt	Points	1pt = 1/72th of 1in -- rarely used
+    px	Pixels	1px = 1/96th of 1in -- rarely used
+
+    relative units of measurement 
+    unit | Relative to 
+    em	    Font size of the parent, in the case of typographical properties like font-size, and font size of the element itself, in the case of other properties like width.
+    ex	    x-height of the element's font.
+    ch	    The advance measure (width) of the glyph "0" of the element's font.
+    rem	    Font size of the root element.
+    lh	    Line height of the element.
+    vw	    1% of the viewport's width.
+    vh	    1% of the viewport's height.
+    vmin    1% of the viewport's smaller dimension.
+    vmax    1% of the viewport's larger dimension.
+
+
+    Box model, inner out content padding border margin
+
+    display border-box, fits content + padding + border in your set height/width
+
+    Box shadow
+
+    border 
+
+    elements default to the position of static, ie, not positioned 
+
+    positioning 
+
+    rgb, rgba functions
+
+    specificity 
+*/
