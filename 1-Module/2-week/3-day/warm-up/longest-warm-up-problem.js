609f5d6969817b65aec7441c664f3cd56a90dd05
westrinhkl <trinhpt@uci.edu>
2022-04-27 19:38:56 -0700

    Updated

--- 1-Module/2-week/3-day/warm-up/longest-warm-up-problem.js
+++ 1-Module/2-week/3-day/warm-up/longest-warm-up-problem.js
@@ -15,7 +15,7 @@ let longWords = function(words){
     return longestWords;
 }
 
-console.log(longWords(['bike', 'skateboard','scooter'])); // [ 'skateboard', 'scooter' ]
+console.log(longWords(['bike', 'skateboard','scooter'])); // [ 'skateboard', 'scooter' ] --> all of these words are longer than 5 characters.
 console.log(longWords(['couscous', 'soup', 'ceviche', 'solyanka' ,'taco'])); // [ 'couscous', 'ceviche', 'solyanka' ]
 console.log(longWords(['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'])); //[]
 
@@ -32,14 +32,30 @@ console.log(longWords(['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'se
 //output: expected number that represent the index
 
 let mostWordsFive = function(twoDiArr){
+  //1. create indexOfLongestSubArr that holds the index of the current longest subarray
+  let indexOfLongestSubArr = 0;
 
+  //2. iterate over twoDiArr to find a subArr that is bigger than the current longest
+  for(let i=1; i<twoDiArr.length; i++){
+    //3. declare two variables, one currentSubArr and the other longestSubArr AFTER APPLYING the longestWords helper function
+    let longestSubArr = longWords(twoDiArr[indexOfLongestSubArr]) //[ 'skateboard', 'scooter' ]
+    let currentSubArr = longWords(twoDiArr[i]); // [ 'couscous', 'ceviche', 'solyanka' ]
+
+    //4. compare which one has the longer length. The old longest or the new current sub array.
+    if(currentSubArr.length > longestSubArr.length){
+      indexOfLongestSubArr = i;
+    }
+  }
+
+  //we now ahve the answer, longest sub array index.
+  return indexOfLongestSubArr
 }
 
 
 let twoDList = [
-  ['bike', 'skateboard','scooter'], 
-  ['couscous', 'soup', 'ceviche', 'solyanka', 'taco'],
-  ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight']
+  ['bike', 'skateboard','scooter'], //after applying longestWords, this then has only 2 items in the array --> ['skateboard', 'scooter']
+  ['couscous', 'soup', 'ceviche', 'solyanka', 'taco'], //after applying longestWords, this then has 3 items in the array --> ['couscous', 'ceviche', 'solyanka']
+  ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'] //after we apply longestWords, how many items? --> 0
 ]
 
  // answer is 1; because twoDList[1]  contains the subArray with the most 5+ letter words of ['couscous', 'ceviche', 'solyanka']
