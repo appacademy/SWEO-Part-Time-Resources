2fd337411ad8f4130fb02e5ebca3dfb1395482fb
westrinhkl <trinhpt@uci.edu>
2022-04-28 17:58:37 -0700

    updated

--- 1-Module/2-week/4-day/problems/christopher-rotate.js
+++ 1-Module/2-week/4-day/problems/christopher-rotate.js
@@ -3,21 +3,21 @@
 //inputs:  ['a', 'b', 'c', 'd', 'e'] , rotate 2 times
 //outputs: [ 'd', 'e', 'a', 'b', 'c' ]
 
-let rotate = function(array, num){
+let rotate = function (array, num) {
 
-    let newNum = (num + array.length)  % array.length;
-    for(let i=0; i<newNum; i++){
+    let newNum = (num + array.length) % array.length;
+    for (let i = 0; i < newNum; i++) {
         array.unshift(array.pop()); //pop removes element and gives us the element
     }
-    
-  }
-  
-  
-  
-  let arr = ['a', 'b', 'c', 'd', 'e'];
-  rotate(arr, 2);
-  console.log(arr); // [ 'd', 'e', 'a', 'b', 'c' ]
-  
-  let animals = ['wombat', 'koala', 'opossum', 'kangaroo'];
-  rotate(animals, -1);
-  console.log(animals); // [ 'koala', 'opossum', 'kangaroo', 'wombat' ]
+
+}
+
+
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+rotate(arr, 2);
+console.log(arr); // [ 'd', 'e', 'a', 'b', 'c' ]
+
+let animals = ['wombat', 'koala', 'opossum', 'kangaroo'];
+rotate(animals, -1);
+console.log(animals); // [ 'koala', 'opossum', 'kangaroo', 'wombat' ]
