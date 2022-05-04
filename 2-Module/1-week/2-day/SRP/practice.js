3162e50c6421dc6280d15e61900deeec5b9072d3
Zaviar Brown <brown.zaviar@gmail.com>
2022-05-03 20:37:06 -0700

    5/3 lecture notes

--- 2-Module/1-week/2-day/SRP/practice.js
+++ 2-Module/1-week/2-day/SRP/practice.js
@@ -1,52 +1,53 @@
-/******************************** CONSTANTS *********************************/ 
+/******************************** CONSTANTS *********************************/
 const applePieRecipe = [
-  { name: "pie crust", cost: 10.00, quantity: 1 },
-  { name: "sugar", cost: 3.00, quantity: 0.5 },
-  { name: "butter", cost: 1.00, quantity: 1 },
-  { name: "apples", cost: 6.00, quantity: 7 },
-  { name: "cinnamon", cost: 5.50, quantity: 1 },
-  { name: "eggs", cost: 2.00, quantity: 1 },
+  { name: 'pie crust', cost: 10.0, quantity: 1 },
+  { name: 'sugar', cost: 3.0, quantity: 0.5 },
+  { name: 'butter', cost: 1.0, quantity: 1 },
+  { name: 'apples', cost: 6.0, quantity: 7 },
+  { name: 'cinnamon', cost: 5.5, quantity: 1 },
+  { name: 'eggs', cost: 2.0, quantity: 1 },
 ];
 
 const pumpkinPieRecipe = [
-  { name: "pie crust", cost: 10.00, quantity: 1 },
-  { name: "sugar", cost: 3.00, quantity: 0.5 },
-  { name: "butter", cost: 1.00, quantity: 1 },
-  { name: "pumpkin", cost: 3.75, quantity: 2 },
-  { name: "cinnamon", cost: 5.50, quantity: 1 },
-  { name: "eggs", cost: 2.00, quantity: 1 },
+  { name: 'pie crust', cost: 10.0, quantity: 1 },
+  { name: 'sugar', cost: 3.0, quantity: 0.5 },
+  { name: 'butter', cost: 1.0, quantity: 1 },
+  { name: 'pumpkin', cost: 3.75, quantity: 2 },
+  { name: 'cinnamon', cost: 5.5, quantity: 1 },
+  { name: 'eggs', cost: 2.0, quantity: 1 },
 ];
 
 const cherryPieRecipe = [
-  { name: "pie crust", cost: 10.00, quantity: 1 },
-  { name: "sugar", cost: 3.00, quantity: 0.5 },
-  { name: "butter", cost: 1.00, quantity: 1 },
-  { name: "cherries", cost: 12.00, quantity: 10 },
-  { name: "eggs", cost: 2.00, quantity: 1 },
+  { name: 'pie crust', cost: 10.0, quantity: 1 },
+  { name: 'sugar', cost: 3.0, quantity: 0.5 },
+  { name: 'butter', cost: 1.0, quantity: 1 },
+  { name: 'cherries', cost: 12.0, quantity: 10 },
+  { name: 'eggs', cost: 2.0, quantity: 1 },
 ];
 
 const recipes = {
   applePie: applePieRecipe,
   pumpkinPie: pumpkinPieRecipe,
-  cherryPie: cherryPieRecipe
+  cherryPie: cherryPieRecipe,
 };
 /* DO NOT CHANGE THE CODE ABOVE */
 
-/*************************** FUNCTION TO REFACTOR ****************************/
-function bakeAndSellPies(pieType, pieQuantity, profitMargin) {
-  // Find the recipe for the pieType specified
-  const recipe = recipes[pieType];
+/*************************** Zaviar's beautiful SRP funcs ****************************/
+
+const printIngredients = (pieType, pieQuantity, recipe) => {
   // Bake the number of pies specified by the pieQuantity
   for (let i = 0; i < pieQuantity; i++) {
     // Print the ingredients for each ingredient in the recipe
-    let combiningMsg = `Combining ingredients for ${pieType}: `
-    combiningMsg += recipe.map(ingredient => ingredient.name).join(', ');
+    let combiningMsg = `Combining ingredients for ${pieType}: `;
+    combiningMsg += recipe.map((ingredient) => ingredient.name).join(', ');
     console.log(combiningMsg);
 
     // Print the nth pie that was baked
     console.log(`Baked pie ${i + 1}!`);
   }
+};
 
+const findTotalCost = (recipe, pieQuantity) => {
   // Print the cost of each pie based on the cost of each ingredient
   const costOfPie = recipe.reduce((prev, current) => {
     return prev + current.cost;
@@ -56,10 +57,53 @@ function bakeAndSellPies(pieType, pieQuantity, profitMargin) {
   // Calculate the total cost of all the pies
   const totalCost = costOfPie * pieQuantity;
 
+  return totalCost;
+};
+
+const sellThosePies = (totalCost, pieQuantity, profitMargin = 1.2) => {
   // Print the total revenue calculated using the given profitMargin
-  const revenue = totalCost * (profitMargin || 1.2);
+  // const revenue = totalCost * (profitMargin || 1.2);
+  const revenue = totalCost * profitMargin;
   console.log(`Sold ${pieQuantity} pies for $${revenue.toFixed(2)}!`);
+};
+
+/*************************** FUNCTION TO REFACTOR ****************************/
+function bakeAndSellPies(pieType, pieQuantity, profitMargin) {
+  // Find the recipe for the pieType specified
+  const recipe = recipes[pieType];
+  printIngredients(pieType, pieQuantity, recipe);
+
+  let totalCost = findTotalCost(recipe, pieQuantity);
+  sellThosePies(totalCost, pieQuantity, profitMargin);
 }
+
+// const oldMain = () => {
+//   // Find the recipe for the pieType specified
+//   const recipe = recipes[pieType];
+//   // Bake the number of pies specified by the pieQuantity
+//   for (let i = 0; i < pieQuantity; i++) {
+//     // Print the ingredients for each ingredient in the recipe
+//     let combiningMsg = `Combining ingredients for ${pieType}: `;
+//     combiningMsg += recipe.map((ingredient) => ingredient.name).join(', ');
+//     console.log(combiningMsg);
+
+//     // Print the nth pie that was baked
+//     console.log(`Baked pie ${i + 1}!`);
+//   }
+
+//   // Print the cost of each pie based on the cost of each ingredient
+//   const costOfPie = recipe.reduce((prev, current) => {
+//     return prev + current.cost;
+//   }, recipe[0].cost);
+//   console.log(`Cost per pie: ${costOfPie}`);
+
+//   // Calculate the total cost of all the pies
+//   const totalCost = costOfPie * pieQuantity;
+
+//   // Print the total revenue calculated using the given profitMargin
+//   const revenue = totalCost * (profitMargin || 1.2);
+//   console.log(`Sold ${pieQuantity} pies for $${revenue.toFixed(2)}!`);
+// };
 
 /******************************* LOCAL TESTS *******************************/
 // bakeAndSellPies("applePie", 5, 2.5);
@@ -69,5 +113,5 @@ function bakeAndSellPies(pieType, pieQuantity, profitMargin) {
 /**************************************************************************/
 /* DO NOT CHANGE THE CODE BELOW */
 module.exports = {
-  bakeAndSellPies
+  bakeAndSellPies,
 };
