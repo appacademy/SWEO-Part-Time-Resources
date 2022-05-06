f411ef39629c7b38fb2d4796eae11fd734a2a9f8
westrinhkl <trinhpt@uci.edu>
2022-05-05 18:40:07 -0700

    updated

--- 1-Module/3-week/4-day/object-warm-ups/answers/value-exist.js
+++ 1-Module/3-week/4-day/object-warm-ups/answers/value-exist.js
@@ -1,9 +1,15 @@
 //given an object and a string, determine if the string exists somewhere in object's values
 //hint: is there a method to get all the values quickly? :)
 
-//write this function
+//write
 let stringExists = function(obj, string){
+    let allValues = Object.values(obj); //["lion", "turtle", "dragon"]
+    if(allValues.includes(string)){
+        return true;
+    }
 
+    //default case, return false
+    return false;
 }
 
 let coolAnimals = {
