b1f00110754acdcc35d71ff495671439677856cd
westrinhkl <trinhpt@uci.edu>
2022-05-05 18:45:38 -0700

    updated

--- 1-Module/3-week/4-day/object-warm-ups/value-exist.js
+++ 1-Module/3-week/4-day/object-warm-ups/value-exist.js
@@ -1,5 +1,6 @@
 //given an object and a string, determine if the string exists somewhere in object's values
 //hint: is there a method to get all the values quickly? :)
+//hint2: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/values
 
 //write this function
 let stringExists = function(obj, string){
