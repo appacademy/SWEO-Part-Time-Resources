89f2fabb4c1c057141069731e77286d8c2d943d3
westrinhkl <trinhpt@uci.edu>
2022-05-07 08:57:29 -0700

    Updated

---
+++ 1-Module/3-week/5-day/object-warm-ups/count-str-obj.js
@@ -0,0 +1,18 @@
+/***********************************************************************
+Write a function `stringConverter(string)` that will intake a
+string as an argument and returns an object representing the count of each
+character in the string. **Hint:** don't forget you can check if a key is present
+in an object by using `obj[key] === undefined`.
+
+Examples:
+
+
+***********************************************************************/
+
+function stringConverter(string) {
+
+}
+
+console.log(stringConverter("apple")); // => {a: 1, p: 2, l: 1, e: 1}
+console.log(stringConverter("banana")); // => {b: 1, a: 3, n: 2}
+console.log(stringConverter("raccoon")); // => {r: 1, a: 1, c: 2, o: 2, n: 1}
