9bfb5fdf85a6cacb1564c0401e2dbfb22a0e061e
westrinhkl <trinhpt@uci.edu>
2022-05-10 14:42:02 -0700

    updated

---
+++ 1-Module/4-week/2-day/code-it-out/key-in-object-array.js
@@ -0,0 +1,38 @@
+/***********************************************************************
+Write a function `keyInObjectArray(objArray, keyString)` that takes in an array 
+of objects as the first parameter and a string as the second. The 
+`keyInObjectArray` function will return `true` if any of the objects contains 
+the `keyString` as a key within them, and `false` if not.
+
+
+Examples:
+let objArray = [
+  { name: "Rupert" },
+  { age: 42 },
+  { planet: "Earth", system: "Milky Way" }
+];
+
+keyInObjectArray(objArray, 'planet'); // => true
+keyInObjectArray(objArray, 'age'); // => true
+keyInObjectArray(objArray, 'food'); // => false
+keyInObjectArray(objArray, 'animal'); // => false
+
+***********************************************************************/
+//if you have solved this problem before, try to see if you can solve it another way!
+
+function keyInObjectArray(objArray, keyString) {
+ 
+}
+
+let objArray = [
+  { name: "Rupert" },
+  { age: 42 },
+  { planet: "Earth", system: "Milky Way" }
+];
+
+console.log(keyInObjectArray(objArray, 'planet')); // => true
+console.log(keyInObjectArray(objArray, 'age')); // => true
+console.log(keyInObjectArray(objArray, 'food')); // => false
+console.log(keyInObjectArray(objArray, 'animal')); // => false
+
+
---
+++ 1-Module/4-week/2-day/notes-reference/callbacks-notes.js
@@ -0,0 +1,9 @@
+console.log("----------First Class Functions----------");
+/*NOTES LINK (ctrl + left-click to open): 
+https://hackmd.io/ibCTcNIERy2irC8OUTRcFQ?view#First-Class-Functions1
+*/
+
+console.log("----------Callbacks----------");
+/*NOTES LINK (ctrl + left-click to open): 
+https://hackmd.io/ibCTcNIERy2irC8OUTRcFQ?view#Callbacks
+*/
