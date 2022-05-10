d3673765460570fb3ec74766a6fb242cb4c1bee8
Zaviar Brown <brown.zaviar@gmail.com>
2022-05-09 20:42:57 -0700

    5/9 lecture notes

---
+++ 2-Module/2-week/1-day/README.md
@@ -0,0 +1,43 @@
+# M2W2D1
+
+## Assessment
+
+---
+
+## Context
+
+### Context refers to the value of `this` within a function and `this` refers to where a function is invoked
+
+- If a function is invoked on the object it was created in, its context stays the same
+- If a function (not invoked) is assigned to variable, its context changes
+- If a function (not invoked) is passed as a callback, its context changes
+
+```js
+class Test {
+  testFunc() {
+    debugger;
+    console.log(this);
+    console.log(`this is equal to test1: ${this === test1}`);
+  }
+}
+
+const test1 = new Test();
+// Calling the method on the instance
+test1.testFunc(); // 
+
+// Calling the method outside the class
+const test1Func = test1.testFunc;
+console.log(test1Func, 14);
+test1Func(); // 
+
+// Calling the method as a callback
+setTimeout(test1.testFunc, 1000); // 
+const arr = ["test",  "array"]
+arr.map(test1.testFunc) // 
+
+// Using this in Function Declaration Syntax
+function someFunc() {
+  console.log(this);
+}
+someFunc(); // 
+```
--- 2-Module/2-week/1-day/scratch.js
+++ 2-Module/2-week/1-day/scratch.js
@@ -1,22 +1,69 @@
-// class Test {
-//   testFunc() {
-//     debugger;
-//     console.log(this)
-//     console.log(`this is equal to test1: ${this === test1}`);
+//? Context asks upon invoking: Where am I?
+// class Games {
+//   static library = [];
+
+//   constructor(name, genre) {
+//     this.name = name;
+//     this.genre = genre;
+//     // Games.library.push(this);
+// 		console.log(this)
 //   }
 // }
 
-// const test1 = new Test();
+// //? Which name and genre am I looking at?
+// const chess = new Games('Chess', 'Strategy');
+// console.log(chess)
+// const animalCrossing = new Games('Animal Crossing', 'Farming Sim');
+// console.log(Games.library)
+// console.log(chess.name)
+
+class Test {
+  testFunc() {
+    debugger;
+    console.log(this);
+    console.log(`this is equal to test1: ${this === test1}`);
+  }
+}
+
+//? Classes just create their methods, they don't invoke them
+const test1 = new Test();
 // Calling the method on the instance
-test1.testFunc(); // true
+// test1.testFunc(); // true
+
+// // Calling on a different instance
+// const test2 = new Test();
+// test2.testFunc();
+
+//!
+
 // Calling the method outside the class
-const test1Func = test1.testFunc; 
-test1Func(); // false
+// const test1Func = test1.testFunc;
+// console.log(test1Func, 41);
+// test1Func(); // false
+
+//! 
+
 // Calling the method as a callback
-setTimeout(test1.testFunc, 1000); // false
+// setTimeout(test1.testFunc, 1000); // false
+// const arr = ["test",  "array"]
+// arr.map(test1.testFunc) // false
+// arr.map((el) => {
+// 	el += 1
+// })
+
+//!
 
 // Using this in Function Declaration Syntax
+//! Functions take the context of the object that wraps them
 function someFunc() {
   console.log(this);
 }
-someFunc(); // Global Object
+// someFunc(); // Global Object
+
+// const someFunc = () => {
+// 	console.log(this)
+// }
+// someFunc(); // Empty?
+
+
+
