4f5e01ff29355ec9a57cd7e841c6cbc22c42810b
baylen doss <baylendoss@baylens-Air.lan>
2022-05-10 23:19:10 -0500

    day 2

---
+++ 3-Module/4-week/2-day/practice-for-week-08-fetch-main/fetch-requests.js
@@ -0,0 +1,26 @@
+/*
+Make fetch requests in the browser for each of the following tasks.
+Paste your code for fetch requests here once you finish each task.
+*/
+
+/* =============== 1. Print the status code of the response =============== */
+
+// Your code here
+
+
+
+/* ====== 2. Print true if the status of the response was successful ====== */
+
+// Your code here
+
+
+
+/* =================== 3. Print the Content-Type Header =================== */
+
+// Your code here
+
+
+
+/* ============== 4. Print the body of the response as text =============== */
+
+// Your code here
