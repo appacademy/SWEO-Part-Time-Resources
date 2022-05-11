97da392f4ade728052df395733bb8792a767048b
westrinhkl <trinhpt@uci.edu>
2022-05-11 14:54:16 -0700

    updated

---
+++ 1-Module/4-week/3-day/code-it-out/get-average-param.js
@@ -0,0 +1,10 @@
+/***********************************************************************
+Write a function getAverage that will take in an arbitrary number of arguments (unknown amount) 
+where each argument represents a number. Find the average of all arguments
+
+
+Examples:
+console.log(getAverage(10,20,30)); // 60 / 3 --> 20
+console.log(getAverage(100, 0)); // 100 / 2 --> 50
+console.log(getAverage(20, 20, 20, 20, 20, 20, 20, 20)); //  --> 20
+***********************************************************************/
