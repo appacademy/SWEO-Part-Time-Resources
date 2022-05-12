5d8699ffa8909c6b5dc7adec1abf2eddb5b3b7a0
westrinhkl <trinhpt@uci.edu>
2022-05-11 20:25:30 -0700

    updated

--- 1-Module/4-week/3-day/code-it-out/answers/get-average-param.js
+++ 1-Module/4-week/3-day/code-it-out/answers/get-average-param.js
@@ -10,3 +10,17 @@ console.log(getAverage(20, 20, 20, 20, 20, 20, 20, 20)); //  --> 20
 ***********************************************************************/
 //tool do we have to take/gather in as many arguments as we can? 
 
+let getAverage = function(...numsArr){
+    let total = 0;
+    // console.log(numsArr);
+    for(let i=0; i<numsArr.length; i++){
+        let num = numsArr[i];
+        total = total + num; //reassigning total with a new total after adding num to original total
+    }
+    let average = total / numsArr.length;
+    return average;
+}
+
+console.log(getAverage(10,20,30)); // 60 / 3 --> 20
+console.log(getAverage(100, 0)); // 100 / 2 --> 50
+console.log(getAverage(20, 20, 20, 20, 20, 20, 20, 20)); //  --> 20
--- 1-Module/4-week/3-day/code-it-out/get-average-param.js
+++ 1-Module/4-week/3-day/code-it-out/get-average-param.js
@@ -8,3 +8,19 @@ console.log(getAverage(10,20,30)); // 60 / 3 --> 20
 console.log(getAverage(100, 0)); // 100 / 2 --> 50
 console.log(getAverage(20, 20, 20, 20, 20, 20, 20, 20)); //  --> 20
 ***********************************************************************/
+//rest will allow to gather all the arguments into an array
+
+let getAverage = function(...numsArr){
+    let total = 0;
+    // console.log(numsArr);
+    for(let i=0; i<numsArr.length; i++){
+        let num = numsArr[i];
+        total = total + num; //reassigning total with a new total after adding num to original total
+    }
+    let average = total / numsArr.length;
+    return average;
+}
+
+console.log(getAverage(10,20,30)); // 60 / 3 --> 20
+console.log(getAverage(100, 0)); // 100 / 2 --> 50
+console.log(getAverage(20, 20, 20, 20, 20, 20, 20, 20)); //  --> 20
