e082f049790d75543e16756ad22772744faaf62c
Zaviar Brown <brown.zaviar@gmail.com>
2022-05-11 20:11:22 -0700

    5/11 lecture notes

---
+++ 2-Module/2-week/3-day/scratch.js
@@ -0,0 +1,42 @@
+// const add = (num1, num2) => { 
+//   try {
+//     if (typeof num1 === 'number' && typeof num2 === 'number') {
+//       return num1 + num2;
+//     } else {
+// 			throw new TypeError('Bruh thats not a num')
+// 		}
+//   } catch (banana) {
+//     // console.error(e)
+//     // console.error(banana.name)
+//     console.error(banana.message);
+//   }
+
+//   return 'This returned';
+// };
+// console.log(add('banana', 'orange'));
+
+// function sum(array) {
+//   let sum = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     sum += array[i];
+//   }
+//   return sum;
+// }
+
+
+// function sum(array) {
+//   let sum = 0;
+//   try {
+//     for (let i = 0; i < array.length; i++) {
+//       sum += array[i];
+//     }
+//   } catch (error) {
+//     console.log('LINE 35', error);
+//   }
+
+//   return sum;
+// }
+
+// let res = sum(null);
+// console.log(res);
+
