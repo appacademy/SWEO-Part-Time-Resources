906ffb5317cb1281a458bf09bb8de7831d67e3e5
westrinhkl <trinhpt@uci.edu>
2022-05-11 18:34:05 -0700

    UpdateD

---
+++ 1-Module/4-week/3-day/code-it-out/answers/get-average-param.js
@@ -0,0 +1,12 @@
+/***********************************************************************
+Write a function getAverage that will take in an arbitrary number of arguments (unknown amount) 
+where each argument represents a number. Find the average of all arguments
+
+
+Examples:
+console.log(getAverage(10,20,30)); // 60 / 3 --> 20
+console.log(getAverage(100, 0)); // 100 / 2 --> 50
+console.log(getAverage(20, 20, 20, 20, 20, 20, 20, 20)); //  --> 20
+***********************************************************************/
+//tool do we have to take/gather in as many arguments as we can? 
+
---
+++ 1-Module/4-week/3-day/lecture-examples/examples.js
@@ -0,0 +1,14 @@
+console.log("-------------What is scope?-------------")
+let getAverage = function(...argsArr){
+    let total = 0;
+    for(let i=0; i<argsArr.length; i++){
+        let arg = argsArr[i];
+        total += arg;
+    }
+    //what variables or functions are available for this line of code? (can be called)
+    
+    return total / argsArr.length;
+}
+// console.log(getAverage(10,20,30)); // 60 / 3 --> 20
+// console.log(getAverage(100, 0)); // 100 / 2 --> 50
+console.log(getAverage(20, 20, 20, 20, 20, 20, 20, 20)); //  --> 20
