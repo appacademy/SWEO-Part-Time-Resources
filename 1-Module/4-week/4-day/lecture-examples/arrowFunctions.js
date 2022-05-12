453d68097d24c3b58d6d21d56fade925ba081ba1
westrinhkl <trinhpt@uci.edu>
2022-05-12 15:18:46 -0700

    updated

--- 1-Module/4-week/4-day/lecture-examples/arrowFunctions.js
+++ 1-Module/4-week/4-day/lecture-examples/arrowFunctions.js
@@ -1,5 +1,5 @@
 console.log("-----------Original Function Expression------------");
-let nameExpression = function(name){
+const nameExpression = function(name){
     return "My name is " + name;
 }
 
@@ -12,15 +12,17 @@ console.log("-----------Arrow Multi Line------------");
 // function should take in `name` as a parameter
 // and then EXPLICITLY returns "My name is `name`"
 
+const multiLineName = (name) => {
+    return "My name is " + name;
+}
 
-
-// console.log(multiLineName("Steph Curry")) // --> "My name is Steph Curry"
+console.log(multiLineName("Steph Curry")) // --> "My name is Steph Curry"
 
 
 console.log("-----------Arrow Single Line------------");
 // Create a `singleLineName` function that is the same as the above function
-// but it IMPLICITLY returns "My name is `name`"
+// but it IMPLICITLY returns "My name is `name`" in a single line.
 
+const singleLineName = name => "My name is " + name;
 
-
-// console.log(singleLineName("Kawhi Leonard")) // --> "My name is Kawhi Leonard"
+console.log(singleLineName("Kawhi Leonard")) // --> "My name is Kawhi Leonard"
