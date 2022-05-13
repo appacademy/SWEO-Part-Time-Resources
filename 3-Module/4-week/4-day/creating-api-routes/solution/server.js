f12e5c0a19950436c0c680375024fcc1c534f96c
baylen doss <baylendoss@baylens-Air.lan>
2022-05-12 19:28:55 -0500

    day 4

--- 3-Module/4-week/4-day/creating-api-routes/solution/server.js
+++ 3-Module/4-week/4-day/creating-api-routes/solution/server.js
@@ -19,20 +19,20 @@ function getNewDogId() {
 const server = http.createServer((req, res) => {
   console.log(`${req.method} ${req.url}`);
 
-  // assemble the request body
   let reqBody = "";
   req.on("data", (data) => {
     reqBody += data;
   });
 
-  req.on("end", () => { // request is finished assembly the entire request body
-    // Parsing the body of the request depending on the Content-Type header
+  // When the request is finished processing the entire body
+  req.on("end", () => {
+    // Parsing the body of the request
     if (reqBody) {
-      switch (req.headers['content-type']) {
+      switch(req.headers['content-type']) {
         case "application/json":
           req.body = JSON.parse(reqBody);
           break;
-        case "application/x-www-form-urlencoded":
+        case "x-www-form-urlencoded":
           req.body = reqBody
             .split("&")
             .map((keyValuePair) => keyValuePair.split("="))
@@ -49,46 +49,71 @@ const server = http.createServer((req, res) => {
       console.log(req.body);
     }
 
-    /* ======================== ROUTE HANDLERS ======================== */
-
+    // route handlers
     // GET /dogs
     if (req.method === 'GET' && req.url === '/dogs') {
-      // Your code here
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.write(JSON.stringify(dogs));
+      return res.end();
     }
-
     // GET /dogs/:dogId
     if (req.method === 'GET' && req.url.startsWith('/dogs/')) {
-      const urlParts = req.url.split('/'); // ['', 'dogs', '1']
+      const urlParts = req.url.split('/');
       if (urlParts.length === 3) {
         const dogId = urlParts[2];
-        // Your code here
+        const dog = dogs.find(dog => dog.dogId == dogId);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify(dog));
+        return res.end();
       }
     }
-
     // POST /dogs
     if (req.method === 'POST' && req.url === '/dogs') {
       const { name, age } = req.body;
-      // Your code here
-    }
+      const dog = {
+        dogId: getNewDogId(),
+        name,
+        age
+      };
+      dogs.push(dog);
 
+      res.statusCode = 201;
+      res.setHeader('Content-Type', 'application/json');
+      res.write(JSON.stringify(dog));
+      return res.end();
+    }
     // PUT or PATCH /dogs/:dogId
     if ((req.method === 'PUT' || req.method === 'PATCH')  && req.url.startsWith('/dogs/')) {
       const urlParts = req.url.split('/');
       if (urlParts.length === 3) {
         const dogId = urlParts[2];
-        // Your code here
+        const { name, age } = req.body;
+        const dog = dogs.find(dog => dog.dogId == dogId);
+        dog.name = name || dog.name;
+        dog.age = age || dog.age;
+
+        res.statusCode = 201;
+        res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify(dog));
+        return res.end();
       }
     }
-
     // DELETE /dogs/:dogId
     if (req.method === 'DELETE' && req.url.startsWith('/dogs/')) {
       const urlParts = req.url.split('/');
       if (urlParts.length === 3) {
         const dogId = urlParts[2];
-        // Your code here
+        const dogIdx = dogs.findIndex(dog => dog.dogId == dogId);
+        dogs.splice(dogIdx, 1);
+        
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify({ message: "Successfully deleted" }));
+        return res.end();
       }
     }
-
     // No matching endpoint
     res.statusCode = 404;
     res.setHeader('Content-Type', 'application/json');
---
+++ 3-Module/4-week/4-day/creating-api-routes/starter/server.js
@@ -0,0 +1,84 @@
+const http = require('http');
+
+const dogs = [
+  {
+    dogId: 1,
+    name: "Fluffy",
+    age: 2
+  }
+];
+
+let nextDogId = 2;
+
+function getNewDogId() {
+  const newDogId = nextDogId;
+  nextDogId++;
+  return newDogId;
+}
+
+const server = http.createServer((req, res) => {
+  console.log(`${req.method} ${req.url}`);
+
+  let reqBody = "";
+  req.on("data", (data) => {
+    reqBody += data;
+  });
+
+  // When the request is finished processing the entire body
+  req.on("end", () => {
+    // Parse the body of the request in JSON
+
+    // route handlers
+
+    // GET /dogs
+    if (req.method === 'GET' && req.url === '/dogs') {
+      // Return the dogs array as JSON in the body of the response
+    }
+
+    // GET /dogs/:dogId
+    if (req.method === 'GET' && req.url.startsWith('/dogs/')) {
+      const urlParts = req.url.split('/');
+      if (urlParts.length === 3) {
+        const dogId = urlParts[2];
+        // Return the specified dog as JSON in the body of the response
+      }
+    }
+
+    // POST /dogs
+    if (req.method === 'POST' && req.url === '/dogs') {
+      // Create a new dog using the `getNewDogId` function and the body of the
+        // request and return the newly created dog as JSON in the body of the
+        // response
+    }
+
+    // PUT or PATCH /dogs/:dogId
+    if ((req.method === 'PUT' || req.method === 'PATCH')  && req.url.startsWith('/dogs/')) {
+      const urlParts = req.url.split('/');
+      if (urlParts.length === 3) {
+        const dogId = urlParts[2];
+        // Edit the specified dog using the body of the request and return the
+          // editted dog as JSON in the body of the response
+      }
+    }
+
+    // DELETE /dogs/:dogId
+    if (req.method === 'DELETE' && req.url.startsWith('/dogs/')) {
+      const urlParts = req.url.split('/');
+      if (urlParts.length === 3) {
+        const dogId = urlParts[2];
+        // Remove the specified dog from the dogs array and return a message of
+          // "Successfully deleted" as JSON in the body of the response
+      }
+    }
+
+    // No matching endpoint
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'application/json');
+    return res.end('Endpoint not found');
+  });
+
+});
+
+const port = 5000;
+
+server.listen(port, () => console.log('Server is listening on port', port));
