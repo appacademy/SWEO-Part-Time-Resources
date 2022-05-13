30d7c9ad54ede0fce7aa957151a28591379023d9
westrinhkl <trinhpt@uci.edu>
2022-05-12 20:26:58 -0700

    updated

--- 1-Module/4-week/4-day/lecture-examples/arrowFunctions.js
+++ 1-Module/4-week/4-day/lecture-examples/arrowFunctions.js
@@ -12,15 +12,19 @@ console.log("-----------Arrow Multi Line------------");
 // function should take in `name` as a parameter
 // and then EXPLICITLY returns "My name is `name`"
 
+//variable multiLineName that holds an anonymous arrow function!
+const multiLineName = (name) => {
+    return "My name is " + name;
+}
 
-
-// console.log(multiLineName("Steph Curry")) // --> "My name is Steph Curry"
+console.log(multiLineName("Steph Curry")) // --> "My name is Steph Curry"
 
 
 console.log("-----------Arrow Single Line------------");
 // Create a `singleLineName` function that is the same as the above function
 // but it IMPLICITLY returns "My name is `name`"
 
+const singleLineName = name => "My name is " + name;
 
 
-// console.log(singleLineName("Kawhi Leonard")) // --> "My name is Kawhi Leonard"
+console.log(singleLineName("Kawhi Leonard")) // --> "My name is Kawhi Leonard"
--- 1-Module/4-week/4-day/lecture-examples/closures.js
+++ 1-Module/4-week/4-day/lecture-examples/closures.js
@@ -13,9 +13,10 @@ let myFunction = function(functionScope){
 
   if(true){
     let blockScope = 'I am block scoped' //this variable belongs to this block's scope
+   
   };
 };
-//tinted car window analogy. Inner can 'see' the outside; but outside cannot see what's inside.
+//tinted car window analogy. Inner can 'see' the outside; but outside cannot see what's inside. - relates to scope chaining
 
 console.log("----------------Simple Counter----------------")
 
@@ -23,16 +24,26 @@ let counter = 0;
 
 //build a function "updateCounter" that when invoked, it will increase the counter by one each time
 
-// --------YOUR CODE HERE---------
+const updateCounter = () => {
+  counter += 1;
+  return counter;
+}
 
-// updateCounter(); //counter === 1
-// updateCounter(); //counter === 2
-// updateCounter(); //counter === 3
-// updateCounter(); //counter === 4
-// console.log("simple counter: " + counter); // 4
+updateCounter(); //counter === 1
+updateCounter(); //counter === 2
+updateCounter(); //counter === 3
+updateCounter(); //counter === 4
+console.log("simple counter: " + counter); // 4
 
 console.log("----------------What is a closure?----------------")
 /*
+- Before we look at closures: Remmember that functions (inner functions) can be return from another function (higher order func)
+
+- Closure, it's the ability for an `inner function` (declared inside of another function) to access, or to change `variables` that were initialized in an `outer function's` scope (outer function's variables).
+    - Summary: `innerFunc` access `outerFunc` variables.
+
+- The inner function is creating a `CLOSURE` around the outer function's (higher order func) variables.
+    -Therefore, the inner function will always have access to those variables even if we return / stop execution of the outer function.    
 
 */
 
@@ -40,35 +51,52 @@ console.log("----------------Creating a Counter Closure----------------")
 
 //1) create a higher order function `counterFactory` (takes in no paramters) that will initialize a `counter2` variable and set it to 0. 
 
-//2) Then build an inner function `updateCounterInner` inside `counterFactory` to update that counter by 1 each time the inner function is called. In `updateCounterInner` return the counter variable at the end.
+const counterFactory = () => {
+  let counter2 = 0;
+  //2) Then build an inner function `updateCounterInner` inside `counterFactory` to update that counter by 1 each time the inner function is called. In `updateCounterInner` return the counter variable at the end.
+  const updateCounterInner = () => {
+    counter2++;
+    return counter2;
+  }
 
-//3) the higher order func `counterFactory` should return that inner function `updateCounterInner`.
+  //3) the higher order func `counterFactory` should return that inner function `updateCounterInner`.
+  return updateCounterInner // notice no (); if updateCounterInner() --> we won't get a function anymore, but instead we get the return value from the updateCounterInner
+}
 
-// -------------YOUR CODE HERE-------------
 
+//outer function
+console.log(counterFactory) //not invoked; what do you think will be logged?
+console.log(counterFactory()) //() invoked; what do you think will be logged?
 
-// console.log(counterFactory) //not invoked; what do you think will be logged?
-// console.log(counterFactory()) //() invoked; what do you think will be logged?
+const updateCounterInside = counterFactory(); // updateCounterInside variable now holds a reference to the inner function (updateCounterInner).
+console.log("from counter2:", updateCounterInside()) //1
+console.log("from counter2:", updateCounterInside()) //2
 
-// const updateCounterInside = counterFactory();
-// console.log("from counter2:", updateCounterInside())
-// console.log("from counter2:", updateCounterInside())
 // console.log(counter2); //will this work?
 
 
 console.log("----------------Favorite Food Factory----------------")
-//1) build a higher order function called `favoriteFoodDB` that will take into its parameter a prefix. 
-
-//2) inside the favoriteFoodDB: initialize an empty array that will hold a list of foods
-
-//3) inside the favoriteFoodDB: DEFINE and RETURN an inner function (call it `innerFunc`) that will take in a food as its parameter and adds the food to the favFoods array (how do we add things to an array?)
-    // - remember to return the `innerFunc`
-
-//4) inside the `innerFunc`: after pushing the food into the favFoods array, return the prefix with the favFoods after joining it with comma separated spaces. (see example below);
-
 // -------------YOUR CODE HERE-----------
 
-// const addFood = favoriteFoodDB("My favorite desserts are ") //--> returns the `innerFunc` inside and stores it in the `addFood` variable.
-// console.log(addFood("wings")); //My favorite desserts are wings
-// console.log(addFood("burgers")); //My favorite desserts are wings, burgers
-// console.log(addFood("chicken sandwiches")); //My favorite desserts are wings, burgers, chicken sandwiches
+//1) build a higher order function called `favoriteFoodDB` that will take into its parameter a prefix. 
+const favoriteFoodDB = (prefix) => {
+  //2) inside the favoriteFoodDB: initialize an empty array that will hold a list of foods
+  const emptyArray = [];
+
+  //3) inside the favoriteFoodDB: DEFINE and RETURN an inner function (call it `innerFunc`) that will take in a food as its parameter and adds the food to the favFoods array (how do we add things to an array?)
+  // - remember to return the `innerFunc`
+  return innerFunc = string => {
+    //4) inside the `innerFunc`: after pushing the food into the favFoods array, return the prefix with the favFoods after joining it with comma separated spaces. (see example below);
+    emptyArray.push(string);
+    let fullSentence = prefix + emptyArray.join(", ");
+    return fullSentence;
+  }
+    
+  
+}
+
+const addFood = favoriteFoodDB("My favorite desserts are ") //--> returns the `innerFunc` inside and stores it in the `addFood` variable.
+
+console.log(addFood("wings")); //My favorite desserts are wings
+console.log(addFood("burgers")); //My favorite desserts are wings, burgers
+console.log(addFood("chicken sandwiches")); //My favorite desserts are wings, burgers, chicken sandwiches
