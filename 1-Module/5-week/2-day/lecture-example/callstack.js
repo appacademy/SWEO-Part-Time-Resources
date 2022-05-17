fcb5b207f08388b7ba4a02a2eb4801b23fbb46c9
westrinhkl <trinhpt@uci.edu>
2022-05-17 15:54:47 -0700

    updated

--- 1-Module/5-week/2-day/lecture-example/callstack.js
+++ 1-Module/5-week/2-day/lecture-example/callstack.js
@@ -7,3 +7,45 @@ console.log("------------the call stack------------")
 
 
 // Recursion uses a call stack for its calls!
+
+const sumArrRecr = function(numsArr){
+    //base case: if empty numsArr empty, then give us 0
+    if(numsArr.length === 0){
+        return 0; //i.e. if numsArr is an empty array, return 0
+    }
+  
+    //starting array: [1,2,3]
+    //1 + sumArrRecr([2,3])
+    //I need the first number from the array to start adding on
+    const firstNum = numsArr[0]; // 1
+    const restOfNums = numsArr.slice(1) //[2,3]
+    //this will solve the smaller problems ([2,3]) and then we add our initial one to that sub-result.
+    const result = firstNum + sumArrRecr(restOfNums); //paused for the next calls
+    //1 + 5 -- > 6
+    return result;
+
+}
+console.log(sumArrRecr([])) // 0
+console.log(sumArrRecr([1,2,3])) // 0 + 1 + 2 + 3 --> 6
+
+// GOING DOWN THE CALL STACK (creating calls)
+//sumArrRecr([1,2,3]
+//        |
+//      1 + sumArrRecr([2,3])
+//                  |
+//                2 + sumArrRecr([3])
+//                          |
+//                        3 + sumArrRecr([])
+//                                  |
+//                                  0
+
+//  GOING BACK UP THE STACK (finish evaluating calls)
+//sumArrRecr([1,2,3]
+//        |
+//      1 + sumArrRecr([2,3])
+//                  |
+//                2 + sumArrRecr([3])
+//                          |
+//                        3 + sumArrRecr([])
+//                                  |
+//                                  0
--- 1-Module/5-week/2-day/lecture-example/recursion.js
+++ 1-Module/5-week/2-day/lecture-example/recursion.js
@@ -8,15 +8,22 @@ console.log("------------What is Recursion------------")
 console.log("------------Regular For Loop------------")
 //write a function that takes an array of numbers and then returns the total sum of all those numbers
 const sumArrIter = function(numsArr){
-
+    let baseTotal = 0;  //0 + num //then reassign baseTotal to that new number
+    for(let i=0; i<numsArr.length; i++){
+        const num = numsArr[i];
+        baseTotal += num; 
+    }
+    return baseTotal;
 } 
 
 console.log(sumArrIter([1])) //0 + 1 --> 1
-console.log(sumArrIter([1,2])) // 1 + 2 --> 3
-console.log(sumArrIter([1,2,3])) // 1 + 2 + 3 --> 6
+console.log(sumArrIter([1,2])) //0 + 1 + 2 --> 3
 
-// let result123 = 1 + sumArrIter([2,3])
-// console.log("1 + sumArrIter([2,3]):", result123)
+
+console.log(sumArrIter([1,2,3])) //0 + 1 + 2 + 3 --> 6
+
+let result123 = 1 + sumArrIter([2,3]) // 1 + 5 // 6
+console.log("1 + sumArrIter([2,3]):", result123)
 
 
 console.log("------------Using Recursion------------")
@@ -26,11 +33,33 @@ console.log("------------Using Recursion------------")
 //3) recursive step - get closer to the base case(sometimes combined with recursive case)
 
 //start with the smallest example first [], then [1], then [1,2], etc.
+
+// const sumArrRecr = function(numsArr){
+//     //base case: if empty numsArr empty, then give us 0
+//     if(numsArr.length === 0){
+//         return 0; //i.e. if numsArr is an empty array, return 0
+//     }
+//     return numsArr[0] + sumArrRecr(numsArr.slice(1));
+// }
 const sumArrRecr = function(numsArr){
+    //base case: if empty numsArr empty, then give us 0
+    if(numsArr.length === 0){
+        return 0; //i.e. if numsArr is an empty array, return 0
+    }
   
+    //starting array: [1,2,3]
+    //1 + sumArrRecr([2,3])
+    //I need the first number from the array to start adding on
+    const firstNum = numsArr[0]; // 1
+    const restOfNums = numsArr.slice(1) //[2,3]
+    //this will solve the smaller problems ([2,3]) and then we add our initial one to that sub-result.
+    const result = firstNum + sumArrRecr(restOfNums); 
+    //1 + 5 -- > 6
+    return result;
+
 }
 console.log(sumArrRecr([])) // 0
 console.log(sumArrRecr([1])) //0 + 1 --> 1
-console.log(sumArrRecr([1,2])) // 1 + 2 --> 3
-console.log(sumArrRecr([1,2,3])) // 1 + 2 + 3 --> 6
+console.log(sumArrRecr([1,2])) // 1 + 2 + 0 + --> 3
+console.log(sumArrRecr([1,2,3])) // 0 + 1 + 2 + 3 --> 6
 
