5b7dc760f743d5c4f5e9887eb42bd2dc6bbf4a4a
westrinhkl <trinhpt@uci.edu>
2022-05-17 19:50:52 -0700

    updated

--- 1-Module/5-week/2-day/lecture-example/callstack.js
+++ 1-Module/5-week/2-day/lecture-example/callstack.js
@@ -7,3 +7,42 @@ console.log("------------the call stack------------")
 
 
 // Recursion uses a call stack for its calls!
+
+//sumArrRecr([1,2,3]
+const sumArrRecr = function(numsArr){
+    //base case where numsArr length is 0, return 0
+    if(numsArr.length === 0) return 0;
+
+    //take the front number
+    const frontNum = numsArr[0];
+
+    //get the final result by combining our frontNum with the recursion of the smaller sub problem
+    const result = frontNum + sumArrRecr(numsArr.slice(1)) // 1 + sumArrRecr([2,3]) //PAUSES H
+    //                                                                   |
+    //                                                                 2 + sumArrRec([3])
+    return result; // 1 + 5 = 6
+}
+
+console.log(sumArrRecr([1,2,3])) // 1 + 2 + 3 --> 6
+
+// in english: going down and creating the callstack
+//result =  sumArrRecr([1,2,3])
+//                  |
+//                1 + sumArrRecr([2,3])
+//                          |
+//                        2 + sumArrRecr([3])
+//                                  |
+//                                3 + sumArrRecr([])
+//                                          |
+//                                          0
+
+// in english: popping the calls off the callstack (back up)
+//result =  sumArrRecr([1,2,3])
+//                  |
+//                  6
+//                          
+//                          
+//                                  
+//                                  
+//                                          
+//                                          
--- 1-Module/5-week/2-day/lecture-example/recursion.js
+++ 1-Module/5-week/2-day/lecture-example/recursion.js
@@ -8,22 +8,28 @@ console.log("------------What is Recursion------------")
 console.log("------------Regular For Loop------------")
 //write a function that takes an array of numbers and then returns the total sum of all those numbers
 const sumArrIter = function(numsArr){
-
+    let baseTotal = 0;
+    for(let i=0; i<numsArr.length; i++){
+        const num = numsArr[i];
+        baseTotal+= num;
+    }
+    return baseTotal;
 } 
 
+console.log(sumArrIter([])) // --> 0
 console.log(sumArrIter([1])) // --> 1
 console.log(sumArrIter([1,2])) // --> 3
 console.log(sumArrIter([1,2,3])) // --> 6
 
-// let result = sumArrIter([1,2,3])
-// let result1 = 1 + sumArrIter([2,3])
-// let result2 = 1 + 2 + sumArrIter([3])
-// let result3 = 1 + 2 + 3 + sumArrIter([])
+let result = sumArrIter([1,2,3]) //6
+let result1 = 1 + sumArrIter([2,3]) // 1 + 5
+let result2 = 1 + 2 + sumArrIter([3]) // 3 + 3
+let result3 = 1 + 2 + 3 + sumArrIter([]) // 6 + 0
 
-// console.log("sumArrIter([1,2,3]):", result)
-// console.log("1 + sumArrIter([2,3]):", result1)
-// console.log("1 + 2 + sumArrIter([3]):", result2)
-// console.log("1 + 2 + 3 + sumArrIter([]):", result3)
+console.log("sumArrIter([1,2,3]):", result)
+console.log("1 + sumArrIter([2,3]):", result1)
+console.log("1 + 2 + sumArrIter([3]):", result2)
+console.log("1 + 2 + 3 + sumArrIter([]):", result3)
 
 
 console.log("------------Using Recursion------------")
@@ -33,8 +39,20 @@ console.log("------------Using Recursion------------")
 //3) recursive step - get closer to the base case(sometimes combined with recursive case)
 
 //start with the smallest example first [], then [1], then [1,2], etc.
+
+//sumArrRecr([1,2,3]
 const sumArrRecr = function(numsArr){
-  
+    //base case where numsArr length is 0, return 0
+    if(numsArr.length === 0) return 0;
+
+    //take the front number
+    const frontNum = numsArr[0];
+
+    //get the final result by combining our frontNum with the recursion of the smaller sub problem
+    const result = frontNum + sumArrRecr(numsArr.slice(1)) // 1 + sumArrRecr([2,3])
+    //                                                                   |
+    //                                                                 2 + sumArrRec([3])
+    return result; // 1 + 5 = 6
 }
 console.log(sumArrRecr([])) // --> 0
 console.log(sumArrRecr([1])) //0 + 1 --> 1
