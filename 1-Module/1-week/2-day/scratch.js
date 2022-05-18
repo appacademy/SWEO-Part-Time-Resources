47a031f83e23a850528a5540cc532a581457198c
J0914 <jordynsechrist@gmail.com>
2022-05-17 20:55:39 -0500

    update examples

--- 1-Module/1-week/2-day/scratch.js
+++ 1-Module/1-week/2-day/scratch.js
@@ -1,15 +1,58 @@
 // Modulo example
+// console.log(5%2)
+// console.log(typeof(NaN))
 
-// loose equality vs strict equality vs strict inequality example
+// let one = true;
+// let two = false;
+
+// // console.log(one && two) // false
+// // console.log(one || two) // true
+// // console.log(one && one) // true
+// // console.log(two || two) // false
+
+// // loose equality vs strict equality vs strict inequality example
+// // let string = '4'
+// // let anotherString = 'I don\'t know'
+// // let num = 4;
+// // console.log(string !== num)
 
 
-// variable example
+// // variable example
+// // let name;
+// // console.log(name);
+// // name = 'Chris';
+// // console.log(name);
+
+// // assignment shorthands +=,-=,*=,/=,
+// let num = 4;
+// let sum = num + 5;
+// // console.log(num);
+// // console.log(sum);
+// // num += 2; // num = num + 2
+// // console.log('with assignment shorthand', num)
+
+// /*
+// commentsssss
+// fjdslj
+// */
 
 
-// assignment operators +=,-=,*=,/=,
+// // prefix and postfix ++,--
+// num++
+// console.log('with ++ postfix', num)
+
+// num--
+// console.log('with -- postfix', num)
 
 
-// prefix and postfix ++,--
+// let isSweet = true;
+// let hasFruit = false;
+// let isCake = !(!isSweet && hasFruit);
+// console.log(isCake);
 
+// let num = 31;
+// let isPositive = num > 0;
+// let isEven = num % 2 === 0;
+// console.log(false);
 
-
+console.log(3 % 6 - 4 / 2 + (5 * 3));
