22237c101aeb183a063b0e768b49d6d6fd31a173
westrinhkl <trinhpt@uci.edu>
2022-05-17 17:58:56 -0700

    updated

--- 1-Module/5-week/2-day/lecture-example/visualize.js
+++ 1-Module/5-week/2-day/lecture-example/visualize.js
@@ -17,20 +17,16 @@ const sumArrRecr = function([1,2,3]){
     if(numsArr.length === 0){
         return 0; 
     }
-  
     const firstNum = 1
     const restOfResult = sumArrRecr([2,3]); //pause here to evaluate sumArrRecr
-
     return 1 + restOfResult;
 }
                 const sumArrRecr = function([2,3]){
                     if(numsArr.length === 0){
                         return 0; 
-                    }
-                
+                    }    
                     const firstNum = 2
                     const restOfResult = sumArrRecr([3]); //pause here to evaluate sumArrRecr
-
                     return 2 + restOfResult;
                 }
                 
@@ -38,10 +34,8 @@ const sumArrRecr = function([1,2,3]){
                                 if(numsArr.length === 0){
                                     return 0; 
                                 }
-                            
                                 const firstNum = 3
                                 const restOfResult = sumArrRecr([]); //pause here to evaluate sumArrRecr
-
                                 return 3 + restOfResult;
                             }
 
@@ -49,7 +43,6 @@ const sumArrRecr = function([1,2,3]){
                                                     if(numsArr.length === 0){
                                                         return 0; 
                                                     }
-                                                
                                                     const firstNum = 3
                                                     const restOfResult = sumArrRecr([]); //pause here to evaluate sumArrRecr
 
