c10694744787b64624f433ff9e8414884013bb2b
westrinhkl <trinhpt@uci.edu>
2022-05-17 18:06:03 -0700

    updated

--- 1-Module/5-week/2-day/lecture-example/visualize.js
+++ 1-Module/5-week/2-day/lecture-example/visualize.js
@@ -19,35 +19,41 @@ const sumArrRecr = function([1,2,3]){
     }
     const firstNum = 1
     const restOfResult = sumArrRecr([2,3]); //pause here to evaluate sumArrRecr
-    return 1 + restOfResult;
-}
+                                |
+    return 1 + restOfResult;    |
+}                               |
+                                |
                 const sumArrRecr = function([2,3]){
                     if(numsArr.length === 0){
                         return 0; 
                     }    
                     const firstNum = 2
                     const restOfResult = sumArrRecr([3]); //pause here to evaluate sumArrRecr
-                    return 2 + restOfResult;
-                }
-                
+                                                |
+                    return 2 + restOfResult;    |
+                }                               |
+                                                |
                             const sumArrRecr = function([3]){
                                 if(numsArr.length === 0){
                                     return 0; 
                                 }
                                 const firstNum = 3
-                                const restOfResult = sumArrRecr([]); //pause here to evaluate sumArrRecr
-                                return 3 + restOfResult;
-                            }
-
+                                const restOfResult = sumArrRecr([]); //pause here to evaluate sumArrRecr                 |
+                                return 3 + restOfResult;   |
+                            }                              |
+                                                           |
                                                 const sumArrRecr = function([]){
                                                     if(numsArr.length === 0){
-                                                        return 0; 
+                                                        return 0;  
+                                                        //HIT BASE CASE----sumArrRecr[]----> 0
                                                     }
+
+                                                    //------IGNORE---------
                                                     const firstNum = 3
                                                     const restOfResult = sumArrRecr([]); //pause here to evaluate sumArrRecr
 
                                                     return 3 + restOfResult;
-                                                } // -------------------> 0
+                                                } 
 
 */
 
