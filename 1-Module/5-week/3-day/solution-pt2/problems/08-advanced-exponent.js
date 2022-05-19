4858d474d2a7e7ed4fb522259fad64c6bb0a113e
westrinhkl <trinhpt@uci.edu>
2022-05-18 18:18:49 -0700

    updated

--- 1-Module/5-week/3-day/solution-pt2/problems/08-advanced-exponent.js
+++ 1-Module/5-week/3-day/solution-pt2/problems/08-advanced-exponent.js
@@ -46,7 +46,16 @@ exponent(b, n) // exponent(b, n / 2) ** 2             [for even n]
 exponent(b, n) // b * (exponent(b, (n - 1) / 2) ** 2) [for odd n]
 */
 function advancedExponent(b, n) {
-  // Your code here
+  if (n === 0) return 1;
+  if (n === 1) return b;
+
+  if (n % 2 === 0) {
+    const halfExp = advancedExponent(b, n/2);
+    return halfExp * halfExp;
+  } else {
+    const halfExp = advancedExponent(b, (n - 1) / 2);
+    return b * halfExp * halfExp;
+  }
 }
 
 
