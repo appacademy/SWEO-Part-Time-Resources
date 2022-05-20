2e0dd2a9c83c7e461870a1baa1ffc0aea2b16750
westrinhkl <trinhpt@uci.edu>
2022-05-19 20:19:39 -0700

    updated

--- 1-Module/5-week/4-day/code-it-out/default-range.js
+++ 1-Module/5-week/4-day/code-it-out/default-range.js
@@ -14,8 +14,24 @@ range(3 ,3)// [];
 //write the recursive range function but utilizing the default parameter of result=[]
 
 function range(start, end, result=[]) {
-  debugger;
+  
+  //-----part 1: the base case----
   //base case; what to return if start >= end?
+  if(start >= end){ //start = 5 , end = 5, is 5 >= 5? it is! so we hit the base case, return, and do nothing after that.
+    return result;
+  }
+
+  //-----part 2: taking the start and adding it to the result array, muating it(total)-----
+  result.push(start); //1
+
+  //-----part 3: Do part 2, but for everything after start (start + 1). Pushing in 2, 3, 4...
+  // range(start + 1, end, result=[1,2])
+  // range(start + 1, end, result=[1,2,3])
+  // range(start + 1, end, result=[1,2,3,4])
+  range(start + 1, end, result); //[1,2,3,4]
+
+
+  return result;
 
 }
 
