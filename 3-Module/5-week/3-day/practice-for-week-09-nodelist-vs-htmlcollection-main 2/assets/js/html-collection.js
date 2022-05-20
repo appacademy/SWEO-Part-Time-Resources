86b71f633189261cbce51aa175af6161540a400c
baylen doss <baylendoss@baylens-Air.lan>
2022-05-19 19:49:18 -0500

    day 4

---
+++ 3-Module/5-week/3-day/practice-for-week-09-nodelist-vs-htmlcollection-main 2/assets/js/html-collection.js
@@ -0,0 +1,14 @@
+export default () => {
+  // const bodyChildElements = document.body.children; // HTMLCollection [div]
+  // console.log(bodyChildElements);
+
+  // const div = bodyChildElements.item(0);
+  // console.log(div)
+
+  // const divChildElements = div.children; // HTMLCollection [span]
+  // console.log(divChildElements)
+  // const helloWorld = div.innerText; // Hello World! Yes!    <-- NOT Hello World!
+  // console.log(helloWorld)
+  // const span = divChildElements[0]; // <span>Yes!</span>
+  // // debugger
+};
