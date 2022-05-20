75a8670a717d8dc33a0cc2f0271f6a2faa6a0c4c
westrinhkl <trinhpt@uci.edu>
2022-05-19 19:52:19 -0700

    updated

--- 1-Module/5-week/4-day/lecture-ex/05-reverse.js
+++ 1-Module/5-week/4-day/lecture-ex/05-reverse.js
@@ -14,7 +14,8 @@ reverse(""); // ""
 
 console.log("-----------iterative reverse-----------");
 let reverseI = word =>{
-  let reverse_word = [];
+  let reverse_word = []; //takes in an total array that accumulates all the letters backwards
+
   for(let i = word.length-1; i>=0; i--){
     reverse_word.push(word[i]);
   }
@@ -25,7 +26,6 @@ console.log("atom:",reverseI("atom")); // "mota"
 console.log("-----------recursive reverse-----------");
 //reverse("cat") ==>  reverse("at") + c ==> reverse("t") + "a" + "c" ==> reverse("") + "t" + "a" + "c" ==> "" + "t" + "a" + "c" ==> "tac"
 function reverse(str) {
-  debugger;
   //base case is when str.length is 0, return ""
   if(str.length === 0){
     return "";
@@ -41,7 +41,28 @@ console.log("cat:", reverse("cat"))
 
 
 console.log("-----------recursive reverse w/ default params-----------");
+//arrays are a reference type. 
+// "cat"
+// ["t", "a", "c"] --> after joining --> "tac"
 function reverseDefault(str, revLetters=[]) {
+  debugger;
+  //start with a base case
+  if(str.length === 0){
+    return "";
+  }
+
+  //get first letter and add it to the front of revLetters
+  const firstLetter = str[0];
+  revLetters.unshift(firstLetter); //mutating revLetters adding the new letter each time
+
+
+ 
+  //now we need to do it for the rest of the letters (adding it to our revLetters array)
+  //don't need the return value of reverseDefault sub problem, because it already adds the next letter to our revLetters reference array.
+  reverseDefault(str.slice(1), revLetters)  //as long as we have a reference to revLetters, we can update it 
+
+
+  return revLetters;
 
 }
 
