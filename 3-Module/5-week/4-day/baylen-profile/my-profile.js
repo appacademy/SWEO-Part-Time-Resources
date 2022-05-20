c23fdaf6602059d61057cb858c07200fe9078d75
baylen doss <baylendoss@baylens-Air.lan>
2022-05-19 23:31:35 -0500

    day 4

--- 3-Module/5-week/4-day/baylen-profile/my-profile.js
+++
@@ -1,7 +0,0 @@
-import {createHeader, createUl, addToUl, createDivComponent, createClock} from './utils.js'
-document.addEventListener('DOMContentLoaded', _ => {
-    createHeader('Baylen Doss')
-    createUl()
-    addToUl(['BasketBall', 'Soccer', 'Football', 'Hockey', 'Curling'])
-    // createClock()
-})
