135e0f7b79a1a1d871f29728a3c3a6e3d5d9e8ff
baylen doss <baylendoss@baylens-Air.lan>
2022-05-21 11:54:10 -0500

    day 5

--- 3-Module/5-week/2-day/indow-object-and-debugger-long-practice-main/assets/js/index.js
+++ 3-Module/5-week/2-day/indow-object-and-debugger-long-practice-main/assets/js/index.js
@@ -19,9 +19,5 @@ document.addEventListener('DOMContentLoaded', e => {
     console.log(findFirstElementOfClass("section")); // <div class="section"><h2>About Me</h2>...</div>
     console.log(findElementsOfTag("H3")); // [h3, h3, h3, h3, h3, h3, h3, h3, h3, h3, h3] (length: 11)
     console.log(findElementsOfClass("section")); // [div.section, div.section, div.section, div.section, div.section, div.section] (length: 6)
-<<<<<<< HEAD
-})
-=======
 
 })
->>>>>>> 2022-Jan-E
