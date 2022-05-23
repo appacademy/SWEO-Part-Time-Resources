8365fce24ab2305e8c8bc8bebb22a3ba0e88b090
J0914 <jordynsechrist@gmail.com>
2022-05-23 17:25:56 -0500

    update days material

---
+++ 1-Module/2-week/1-day/lect-nested-loops.js
@@ -0,0 +1,37 @@
+let people = ["jordyn", "bill", "andres", "sarah"];
+
+console.log("---------------nested loops pairing-------------");
+
+for(let i=0; i<people.length; i++){
+    let person1 = people[i];
+    // console.log(person1);
+    for(let j=0; j<people.length; j++){
+        let person2 = people[j];
+        console.log(person1, person2)
+    }
+}
+
+console.log("--------------nested loops unique pairing----------")
+
+for(let i=0; i<people.length; i++){
+    let person1 = people[i];
+    for(let j=i+1; j<people.length; j++){
+        let person2 = people[j];
+        console.log(person1, person2);
+    }
+}
+
+console.log("--------------pair two arrays to get names and fields----------")
+
+let names = ["wes", "jesse"];
+let fields = ["name", "occupation", "hobby"];
+for(let i=0; i<names.length; i++){
+    let person = names[i];
+    for(let j=0; j<fields.length; j++){
+        let property = fields[j];
+        console.log(person, property);
+    }
+}
+
+
+
