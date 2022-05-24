175a433b57c3d393cf976cec4a2d2da4c288b105
baylen doss <baylendoss@baylens-Air.lan>
2022-05-23 23:51:45 -0500

    day 1

--- 3-Module/6-week/1-day/lecture.js
+++ 3-Module/6-week/1-day/lecture.js
@@ -1,6 +1,16 @@
 document.addEventListener('DOMContentLoaded', e => {
+    // document.body.addEventListener('click', e => {
+    //     console.log(e.target)
+    // })
     let form = document.querySelector('form').addEventListener('click', e => {
         e.preventDefault()
+        // e.target.innerText = e.target.tagName
+        // console.log(e.target)
+        // if(e.target.tagName === 'FORM'){
+        //     console.log('do Form Stuff')
+        // }else{
+        //     console.log('you didnt click on the form')
+        // }
         e.target.innerText = e.target.tagName
     })
     // let div = document.getElementById('form-div').addEventListener('click', e => {
