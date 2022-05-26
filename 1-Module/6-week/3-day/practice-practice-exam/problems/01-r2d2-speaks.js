ee8dd08447fa05a48db55553759260961e67cdd9
westrinhkl <trinhpt@uci.edu>
2022-05-25 18:30:48 -0700

    updated

---
+++ 1-Module/6-week/3-day/practice-practice-exam/problems/01-r2d2-speaks.js
@@ -0,0 +1,32 @@
+/*
+    Write a RECURSIVE function r2d2Speaks that takes in a series of 0s and 1s.
+    The function should console.log a 'beep' for a 0 followed by a pause of 400 ms and a 'boop' for a 1 followed by 800 ms.
+    
+    let code = [0, 1, 1, 0];
+    r2d2Speaks(code);
+        print 'beep'
+        pause for 400ms
+        print 'boop'
+        pause for 800ms
+        print 'boop'
+        pause for 800ms
+        print 'beep'
+        pause for 400ms
+*/
+
+//problem neeeds needs to be solved recursively!
+//hint setTimeout can take in a function (try r2d2, itself :p)
+
+function r2d2Speaks(code) {
+    
+}
+
+let code = [0, 1, 1, 0];
+r2d2Speaks(code);
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+try {
+    module.exports = r2d2Speaks;
+} catch (e) {
+    module.exports = null;
+}
---
+++ 1-Module/6-week/3-day/practice-practice-exam/problems/04-dynamic-slice.js
@@ -0,0 +1,44 @@
+/* Dynamic Slice
+
+    Write a function that accepts two numbers, and returns another function that accepts an array.
+    The new function, when invoked, should return a copy of a portion of the array between the indices of
+    the arguments passed to the first function. Like the built in Array#slice, it should include the 
+		"start" index and exclude the "end" index. Do not use the built-in Array#slice method.
+
+    If the start index is less than 0, the returned function should slice from the beginning of the array.
+    Similarly if the end index is greater than the length of the array, the returned function should 
+		slice up to the end of the array. See below for examples.
+
+    const slicer = dynamicSlice(1, 3);
+    console.log(slicer([0, 1, 2, 3])); // prints [ 1, 2 ]
+
+    const slicer2 = dynamicSlice(2, 7);
+    console.log(slicer2(['kittens', 'puppies', 'cats', 'dogs'])); // prints [ 'cats', 'dogs' ]
+
+		const slicer3 = dynamicSlice(-10, 2);
+    console.log(slicer3([99, 40, 131, 8])); // prints [ 99, 40 ]
+*/
+
+//hint: what are the two edge cases they want us to cover for start and end?
+//going be a closure problem (inner func access outer func's variables)
+
+function dynamicSlice(start, end) {
+
+}
+
+
+const slicer = dynamicSlice(1, 3);
+console.log(slicer([0, 1, 2, 3])); // prints [ 1, 2 ]
+
+const slicer2 = dynamicSlice(2, 7);
+console.log(slicer2(['kittens', 'puppies', 'cats', 'dogs'])); // prints [ 'cats', 'dogs' ]
+
+const slicer3 = dynamicSlice(-10, 2);
+console.log(slicer3([99, 40, 131, 8])); // prints [ 99, 40 ]
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+try {
+    module.exports = dynamicSlice;
+} catch (e) {
+    module.exports = null;
+}
