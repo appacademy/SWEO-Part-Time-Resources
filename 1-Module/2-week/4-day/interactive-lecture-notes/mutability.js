11187247444683cd60af6d1e56e735c1c4577655
J0914 <jordynsechrist@gmail.com>
2022-05-26 16:45:18 -0500

    update w2d4 content

---
+++ 1-Module/2-week/4-day/interactive-lecture-notes/mutability.js
@@ -0,0 +1,25 @@
+/*NOTES LINK (ctrl + left-click to open): 
+https://hackmd.io/ibCTcNIERy2irC8OUTRcFQ?view#Mutability
+*/
+
+// Please answer the questions below, and provide an example using
+// the following data, then we will go over it together briefly.
+// You may not use ANY resources for this! If you don't know, guess!
+
+let arr = [1,2,3,4,5,6]
+let str = 'Witcher'
+
+// mutable example
+// What does it mean if something is mutable, and what are 
+// the 2 mutable data types in javascript?
+
+// please demonstrate how to mutate the mutable data type above
+// using any method you prefer.
+
+
+//immutable example
+// what does it mean if something is immutable and what are 
+// 3 immutable data types?
+
+// please demonstrate how the immutable data type above is immutable.
+
