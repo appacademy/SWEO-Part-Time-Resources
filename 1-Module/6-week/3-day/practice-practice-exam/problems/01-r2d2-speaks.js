65b627c993403eb862ca06eac1883cec70a60383
westrinhkl <trinhpt@uci.edu>
2022-05-25 22:05:02 -0700

    updated

--- 1-Module/6-week/3-day/practice-practice-exam/problems/01-r2d2-speaks.js
+++ 1-Module/6-week/3-day/practice-practice-exam/problems/01-r2d2-speaks.js
@@ -18,6 +18,25 @@
 //hint setTimeout can take in a function (try r2d2, itself :p)
 
 function r2d2Speaks(code) {
+    //base case: when code is empty, then stop the execution of r2d2Speaks
+    if(code.length === 0){
+        return; //this stops the continuation of recursive calls because there's nothing left inside code array
+    }
+
+    //1) get the front element, then handle the 2 main cases 
+    const front = code.shift(); //recursive step, remove the front element in code and placing in the front variable. //i.e. 0
+
+    //handle case where front is 0
+    if(front === 0){
+        console.log("beep");
+        setTimeout(r2d2Speaks, 400, code) //call setTimeout on r2d2Speaks to process the setTimeout for the REST of the elements in code
+    }
+
+    //handle case where front is 1
+    else if(front === 1){
+        console.log("boop"); //print first
+        setTimeout(r2d2Speaks, 800, code); //then pause, then evaluate the REST of the items in code array.
+    }
     
 }
 
