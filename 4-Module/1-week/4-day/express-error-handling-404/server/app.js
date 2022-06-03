71bd2dc768c3a90e40e6cd17558c12720f61e3bd
baylen doss <baylendoss@baylens-Air.lan>
2022-06-02 20:55:33 -0500

    day 4

---
+++ 4-Module/1-week/4-day/express-error-handling-404/server/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('GET / This is the root URL');
+});
+
+// "Resource Not Found Error" middleware
+app.use((req, res, next) => {
+  const err = new Error('Sorry, the requested resource couldn\'t be found.');
+  err.statusCode = 404;
+  next(err);
+});
+
+// Catch-all error-handling middleware
+app.use((err, req, res, next) => {
+  console.log(err);
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message || 'Something went wrong',
+    statusCode
+  });
+});
+
+const port = 5000;
+app.listen(port, () => console.log('Server is listening on port', port));
