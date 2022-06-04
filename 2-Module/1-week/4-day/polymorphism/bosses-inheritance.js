ddc60b2bf5a3c6dc30805f84ce779f2ea6b0686b
westrinhkl <trinhpt@uci.edu>
2022-06-04 11:42:12 -0700

    updated

--- 2-Module/1-week/4-day/polymorphism/bosses-inheritance.js
+++ 2-Module/1-week/4-day/polymorphism/bosses-inheritance.js
@@ -2,9 +2,9 @@
 const MonsterNPC = require("./monsterNPC-class");
 
 console.log("--------------------------inheritance------------------------")
-class Bosses extends MonsterNPC{
-    
-    constructor(name, race, lifepoint, attackable, description, lootTable){
+class Bosses extends MonsterNPC {
+
+    constructor(name, race, lifepoint, attackable, description, lootTable) {
         //super() gets the parent constructor and EXPLICITLY give it the properties to set. 
         super(name, race, lifepoint, attackable, description);
         this.lootTable = lootTable;
@@ -17,19 +17,19 @@ class Bosses extends MonsterNPC{
         // this.lootTable = lootTable;
     }
 
-    getRace(){ //instance method to get the boss's race
+    getRace() { //instance method to get the boss's race
         return this.race;
     }
 
-    getLootTable(){ 
+    getLootTable() {
         return this.lootTable;
     }
 
-    displayLootTable(){
+    displayLootTable() {
         console.log(this.lootTable);
     }
 
-    displayData(){
+    displayData() {
         console.log(`name: ${this.name}`);
         console.log(`race: ${this.race}`);
         console.log(`lifePoint remaining: ${this.getLifepoint()}`);
@@ -63,6 +63,35 @@ For example: SoulsBosses, SM64Bosses, WOWBosses, etc...
 You can do whatever you want, the only rule is that you need to:
 OVERRIDE the displayData() method to be DRY and also accomodating to your boss class.
 */
+class SuperMarioBosses extends Bosses {
+    //default inherit parent constructor: don't really need to add any raw data besides the original, therefore i'm going to just leave it as default (inherit the constructor from the parent class)
+
+    //take the display method, and OVERRIDING it with my new method
+    //YOUR TASK: CREATE YOUR OWN BOSS EXTENSION AND DRY THIS UP!
+    displayData() {
+        console.log(`----------Super Mario Bros Boss: ${this.name} INFO------`)
+        const bossKeys = Object.keys(this);// ['name', 'race', ...]
+        // console.log("bossss keys!:", bossKeys);
+        for (let i = 0; i < bossKeys.length; i++) {
+            const currentKey = bossKeys[i];
+            const value = this[currentKey] //fakeBowser["name"] --> "Fake Bowser"
+            // console.log("valueeee:" , value)
+            console.log(`${currentKey}:`, value)
+        }
+    }
+
+    // // christopher's solution!
+    // displayData() {
+    //     for (let i in this) {
+    //         console.log(`${i}: ${this[i]}`)
+    //     }
+    // }
+
+
+}
+
+const fakeBowser = new SuperMarioBosses("Fake Bowser", "fake turlte", 100, true, "Fake, but still evil!", { point: 100 });
+fakeBowser.displayData();
 
 
 
