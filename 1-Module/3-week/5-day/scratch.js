85785ecb05008cf2f5936165a0b237569a0fef76
J0914 <jordynsechrist@gmail.com>
2022-06-04 10:45:10 -0500

    update w3d5 content

---
+++ 1-Module/3-week/5-day/scratch.js
@@ -0,0 +1,51 @@
+let friends = ['Ludo', 'Hoggle', 'Sarah', 'Sir Didymus']
+let nums = [2,4,6,8,10,12,14,16,18]
+
+// for each example
+// YOU CANNOT RETURN FROM A FOREACH 
+// used to do something to each thing in an array,
+// but will not return anything.
+// this method can mutate the original array if the 
+// code in your callback function mutates it.
+
+
+// Every other advanced method will return something
+// set it to a variable to get access to it!
+// These methods do not mutate the original array
+// unless the code in your callback function mutates it.
+
+// map example
+// returns and array with every element in the array
+// after the changes have been made to each element.
+
+
+// filter example
+// returns an array with only the elements that returned
+// true from the callback function inside.
+
+// reduce example
+// reduce is going to return a single value (the accum's 
+// final value).
+// the default value of the accum is the first ele in
+// the array, or you can give it a different value as the
+// second param after the callback function.
+// Usually good to name this param accum while you're still
+// learning, but you can name it whatever you want.
+// the first arg to the callback function will always
+// be the accumulator, the second arg is the ele in the
+// array.
+
+
+// const names = ['Alice', 'Bob', 'Tiff', 'Bruce', 'Alice']
+// let countedNames = names.reduce(function (allNames, name) {
+//   if (name in allNames) {
+//     allNames[name]++
+//   }
+//   else {
+//     allNames[name] = 1
+//   }
+//   return allNames
+// }, {})
+// // countedNames is:
+// // { 'Alice': 2, 'Bob': 1, 'Tiff': 1, 'Bruce': 1 }
+
