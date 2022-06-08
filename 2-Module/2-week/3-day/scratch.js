eb78645787abcd71bbae4923c5e2f696ac9d00ff
westrinhkl <trinhpt@uci.edu>
2022-06-08 14:20:00 -0700

    updated

---
+++ 2-Module/2-week/3-day/scratch.js
@@ -0,0 +1,17 @@
+// const add = (num1, num2) => num1 + num2;
+// console.log(add('banana', 'orange'));
+
+// const add = (num1, num2) => {
+//   try {
+// 		if (typeof num1 === "number" && typeof num2 === "number"){
+// 			return num1 + num2;
+// 		} else {
+// 			throw new TypeError('Please input a number')
+// 		}
+//   } catch (e) {
+//     console.error(e.message);
+//   }
+// 	return "nothing"
+// };
+
+// console.log(add("banana", "orange"))
