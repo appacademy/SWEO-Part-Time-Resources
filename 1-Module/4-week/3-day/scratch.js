e5cd31b2cc47ffdff8fed002b48f9f7fac3a438e
J0914 <jordynsechrist@gmail.com>
2022-06-08 16:49:26 -0500

    update w4d3 content

---
+++ 1-Module/4-week/3-day/scratch.js
@@ -0,0 +1,11 @@
+// scope
+// global scope
+
+// local scope (function scope)
+
+// block scope
+
+// how they all work together
+// can we 'let' the same variable more than 
+// once in the same scope?
+
