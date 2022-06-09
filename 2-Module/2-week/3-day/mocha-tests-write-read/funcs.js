9d85ad576527d1835414f0da21f5262b328386e6
westrinhkl <trinhpt@uci.edu>
2022-06-08 17:23:00 -0700

    updated

---
+++ 2-Module/2-week/3-day/mocha-tests-write-read/funcs.js
@@ -0,0 +1,15 @@
+// DOING THE GREEN (writing out the functions based on the test)
+function isFive(num) {
+  return num === 5;
+}
+
+function isOdd(number) {
+  // Your code here
+}
+
+function myRange(min, max, step = 1) {
+  // Your code here
+}
+
+
+module.exports = { isFive, isOdd, myRange };
