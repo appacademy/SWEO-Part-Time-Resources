2a7d9f3815270596444108b16b9be4c1f08c0f2d
J0914 <jordynsechrist@gmail.com>
2022-06-09 13:34:01 -0500

    update w4d4 content

---
+++ 1-Module/4-week/4-day/scratch.js
@@ -0,0 +1,10 @@
+// arrow function example
+// WE LOVE ARROW FUNCTIONS
+
+// arrow functions are always anonymous?
+// every time we've used function expression
+// syntax, we've created an anonymous function!
+
+// named function
+
+// anonymous function
