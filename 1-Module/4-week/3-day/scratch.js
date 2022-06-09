6c88f571be2e99e2a3403e4ef231105955716f02
J0914 <jordynsechrist@gmail.com>
2022-06-08 19:56:30 -0500

    update w4d3 content

--- 1-Module/4-week/3-day/scratch.js
+++ 1-Module/4-week/3-day/scratch.js
@@ -1,11 +1,25 @@
 // scope
 // global scope
+// let animal = 'cow';
 
 // local scope (function scope)
 
-// block scope
+const theAnimal = function(){ // local or function scope
+  var animal = 'dog';
+  // block scope
+  if (true){
+    var animal = 'pig';
+    console.log('in the block scope', animal)
+    
+  }
+  console.log('in the function scope', animal)
+}
+
+theAnimal();
+// console.log('in the global scope', animal);
+
+
 
 // how they all work together
 // can we 'let' the same variable more than 
 // once in the same scope?
-
