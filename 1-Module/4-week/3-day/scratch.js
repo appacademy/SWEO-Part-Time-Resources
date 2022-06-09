f9a53bf985c2fc314bcf705967594a06f6ee3841
J0914 <jordynsechrist@gmail.com>
2022-06-08 21:40:49 -0500

    update w4d3 content

--- 1-Module/4-week/3-day/scratch.js
+++ 1-Module/4-week/3-day/scratch.js
@@ -9,3 +9,20 @@
 // can we 'let' the same variable more than 
 // once in the same scope?
 
+// let animal = 'cow';
+
+function hello(){
+
+  const animal = 'dog';
+
+  if(true){
+    let animal = 'pig'
+    console.log('in the block scope', animal)
+  }
+  
+  console.log('in the function scope', animal);
+}
+
+
+hello();
+// console.log('in the global scope', animal);
