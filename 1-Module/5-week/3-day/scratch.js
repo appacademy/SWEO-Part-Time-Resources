fe4ec43fac92329864ff7413c6118d8fc389a935
J0914 <jordynsechrist@gmail.com>
2022-06-15 15:01:53 -0500

    update w5d3 content

---
+++ 1-Module/5-week/3-day/scratch.js
@@ -0,0 +1,14 @@
+// > Write a function called recRev that takes in an `array` and outputs
+// > that array reversed.
+// > You must solve this problem using recursion and without mutating
+// > the original array.
+
+
+
+// console.log(recRev([0])) // [0]
+// console.log(recRev([0, 1])) // [1, 0]
+// console.log(recRev([9,0,1,2])) // [2,1,0,9]
+
+// Default Params
+// the assignment operator is useful!
+
