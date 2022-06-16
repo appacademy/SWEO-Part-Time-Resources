b4007ce58385b8ecc2535c282d50d91266df1e27
J0914 <jordynsechrist@gmail.com>
2022-06-15 19:00:48 -0500

    update w5d3 content

--- 1-Module/5-week/3-day/scratch.js
+++ 1-Module/5-week/3-day/scratch.js
@@ -3,7 +3,16 @@
 // > You must solve this problem using recursion and without mutating
 // > the original array.
 
+let recRev = arr => {
+ // use arr.length === 0 as my base case
+ // use .slice to lower the length on each recursive call
+ // return an array that contains the retur of recRev and the first ele in the array on each call
+  debugger
+  if (arr.length === 0) return [];
 
+  let result = [...recRev(arr.slice(1)), arr[0]];
+  return result
+}
 
 // console.log(recRev([0])) // [0]
 // console.log(recRev([0, 1])) // [1, 0]
@@ -11,4 +20,17 @@
 
 // Default Params
 // the assignment operator is useful!
+
+let animal = 'dog';
+
+const range = (start, end, arr = []) => {
+  console.log(start);
+  console.log(end);
+  console.log(arr);
+};
+
+range()
+range(1, 2)
+range(1, 2, [1,2,3])
+
 
