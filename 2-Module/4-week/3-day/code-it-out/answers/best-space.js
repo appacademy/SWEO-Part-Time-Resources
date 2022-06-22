2b25b28680cb7fdf543a038a219dcb545cbe01e0
westrinhkl <trinhpt@uci.edu>
2022-06-22 15:56:28 -0700

    Updated

---
+++ 2-Module/4-week/3-day/code-it-out/answers/best-space.js
@@ -0,0 +1,6 @@
+// which of the following datastructures have a Big O space compleixty of O(1) ?
+// choices: arrays, stack, queues, singly ll, doubly ll, hashmap (POJOs), or none.
+// hint: (hint: what is the growth curve of the space compared to the growth of `n` number of elements)
+
+
+//your answer: None! They have a O(n) space complexity for n given number of elements!
