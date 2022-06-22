e46656d8e5e726149e99381bfab6d44889847137
baylen doss <baylendoss@baylens-Air.lan>
2022-06-21 20:00:59 -0500

    day 2

---
+++ 4-Module/4-week/2-day/practice-for-week-11-sequelize-foreign-key-migrations-short-practice-main/server/db/migrations/20220621224339-create-musician-instrument.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('MusicianInstruments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      musicianId: {
+        type: Sequelize.INTEGER,
+        references : {
+          model : 'Musicians'
+        }
+      },
+      instrumentId: {
+        type: Sequelize.INTEGER,
+        references : {
+          model : 'Instruments'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('MusicianInstruments');
+  }
+};
