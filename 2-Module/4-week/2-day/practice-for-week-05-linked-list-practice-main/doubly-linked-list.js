8fb1458638d3626cbe8de885a838a9b8b2136ad7
westrinhkl <trinhpt@uci.edu>
2022-06-21 21:29:45 -0700

    updated

---
+++ 2-Module/4-week/2-day/practice-for-week-05-linked-list-practice-main/doubly-linked-list.js
@@ -0,0 +1,35 @@
+class DoublyLinkedListNode {
+    constructor(val) {
+      this.value = val;
+      this.prev = null;
+      this.next = null;
+    }
+  }
+  
+  class DoublyLinkedList {
+    constructor() {
+      // Your code here
+    }
+  
+    addToHead(val) {
+      // Your code here
+    }
+  
+    addToTail(val) {
+      // Your code here
+    }
+  
+    // You can use this function to help debug
+    print() {
+      let current = this.head;
+  
+      while (current) {
+        process.stdout.write(`${current.value} <-> `);
+        current = current.next;
+      }
+  
+      console.log("NULL");
+    }
+  }
+  
+  module.exports = DoublyLinkedList;
