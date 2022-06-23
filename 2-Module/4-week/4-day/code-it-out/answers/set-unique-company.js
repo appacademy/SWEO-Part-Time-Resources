09a2bda600d8346457aaac786ee9890895dbddd8
westrinhkl <trinhpt@uci.edu>
2022-06-23 14:26:50 -0700

    updated

---
+++ 2-Module/4-week/4-day/code-it-out/answers/set-unique-company.js
@@ -0,0 +1,48 @@
+//Open Set Documentation: 
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set/Set
+//Look on the left hand side for Set methods and at the example to instantiate a new Set.
+
+// ----------Properties of Set Summary--------
+//No Dupes: A set contains no duplicate elements
+
+//Only Keys, NO Values: Set is just like a hashmap (key value pair object) but only holding the keys only, no values.
+
+//O(1) Search Time: A set can check if an element is contained in O(1) time (whereas arrays we have to potentially iterate through the whole array to find the element)
+
+// Your Task: 
+//1) Analyze and break down the problem. What is each function doing?
+//2) Replace the array datastructure w/ a Set() to take in unique names of companies. Then REFACTOR the code to make it work with a set.
+
+const companies = ["Amazon", "Google", "App Academy", "Nintendo", "Samsung", "Apple", "CoolestCompanyCo"];
+
+//# add comment here, analyzing what this function does.
+function nameIsTaken(companyName){
+    
+    //return companies.includes(companyName)
+    //below is same as^:
+    for(let i=0; i<companies.length; i++){
+        const takenCompany = companies[i];
+        if(takenCompany === companyName){
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log("Is Amazon taken?:", nameIsTaken("Amazon"))
+
+//# add comment here, analyzing what this function does.
+function addCompanyIn(companyName){
+    // # add comment, what does this code block do
+    if(nameIsTaken(companyName) === false){
+        companies.push(companyName);
+        return "Your company has been added!"
+    }
+    else{
+        return "Company name is taken";
+    }
+}
+
+console.log(addCompanyIn("Amazon")); //"Company name is taken"
+console.log(addCompanyIn("NewestCoolestBiz")); //"Your company has been added!"
+console.log("current companies:", companies);
