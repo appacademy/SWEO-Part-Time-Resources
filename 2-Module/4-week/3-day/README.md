d0fe5c620adec7a92a9745a62c4b882e5cf82a94
westrinhkl <trinhpt@uci.edu>
2022-06-22 19:12:04 -0700

    updated

---
+++ 2-Module/4-week/3-day/README.md
@@ -0,0 +1,24 @@
+## Code-it-out
+- 1) `best-space.js`
+- 2) `draw-out-diagram.js`
+- 3) `queue analysis.js`
+
+## Project Linked-List Pair Programming
+- Link List will be a big part of this week's assessment!
+- Pair Programming Etiquette:
+  - Use I and We
+  - Let other person finish speaking (we shouldn't cut in)
+  - Focus on the code!! (nothing like politics, etc.)
+  - Respect one another and treat one another like how we want to be treated (put ourselves in their shoes)
+
+- If you're stuck for 5 minutes. Mandatory to ask question. You can get a strike for not doing this.
+- If you're struggling with any concepts or need more breakdown, let us know! 
+  - Einstein: doesn't matter if we think our question is stupid, what matters is we learn and get the answer we need from the question. 
+
+
+## Homework IMPORTANT
+- Remember that homework is mandatory in order to succeed in a reverse classroom style. During class is where we review a bit, but more importantly, apply the material.
+  - Apply active learning during homework: run examples from the homework and analyze the code; avoid `just reading`.
+  - If there's a concept or example that doesn't make sense, `write down a question for it` and bring it to class the next day. I don't know if you're struggling with it unless you let me know.
+- Go over today's homework,; fairly short
+- IMPORTANT: Review / do Homework for Wednesday (hashmap homework) as it will be the focus of tomorrow's project.
---
+++ 2-Module/4-week/3-day/code-it-out/draw-out-diagram.js
@@ -0,0 +1,105 @@
+//first create a class to represent the nodes
+class LinkedListNode {
+    constructor(value) {
+        this.value = value; //represents the data (i.e. a number like 10)
+        this.next = null; //represents the LinkedListNode instance that we are pointing to next.
+    }
+}
+
+
+//create a class for creating our linked list - O(n) space, where n is the number of nodes.
+class LinkedList {
+    constructor() {
+        this.head = null; //this.head is a pointer that points to a LinkListNode instance.
+        this.tail = null; //trade off a bit of space to keep track of a tail.
+    }
+
+    //should have a print functionality O(n)
+    print() {
+        let current = this.head;
+
+        //traverse until we hit null.
+        while (current) {
+            process.stdout.write(`${current.value} -> `);
+            current = current.next;
+        }
+
+        //if current.next is null, then while loop will not be hit, and this will be hit instead
+        console.log("NULL");
+    }
+
+    //add to the front of the list O(1)
+    addToHead(value) {
+        //create new node w/ passed in value
+        const newNode = new LinkedListNode(value);
+
+        //0) edge case: if head's pointer is null, then make the head and tail to newNode
+        if (this.head === null) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else {
+            //1) point newNode's next to the node where head is currently pointing
+            newNode.next = this.head;
+            //2) Once we connected the newNode, reassign this.head to the newNode (this is the new head)
+            this.head = newNode;
+        }
+    }
+
+    //addToTail should add a new node to the end of a list
+    addToTail(value) {
+        //create new node w/ the passed in value
+        const newNode = new LinkedListNode(value);
+
+        //0) edge case: what if head --> null
+        if (this.head === null) {
+            //set both head and tail to the newNode
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else {
+            //first we need to point end.next (tail.next) to the new node first
+            this.tail.next = newNode;
+            //take the tail which is `end` and point it to newNode
+            this.tail = newNode;
+        }
+    }
+
+    // to remove from head, we just need to set head = head.next and allow garbage collection to clean up (if no reference points to that data, it will get removed.)
+    removeFromHead() {
+        //first check if this.head exist since if we tried null.next, it will give us error.
+        if (this.head) {
+            this.head = this.head.next;
+        }
+
+        //edge case to handle when head is set to null. Tail should also be set to null
+        // if there's only one node and head is now pointing at null (from above), tail needs to also move to null (where head is)
+        if (this.head === null) {
+            this.tail = null;
+        }
+    }
+}
+
+
+//TASK: Based on how this linked list was implemented... diagram or draw out each example
+console.log("----------Linked List Start Example--------")
+const ll = new LinkedList();
+
+console.log("----------test addtoHead()--------")
+//Linked list currently looks like:  NULL
+ll.addToHead("one!");
+ll.addToHead("zero!")
+ll.print(); //zero! -> one! -> NULL
+
+console.log("----------test addToTail()--------")
+//Linked list currently looks like:  zero! -> one! -> NULL
+ll.addToTail(2)
+ll.print();  //zero! -> one! -> 2 -> NULL
+
+console.log("----------test removeFromHead()--------")
+//Linked list currently looks like:  //zero! -> one! -> 2 -> NULL
+ll.removeFromHead(); 
+ll.print(); //  one! -> 2 -> NULL
+
+
+//execute node this file after.
