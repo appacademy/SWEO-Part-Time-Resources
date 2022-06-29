a27a80ec3a6750ccc82f3e720ddba56a78484d7a
J0914 <jordynsechrist@gmail.com>
2022-06-28 22:00:39 -0500

    update dry project

--- 2-Module/1-week/2-day/dryProject/practice.js
+++ 2-Module/1-week/2-day/dryProject/practice.js
@@ -1,27 +1,45 @@
-const findBiggest = (num1, num2) => {
-  if (num1 > num2) return num1
-  return num2;
-}
-
 function multiplyBiggerNumByTwo(num1, num2) {
-  let bigNum = findBiggest(num1, num2)
-  return bigNum * 2; 
+  let bigNum;
+  if (num1 > num2) {
+    bigNum = num1;
+    return bigNum * 2;
+  } else {
+    bigNum = num2;
+    return bigNum * 2;
+  }
 }
 
 function divideBiggerNumByThree(num1, num2) {
-  let bigNum = findBiggest(num1, num2)
-  return bigNum / 3;
+  let bigNum;
+  if (num1 > num2) {
+    bigNum = num1;
+    return bigNum / 3;
+  } else {
+    bigNum = num2;
+    return bigNum / 3;
+  }
 }
 
 function eatMostTacos(sum1, sum2) {
-  let bigNum = findBiggest(sum1, sum2)
-  return `I ate ${bigNum} tacos.`;
+  let bigNum;
+  if (sum1 > sum2) {
+    bigNum = sum1;
+    return `I ate ${bigNum} tacos.`;
+  } else {
+    bigNum = sum2;
+    return `I ate ${bigNum} tacos.`;
+  }
 }
 
 function adoptSmallerDog(weight1, weight2) {
-  let smallDog = findBiggest(weight1, weight2) === weight1 ? weight2 : weight1; // ternary!
-  console.log(smallDog)
+  let smallDog;
+  if (weight1 < weight2) {
+    smallDog = weight1;
     return `I adopted a dog that weighs ${smallDog} pounds.`;
+  } else {
+    smallDog = weight2;
+    return `I adopted a dog that weighs ${smallDog} pounds.`;
+  }
 }
 
 
