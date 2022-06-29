f18641443d3f48bfd3574d690846f1636308b669
westrinhkl <trinhpt@uci.edu>
2022-06-28 21:59:44 -0700

    updated

--- 2-Module/5-week/2-day/practice-for-week-06-binary-search-tree-main/binary-search-tree.js
+++ 2-Module/5-week/2-day/practice-for-week-06-binary-search-tree-main/binary-search-tree.js
@@ -13,39 +13,39 @@ class BinarySearchTree {
     this.root = null;
   }
 
-  insert(val, currentNode=this.root) {
+  insert(val, currentNode = this.root) {
     // when we first add an item, if this.root is null, we will make the first insertion our root node
-    if(this.root === null){
+    if (this.root === null) {
       this.root = new TreeNode(val);
       return; //once we insert the desired val, then just exit the insert function.
     }
     //1) determine if our val is less than currentNode.val
-    else if(val < currentNode.val){
+    else if (val < currentNode.val) {
       //1.1) we need to check if currentNode.left is empty (currentNode.left === null)
-      if(currentNode.left === null){
+      if (currentNode.left === null) {
         //we need to create the node based on the value, then add it in (currentNode.left = new TreeNode(val))
         currentNode.left = new TreeNode(val);
       }
       //1.2) if a node exists on currentNode.left (currentNode.left !== null)
-      else if(currentNode.left !== null){
+      else if (currentNode.left !== null) {
         const nextNodeToTravel = currentNode.left;
-        this.insert(val, nextNodeToTravel )
+        this.insert(val, nextNodeToTravel)
 
         //^same as above: this.insert(val, currentNode.left)
       }
     }
 
     //2) determine if our val is greater than currentNode.val (since our val is greater than the currentNode.val, we add it to the right)
-    else if(val > currentNode.val){
+    else if (val > currentNode.val) {
       //1.1) we need to check if currentNode.right is empty (currentNode.right === null)
-      if(currentNode.right === null){
+      if (currentNode.right === null) {
         //we need to create the node based on the value, then add it in (currentNode.right = new TreeNode(val))
         currentNode.right = new TreeNode(val);
       }
       //1.2) if a node exists on currentNode.right (currentNode.right !== null)
-      else if(currentNode.right !== null){
+      else if (currentNode.right !== null) {
         const nextNodeToTravel = currentNode.right;
-        this.insert(val, nextNodeToTravel )
+        this.insert(val, nextNodeToTravel)
         //^same as above: this.insert(val, currentNode.right)
       }
     }
@@ -53,7 +53,17 @@ class BinarySearchTree {
   }
 
   search(val) {
-    // Your code here
+    //lets create a currentNode that will be a POINTER to traverse the tree for us
+
+    //while currentNode is not null
+    //  1) check if the currentNode's value is equal to the target val, return true if so.
+
+    //  2) else if the target is less than the currentNode's value, reassign currentNode to be the currentNode.left
+
+    //  3) else if the target is greater than the currentNode's value, reassign currentNode to be the currentNode.right
+
+    
+    //after the while loop, if the while loop didn't find any match, return false by default.
   }
 
 
@@ -71,7 +81,7 @@ class BinarySearchTree {
     // Your code here
   }
 
-    // Breadth First Traversal - Iterative
+  // Breadth First Traversal - Iterative
   breadthFirstTraversal() {
     // your code here
   }
@@ -79,7 +89,7 @@ class BinarySearchTree {
   // Depth First Traversal - Iterative
   depthFirstTraversal() {
     // your code here
-}
+  }
 }
 
 module.exports = { BinarySearchTree, TreeNode };
