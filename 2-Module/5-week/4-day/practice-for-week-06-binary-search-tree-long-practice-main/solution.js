348f20710cb8a10413e871db20b8ec04bf2acf3b
westrinhkl <trinhpt@uci.edu>
2022-06-30 17:55:04 -0700

    updated

--- 2-Module/5-week/4-day/practice-for-week-06-binary-search-tree-long-practice-main/solution.js
+++ 2-Module/5-week/4-day/practice-for-week-06-binary-search-tree-long-practice-main/solution.js
@@ -17,15 +17,20 @@ function findMinBST(rootNode) {
   //min is the the most left node the the deepest level.
 
   //check to make sure rootNode is not null (not needed to pass test, but good to have)
-
+  if (rootNode === null) {
+    return null;
+  }
 
   //create a pointer first to traverse and set at the starting point rootNode
+  let currentNode = rootNode;
 
   //while currentNode.left does not equal to null, traverse down the left and reassign the currentNode to be currenNode.left as the most left will be the smallest
+  while (currentNode.left !== null) {
+    currentNode = currentNode.left;
+  }
 
-
-  //once while loop finishes, return currentNode's value.
-
+  //once while loop finishes, currentNode will point to the smallest node
+  return currentNode.val;
 }
 
 
@@ -37,13 +42,19 @@ function findMaxBST(rootNode) {
   // }
   // return currentNode.val;
 
-
   //******************or use recursion!!****************
-  //if rootNode.right exists, continue to traverse recursively on the right hand side and return the answer of the most right node
+  //if rootNode.right exists, continue to traverse recursively and return the answer of the most right node
+  if (rootNode === null) {
+    return null;
+  }
 
+  if (rootNode.right) {
+    const maxFoundVal = findMaxBST(rootNode.right);
+    return maxFoundVal;
+  }
 
   //otherwise return the rootNode since rootNode.right does not exist.
-
+  return rootNode.val;
 }
 
 
@@ -52,22 +63,25 @@ function findMaxBST(rootNode) {
 function findMinBT(rootNode) {
   //there's many ways to solve this, select a traversal method and try it out.
 
+  const queue = [];
+  if (rootNode !== null) queue.push(rootNode);
 
-  //create a queue and intialize it with rootNode
-
-
-  //default initially the min value to Infinity (everything is less than infinity)
-
+  let min = Infinity;
 
   //while the queue length, add all the nodes and process it
-
+  while (queue.length) {
     //pop the node and then compare it to see if it's smaller
-
+    const currentNode = queue.shift();
+    if (currentNode.val < min) min = currentNode.val;
 
     //add the currentNode's left and right to the queue and repeat
-
+    if (currentNode.left) queue.push(currentNode.left);
+    if (currentNode.right) queue.push(currentNode.right);
+  }
 
   //finally return answer after traversing all node and comparing it to find min
+  return min;
+
 }
 
 function findMaxBT(rootNode) {
@@ -77,34 +91,39 @@ function findMaxBT(rootNode) {
   //Draw out a small tree and walk through the example below if we're not sure what is happening.
   //tldr: we are going all the way down one end first, finding the max value of the small subtree/subNode and then comparing it with the one above it. (recursion to solve smaller sub problem)
 
-  //initially set max val as the value from rootNode.
+  let currentMaxVal = rootNode.val; //initially set max val as the value from rootNode.
 
-  //if rootNode left exists
+  if (rootNode.left) {
     //set the max between the currentMaxVal and the left hand's max
-
-
-   //if rootNode right exists
+    const maxFromLeft = findMaxBT(rootNode.left);
+    currentMaxVal = Math.max(currentMaxVal, maxFromLeft);
+  }
+  if (rootNode.right) {
     //set the max between the currentMaxVal and the right hand's max
+    const maxFromRight = findMaxBT(rootNode.right);
+    currentMaxVal = Math.max(currentMaxVal, maxFromRight);
+  }
 
   //at the very end, after comparisions, return the max value.
-
+  return currentMaxVal;
 }
 
 function getHeight(rootNode) {
 
-  //what is the height of a tree with zero node (root === null)? it's 0. 
-  
-  // what is the height of a tree with only one node? it's 0.
-
+  //what is the height of a tree with only one node or zero node (root === null)? it's 0. 
+  if(rootNode === null) return 0;
+  if(rootNode.left === null && rootNode.right === null) return 0;
 
   //recurse to get height of left subtree
-
+  const leftHeight = getHeight(rootNode.left);
 
   //recurse to get height of right subtree
-
+  const rightHeight = getHeight(rootNode.right);
 
   //whichever one is bigger (between left and right), we'll take that and add one to it to get the final height, return it.
-
+  let max = Math.max(leftHeight, rightHeight);
+  const finalHeight = max + 1;
+  return finalHeight;
 
   
 }
