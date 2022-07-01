50831d413e6346a337ad42c7badfb6d6f0a121ed
J0914 <jordynsechrist@gmail.com>
2022-06-30 20:57:37 -0500

    update w7d4 content

---
+++ 2-Module/1-week/4-day/FRQ/polymorphism-frq.js
@@ -0,0 +1,16 @@
+// FRQ: what is polymorphism? Does it apply to parent classes or child classes?
+// FRQ: what are the two types of polymorphism? 
+
+// --remember, try with our own knowledge first! 
+// --Even if it's the wrong answer, write it out, we can always learn from it later
+// --It's okay to be wrong, this isn't the exam, 
+// --there's no risk in being wrong, there's only an opportunity to learn
+
+
+
+/* 
+
+WRITE YOUR ANSWER HERE:
+
+
+*/
