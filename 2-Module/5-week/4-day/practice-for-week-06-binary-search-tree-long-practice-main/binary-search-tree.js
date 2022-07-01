88219ef65aeebd49926ee75e4cc55c010a566f32
westrinhkl <trinhpt@uci.edu>
2022-06-30 21:54:14 -0700

    updated

--- 2-Module/5-week/4-day/practice-for-week-06-binary-search-tree-long-practice-main/binary-search-tree.js
+++ 2-Module/5-week/4-day/practice-for-week-06-binary-search-tree-long-practice-main/binary-search-tree.js
@@ -1,4 +1,194 @@
-// Before starting, copy and paste your guided practice work from
-// `binary-search-tree.js` into this file
-
-// Your code here
+// Do not change this
+class TreeNode {
+    constructor(val) {
+      this.val = val;
+      this.left = null;
+      this.right = null;
+    }
+  }
+  
+  class BinarySearchTree {
+  
+    constructor() {
+      this.root = null;
+    }
+  
+    // insert(val, currentNode=this.root) {
+    //   //edge case: handle initial start where root is null
+    //   if(this.root === null){
+    //     // we want to add the val as our root node
+    //     this.root = new TreeNode(val);
+    //     return;
+    //   }
+    //   //if the val is less than the currentNode.val then...
+    //   if(val < currentNode.val){
+    //     //if the currentNode.left is null, then we add in the node
+    //     if(currentNode.left === null){
+    //       currentNode.left = new TreeNode(val);
+    //     }
+    //     //else if the currentNode.left is NOT null, then we continue to traverse by calling insert again passing to it (val, currentNode.left)
+    //     else if(currentNode.left !== null){
+    //       this.insert(val, currentNode.left);
+    //     }
+    //   }
+    //   //if the val is greater than the currenNode.val then...
+    //   else{
+    //     if(val > currentNode.val){
+    //       if(currentNode.right === null){
+    //         //reassign currentNode.right from null to the new node to add in
+    //         currentNode.right = new TreeNode(val);
+    //       }
+    //       //else it's occupied w/ a node already, so call insert again on the next right node. In order to find the correct spot that is null.
+    //       else if(currentNode.right !== null){
+    //         this.insert(val, currentNode.right);
+    //       }
+    //     }
+    //   }
+    // }
+  
+    //christopher's and david's
+    insert(val, currentNode = this.root) {
+      let newNode = new TreeNode(val);
+  
+      if (this.root === null) {
+        this.root = newNode;
+        return;
+      }
+      if (newNode.val < currentNode.val) {
+        if (currentNode.left !== null) {
+          this.insert(val, currentNode.left)
+        }
+        else {
+          currentNode.left = newNode;
+          return;
+        }
+      }
+      else {
+        if (currentNode.right !== null) {
+          this.insert(val, currentNode.right)
+        }
+        else {
+          currentNode.right = newNode;
+          return;
+        }
+      }
+    }
+  
+  
+    search(target) {
+      //first create a pointer that will allow us to traverse the BST, and initially set it to the root node (kinda like link list head)
+      let currentNode = this.root;
+  
+      //while the currentNode is not null, we continue to traverse to find the if the val exists!
+      while(currentNode !== null){
+        //1) does the currentNode.val === target? if so, return true
+        if(currentNode.val === target) return true;
+  
+        //2) if target is less than currentNode.val, then we reassign the currentNode to be the currentNode.left node.
+        else if(target < currentNode.val){
+          currentNode = currentNode.left;
+        }
+  
+        //3) if target is greater than currentNode.val, then we reassign the currentNode to be currentNode.right node.
+        else if(target > currentNode.val){
+          currentNode = currentNode.right;
+        }
+      }
+  
+      //if our while loop didn't find that the target exist, then return false
+      return false;
+    }
+  
+  
+    preOrderTraversal(currentNode = this.root) {
+      //base case, if currentNode is null, then stop and just return. This will prevent any code below to excute and stop the recursion
+      if(currentNode === null){
+        return;
+      }
+      //access/ log the data first at the currentNode
+      console.log(currentNode.val);
+  
+      //recursively traverse on the left node (currentNode.left)
+      this.preOrderTraversal(currentNode.left);
+  
+      //recursively traverse on the right node (currentNode.right)
+      this.preOrderTraversal(currentNode.right);
+    }
+  
+  
+    inOrderTraversal(currentNode = this.root) {
+      //if currentNode doesn't exist, this handles the edge case at the start and prevent continuation if currentNode is null
+      if (!currentNode) return;
+  
+      //if currentNode.left exists, recursive call w/ currentNode = currentNode.left
+      this.inOrderTraversal(currentNode.left);
+  
+      //log the data
+      console.log(currentNode.val)
+  
+      //if currentNode.right exists, recursive call w/ currentNode = currentNode.right
+      this.inOrderTraversal(currentNode.right);
+    }
+    
+    
+    postOrderTraversal(currentNode = this.root) {
+      //if currentNode doesn't exist, this handles the edge case at the start and prevent continuation if currentNode is null
+      if (!currentNode) return;
+  
+      //if currentNode.left exists, recursive call w/ currentNode = currentNode.left
+      this.postOrderTraversal(currentNode.left);
+  
+      //if currentNode.right exists, recursive call w/ currentNode = currentNode.right
+      this.postOrderTraversal(currentNode.right);
+  
+      //log the data
+      console.log(currentNode.val)
+    }
+  
+      // Breadth First Traversal - Iterative
+    breadthFirstTraversal() {
+      //create queue (for now we will use an array)
+      const queue = [];
+      queue.push(this.root) //add the root to the queue initially
+  
+      //while queue is not empty (q.length, we want to continue processing the nodes)
+      while(queue.length > 0){
+        //dequeue to get the current node and then process it
+        let node = queue.shift();
+        console.log(node.val);
+  
+        //we check to see if left sub tree exists, so we can queue it to be processed later 
+        if(node.left !== null){
+          queue.push(node.left);
+        }
+        //we check to see if right sub tree exists, so we can queue it to be processed later 
+        if(node.right !== null){
+          queue.push(node.right);
+        }
+      }
+    }
+  
+    // Depth First Traversal - Iterative
+    depthFirstTraversal() {
+      const stack = [this.root];
+  
+      while(stack.length){
+        //pop from the stack, and log the data
+        const currentNode = stack.pop()
+        console.log(currentNode.val);
+  
+        //then check if the left of the stack exist, push it to the stack
+        if(currentNode.left){
+          stack.push(currentNode.left);
+        }
+  
+        //then check if the right of the stack exist, push it to the stack
+        if(currentNode.right){
+          stack.push(currentNode.right);
+        }
+      }
+  
+  }
+  }
+  
+  module.exports = { BinarySearchTree, TreeNode };
--- 2-Module/5-week/4-day/practice-for-week-06-binary-search-tree-long-practice-main/tree-practice.js
+++ 2-Module/5-week/4-day/practice-for-week-06-binary-search-tree-long-practice-main/tree-practice.js
@@ -4,39 +4,124 @@ const { BinarySearchTree, TreeNode } = require('./binary-search-tree.js');
 
 // Practice problems on binary trees
 
-function findMinBST (rootNode) {
-  // the most left node of the binary search tree is our smallest node
+bstRoot = new TreeNode(4);
+bstRoot.left = new TreeNode(2);
+bstRoot.left.left = new TreeNode(1);
+bstRoot.left.right = new TreeNode(3);
+bstRoot.right = new TreeNode(6);
+bstRoot.right.left = new TreeNode(5);
+bstRoot.right.right = new TreeNode(7);
+
+function findMinBST(rootNode) {
+  debugger;
+  //min is the the most left node the the deepest level.
+
+  //check to make sure rootNode is not null (not needed to pass test, but good to have)
+
+
+  //create a pointer first to traverse and set at the starting point rootNode
+
+  //while currentNode.left does not equal to null, traverse down the left and reassign the currentNode to be currenNode.left as the most left will be the smallest
+
+
+  //once while loop finishes, return currentNode's value.
+
 }
 
-function findMaxBST (rootNode) {
+
+function findMaxBST(rootNode) {
+  // //******************we can do a similar process w/ while loop like findMinBST****************
+  // let currentNode = rootNode;
+  // while(currentNode.right !== null){
+  //   currentNode = currentNode.right;
+  // }
+  // return currentNode.val;
+
+
+  //******************or use recursion!!****************
+  //if rootNode.right exists, continue to traverse recursively on the right hand side and return the answer of the most right node
+
+
+  //otherwise return the rootNode since rootNode.right does not exist.
+
+}
+
+
+
+
+function findMinBT(rootNode) {
+  //there's many ways to solve this, select a traversal method and try it out.
+
+
+  //create a queue and intialize it with rootNode
+
+
+  //default initially the min value to Infinity (everything is less than infinity)
+
+
+  //while the queue length, add all the nodes and process it
+
+    //pop the node and then compare it to see if it's smaller
+
+
+    //add the currentNode's left and right to the queue and repeat
+
+
+  //finally return answer after traversing all node and comparing it to find min
+}
+
+function findMaxBT(rootNode) {
+
+  //once again, can repeat the queue traversal like above, but let's try something new. Let's do DFS Recursive traversal (solving smaller subtrees to help us solve the root tree!)
+
+  //Draw out a small tree and walk through the example below if we're not sure what is happening.
+  //tldr: we are going all the way down one end first, finding the max value of the small subtree/subNode and then comparing it with the one above it. (recursion to solve smaller sub problem)
+
+  //initially set max val as the value from rootNode.
+
+  //if rootNode left exists
+    //set the max between the currentMaxVal and the left hand's max
+
+
+   //if rootNode right exists
+    //set the max between the currentMaxVal and the right hand's max
+
+  //at the very end, after comparisions, return the max value.
+
+}
+
+function getHeight(rootNode) {
+
+  //what is the height of a tree with zero node (root === null)? it's 0. 
+  
+  // what is the height of a tree with only one node? it's 0.
+
+
+  //recurse to get height of left subtree
+
+
+  //recurse to get height of right subtree
+
+
+  //whichever one is bigger (between left and right), we'll take that and add one to it to get the final height, return it.
+
+
+  
+}
+
+function countNodes(rootNode) {
   // Your code here
 }
 
-function findMinBT (rootNode) {
+function balancedTree(rootNode) {
   // Your code here
 }
 
-function findMaxBT (rootNode) {
+function getParentNode(rootNode, target) {
   // Your code here
 }
 
-function getHeight (rootNode) {
-  // Your code here
-}
-
-function countNodes (rootNode) {
-  // Your code here
-}
-
-function balancedTree (rootNode) {
-  // Your code here
-}
-
-function getParentNode (rootNode, target) {
-  // Your code here
-}
-
-function inOrderPredecessor (rootNode, target) {
+function inOrderPredecessor(rootNode, target) {
   // Your code here
 }
 
@@ -63,14 +148,14 @@ function deleteNodeBST(rootNode, target) {
 }
 
 module.exports = {
-    findMinBST,
-    findMaxBST,
-    findMinBT,
-    findMaxBT,
-    getHeight,
-    countNodes,
-    balancedTree,
-    getParentNode,
-    inOrderPredecessor,
-    deleteNodeBST
+  findMinBST,
+  findMaxBST,
+  findMinBT,
+  findMaxBT,
+  getHeight,
+  countNodes,
+  balancedTree,
+  getParentNode,
+  inOrderPredecessor,
+  deleteNodeBST
 }
