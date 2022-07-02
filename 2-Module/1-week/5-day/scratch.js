360779abf49a022455b1d667e1d41df8ad24760d
J0914 <jordynsechrist@gmail.com>
2022-07-02 16:00:17 -0500

    update w7d5 content

--- 2-Module/1-week/5-day/scratch.js
+++ 2-Module/1-week/5-day/scratch.js
@@ -1,1 +1,23 @@
-// blank on purpose
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+
+  speak(noise) {
+    return `${this.name} ${noise}s`;
+  }
+
+  potty(){
+    return `${this.name} just went potty!`;
+  }
+}
+
+class Dog extends Animal {
+  constructor(name){
+    super(name)
+  }
+
+  speak() {
+    return `${this.name} barks`;
+  }
+}
