f1247a92726ddb70c86fd8b0a4e9ddc477e119c3
J0914 <jordynsechrist@gmail.com>
2022-07-11 19:58:42 -0500

    update w8d1 content

--- 2-Module/2-week/1-day/context/problems/09-call-on-target.js
+++
@@ -1,9 +0,0 @@
-function callOnTarget(func, obj1, obj2) {
-  // Your code here
-}
-
-
-/*****************************************************************************/
-/***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
-
-module.exports = callOnTarget;
