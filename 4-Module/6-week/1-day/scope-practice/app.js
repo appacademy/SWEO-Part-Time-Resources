0910f12c5bc4e72a12fab599549be4aac9b2180c
baylen doss <baylendoss12@gmail.com>
2022-07-12 13:24:45 -0700

    day 1

--- 4-Module/6-week/1-day/scope-practice/app.js
+++ 4-Module/6-week/1-day/scope-practice/app.js
@@ -10,17 +10,19 @@ const app = express()
 app.use(express.json())
 
 app.get('/users', async(req, res) => {
+    // default scope gets applied to all queries
     const users = await User.findAll()
     res.json(users)
 
 })
 
 app.get('/users/inactive', async(req, res) => {
+    // non default scopes must be called 
     // calling a scope that takes args
     // const inactiveUsers = await User.scope({method : ['isInactive', 3]}).findAll()
 
     //calling multiple scopes
-    const users = await User.scope(['isInactive', 'isActive']).findAll()
+    const users = await User.scope([{method :[ 'isInactive', 1]}, {method : ['isActive', 2]}]).findAll()
     res.json(users)
 })
 
