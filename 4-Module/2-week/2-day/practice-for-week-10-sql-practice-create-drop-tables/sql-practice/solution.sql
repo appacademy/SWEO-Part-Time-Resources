e8cb163edeabdbbff0fb58d4fc24b9fc0fc30a11
jt989073 <thompsonjames0120@gmail.com>
2022-07-12 18:26:01 -0500

    added sqlite3 and create practice

---
+++ 4-Module/2-week/2-day/practice-for-week-10-sql-practice-create-drop-tables/sql-practice/solution.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS puppies;
+
+CREATE TABLE puppies (
+id INTEGER PRIMARY KEY,
+name VARCHAR(50),
+age_yrs NUMERIC(3,1),
+breed VARCHAR(100),
+weight_lbs INTEGER,
+microchipped BOOLEAN
+);
+
