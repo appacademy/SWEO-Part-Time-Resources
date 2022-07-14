1f1ca99dc90bd47648b2e176b4ad02ec4a66ec66
Philip Ling <66927690+philling83@users.noreply.github.com>
2022-07-13 21:09:26 -0400

    Update scratch.js

---
+++ 1-Module/4-week/3-day/scratch.js
@@ -0,0 +1,42 @@
+let globalScope = 'I am globally scoped'
+
+let myFunction = function(functionScope){
+  let alsoFunctionScope = "I am also function scoped"
+
+  if(true){
+    let blockScope = 'I am block scoped'
+  };
+};
+
+myFunction("I am function scoped");
+
+/****************************************/
+
+let car = "vroom";
+function makeSounds(dogSound) {
+  let cow = "moo";
+
+  if (true) {
+    let turkey = "gobble";
+
+    for (let i = 0; i < 2; i++) {
+      console.log(car, dogSound, cow, turkey, i);
+    }
+  }
+}
+makeSounds("woof");
+
+/****************************************/
+
+let hungry = false;
+
+function sayHungry() {
+  let hungry = true;
+  if (hungry) { //hungry is being called, but where is hungry defined?
+    console.log("I'm hungry!");
+  } else {
+    console.log("I'm coding");
+  }
+}
+
+sayHungry();
