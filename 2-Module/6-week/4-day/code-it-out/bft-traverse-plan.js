8a1a7c94c8e11012e2985363c676613dbb2e046e
westrinhkl <trinhpt@uci.edu>
2022-07-14 16:42:19 -0700

    updated

--- 2-Module/6-week/4-day/code-it-out/bft-traverse-plan.js
+++ 2-Module/6-week/4-day/code-it-out/bft-traverse-plan.js
@@ -2,15 +2,3 @@
 
 
 // *****Plan here******
-/*
-1) create a queue to contain the nodes that we are going to visit
-2) create a set to keep track of visited notes
-3) to add initial starting node to queue as the set
-4) while our queue is not empty do:
-    5) dequeue the first element and mark it as visited
-    6) do the thing (log node data, adding to a length, etc.)
-    7) apply the functionality of the `getNeighbors` function which will grab the array of unvisited neighbors
-    8) for each unvisited neighbor of the current node add it to the back of the queue
-
-
-*/
