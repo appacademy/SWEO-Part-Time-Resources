fd115f2f5056616eec017e7809b65be154778769
westrinhkl <trinhpt@uci.edu>
2022-07-14 17:56:41 -0700

    updated

---
+++ 2-Module/6-week/4-day/island-graph/island.js
@@ -0,0 +1,66 @@
+// the goal of `getNeighbors` is to get all the neighbors that have the value of 1 in them and put them into an array.
+const matrix = [
+  [1,1,1,0,0],
+  [0,1,1,0,1],
+  [0,1,1,0,1],
+]
+console.log(getNeighbors(0, 1, matrix))
+
+function getNeighbors(row, col, graph) {
+  //we need an array to hold all of the node's neighbors that are 1
+  const neighborsArr = [];
+
+
+  // THIS ERROR: graph[row-1][col] -->  console.log("testing:", graph[row-1]) //graph[-1] ==> undefined // graph[-1][col] ==> undefined[col]
+
+  // Check top, get neighbor from the top if it's equal to one
+  // we want row - 1 to always be greater than or equal to 0, so we have to make sure row > 0.
+  if(row > 0  && graph[row-1][col] === 1){ //this will work too: if(graph[row-1] !== undefined &&  graph[row-1][col] === 1)
+    neighborsArr.push([row-1, col]) //[0, 1]
+  }
+
+  // Check bottom 
+  //graph[row-1] exists (does not equal to undefined)
+  if(graph[row+1] !== undefined &&  graph[row+1][col] === 1){
+    neighborsArr.push([row+1, col])
+  }
+
+  // Check left
+
+  // Check right
+
+  // Return neighbors
+
+  // Your code here
+  return neighborsArr;
+}
+
+
+
+function islandSize(row, col, graph) {
+
+  // Create a visited set to store visited nodes
+
+  // Create a stack, put the starting node in the stack
+
+  // Put the stringified starting node in visited
+
+  // Initialize size to 0
+
+  // While the stack is not empty,
+
+    // Pop the first node
+
+    // DO THE THING (increment size by 1)
+
+    // Then push all the UNVISITED neighbors on top of the stack
+    // and mark them as visited
+    // HINT: This is what your helper function `getNeighbors` is for
+    // HINT: Remember, you're storing your visited nodes as strings!
+
+  // return size
+
+  // Your code here
+}
+
+module.exports = [getNeighbors, islandSize];
---
+++ 2-Module/6-week/4-day/island-graph/islandv2Solution.js
@@ -0,0 +1,84 @@
+let matrix = [
+    [1, 1, 1, 0, 0],
+    [0, 1, 1, 0, 1],
+    [0, 1, 1, 0, 1],
+  ]
+  
+  console.log(getNeighbors(1, 1, matrix)) //[[0, 1], [1, 2], [2, 1]]
+  
+  //the goal of getNeighbors is to get all the neighbors of a given node into an array only if each neighbor value is 1.
+  function getNeighbors(row, col, graph) {
+    //crate an array to host the neighbors.
+    const neighborArr = [];
+    // we need to becareful and make sure the row doesn't go out of bounds as well as the value of the neighbor has to be 1.
+    if (matrix[row - 1] !== undefined && matrix[row - 1][col] === 1) {
+      neighborArr.push([row - 1, col])
+    }
+  
+    // Check bottom
+    if (matrix[row + 1] !== undefined && matrix[row + 1][col] === 1) {
+      neighborArr.push([row + 1, col])
+    }
+    // Check left
+    if (matrix[row][col - 1] !== undefined && matrix[row][col - 1] === 1) {
+      neighborArr.push([row, col - 1])
+    }
+  
+    // Check right
+    if (matrix[row][col + 1] !== undefined && matrix[row][col + 1] === 1) {
+      neighborArr.push([row, col + 1])
+    }
+    // Return neighbors
+    return neighborArr;
+    // Your code here
+  }
+  
+  
+  function islandSize(row, col, graph) {
+  
+    // Create a visited set to store visited nodes
+    const visitedSet = new Set();
+  
+  
+    // Create a stack, put the starting node in the stack.
+    // here we will create a stack and the item we add to the stack is an array that contains the coordinates for the node on the graph [row, col]
+    const startNode = [row, col]
+    const stack = [startNode]; //stack = [[row, col]]
+  
+    // Put the stringified starting node in visited
+    //this is the tricky part, analyze why we CANNOT just add an array of [row,col] 
+    //instead we have to create a hash like string w/ interpolation
+    visitedSet.add(`${row},${col}`)
+  
+    // Initialize size to 0
+    let size = 0;
+    // While the stack is not empty,
+    while (stack.length > 0) {
+      // Pop the first node
+      let currentNode = stack.pop(); //i.e. currentNode = [1,1]
+      let currentRow = currentNode[0];
+      let currentCol = currentNode[1];
+  
+      // DO THE THING (increment size by 1)
+      size += 1;
+      // Then push all the UNVISITED neighbors on top of the stack
+      // HINT: This is what your helper function `getNeighbors` is for
+      const neighborArr = getNeighbors(currentRow, currentCol, graph);
+      for (let i = 0; i < neighborArr.length; i++) {
+        const neighborEl = neighborArr[i]; //i.e. neighborEl = [1,2]
+        // HINT: Remember, you're storing your visited nodes as strings!
+        // WHY will !visitedSet.has(neighborEl) fail?
+        if (!visitedSet.has(`${neighborEl[0]},${neighborEl[1]}`)) {
+          // and mark them as visited
+          // WHY will  visitedSet.add(neighborEl) fail?
+          visitedSet.add(`${neighborEl[0]},${neighborEl[1]}`);
+          stack.push(neighborEl);
+        }
+      }
+  
+    }
+    // return size
+    return size;
+  }
+  
+  module.exports = [getNeighbors, islandSize];
