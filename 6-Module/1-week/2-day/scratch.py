b4ce1c8c941d98dd0c6699af9ccf6f64e2ec138c
WJVincent <william@william-vincent.dev>
2022-07-19 14:40:48 -0400

    m6w1d2

---
+++ 6-Module/1-week/2-day/scratch.py
@@ -0,0 +1,14 @@
+# Truthy vs Falsy
+
+if not None:
+  print('None is falsy')
+if not False:
+  print('False is falsy')
+if not 0:
+  print('0 is falsy')
+if not "":
+  print('"" is falsy')
+if not []:
+  print('[] is falsy')
+if not {}:
+  print('{} is falsy')
