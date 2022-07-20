d6c73f329befb675b3476c3eb0e1be860abb06d4
James Thompson <thompsonjames0120@gmail.com>
2022-07-19 23:48:01 -0500

    added notes to west cohort

--- 1-Module/1-week/2-day/scratch.js
+++ 1-Module/1-week/2-day/scratch.js
@@ -1,4 +1,112 @@
 /* EAST */
 
 /* WEST */
-//west2
+ 
+
+/* 
+Numbers 
+    JavaScript doesnt care about types of numbers 
+    a number is just a number
+
+
+
+
+
+*/
+
+// console.log(12)
+// console.log(-12)
+// console.log('-12')
+
+//Arithmetic operators
+// Addition operator -- + 
+// Subtraction operator -- -
+// Multiplication operator -- *
+// division opperator /
+// power of operator -- ** 
+
+// console.log(6 + 7) // 13
+// console.log(6 - 7) // -1
+// console.log(6 * 7) // 42
+// console.log(49 / 7) // 7
+// console.log(7 ** 2) // 49
+// console.log((32 - 14) * 10) // 180
+
+// console.log(49 % 7) // 0
+// console.log(2 % 12) // 2
+// console.log(7 % 3) // 1
+
+
+// console.log('james' / 2) // NaN
+
+
+// Not Operator !
+// console.log(!true) // false
+// console.log(!!true) // true
+
+// And Operator && 
+
+// console.log(true && true) // true
+// console.log(true && false) // false
+// console.log(false && true) // false
+// console.log(false && false) // false
+
+// De Morgans Law
+// console.log(!(true && true)) // false !true || ! true 
+// console.log(!(true && false)) // true !true || !false
+
+// Or Operator || 
+
+// console.log(true || false) // true 
+// console.log(false || false) // false 
+
+// console.log(!(true || true)) // false !true && !true
+// console.log(!(true || false)) // false !true && !false
+
+
+// comparison operators with greater and less than -- <, >, <=, >=
+// console.log(2 > 6) // false
+// console.log(4 < 18) // true
+// console.log(10 <= 10) // true
+// console.log(10 <= -Infinity) // false 
+// console.log(Infinity < -Infinity) // false
+
+// Single = -- assignment opperator assigns a value 
+// Double == -- loosely equals 99.99% of the time you will NEVER use this
+// Triple === -- Strictly equals 
+
+// console.log('42' == 42) // true
+// console.log('42' === 42) // false
+// console.log(42 !== '42') // true
+// console.log(42 != '42') // false // DONT DO THIS 
+
+
+
+//Vaariable Declaration 
+// let strawberry
+// console.log(strawberry) 
+// strawberry = 'tasty fruit'
+// console.log(strawberry) 
+
+// Variable Init
+// let strawberry = 'tasty fruit'
+// console.log(strawberry) 
+
+// let num = 100
+// console.log(num)
+// // num = num + 1
+// // num += 1
+// // num *= 10
+// // num /= 10
+// num /= 25
+// console.log(num) // 4
+
+let num1 = 10
+let num2 = num1 + 1
+num2 = ++num1  // reassigns num2 at the same time as reassigning num1 where if you did num2 = num1++ only num1 gets reassigned to the extra ++ 
+console.log(num1, num2)
+
+
+
+
+
