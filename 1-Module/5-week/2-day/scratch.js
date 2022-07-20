9a0d36772fc2e5aa40e0af875b8d564642479565
Philip Ling <66927690+philling83@users.noreply.github.com>
2022-07-20 00:30:04 -0400

    Update scratch.js

--- 1-Module/5-week/2-day/scratch.js
+++ 1-Module/5-week/2-day/scratch.js
@@ -9,3 +9,21 @@ const iter_sum = (n) => {
 }
 
 console.log(iter_sum(3));
+
+const rec_sum = (n) => {
+  // base case: when n = 0
+  // recursive case: when n > 0 
+  // recursive step: n - 1
+
+  if (n === 0) return 0;
+
+  return n + rec_sum(n - 1)
+}
+
+// console.log(iter_sum(3));
+console.log(rec_sum(3))
+
+// 4 rec_sum(0) = undefined
+// 3 rec_sum(1) = 0
+// 2 rec_sum(2) = 2 + rec_sum(1)
+// 1 rec_sum(3) = 3 + rec_sum(2)
