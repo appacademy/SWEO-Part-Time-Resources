e52d877832289e50c5ca678fb25658bbd5e93c76
James Thompson <thompsonjames0120@gmail.com>
2022-07-20 19:55:50 -0500

    added week 1 day 3 notes

--- 1-Module/1-week/3-day/scratch.js
+++ 1-Module/1-week/3-day/scratch.js
@@ -1,153 +1,104 @@
 /* EAST */
-// console.log('tacos');
-// console.log("tacos");
-// console.log("What's up!");
-// // console.log('What's up!') what??
-// console.log('He said, "Hello"');
-// console.log('He said, "What\'s up?');
+// console.log("this is a string")
+// console.log('this is also a string single quotes')
+
+// console.log('that\'s my boat')
+
 
 // Length - property (a part of a data type that isn't a function)
-// Length starts at 1... more later
-// console.log("pizza".length);
-// console.log("5 _.!asdf".length);
+// let myString = 'this is a super long string with gibberish at the end ajksdfhgaksdjghaoljashdfkljahsdfklasjdhf'
+
+// console.log(myString[myString.length - 1]) // this is to get last character of a string 
+// console.log(myString[-1]) // no negative indexing in JS this undefined
 
 // Indexing
-// 'dogs'
-// // 0 - d, 1 - o, 2 - g, 3 - s
-// 'dogs'.length // 4
+let newString = 'james thompson'
+// console.log(newString.substring(0, 5))
+// strings are zero index meaning start at 0 instead of 1 
+// j - 0 a - 1 m - 2 e - 3 s - 4 etc....
+// console.log(newString.indexOf('t')) // use indexOf to find the index and then log the actual character
+// console.log(newString[6])
+// console.log(newString[newString.length - 1]) // this is to get last character of a string 
 
-// let animal = 'elephant';
-
-// // console.log the letter h;
-// console.log(animal[4]);
-
-// //console.log the letter t;
-// console.log(animal[animal.length - 1]);
-// console.log(animal[-1]); // not in JS :'(
 
 // indexOf - method (a part of a data type that is a function)
 // let game = "Dota 2 is cool, dude";
-// let o = ",";
-
-// console.log(game.indexOf("t"));
-// console.log(game.indexOf("x"));
-// console.log(game.indexOf("o"));
-// console.log(game.indexOf(" co"));
-// console.log(game.indexOf("d"));
-// console.log(game.indexOf(o))
+// console.log(game.indexOf(','))
 
 // Concat - join
-// let drink = 'coffee';
-// let flavor = 'pumpkin';
+// let color = 'red'
+// let fruit = ' cherry'
+// // color += fruit 
+// color.concat(fruit)
+// console.log(color) 
 
-// let fallDrink = flavor + " " + drink;
 
-// console.log(fallDrink);
-
-// let numOfCoffee = 3 + 1 + drink
-// console.log(numOfCoffee);
-
-// // String.toLowerCase()
 
 // Functions!
-function aName (optionalParams) {
-  // may have a return value
-}
+function aName(optionalParameter) {
+  // we do something and make something happen
+  // after we do something we usually have a return value not always, but usually 
+} // A FUNCTION EVALUATES TO ITS RETURN VALUE 
 
 // // declare a function called two that console.logs the number two
-// function two () {
-//    console.log(2);
-//    return "Here is a string";
+// function two(){
+//   console.log(2)
+//   return 'this is the return value'
 // }
 
 // //Invoking functions
-// console.log(two());
-
+// two()
+// console.log(two())
 // declare a function called divide that intakes two numbers and returns the first
 // number divided by the second number
-
-// function divide (num1, num2) {
-//   return num1 / num2;
+// function divide(num1, num2){
+//   console.log(num1 * num2 + 100)
+//   let result =  num1 / num2
+//   return result
 // }
 
-// console.log(divide(4,2)); // 2 let num1 = 4, let num2 = 2
-// console.log(divide(6,2)); // 3
-
-// function test(num1, num2) {
-//   console.log(num1, num2)
-//   return num1 + num2
+// function divide (num1, num2, num3) {
+//   console.log(num1 * num2 + 100)
+//   let result =  num1 / num2
+//   return result / num3
 // }
 
-// console.log(test(1))
-
-// too few args makes undefined
-// too many args we don't care
-
-/* WEST */
+// console.log(divide(81, 9))
+// console.log(divide(1777, 64))
+// console.log(divide(819, 11, 1))
 
 /*
-Understand the problem
-0 => 5
--1 => 4
-3 => 8
+write a function called average takes in 3 parameters which are numbers and returns the average of the 3 numbers
 
-Make a plan
+understand the problem 
+  function name average
+  takes 3 params
+  return the average of 3 nums
 
-Declare a function that will intake a number
-  declare a variable called sum and set it to the num plus five
-  return the sum
-  
-Carry out the plan
+come up with a plan 
+  write function
+  add all 3 numbers up and save in a variable
+  save another variable that divides the addition by 3 
+  return average variable
 
-Look back and improve your solution
+carry out that plan 
+
+refactor 
+
+
+
 */
 
-// function plusFive(num) {
-//   return num + 5;
+// function average(num1, num2, num3){
+//   // let sum = num1 + num2 + num3
+//   return (num1 + num2 + num3) / 3
 // }
 
-// console.log(plusFive(10));  // => 15
-// console.log(plusFive(2));   // => 7
-// console.log(plusFive(-8));  // => -3
+// console.log(average(3, 5, 70))
+// console.log(average(100, 200, 300))
+// console.log(average(14, 999, 100000999988))
 
-/* 
-Define a function called alphabetical that intakes two 
-words and will return true if the first word comes before 
-the second word in the dictionary, or false if it doesn't.
-
-Understand the problem
-
-cat, hat => true
-cat, cat => false
-Cat, cat => false
-Cat, hat => true
-hat, cat => false
-zebra, fish => false
-
-Make a Plan in Plain English
-
-Define a function with two params
-  lowercase each word and assign them to variables
-  compare the two words
-  return the comparison
-
-Execute the plan
-
-
-Review/Improve
-
-
-*/
-function alphabetical (word1, word2) {
-  return word1.toLowerCase() < word2.toLowerCase();
-}
-
-console.log(alphabetical("cat", "hat")) //=> true
-console.log(alphabetical("cat", "cat")) //=> false
-console.log(alphabetical("Cat", "cat")) //=> false
-console.log(alphabetical("Cat", "hat")) //=> true
-console.log(alphabetical("hat", "cat")) //=> false
-console.log(alphabetical("zebra", "fish")) //=> false
+/* WEST */
 
 
 
