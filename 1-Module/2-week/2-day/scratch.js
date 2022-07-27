3ff455ad262a15b6be4d91c14441fb75c581bdb5
James Thompson <thompsonjames0120@gmail.com>
2022-07-27 00:30:04 -0500

    added solution

--- 1-Module/2-week/2-day/scratch.js
+++ 1-Module/2-week/2-day/scratch.js
@@ -9,11 +9,11 @@
 
 
 //Iterating 2d Array
-// let arr = [
-//     [1, 2, 3], 
-//     [4, 5, 6], 
-//     [7, 8, 9]
-// ]
+let arr = [
+    [1, 2, 3], 
+    [4, 5, 6], 
+    [7, 8, 9]
+]
 
 // for(let i = 0; i < arr.length; i++){
 //     let subArr = arr[i]
