e5fde7eb3fdb61156d2ad6278005481b64263f91
James Thompson <thompsonjames0120@gmail.com>
2022-07-27 16:54:16 -0500

    changed read me

---
+++ 1-Module/2-week/3-day/scratch.js
@@ -0,0 +1,24 @@
+// let removeEWords = function(str){
+// 	let strArr = str.split(' ')
+//     let resultArr = []
+//     for(let i = 0; i < strArr.length; i++){
+//     	let word = strArr[i]
+//         let lower = word.toLowerCase()
+//         if(lower.indexOf('e') === -1){
+//         	resultArr.push(word)
+//         }
+//     }
+//   return resultArr.join(' ')
+// }
+
+
+// console.log(removeEWords('Hello World'));
+// console.log(removeEWords('We are here to learn how to code'));
+
+
+//Echo
+
+
+
+// console.log(echo('Hello')); // 'HELLO ... Hello ... hello
+// console.log(echo('PiNeAppLE')); // 'PINEAPPLE ... PiNeAppLE ... pineapple
