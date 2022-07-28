10582cc6f78abcd5ab9804c480a4ac657bd4b920
James Thompson <thompsonjames0120@gmail.com>
2022-07-27 21:17:28 -0500

    changed readme and added helper lecture

--- 1-Module/2-week/3-day/scratch.js
+++ 1-Module/2-week/3-day/scratch.js
@@ -10,3 +10,48 @@
 //     }
 //   return resultArr.join(' ')
 // }
+
+let hasE = function(word){
+    if(word.toLowerCase().includes('e')){
+        return true
+    }
+    return false 
+}
+
+let looper = function(arr){
+    let newArr = []
+    for(let i = 0; i < arr.length; i++){
+        let ele = arr[i]
+        if(!hasE(ele)){
+            newArr.push(ele)
+        }
+    }
+    return newArr
+}
+
+let removeEWords = function(str){
+    let words = str.split(' ')
+    let finalArr = looper(words)
+
+    return finalArr.join(' ')
+}
+
+
+// console.log(removeEWords('What time is it everyone?')); // 'What is it'
+// console.log(removeEWords('EntEr the building')); // 'building'
+
+let yell = function(word){
+    return word.toUpperCase()
+}
+
+let whisper = function(word){
+    return word.toLowerCase()
+}
+
+
+let echo = function (string) {
+    return yell(string) + ' ... ' + string + ' ... ' + whisper(string)
+} 
+
+ console.log(echo('Hello')); // 'HELLO ... Hello ... hello
+ console.log(echo('PiNeAppLE')); // 'PINEAPPLE ... PiNeAppLE ... pineapple
