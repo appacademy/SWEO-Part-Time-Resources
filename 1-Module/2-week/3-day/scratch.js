f889eb23318e8b4e18a35231b964a260b8ce19e6
James Thompson <thompsonjames0120@gmail.com>
2022-07-27 20:02:47 -0500

    changed md and added scratch

---
+++ 1-Module/2-week/3-day/scratch.js
@@ -0,0 +1,12 @@
+// let removeEWords = function(str){
+// 	let strArr = str.split(' ')
+//     let resultArr = []
+//     for(let i = 0; i < strArr.length; i++){
+//     	let word = strArr[i]
+//         let lower = word.toLowerCase()
+//         if(lower.indexOf('e') === -1){
+//         	resultArr.push(word)
+//         }
+//     }
+//   return resultArr.join(' ')
+// }
