0ae47ce539f784363e94fdd72012572965605bc9
Zaviar Brown <brown.zaviar@gmail.com>
2022-07-30 15:08:19 -0700

    PA solution

---
+++ 3-Module/6-week/5-day/scratch.js
@@ -0,0 +1,21 @@
+// print myFavCat
+
+const zavObj = {
+  names: ['Zaviar', 'Brown'],
+  locations: {
+    california: 'born',
+    florida: 'palm bay',
+    washington: ['Issaquah', 'Bellevue', 'Bothell'],
+  },
+  houseMates: {
+    pets: {
+      dead: {},
+      alive: {
+        cats: {
+          all: ['all names'],
+          myFavCat: 'Momo',
+        },
+      },
+    },
+  },
+};
