4a4e314c9cac28d71a7e55458cb462c73d924030
Zaviar Brown <brown.zaviar@gmail.com>
2022-08-02 15:16:30 -0700

    Tuesday Readme

--- 4-Module/1-week/1-day/express-server/app.js
+++ 4-Module/1-week/1-day/express-server/app.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send("Wow this is quite express")
-})
+  res.send('Wow this is quite express');
+});
 
 const port = 5000;
 
