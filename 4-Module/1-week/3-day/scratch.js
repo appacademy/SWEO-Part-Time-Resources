6add83cdc1c2d3f09128723ce28fa891d6f8db9d
Zaviar Brown <brown.zaviar@gmail.com>
2022-08-03 19:27:48 -0700

    Ignore example

---
+++ 4-Module/1-week/3-day/scratch.js
@@ -0,0 +1,6 @@
+// require('dotenv').config(); // if using dotenv
+
+console.log(process.env.NAME); // Zaviar
+// console.log(process.env.name); // undefined
+console.log(process.env.MUSIC); // lofi
+console.log(process.env.PASSWORD); // YouWillNeverKnow
