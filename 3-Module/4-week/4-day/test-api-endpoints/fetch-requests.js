686c3c03d94f0751726b16d7499d656d83b8899c
westrinhkl <trinhpt@uci.edu>
2022-08-11 18:36:13 -0700

    updated

--- 3-Module/4-week/4-day/test-api-endpoints/fetch-requests.js
+++ 3-Module/4-week/4-day/test-api-endpoints/fetch-requests.js
@@ -33,8 +33,30 @@ fetch('/posts', {
   body: JSON.stringify({
     message: "New Post!"
   })
-})
+}).then(response => response.json()).then(data=>console.log(data));
 
 /* =============================== Phase 3 ================================ */
 // REFACTOR ABOVE TO USE ASYNC AWAIT
 // write an async function called "makePost" that will make a POST Fetch request to /posts and log the response data from the server.
+async function makePost(){
+  const url = "/posts"
+
+  // regular POJO
+  const bodyObj = {
+    message: "request made using async / await"
+  }
+  const options = {
+    method: "POST",
+    headers: {
+      "content-type": "application/json"
+    },
+    // in order to send json data to the server as a body, we need to STRINGIFY OUR DATA => turning it into json!
+    body: JSON.stringify(bodyObj)
+  }
+
+  const responseObj = await fetch(url, options);
+  const data = await responseObj.json();
+  console.log("data from server after we made a post request:", data);
+  return data; 
+}
+makePost();
