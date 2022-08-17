4815842e9e5150b218adcb9c64f79ae8b27514d3
westrinhkl <trinhpt@uci.edu>
2022-08-16 18:26:11 -0700

    updated

---
+++ 3-Module/5-week/2-day/code-it-out/dom-manipulation/js-script.js
@@ -0,0 +1,7 @@
+// 1) One of the the properties of the window object is `window.document` which allows us to access the Document Object Model, AKA our HTML page. 
+// 2) Try this out in the browser's console; enter: `window.document.body` on any website and what did you get? (ignore the ``)
+// 3) now try: `window.document.body.children` and `document.body.children`
+// 4) finally try: `document.body.children[1]`
+
+
+// Now add your answer here to follow the instructions.md
---
+++ 3-Module/5-week/2-day/code-it-out/window-object.js
@@ -0,0 +1,13 @@
+// We know that the `window` object is the chief object within the BOM with many properties and methods that allow us to work with the window.
+
+// Determine below which window properties or methods are described here. USE MDN!!! (i.e. window.location)
+
+
+// 1) Display the host of the current page that the window is viewing (hint: look up `window.location` on mdn, see if you can find a property to do that in there)
+// you code here
+
+// 2) Display the page's HTML body
+// 
+
+// 3) Shows how many pages are in the current tab's history (hint: look up 'window.history`)
+//
