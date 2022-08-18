fd1f4f362c0bf946dcb7d6de9286b3a690fe3a51
westrinhkl <trinhpt@uci.edu>
2022-08-17 21:08:44 -0700

    updated

--- 3-Module/5-week/3-day/code-it-out/change-box.js
+++ 3-Module/5-week/3-day/code-it-out/change-box.js
@@ -1,12 +1,14 @@
 
 export const changeBox = function(){
-    // 1. Select the single element by its id and put it in a 
-
+    // 1. Select the single element by its id and put it in a variable
+    const box1 = document.getElementById("box-1")
     // 1.1 log box1 and see what you'll get in the console
+    console.log("element box 1:", box1);
 
 
 
     // 2. select the box1 and make the change to the border-color property, give it a "green" color. ("border-color" is the property --> "borderColor" is the property when doing dot notation)
+    box1.style.borderColor = "green";
 
 }
 
--- 3-Module/5-week/3-day/code-it-out/text-style.js
+++ 3-Module/5-week/3-day/code-it-out/text-style.js
@@ -8,15 +8,22 @@ export const changeText = function () {
     console.log(textCollection);
 
     //1.2 iterate through our HTMLCollection (can either use a forloop, or convert HTMLCollection to an array w/ Array.from or spread... to use array methods)
+    for(let i=0; i<textCollection.length; i++){
+        const el = textCollection[i];
+        console.log(el);
+    }
 
 
     // 1.3 now select the second text by. Try indexing into it since we know that textCollection is an HTMLCollection. use the variable `const text2`
-
+    const text2 = textCollection[1];
+    // const text2 = document.body.children[2];
+    console.log("text2:", text2)
 
     // 2.1 change the color on the second text to red
-
+    text2.style.color = "red";
 
     // 2.2 give the text a bold ("font-weight" is the property --> "fontWeight" is the property when doing dot notation)
+    text2.style.fontWeight = "bold";
 
 }
 
