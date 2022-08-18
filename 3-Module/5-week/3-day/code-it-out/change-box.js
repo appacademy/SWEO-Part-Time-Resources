0e692a12d3d53ffb43397ce525a2ca11f864a029
westrinhkl <trinhpt@uci.edu>
2022-08-17 17:32:56 -0700

    updated

--- 3-Module/5-week/3-day/code-it-out/change-box.js
+++ 3-Module/5-week/3-day/code-it-out/change-box.js
@@ -1,10 +1,15 @@
 
+export const changeBox = function(){
+    // 1. Select the single element by its id and put it in a varaible called `box1`
+    const box1 = document.getElementById("box-1");
 
-// 1. Select the single element by its id and put it in a varaible called `box-1`
+    // 1.1 log box1 and see what you'll get in the console
+    console.log(box1);
 
-// 1.1 log box1 and see what you'll get in the console
 
+    // 2. select the box1 and make the change to the border-color property, give it a "green" color. ("border-color" is the property --> "borderColor" is the property when doing dot notation)
+    box1.style.borderColor = "green";
+}
 
-// 2. select the box1 and make the change to the border-color property, give it a "green" color. ("border-color" is the property --> "borderColor" is the property when doing dot notation)
 
 
--- 3-Module/5-week/3-day/code-it-out/text-style.js
+++ 3-Module/5-week/3-day/code-it-out/text-style.js
@@ -1,16 +1,28 @@
 
-// 1. Select the elements by class name and store it in a variable called "textCollection" (note elements is plural, class selector gives a collection)
-// hint look up on MDN: document.getElementsBy...
+export const changeText = function () {
+    // 1. Select the elements by class name and store it in a variable called "textCollection" (note elements is plural, class selector gives a collection)
+    // hint look up on MDN: document.getElementsBy...
+    const textCollection = document.getElementsByClassName("text")
 
-// 1.1 log the `textCollection` variable, what do you see in the console browser?
+    // 1.1 log the `textCollection` variable, what do you see in the console browser?
+    console.log(textCollection);
+
+    //iterate through our HTMLCollection
+    for (let i = 0; i < textCollection.length; i++) {
+        const childEl = textCollection[i];
+        console.log(childEl);
+    }
 
 
-// 1.2 now select the text 2 by index into it since we know that textCollection is an HTMLCollection. use the variable `const text2`
+    // 1.2 now select the text 2 by index into it since we know that textCollection is an HTMLCollection. use the variable `const text2`
+    const text2 = textCollection[1];
+    console.log("second text2", text2);
 
+    // 2.1 change the color on the text to red
+    text2.style.color = "red";
 
-// 2.1 change the color on the text to red
-
-
-// 2.2 give the text a bold ("font-weight" is the property --> "fontWeight" is the property when doing dot notation)
+    // 2.2 give the text a bold ("font-weight" is the property --> "fontWeight" is the property when doing dot notation)
+    text2.style.fontWeight = "bold";
+}
 
 
