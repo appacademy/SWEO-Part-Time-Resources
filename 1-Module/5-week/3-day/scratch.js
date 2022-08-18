025782120bfc1dae9bda0c66a1e2b70ed121695b
James Thompson <thompsonjames0120@gmail.com>
2022-08-17 21:25:22 -0500

    added deffault walkthrough

--- 1-Module/5-week/3-day/scratch.js
+++ 1-Module/5-week/3-day/scratch.js
@@ -6,23 +6,24 @@ const range = (start, end) => {
   return [start, ...range(start + 1, end)];
 };
 
-
 // const rangeDefault = (start, end, resArr = []) => {
-//   // base case
 //   if(start >= end) return resArr
+
 //   resArr.push(start)
-//   // recursive case and our recursive step
+
 //   return rangeDefault(start + 1, end, resArr)
 // }
 
 // console.log(range(1,5))
 // console.log(rangeDefault(1,5))
 
-// const sumArr = (arr, sum = 0) => {
-//   if(!arr.length) return sum
-//   const lastEle = arr.pop()
-//   sum += lastEle
-//   return sumArr(arr, sum)
-// }
 
-// console.log(sumArr([1,2,3,4]))
+const sumArray = (arr, sum = 0) => {
+  if(!arr.length) return sum
+
+  let lastEle = arr.pop()
+  sum += lastEle
+  return sumArray(arr, sum)
+}
+
+console.log(sumArray([1,2,3,4]))
