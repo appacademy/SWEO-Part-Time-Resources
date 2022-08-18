5fc28c6c6f7234ce7f2915e5448df83268349869
westrinhkl <trinhpt@uci.edu>
2022-08-18 14:57:04 -0700

    Updated

--- 3-Module/5-week/4-day/code-it-out/change-all.js
+++ 3-Module/5-week/4-day/code-it-out/change-all.js
@@ -10,7 +10,7 @@ export const changeAll = function(){
 
     // 2. give a change to all the elements with the "square" class name a border of "5px solid black"; 
     
-    //hint: what is the datatype that we have selected in step 1? Is it an element, a collection, etc. If it's an array like collection and we want to apply each change to the items inside it, should we convert the collection to an array first? Or use a traditional for loop?
+    //hint: what is the datatype that we have selected in step 1? Is it an element, a collection, etc. If it's an array like collection and we want to apply each change to the items inside it, should we convert the collection to an array first? maybe even use a traditional for loop?
 
     
     // we should now see all the box have a 5px thick border and it should all be black
