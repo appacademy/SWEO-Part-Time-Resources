eb1cbe3e3591e635107674d9364ec60b650da7ca
westrinhkl <trinhpt@uci.edu>
2022-08-18 21:12:29 -0700

    updated

---
+++ 3-Module/5-week/4-day/my-profile-dom-manipulation/my-profile.js
@@ -0,0 +1,45 @@
+// Set up listener and test with console.log
+console.log("This is my profile page!");
+
+window.onload = (() => {
+    // Create and append new h1 element
+    const h1 = document.createElement("h1");
+    h1.setAttribute("id", "name");
+    h1.setAttribute("class", "my-name");
+    h1.innerText = "Wes Trinh";
+    document.body.appendChild(h1);
+
+    // Create and append new ul element
+    const myDetailsList = document.createElement("ul");
+    myDetailsList.setAttribute("id", "details");
+    myDetailsList.setAttribute("class", "my-details");
+    document.body.appendChild(myDetailsList);
+
+    // Insert the list items programmatically
+    const myDetailsArr = [
+        "I like to drink no sugar ice tea.",
+        "I am not pets.",
+        "I spend too much money on doordash.",
+        "On the weekends, I play games with friends on discord."
+    ]
+    myDetailsArr.forEach(detail => {
+        const li = document.createElement('li');
+        li.setAttribute("class", "details");
+        li.innerText = detail;
+        myDetailsList.appendChild(li);
+    });
+
+    // Add a new element and insert a clock into it
+    const clockDiv = document.createElement("div");
+    clockDiv.setAttribute("id", "clock");
+    document.body.appendChild(clockDiv);
+    const time = () => {
+        const date = new Date();
+        const seconds = date.getSeconds();
+        const minutes = date.getMinutes();
+        const hours = date.getHours();
+
+        clockDiv.innerText = "Current time is " + hours + ":" + minutes + ":" + seconds;
+    };
+    setInterval(time, 1000);
+});
