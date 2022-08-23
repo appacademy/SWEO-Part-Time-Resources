7fd0c0d3ce8ac87145de6b8bc5cf0c723300849f
westrinhkl <trinhpt@uci.edu>
2022-08-23 14:44:21 -0700

    updated

--- 3-Module/6-week/2-day/code-it-out/store-data-elements/data.js
+++ 3-Module/6-week/2-day/code-it-out/store-data-elements/data.js
@@ -4,7 +4,7 @@
 window.addEventListener("DOMContentLoaded", e => {
 
     // select the add button (i.e. variable name: buttonEl)
-   
+    
     
     // add an event listener to the add button (buttonEl) w/ a click event
     
@@ -29,7 +29,9 @@ window.addEventListener("DOMContentLoaded", e => {
         //set li attribute: get the value of the `select element` and set the attribute on the li to have a "data-type" w/ the value of the `select element's` value. Hint: reference the hoemwork on data-
   
 
-        // conolse log the li to see if you're getting everything correct when entering a new input
+        //console log the li to see if you're getting everything correct when entering a new input
+
+        //now take the li element and log the data- attribute's value using "dataset." 
 
 
         //append the child li to its ul parent.
