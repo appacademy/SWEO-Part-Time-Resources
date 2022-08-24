744fe4d3c7874a95d139dd227bf5b1fce2b9fecf
westrinhkl <trinhpt@uci.edu>
2022-08-23 17:00:46 -0700

    updated

---
+++ 3-Module/6-week/2-day/catstagram-2/public/index.js
@@ -0,0 +1,17 @@
+import { createMainContent } from './main.js';
+
+const initializePage = () => {
+    // Create container 
+    const container = document.createElement("section");
+    container.className = "container";
+    container.style.display = "flex";
+    container.style.flexDirection = "column";
+    container.style.alignItems = "center";
+    container.style.marginTop = "20px";
+    document.body.appendChild(container);
+};
+
+window.onload = () => {
+    initializePage();
+    createMainContent(); //main content added to container
+};
