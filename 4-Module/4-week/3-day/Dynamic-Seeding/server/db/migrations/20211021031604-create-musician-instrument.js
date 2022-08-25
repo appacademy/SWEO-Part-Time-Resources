b5da3592ba63d9c8dc9b2f8ad6e0db21fb92850c
Zaviar Brown <brown.zaviar@gmail.com>
2022-08-24 18:58:27 -0700

    Seeding and Ordering

---
+++ 4-Module/4-week/3-day/Dynamic-Seeding/server/db/migrations/20211021031604-create-musician-instrument.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('MusicianInstruments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      musicianId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {model: 'Musicians'}
+      },
+      instrumentId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {model: 'Instruments'}
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('MusicianInstruments');
+  }
+};
