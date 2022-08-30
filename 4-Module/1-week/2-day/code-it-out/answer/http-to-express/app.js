154d1fa76bf8d5cea51e9072bbad7e44c2753678
westrinhkl <trinhpt@uci.edu>
2022-08-30 15:45:55 -0700

    updated

---
+++ 4-Module/1-week/2-day/code-it-out/answer/http-to-express/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+// console.log(express);
+const app = express();
+// console.log(app);
+const port = 5000;
+
+app.use(express.json())
+
+app.get("/users/:userId", (req,res )=>{
+    console.log("-------req:", req);
+    console.log("req params:", req.params)
+    const userId = req.params.userId; //parse userId from parameters to grab the exact user id
+    res.status(200);
+    // -----------sending plain text--------
+    // res.set("content-type", "text/plain") //you don't need this, res.send will default to text as content type
+    // res.send(`User details for userId: ${userId}`)
+    // -----------sending json--------
+    res.json(`User details in JSON for userId: ${userId}`)
+})
+
+// wildcard path, anything that doesn't match above, will hit this route here.
+app.get("*", (req,res)=>{
+    res.status(404)
+    res.send("not found");
+} )
+
+
+app.listen(port, ()=>{
+    console.log("listening on port:", port )
+})
