89543b23099e420860ba71ffa3d72a276afcedd3
westrinhkl <trinhpt@uci.edu>
2022-08-29 18:06:45 -0700

    updated

--- 4-Module/1-week/1-day/example/express-server/app.js
+++ 4-Module/1-week/1-day/example/express-server/app.js
@@ -3,8 +3,17 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Wow this is quite express');
+  res.json('Wow this is quite express');
 });
+
+app.post('/', (req, res) => {
+  res.json('Got a POST request')
+})
+
+app.put('/user', (req, res) => {
+  res.json('Got a PUT request at /user')
+})
+
 
 const port = 5000;
 
