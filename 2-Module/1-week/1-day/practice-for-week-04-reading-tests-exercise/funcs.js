9827be20a65621e894a085957f0641942265228b
James Thompson <thompsonjames0120@gmail.com>
2022-08-30 00:08:10 -0500

    added solution

---
+++ 2-Module/1-week/1-day/practice-for-week-04-reading-tests-exercise/funcs.js
@@ -0,0 +1,23 @@
+const isFive = num => num === 5
+
+const isOdd = num => {
+    if(isNaN(num)){
+        throw new Error
+    }
+   return num % 2 !== 0
+}
+
+const myRange = (min, max, step = 1) => {
+    const newArr = []
+    for(let i = min; i <= max; i += step){
+        newArr.push(i)
+    }
+    return newArr
+}
+
+
+module.exports = {
+    isFive,
+    isOdd,
+    myRange
+}
