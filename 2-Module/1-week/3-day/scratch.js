9b25a9918b7fadf147df38f47179c980a9469a35
James Thompson <thompsonjames0120@gmail.com>
2022-08-31 17:41:19 -0500

    updated readme

--- 2-Module/1-week/3-day/scratch.js
+++ 2-Module/1-week/3-day/scratch.js
@@ -1,2 +1,6 @@
+const myArr = [1,2,3,4]
+// const myArr = new Array(1,2,3,4)
+console.log(myArr)
+
 // // Define a class with the class keyword followed by the name with the first
 // // letter capital and the rest of the words camel case
