b265ebadc1a920cce111ecd8fa5c4068d0a369dc
Zaviar Brown <brown.zaviar@gmail.com>
2022-08-30 20:15:52 -0700

    Poly Asso solution

---
+++ 4-Module/5-week/2-day/poly-asso/server/db/models/image.js
@@ -0,0 +1,57 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Image extends Model {
+    // getImageable(options) {
+    //   if (this.imageableType === 'UserProfile') {
+    //     return this.getUserProfile(options);
+    //   } else if (this.imageableType === 'BlogPost') {
+    //     return this.getBlogPost(options);
+    //   } else {
+    //     return Promise.resolve(null);
+    //   }
+    // }
+
+    getImageable(options) {
+      if (!this.imageableType) return Promise.resolve(null);
+
+      const mixin = `get${this.imageableType}`;
+
+      return this[mixin](options);
+
+			/**
+			 * this.imageableType === "UserProfile"
+			 * 		this[mixin] === this['getUserProfile'] === this.getUserProfile
+			 * 
+			 * this.imageableType === 'AnyOtherThing'
+			 * 		this[mixin] === this['AnyOtherThing] === this.getAnyOtherThing
+			 */
+    }
+
+    static associate(models) {
+      Image.belongsTo(models.UserProfile, {
+        foreignKey: 'imageableId',
+        constraints: false,
+      });
+      Image.belongsTo(models.BlogPost, {
+        foreignKey: 'imageableId',
+        constraints: false,
+      });
+    }
+  }
+  Image.init(
+    {
+      url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      imageableType: DataTypes.ENUM('UserProfile', 'BlogPost'),
+      imageableId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: 'Image',
+    }
+  );
+  return Image;
+};
