41fb229a1715c98280831986b706b64b43961259
westrinhkl <trinhpt@uci.edu>
2022-09-01 18:14:02 -0700

    updated

--- 4-Module/1-week/4-day/code-it-out/router-example/server/app.js
+++ 4-Module/1-week/4-day/code-it-out/router-example/server/app.js
@@ -9,12 +9,12 @@ app.get("/", (req,res)=>{
 
 
 // 1) start server: npm run dev
-// simple router (mini app) example. Why
+// simple router (mini app) example. 
 const router = express.Router();
 router.get('/:page', (req, res) => { 
     res.send(`this is from the router with the path of /posts/${req.params.page}`)
  });
-// task: add the code necessary so we can access the router and go to: http://localhost:5000/posts/apple (hint: what is the prefix before /:page?)
+// task: add the code necessary so we can access the router, and mount it to the express application and go to: http://localhost:5000/posts/apple (hint: what is the prefix before /:page?)
 // -----your code here-----
 
 // 2) if we finish early, refactor this by putting it in its own "router" folder and file of "posts.js". Make sure to remember to export and mount the router onto the express app!
