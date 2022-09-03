782bab0683dbfbdb49efcb462788fbed9851cdb5
westrinhkl <trinhpt@uci.edu>
2022-09-03 15:43:44 -0700

    updated

--- 4-Module/1-week/5-day/practice-exam-testing/problem-13/app.js
+++ 4-Module/1-week/5-day/practice-exam-testing/problem-13/app.js
@@ -3,6 +3,7 @@ const express = require("express"); //Import in the express module
 const app = express(); //this is where we INITIALIZE IT our unnaned express app
 const port = 5000;
 
+// how we would refactor route handlers for a resource (like tweets) to be inside of its own router to make the code more DRY and readable and workable.
 console.log("problem 13");
 
 const tweetsRouter = require("./routes/tweets.js")
