1cbd2f9dcfcb977609c07fc97b124bba8dd017ad
James Thompson <thompsonjames0120@gmail.com>
2022-09-07 17:32:39 -0500

    added lecture

--- 2-Module/2-week/3-day/scratch.js
+++ 2-Module/2-week/3-day/scratch.js
@@ -1,29 +1,31 @@
-class Cat {
-    constructor(name) {
-      this.name = name;
-    }
-    sayName() {
-        console.log(this.name);
-    }
-  }
+// class Cat {
+//     constructor(name) {
+//       this.name = name;
+//     }
+//     sayName() {
+//         console.log(this.name);
+//     }
+//   }
   
-  let cat = new Cat("Meowser");
+//   let cat = new Cat("Meowser");
   
-  class Dog {
-    constructor(name) {
-      this.name = name;
-    }
-  }
+//   class Dog {
+//     constructor(name) {
+//       this.name = name;
+//     }
+//   }
   
-  let dog = new Dog("Fido");
+//   let dog = new Dog("Fido");
   
-  let sayNameFunc = cat.sayName;
+//   let sayNameFunc = cat.sayName;
+// //   console.log(sayNameFunc()) // throws an error 
   
-  let sayHelloCat = sayNameFunc.bind(cat);
-  sayHelloCat(); // prints Meowser
+//   let sayHelloCat = sayNameFunc.bind(cat);
+//   console.log(sayHelloCat) // returns the function that is bound 
+//   sayHelloCat(); // prints meowser
   
-  let sayHelloDog = sayNameFunc.bind(dog);
-  sayHelloDog(); // prints Fido
+//   let sayHelloDog = sayNameFunc.bind(dog);
+//   sayHelloDog(); // prints Fido
 
 
 // class Dog {
@@ -36,10 +38,12 @@ class Cat {
 //     constructor(name) {
 //         this.name
 //     }
+
 //     purrNTimes(n) {
 //         for(let i = 0; i < n; i++) {
 //             console.log(`${this.name} says: meow`);
 //         }
+//         return n
 //     }
 //   }
   
@@ -47,27 +51,34 @@ class Cat {
 //   let cat = new Cat('Meowser');
   
 //   // make Fido meow 5 times using `call`
-//   cat.purrNTimes.call(dog, 5);
+//   let purrTimes = cat.purrNTimes
+//   let called = purrTimes.call(dog, 10);
+//   console.log(called) // call evaluates to its return value of the function that is bound 
   
 //   // make Fido meow 5 times using `apply`
-//   cat.purrNTimes.apply(dog, [5]);
+// let purrTimesApply = cat.purrNTimes
+//   let applied = purrTimesApply.apply(dog, [5]); 
+//   console.log(applied) //  apply evaluates to its return value of the function that is bound 
 
 
 // class Dog {
-//     // ...constructor same as before
-//     arrowBoundBark() {
-//       setTimeout(() => {
+//     constructor(name) {
+//       this.name = name;
+//     }
+//     delayedBark() {
+//       setTimeout(function() {
 //         console.log(this.name);
 //       }, 1000);
 //     }
 //   }
   
 //   let dog = new Dog("Fido");
-//   dog.arrowBoundBark();   
-
+//   dog.delayedBark()  // undefined because setTimeout is on the global object and so this in setTimeout is global not what its in
 
 // class Dog {
-//     // ...constructor same as before
+//     constructor(name) {
+//       this.name = name;
+//     }
 //     arrowBoundBark() {
 //       setTimeout(() => {
 //         console.log(this.name);
@@ -77,3 +88,9 @@ class Cat {
   
 //   let dog = new Dog("Fido");
 //   dog.arrowBoundBark();     // "Fido" (after 1 second)
+
+// function SomeFunc(){
+//     console.log(this)
+// }
+
+// SomeFunc()
