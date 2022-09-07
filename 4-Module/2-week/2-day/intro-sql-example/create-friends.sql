a680152b9047184bd522dad732480762a785c178
westrinhkl <trinhpt@uci.edu>
2022-09-07 02:29:03 -0700

    updated

--- 4-Module/2-week/2-day/intro-sql-example/create-friends.sql
+++ 4-Module/2-week/2-day/intro-sql-example/create-friends.sql
@@ -1,7 +1,7 @@
 DROP TABLE friends;
 
 CREATE TABLE friends(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     middle_name VARCHAR(255) NOT NULL
