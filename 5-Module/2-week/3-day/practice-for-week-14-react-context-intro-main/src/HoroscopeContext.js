632ffc1da32426d52d1e506aefbf7ef58b7d37c8
AlexBetita <alexbheb25@gmail.com>
2022-09-08 11:42:40 -0700

    updated lecture notes

---
+++ 5-Module/2-week/3-day/practice-for-week-14-react-context-intro-main/src/HoroscopeContext.js
@@ -0,0 +1,16 @@
+import {createContext, useState} from 'react'
+import horoscopes from './data/horoscopes'
+export const HoroscopeContext = createContext()
+
+function HoroscopeProvider(props){
+    const [currentSign, setCurrentSign] = useState("Leo")
+    const sign = horoscopes[currentSign]
+
+    return (
+        <HoroscopeContext.Provider value={{sign, setCurrentSign}}>
+            {props.children}
+        </HoroscopeContext.Provider>
+    )
+}
+
+export default HoroscopeProvider
