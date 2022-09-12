dfe84b9ec5cae53c0c21ff81654db3472d1e1041
westrinhkl <trinhpt@uci.edu>
2022-09-12 14:44:12 -0700

    updated

---
+++ 4-Module/3-week/1-day/intermediate-select-query/sql-practice/queries.sql
@@ -0,0 +1,4 @@
+-------------------------- Query --------------------------
+-- From the albums, select: title, num_sold, year
+-- Where the year is between 2010 and 2020.
+-- Then order by the album's year (ascending), if they are the same, then num_sold (ascending)
