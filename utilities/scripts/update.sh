3bcf71182282082baaecf1e8f0a076a046c3cd8e
WJVincent <william@william-vincent.dev>
2022-09-14 19:12:24 -0400

    adding -e flag to colored echos

--- utilities/scripts/update.sh
+++ utilities/scripts/update.sh
@@ -60,12 +60,12 @@ while true; do
     echo
 
     if [ -z "$mod" ]; then
-        echo "${RED}Module cannot be blank please try again!${NO_COLOR}"
+        echo -e "${RED}Module cannot be blank please try again!${NO_COLOR}"
         continue
     fi
 
     if ! [[ "$mod" =~ ^\s*[1-7]{1}\s*$ ]]; then
-        echo "${RED}Module must be a number between 1 and 7!${NO_COLOR}"
+        echo -e "${RED}Module must be a number between 1 and 7!${NO_COLOR}"
         continue
     else
         break
@@ -78,12 +78,12 @@ while true; do
     echo
 
     if [ -z "$week" ]; then
-        echo "${RED}Week cannot be blank please try again!${NO_COLOR}"
+        echo -e "${RED}Week cannot be blank please try again!${NO_COLOR}"
         continue
     fi
 
     if ! [[ "$week" =~ ^\s*[1-8]{1}\s*$ ]]; then
-        echo "${RED}Week must be number between 1 and 8!${NO_COLOR}"
+        echo -e "${RED}Week must be number between 1 and 8!${NO_COLOR}"
         continue
     else
         break
@@ -96,12 +96,12 @@ while true; do
     echo
 
     if [ -z "$day" ]; then
-        echo "${RED}Day cannot be blank please try again!${NO_COLOR}"
+        echo -e "${RED}Day cannot be blank please try again!${NO_COLOR}"
         continue
     fi
 
     if ! [[ "$day" =~ ^\s*[1-5]{1}\s*$ ]]; then
-        echo "${RED}Day must be a number between 1 and 5!${NO_COLOR}"
+        echo -e "${RED}Day must be a number between 1 and 5!${NO_COLOR}"
         continue
     else
         break
@@ -150,7 +150,7 @@ if [ -d "./appacademy-${AA_RESOURCES_BRANCH_NAME}/${mod}-Module/${week}-week/${d
         break
     done
 fi
-echo "${Green}Copying Files for mod:${mod}, week:${week}, day:${day}...${NO_COLOR}"
+echo -e "${Green}Copying Files for mod:${mod}, week:${week}, day:${day}...${NO_COLOR}"
 cp -rp ~/appacademy-${AA_RESOURCES_BRANCH_NAME}/SWEO-Part-Time-Resources/${mod}-Module/${week}-week/${day}-day ~/appacademy-${AA_RESOURCES_BRANCH_NAME}/${mod}-Module/${week}-week/${day}-day/lecture
 echo "Done."
 exit 0
