584c3246b4725032b4c76036f91476eae44f0814
WJVincent <william@william-vincent.dev>
2022-09-14 18:57:04 -0400

    testing

--- utilities/scripts/update.sh
+++ utilities/scripts/update.sh
@@ -29,7 +29,7 @@ if [ $SHELL = '/bin/bash' ]; then
     if [ $PROFILE_FILE != '.bashrc' ]; then
         BASHRC_IN_BASH_PROFILE=$(cat $HOME/$PROFILE_FILE | grep '$HOME/.bashrc')
 
-        if [ -e $HOME/.bashrc ] && [ ! -z $BASHRC_IN_BASH_PROFILE ]; then
+        if [[ -e $HOME/.bashrc  && ! -z $BASHRC_IN_BASH_PROFILE ]]; then
             PROFILE_FILE='.bashrc'
         fi
     fi
