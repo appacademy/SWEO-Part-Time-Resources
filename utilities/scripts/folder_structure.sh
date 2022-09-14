971aacaa53beea26f39cf462bc994190ee579cd3
WJVincent <william@william-vincent.dev>
2022-09-14 17:17:20 -0400

    loop error test

--- utilities/scripts/folder_structure.sh
+++ utilities/scripts/folder_structure.sh
@@ -16,7 +16,7 @@ while true; do
 
     # ask a question & store the answer into the cohortID variable
     echo "What is your Cohort ID?"
-    exec temp <> /dev/tty
+    exec temp<> /dev/tty
     read -u temp -p "> " cohortID
     echo
 
