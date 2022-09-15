e08e140bf9ecb5130defd16f386503ff3e516088
will <w.duffy@outlook.com>
2022-09-14 22:27:54 -0500

    updated

---
+++ 1-Module/1-week/3-day/scratch.js
@@ -0,0 +1,45 @@
+//STRINGS
+// let string = 'That\'s a good string'
+// let string1 = "That's a good string"
+
+// let hello = "hello"
+// // console.log(hello[0])
+
+// // console.log("hello"[5])
+// // console.log("hello ! 3".length)
+
+// // console.log("hello".charAt(4))
+
+// console.log("hello"[5])
+// console.log("hello".indexOf("d"))
+
+// console.log("hello " + "world")
+// let word = "hello"
+// let anotherWord = "yellow"
+// console.log("hello, world")
+
+// console.log(word, "world")
+// function newFunc(word){
+
+//   return word + " world"
+// }
+// console.log(newFunc("hello"))
+
+
+//FUNCTIONS
+
+// function aName (optionalParam){
+//   return optionalParam
+// }
+// aName("argument")
+// console.log(aName("hello"))
+
+function personsHouse(person, word){
+  // console.log( person + "'s" + " house")
+  return person + "'s" + " " + word
+}
+
+// let willsHouse = personsHouse("Will")
+console.log(personsHouse("Blake", "house"))
+
+// console.log(willsHouse)
