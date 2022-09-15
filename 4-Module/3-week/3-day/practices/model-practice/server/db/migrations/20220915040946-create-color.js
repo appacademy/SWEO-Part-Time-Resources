769c85af7e87032898803ed578a75161160b2d71
westrinhkl <trinhpt@uci.edu>
2022-09-14 21:21:36 -0700

    updated

---
+++ 4-Module/3-week/3-day/practices/model-practice/server/db/migrations/20220915040946-create-color.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Colors', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Colors');
+  }
+};
