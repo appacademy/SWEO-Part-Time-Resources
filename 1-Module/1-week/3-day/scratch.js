89e15ccee4f95863b6e1c3d244e9b5ff39e33bf1
will <w.duffy@outlook.com>
2022-09-14 19:16:26 -0500

    t

---
+++ 1-Module/1-week/3-day/scratch.js
@@ -0,0 +1,49 @@
+// STRINGS
+
+// console.log('Will said, "to be or not to be"')
+// console.log('That\'s not a good string')
+// console.log("That's not a good string")
+// console.log('That's not a good string') // error
+// console.log('test\'s')
+// console.log("test\'s")
+// console.log("test's")
+
+// console.log("".length)
+
+
+// console.log("test"[0])
+
+// console.log("test"[4])
+// console.log("test"[0])
+
+// console.log("test".charAt(0))
+
+
+// console.log("apple".indexOf("ppl"))
+
+// console.log("hello" + " " + "world");
+
+// console.log('run'[-1]);
+
+
+//FUNCTIONS
+
+// function functionName (optionalParams){
+//     // console.log("within a function")
+//     return optionalParams
+//     console.log("within a function")
+
+// }
+
+// // functionName("Will")
+// console.log(functionName("Will"))
+
+// function printOwnersHouse(owner, color){
+//     return owner + "\'s" + " " + 'house color is' + " "+ color;
+// }
+
+// let wills_house = printOwnersHouse('Will', 'yellow')
+// console.log(wills_house)
+
+// let blakes_house = printOwnersHouse('Blake')
+// console.log(blakes_house)
