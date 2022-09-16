ca5e19062377d24b6196dfae1fdae2de70515de6
will <w.duffy@outlook.com>
2022-09-15 21:42:01 -0500

    updated west

---
+++ 1-Module/1-week/4-day/scratch.js
@@ -0,0 +1,37 @@
+// let string = "someString"
+
+// let animalArr = ['dog', 'cat', 'zebra']
+
+// console.log(animalArr[2])
+
+//IF
+
+// let dog = 'house'
+
+// if (dog === 'dog3'){
+//   console.log("This worked")
+// } else if (dog === 'cat') {
+//   console.log('cat')
+// } else {
+//   console.log("not an animal")
+// }
+
+// let string = 'aeiou'
+
+// if (string.includes('a')){
+//   console.log('this a vowel')
+// }
+
+// LOOP
+
+// let arr = ['dog', 'cat', 'zebra']
+
+// for (let i = arr.length - 1; 0 <= i; i--){
+//   console.log(arr[i])
+// }
+
+// let i = 0;
+// while (i < arr.length){
+//   console.log(arr[i])
+//   i++;
+// }
