716e26c5dfbb86a46d1e7fc7777bfe5d45da955e
will <w.duffy@outlook.com>
2022-09-17 17:19:55 -0500

    T

--- 1-Module/1-week/5-day/scratch.js
+++ 1-Module/1-week/5-day/scratch.js
@@ -1,237 +1,72 @@
-// Function Declaration Syntax
-
-// function printer (str) {
-//   console.log(str);
+//function declartion
+// function helloDeclarationFunc() {
+//   helloDeclarationFunc()
+//   return 'hello I am a function declaration';
 // }
 
-// Function Declaration Syntax
-
-// let printer = function (str) {
-//   console.log(str);
-// }
-
-// printer('hello');
-
-// mutable
-// Array and Objects
-
-// let arr = [1, 2, 3];
-// arr[1] = 6;
-// console.log(arr);
-
-// // immutable
-// // Strings, nums, bools, NaN, null
-
-// let str = 'hello';
-// str[1] = 'a';
-// console.log(str);
-
-// Array.push()
-
-// let arr = [3, 2, 1];
-// console.log(arr);
-// let newLen = arr.push(0);
-// console.log(newLen);
-// console.log(arr);
-
-// Array.pop()
-// let arr = [3, 2, "hello"];
-// let removed = arr.pop();
-// console.log(removed);
-// console.log(arr);
-
-// Array.unshift()
-// let arr = [3, 2, 1];
-// console.log(arr);
-// let newLen = arr.unshift(0);
-// console.log(newLen);
-// console.log(arr);
-
-// Array.shift()
-// let arr = [3, 2, "hello"];
-// let removed = arr.shift();
-// console.log(removed);
-// console.log(arr);
-
-// Nested Loops
-
-// make a for loop that goes from 0 - 5
-// for (let i = 0; i <= 5; i++) {
-//   console.log(i);
-//   for (let j = 2; j <= 7; j++) {
-//     console.log("     ",j);
-//   }
-// }
-
-// Write a function that intakes a 2D and sums up ALL of the numbers in ALL of arrays
-
-// [ [1,2,3], [4], [5, 6] ]
-
-/* 
-Plan
-define a function called sum that intakes a 2d array
-define a variable to hold the sum
-Iterate over the OUTER array
-  define a variable to hold the SUBARRAY
-  Iterate over the SUBARRAY
-    define a variable to hold the number 
-    add that number to the sum
-return sum
-*/
-// let sum = function(arr) {
-//   let res = 0;
-//   for (let i = 0; i < arr.length; i++) {
-//     let subArr = arr[i];
-//     for(let j = 0; j < subArr.length; j++) {
-//       let num = subArr[j];
-//       res += num;
-//     }
-//   }
-//   return res;
-// }
-
-// console.log(sum([[1,2,3], [4], [5, 6]]))
-
-// Write a function that intakes an array and a target sum and returns true if any two numbers
-// equal the targetsum 2sum
-
-/*
-[1,2,3], 4 => true;
-[2,6,7], 9 => true;
-[1,2,3], 22 => false;
-
-
-            i
-            j            	          
-[1    2     3]
-*/
-
-/* 
-define a function pairSum(arr, target)
-Iterate over the array
-  define a variable to hold the FIRST
-  Iterate over the array
-    define a variable to hold the SECOND number
-    see if the firstNum + secondNum == target
-       return true
-return false
-*/
-// let pairSum = function (arr, target) {
-//   for (let i = 0; i < arr.length; i++) {
-//     let num1 = arr[i];
-//     for (let j = 0; j < arr.length; j++) {
-//       let num2 = arr[j];
-//       if (num1 + num2 === target) {
-//         return true;
-//       }
-//     }
-//   }
-//   return false;
-// }
-
-// console.log(pairSum([1, 2, 3], 2))
-// console.log(pairSum([1, 2, 3], 4))
-// console.log(pairSum([1, 2, 3], 22))
-
-// Write a function that intakes an array and a target sum and returns a 2d
-// array containing ALL instances where two unique numbers equal the target sum
-
-/*
-[1,2,3], 4 => [[1, 3]]
-[2, 3, 6,7], 9 => [[2, 7], [3, 6]]
-[1,2,3], 22 => [];
-
-
- 	i           
-    	j		                    	          
-[2	3	6	7]
-*/
-
-/* 
-Plan
-Define a function called arrSums that intakes a 2d Arr and a target sum
-define a res to store all of my pairs that equal the target
-Iterate over the array
-  define a variable to hold num1
-  Iterate over the array starting at i + 1
-  	define a var to hold the num2
-  	if num1 + num2 is the target
-      push [num1, num2]
-return res
-/* 
-
-*/
-// let arrSums = function (arr, target) {
-//   let res = [];
-//   for (let i = 0; i < arr.length; i++) {
-//     let num1 = arr[i];
-//     for (let j = i + 1; j < arr.length; j++) {
-//       let num2 = arr[j];
-//       if (num1 + num2 === target) {
-//         res.push([num1, num2]);
-//       }
-//     }
-//   }
-//   return res;
+// // function expression syntax
+// let helloExpressionFunc = function() {
+//   return 'hello I am a function expression';
 // };
 
-// console.log(arrSums([2, 3, 6, 7], 9));
-// console.log(arrSums([1, 2, 3], 4));
 
-// takes in a word - a string!
-// takes in an operation - also a string!
-// only have to do real work for "capitalize" "uppercase" "double" "reverse"
-// anything not in those words, return the original word unchanged
 
-// check what operation we're being asked to perform
-// based on the operation we're given, we need to
-// "capitalize" - first letter of the string
-// word[0].toUpperCase() - store this and combine it with the rest of the word
-// to make a new word that fits our conditions - slice the rest of the word?
-// "uppercase" - word.toUpperCase() ? does not mutate the word, but returns new string, "FOO"
-// "double" - repeating each character of the string after the end of the original string
-// iterate through string? for each letter, add onto original string, return
-// word.repeat()
-// "reverse" - iterate through the string - backwards? - concatenate final output string together
-// what if stringChanger("foo", "banana") - return word? don't do anything else
+// console.log(helloDeclarationFunc())
+// console.log(helloExpressionFunc())
 
-function stringChanger (word, operation) {
-  if (operation === "capitalize") {
-    word[0]
-    let letter = word[0];
-    let slicedWord = word.slice(1);
-    return letter.toUpperCase() + slicedWord;
-  } else if (operation === "uppercase") {
-    return word.toUpperCase();
-  } else if (operation === "double") {
-    return word.repeat(2);
-  } else if (operation === "reverse") {
-    let reversedString = "";
-    for (let i = word.length - 1; i >= 0; i--) {
-      reversedString += word[i];
-    }
-    return reversedString;
-  } else {
-    return word;
-  }
-}
 
-console.log(stringChanger("foo", "capitalize")); // Foo
-console.log(stringChanger("foo", "uppercase")); // FOO
-console.log(stringChanger("foo", "double")); // foofoo
-console.log(stringChanger("foo", "reverse")); // oof
-console.log(stringChanger("foo", "unknown")); // foo
+// let name = "Alvin";
+// let age = 1000;
+// let getAvg = function (num1, num2) {
+//   return (num1 + num2) / 2;
+// };
 
-// TESTS
-// DO NOT MODIFY ANYTHING BELOW THIS LINE
+// console.log(name, age, getAvg(4, 2))
 
-let score = 0;
+//Mutability
+// let nums = [1, 2, 3];
+// console.log(nums);
+// nums[0] = 100;
+// console.log(nums);
 
-if (stringChanger("foo", "capitalize") === "Foo") score++;
-if (stringChanger("foo", "uppercase") === "FOO") score++;
-if (stringChanger("foo", "double") === "foofoo") score++;
-if (stringChanger("foo", "reverse") === "oof") score++;
+// let name1 = "WIZ";
+// console.log(name1);
+// name1[1] = "E"; // This will not work
+// name1[2] = "S"; // This will not work
+// console.log(name1); //still "WIZ"
 
-if (stringChanger("foo", "unknown") === "foo") score++;
+// // reassigning !== mutable
+// let str = "abc";
+// str += "d"; // str = str + 'd';
+// console.log(str); // 'abcd'
 
-console.log("You have scored " + score + "/5 points.");
+
+//ADVANCED ARRAY METHODS
+//PUSH
+// let arr = [4, 3, 2];
+// console.log(arr, "length:", arr.length);
+// let newLength = arr.push('string'); // Push will accept any number of arguments
+
+// console.log(arr, "length:", newLength);
+
+
+//POP
+// console.log(arr);
+// let el = arr.pop();
+// console.log(el, arr);
+
+let arr = [4, 3, 2];
+
+// //shift
+// console.log(arr)
+// let unshifted = arr.shift()
+// console.log(unshifted, "Old length: ", arr)
+
+
+// UNSHIFT
+newLength = arr.unshift(1);
+console.log("New length: :", newLength, arr);
+
+
+// let firstEle = arr.shift();
+// console.log(firstEle, arr);
