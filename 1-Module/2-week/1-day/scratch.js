724fc324922f8eeb999fd58c35039faecef0f1e6
will <w.duffy@outlook.com>
2022-09-19 22:24:04 -0500

    T

---
+++ 1-Module/2-week/1-day/scratch.js
@@ -0,0 +1,16 @@
+for (let i = 0; i < 4; i++) {
+  for (let j = 0; j < 5; j++) {
+    console.log(i, j);
+  }
+}
+
+
+let dogs = ["belka", "strelka", "laika", "dezik"];
+
+for (let i = 0; i < dogs.length; i++) {
+  let dog1 = dogs[i];
+  for (let j = 0; j < dogs.length; j++) {
+    let dog2 = dogs[j];
+    console.log(dog1, dog2);
+  }
+}
