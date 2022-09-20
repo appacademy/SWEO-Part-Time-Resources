ec7f9c4b543cab22aed4e6e9138ebf38301ffe5a
will <w.duffy@outlook.com>
2022-09-19 23:29:01 -0500

    T

--- 1-Module/2-week/1-day/scratch.js
+++ 1-Module/2-week/1-day/scratch.js
@@ -1,5 +1,17 @@
-for (let i = 0; i < 4; i++) {
-  for (let j = 0; j < 5; j++) {
-    console.log(i, j);
-  }
+let students = ["jim", "bob", "tom", "sally"] // an array of students
+
+//i === 4
+for (let i = 0; i < students.length; i++) {
+  //sally
+  let firstStudent = students[i];
+
+  // i === 1
+  // j === 4
+ for (let j = i + 1; j < students.length; j++){
+
+  //student sally
+  let secondStudent = students[j];
+
+  console.log(firstStudent, secondStudent)
+ }
 }
