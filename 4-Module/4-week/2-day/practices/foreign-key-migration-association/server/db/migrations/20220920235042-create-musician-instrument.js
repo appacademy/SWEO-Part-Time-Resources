112974df2c9a1f71c0a8492e60496b0e999f76ee
westrinhkl <trinhpt@uci.edu>
2022-09-20 17:00:25 -0700

    updated

---
+++ 4-Module/4-week/2-day/practices/foreign-key-migration-association/server/db/migrations/20220920235042-create-musician-instrument.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('MusicianInstruments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      musicianId: {
+        type: Sequelize.INTEGER,
+        references:{
+          model: 'Musicians'
+        }
+      },
+      instrumentId: {
+        type: Sequelize.INTEGER,
+        references:{
+          model: 'Instruments'
+        }
+        
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('MusicianInstruments');
+  }
+};
