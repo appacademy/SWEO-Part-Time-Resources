ae8c43ee5c80bbadf837bda57d7f823912accf58
westrinhkl <trinhpt@uci.edu>
2022-09-20 20:03:08 -0700

    updated

---
+++ 4-Module/4-week/2-day/practices/foreign-key-sequelize-practice/server/db/models/instrument.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Instrument extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // Your code here
+    }
+  };
+  Instrument.init({
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    }
+  }, {
+    sequelize,
+    modelName: 'Instrument',
+  });
+  return Instrument;
+};
