b0f444811387f82308d63de489dd387b585d3be4
westrinhkl <trinhpt@uci.edu>
2022-09-20 19:24:02 -0700

    updated

---
+++ 4-Module/4-week/2-day/code-it-out/answers/migration-add-column/server/db/migrations/20220921021828-add-col-shade.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('Colors', 'shade', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('Colors', 'shade');
+  }
+};
