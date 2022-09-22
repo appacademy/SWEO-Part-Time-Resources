32d821871af85b8e779c60ce542a2c6cf01377c7
will <w.duffy@outlook.com>
2022-09-21 21:04:11 -0500

    T

---
+++ 1-Module/2-week/3-day/scratch.js
@@ -0,0 +1,46 @@
+let changeWordsNoHelper = function(sentence){
+    let words = sentence.split(' ');
+    let vowels = 'aeiou';
+
+    for (let i = 0; i < words.length; i++){
+      let word = words[i];
+      let newWord = '';
+      for (let j = 0; j < word.length; j++){
+        if (!vowels.includes(word[j])){
+            newWord += word[j]
+        }
+      }
+      words[i] = newWord;
+      }
+      return words.join(' ');
+    }
+
+    // let tweet = 'I just spent 3 hours walking around town playing pokemon go and I feel great!'
+    // console.log(changeWordsNoHelper(tweet));
+
+//HELPER FUNCTION
+ let removeVowels = function(word){
+    let vowels = 'aeiou';
+    let newWord = '';
+
+    for (let i = 0; i < word.length; i++){
+      if (!vowels.includes(word[i])) newWord += word[i]
+    }
+    return newWord;
+  }
+
+//FUNCTION USING HELPER FUNCTION
+  let changeWords = function(sentence){
+    let words = sentence.split(' ');
+
+    for (let i = 0; i < words.length; i++){
+      let word = words[i];
+      words[i] = removeVowels(word);
+      }
+      return words.join(' ');
+    }
+
+
+
+  let tweet = 'I just spent 3 hours walking around town playing pokemon go and I feel great!'
+  console.log(changeWords(tweet));
