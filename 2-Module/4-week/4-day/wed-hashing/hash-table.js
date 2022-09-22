8375f9519d9893bb757c2d95a95bc133a64e30e4
James Thompson <thompsonjames0120@gmail.com>
2022-09-22 17:59:40 -0500

    added phase 1 solution

--- 2-Module/4-week/4-day/wed-hashing/hash-table.js
+++ 2-Module/4-week/4-day/wed-hashing/hash-table.js
@@ -12,14 +12,20 @@ class HashTable {
 
   constructor(numBuckets = 4) {
     // Your code here
+    this.count = 0
+    this.capacity = numBuckets
+    this.data = new Array(this.capacity).fill(null)
   }
 
   hash(key) {
     // Your code here
+    const hashed = sha256(key).slice(0,8)
+    return parseInt(`0x${hashed}`)
   }
 
   hashMod(key) {
     // Your code here
+    return this.hash(key) % this.capacity
   }
 
   insertNoCollisions(key, value) {
