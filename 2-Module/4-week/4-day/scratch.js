f4377422e61b6563e64b6b4e75eff9c594637583
James Thompson <thompsonjames0120@gmail.com>
2022-09-23 00:06:54 -0500

    added solutions

--- 2-Module/4-week/4-day/scratch.js
+++ 2-Module/4-week/4-day/scratch.js
@@ -16,12 +16,29 @@
 
 
 
-const isUnique = (arr) => {
-  let set = new Set(arr);
-  return set.size === arr.length;
-};
+// const isUnique = (arr) => {
+//   let set = new Set(arr);
+//   return set.size === arr.length;
+// };
 
-console.log(isUnique([1, 2, 3]));
-console.log(isUnique([1, 2, 3, 1]));
+// console.log(isUnique([1, 2, 3])); // t or f
+// console.log(isUnique([1, 2, 3, 1])); ///t or f
 
+// const mySet = new Set()
+// let thing = {name: 'jamees'}
 
+// mySet.add(1)
+// mySet.add(2)
+// mySet.add(3)
+// mySet.add(1)
+// mySet.add(thing)
+// mySet.add([1,2,3,4,5,6,7,8,9])
+// mySet.add([1,2,3,4,5])
+
+// console.log(mySet, 'before deletion')
+// mySet.delete(thing)
+// console.log(thing, 'line 40')
+// console.log(mySet, 'afeter deletion')
+// for(let item of mySet){
+//   console.log(item)
+// }
