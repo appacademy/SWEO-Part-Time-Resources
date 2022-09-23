c2f891fcadbb620f3200d3ce99c94692166b3042
Baylen <baylendoss12@gmail.com>
2022-09-22 22:35:25 -0500

    day 4

---
+++ 6-Module/2-week/4-day/class.py
@@ -0,0 +1,48 @@
+
+class Employee:
+    def __init__(self, name=None):
+        if name is None:
+            raise ValueError('name must be provided')
+        else:
+            self.name = name
+
+    @staticmethod
+    def status_checker(status, inst):
+        print(inst)
+        completed = ['1', '3', '4']
+        non_completed = ['2']
+        if status == 'complete':
+            return completed
+        elif status == 'incomplete':
+            return non_completed
+        else:
+            return 'provided status is not valid'
+    
+    @classmethod
+    def class_factory(cls, names_list):
+        return [cls(name) for name in names_list]
+
+    def parent_printer(self):
+        print('This is coming from the parent')
+
+
+    def __repr__(self):
+        return self.name
+
+
+class Associate(Employee):
+    def __init__(self, name=None, title=None):
+        super().__init__(name)
+        self.title = title
+
+baylen = Associate('baylen')
+# print(type(baylen.class_factory(['james'])[0]))
+baylen.parent_printer()
+
+
+
+
+
+
+# print(baylen.class_factory(['zavier', 'james', 'yake']))
+# print(Employee.status_checker('incomplete', baylen))
