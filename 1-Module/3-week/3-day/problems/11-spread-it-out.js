50e61ab0b53f244d0e3bb944525f0b77de99d67b
will <w.duffy@outlook.com>
2022-09-28 19:08:09 -0500

    T

---
+++ 1-Module/3-week/3-day/problems/11-spread-it-out.js
@@ -0,0 +1,21 @@
+/***********************************************************************
+Write a function named `spreadItOut(array1, array2)` that accepts two arrays and
+uses *spread operator* syntax to return a single array. 
+
+
+
+Examples:
+spreadItOut([3,5,6], [1,2,3]); // => [3,5,6,1,2,3];
+spreadItOut([], [1,2,3]); // => [1,2,3];
+spreadItOut(["apple", "banana"], [1,2,3]); // => ["apple", "banana", 1, 2, 3];
+***********************************************************************/
+
+function spreadItOut(array1, array2) {
+  //!!START
+  let joinedArray = [...array1, ...array2];
+  return joinedArray;
+  //!!END
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = spreadItOut;
