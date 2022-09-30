0f5b47087fef6b02a382fb94aabca742b83c3078
will <w.duffy@outlook.com>
2022-09-29 23:15:39 -0500

    T

---
+++ 1-Module/3-week/4-day/assessment-for-sprint-02-practice-pt-3-unit-test-problems/problems/01-key-adder-unique-val.js
@@ -0,0 +1,33 @@
+/***********************************************************************
+Write a function `keyAdderUniqueVal(object, key, value)` that accepts an
+object, a string to be set as a key in that object, and a value. When invoked
+the function should assign the key and value arguments as a key value pair in
+the object argument ONLY IF the value is not already a value for a different
+key in the object. Return the mutated object.
+
+See below for examples
+
+let cat = { name: 'Willie', color: 'orange' };
+keyAdderUniqueVal(cat, "toy", "yarn"); // => {name: "Willie", color: "orange", toy: "yarn"}
+keyAdderUniqueVal(cat, "fruit", "orange"); // => {name: "Willie", color: "orange", toy: "yarn"}
+console.log(cat); // { name: "Willie", color: "orange", toy: "yarn" }
+***********************************************************************/
+
+function keyAdderUniqueVal(object, key, value) {
+// console.log(object)
+let values = Object.values(object)
+// console.log(values)
+//in an if statement as long as values doesn't include VALUE
+if(!values.includes(value)){
+    object[key] = value
+}
+return object
+}
+
+let cat = { name: 'Willie', color: 'orange' };
+console.log(keyAdderUniqueVal(cat, "toy", "yarn")); // => {name: "Willie", color: "orange", toy: "yarn"}
+keyAdderUniqueVal(cat, "fruit", "orange"); // => {name: "Willie", color: "orange", toy: "yarn"}
+console.log(cat); // { name: "Willie", color: "orange", toy: "yarn" }
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = keyAdderUniqueVal;
---
+++ 1-Module/3-week/4-day/assessment-for-sprint-02-practice-pt-3-unit-test-problems/problems/02-dupe-char-min-count.js
@@ -0,0 +1,51 @@
+/***********************************************************************
+Write a function `duplicateCharMinCount(string, minCount)` that will takes
+a string as an argument and returns an array of characters that show up
+at least `minCount` number of times. The string will have at least one
+character.
+
+Examples:
+
+duplicateCharMinCount("apple", 2) // ["p"]
+duplicateCharMinCount("banana", 2) // ["a", "n"]
+duplicateCharMinCount("What about a longer string?", 3) // ["a", "t", " "]
+***********************************************************************/
+
+function countCharacters(string) {
+	// Your code here
+	let obj = {}
+	for (let i = 0; i < string.length; i++){
+		let letter = string[i]
+		// console.log(letter)
+		if (obj[letter] === undefined){
+			obj[letter] = 1
+		} else {
+			obj[letter] += 1
+		}
+	}
+	return obj
+}
+
+// console.log(countCharacters("apple"))
+
+function duplicateCharMinCount(string, minCount) {
+	// Your code here
+let arr = []
+let countedString = countCharacters(string)
+// console.log(countedString)
+
+for (let key in countedString){
+	let value = countedString[key]
+	// console.log(key, value)
+	if (value >= minCount){
+		arr.push(key)
+	}
+}
+return arr
+}
+console.log(duplicateCharMinCount("apple", 2)) // ["p"]
+console.log(duplicateCharMinCount("banana", 2)) // ["a", "n"]
+console.log(duplicateCharMinCount("What about a longer string?", 3)) // ["a", "t", " "]
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = duplicateCharMinCount;
