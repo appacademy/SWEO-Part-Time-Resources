4334a9cd8399e93bf2ba328fd93cf50b9ec916e3
Zaviar Brown <brown.zaviar@gmail.com>
2022-10-01 14:46:59 -0700

    Long practice walkthroughs

---
+++ 5-Module/1-week/5-day/useEffectLongPractice/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+//!!START SILENT
+// To run the bonus files, comment out the preceding line and uncomment the 
+// following line.
+// import App from './bonus/App'; 
+//!!END
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
