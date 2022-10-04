08e9ac8d8e6a7ae65b140cc2d4ef5c2c2b0afdf8
James Thompson <thompsonjames0120@gmail.com>
2022-10-03 23:43:59 -0500

    cloned insertion

---
+++ 2-Module/6-week/1-day/insertion-sort/insertion-sort.js
@@ -0,0 +1,46 @@
+// Insertion Sort out-of-place
+// Do not modify the original array
+function insertionSort(arr) {
+  /*
+  Pseudocode:
+
+  Copy the original array
+  Create an array to store the sorted values
+  While the array is not empty:
+  - make sure you have a console.log(sorted.join(',')) as your first line in the while loop
+  - Pop a value from the array
+  - Create a new spot at the end of the array with null to help with comparisons
+  - Walk through the sorted array in reverse order
+  - Check if the value to the left is smaller than the new value
+  - If so, you've reached the insertion point so exit the loop
+  - If not shift the value to the right by 1 and continue
+  - Insert the unsorted value at the break point
+  Return the sorted array
+  */
+
+  // Your code here
+}
+
+// In-place Insertion Sort
+// Mutates the original array
+function insertionSortInPlace(arr) {
+  /*
+  Pseudocode:
+
+  Set a pointer dividing the array into sorted and unsorted halves
+  Repeat while the unsorted half is not empty:
+  - make sure you have a console.log(sorted.join(',')) as your first line in the while loop
+  - Grab the first value from the unsorted half
+  - For each value starting from the divider,
+  - Check if the value to the left is smaller than the unsorted value
+  - If so, you've reached the insertion point so exit the loop
+  - If not shift the value to the right by 1 and continue
+  - Insert the unsorted value at the break point
+  - Increment the dividing pointer and repeat
+  Return the mutated array
+  */
+
+  // Your code here
+}
+
+module.exports = [insertionSort, insertionSortInPlace];
