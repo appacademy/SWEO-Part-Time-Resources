b3d8808e2c4ff4f7bad1afbd4b199181d27ad04f
James Thompson <thompsonjames0120@gmail.com>
2022-10-05 16:19:09 -0500

    added scratch

--- 2-Module/6-week/3-day/scratch.js
+++ 2-Module/6-week/3-day/scratch.js
@@ -1,70 +1,52 @@
-const adjList = {
-    1: [2, 5],
-    2: [1, 3, 5],
-    3: [2, 4],
-    4: [3, 5, 6],
-    5: [1, 2, 4],
-    6: [4]
-  }
+const matrix = [
+    [1, 1, 1, 0, 0], // 0
+    [0, 1, 1, 0, 1], // 1
+    [0, 1, 1, 0, 1], // 2
+    // 0 1 2 3 4
+  ];
   
-  function printBreadthFirst(start) {
-    // your code here
-    const queue = [start]
-    const visited = new Set()
-    visited.add(start)
-    const res = []
-
-    while(queue.length){
-        let curr = queue.shift()
-
-        res.push(curr)
-        adjList[curr].forEach(ele => {
-            if(!visited.has(ele)){
-                visited.add(ele)
-                queue.push(ele)
-            }
-        })
+  function getNeighbors(row, col, graph) {
+    const neighbors = [];
+  
+    // Check top
+    if (row > 0 && graph[row - 1][col] === 1) {
+      neighbors.push([row - 1, col]);
     }
-    console.log(res)
-  }
   
-  console.log("First Test:")
-  printBreadthFirst(3); // Prints 1 through 6 in Breadth-first order, starting with 3
-                        // One possible solution:  3, 2, 4, 1, 5, 6
-  console.log("Second Test:")
-  printBreadthFirst(6); // Prints 1 through 6 in Breadth-first order, starting with 6
-                        // One possible solution:  6, 4, 3, 5, 2, 1
-  console.log("Third Test:")
-  printBreadthFirst(4); // Prints 1 through 6 in Breadth-first order, starting with 4
-                        // One possible solution:  4, 3, 5, 6, 2, 1
-
-function printDepthFirst(start) {
-    // your code here
-
-    const stack = [start]
-    const visited = new Set()
-    const res = []
-    visited.add(start)
-    while(stack.length){
-        let curr = stack.pop()
-
-        res.push(curr)
-        adjList[curr].forEach(ele => {
-            if(!visited.has(ele)){
-                visited.add(ele)
-                stack.push(ele)
-            }
-        })
+    // Check bottom
+    if (row < graph.length - 1 && graph[row + 1][col] === 1) {
+      neighbors.push([row + 1, col]);
     }
-    console.log(res)
+  
+    // Check left
+    if (col > 0 && graph[row][col - 1] === 1) {
+      neighbors.push([row, col - 1]);
+    }
+  
+    // Check right
+    if (col < graph[0].length - 1 && graph[row][col + 1] === 1) {
+      neighbors.push([row, col + 1]);
+    }
+  
+    return neighbors;
+  
+    // Your code here
   }
   
-  console.log("First Test:")
-  printDepthFirst(3); // Prints 1 through 6 in Depth-first order, starting with 3
-                      // One possible solution:  3, 4, 6, 5, 1, 2
-  console.log("Second Test:")
-  printDepthFirst(6); // Prints 1 through 6 in Depth-first order, starting with 6
-                      // One possible solution:  6, 4, 5, 2, 1, 3
-  console.log("Third Test:")
-  printDepthFirst(4); // Prints 1 through 6 in Depth-first order, starting with 4
-                      // One possible solution:  4, 6, 5, 2, 1, 3
+  
+  const adjList = {};
+  
+  matrix.forEach((el, row) => {
+    el.forEach((value, col) => {
+      value ? (adjList[`${row}, ${col}`] = getNeighbors(row, col, matrix)) : null;
+    });
+  });
+  
+  console.log(adjList);
+  
+  /*//!
+  Don't worry about understanding the code in this file. If you run this file, 
+  you'll see an adjacency list print to the console for every 1 in the matrix.
+  This is to show you the relationship between an adjacency list and a matrix.
+  Usually, one is better suited for a problem than the other.
+  *///!
