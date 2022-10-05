771aae1c1047cc79586b347080a609572fdcc938
will <w.duffy@outlook.com>
2022-10-05 18:54:07 -0500

    updated

--- 1-Module/4-week/3-day/scratch.js
+++ 1-Module/4-week/3-day/scratch.js
@@ -1,47 +1,158 @@
-let globalScope = "this is global scope";
-
-// let myFunc = function(arg){
-//   let functionScope = "this is function scope";
-//   if(true){
-//     let blockScope = "this is block scoped";
-//   }
-//   console.log({functionScope, globalScope, arg, blockScope});
+//Truthy vs true
+// let cat = "null"
+// if(cat){
+//   console.log(cat)
 // }
 
-// myFunc("this is also function scoped");
+// let func = function(firstArg) {
+//   return (function(secondArg) {
+//       console.log(firstArg(secondArg));
+//   });
+// };
+
+// let triple = function(num) {
+//   return num * 3;
+// }
+// let newFunc = func(triple)
+// newFunc(9)
 
 
-// let firstName = "James";
+// let newFunc2 = func(triple, 9)
+// console.log(newFunc2)
+// newFunc2(9)
 
-// let nameFunction = function(){
-//   // let firstName = "Sarah";
-//   if(true){
-//     // let firstName = "William"
-//     console.log(firstName);
+
+// scope
+// global scope
+// let animal = 'cow';
+
+// // // local scope (function scope)
+
+// const theAnimal = function(){ // local or function scope
+//   let animal = 'dog';
+//   // block scope
+//   if (true){
+//     let animal = 'pig';
+//     console.log('in the block scope', animal)
+
+//   }
+//   console.log('in the function scope', animal)
+// }
+// console.log("global scope", animal)
+
+// theAnimal();
+
+//------------------------------------------------------------------
+// console.log('in the global scope', animal);
+// let blockScope = "Global Scope"
+
+// let myFunction = function (functionScope) {
+
+// // let blockScope ="Function Scoped"
+//   if (functionScope) {
+//     // let blockScope = 'Block Scoped';
+//     console.log("BLOCK SCOPE:", blockScope)
+//   }
+//   console.log("GLOBAL:", blockScope)
+// };
+
+// myFunction('I am function scoped');
+
+
+//-----------------------------------------------------------
+// let car = 'vroom';
+// let turkey = 'gobbledoblle';
+
+// function makeSounds(dogSound) {
+//   let cow = 'moo';
+//   if (true) {
+//     let turkey = 'gobble';
+//     for (let i = 0; i < 2; i++) {
+//       let turkey = "don't gobble";
+//     }
+//   }
+//   console.log(car, dogSound, cow, turkey);
+// }
+// // console.log("TURKEY", turkey)
+// // console.log(dogSound)
+
+// makeSounds('woof');
+
+
+//-----------------------------------------------------------
+// let hungry = false;
+
+// function sayHungry() {
+//   let hungry = true;
+//   if (hungry) {
+//     console.log("I'm hungry!");
+//   } else {
+//     console.log("I'm coding");
 //   }
 // }
-// nameFunction();
 
-// console.log(__dirname);
+// sayHungry();
 
-// const num = 5;
-// const newNum = num + 2
-// console.log({new: newNum, num: num});
+
+//-----------------------------------------------------------
 
 // const dontUseVar = function(){
 //   var test = "hello";
 //   if(true){
 //     var test = "hi";
 //   }
-//   console.log(test);
+// 	console.log(test);
 // }
 
-// dontUseVar();
+// const useLet = function(){
+//   let test = "hello";
+//   if(true){
+//     let test = "hi";
+//   }
+// 	console.log(test);
+// }
 
-const dontUseVar2 = function(){
-  // var test = undefined;
-  console.log(test);
-  let test = "test";
+// // dontUseVar();
+// useLet()
+
+//-----------------------------------------------------------
+// const dontUseVar2 = function () {
+// 	console.log(test);
+//   var test = "test";
+//   // let test = 'test';
+// };
+
+// dontUseVar2();
+
+// let str = 'not apple';
+// if (true) {
+//   console.log(str);
+//   let str = 'apple';
+// }
+
+
+//-----------------------------
+//CONST
+
+// let - allows reassignment, block scoped
+
+// let num = 5;
+// num = 3;
+// console.log(num);
+
+// const - does not allow reassignment, block scoped
+
+// const num = 5;
+// num = 3;
+// console.log(num);
+
+//---------------------------------------
+// let cat = "cat"
+const cat = "cat"
+if(cat){
+  const cat = "dog"
+  // cat = "apple"
+  console.log("BLOCK:", cat)
 }
 
-dontUseVar2();
+console.log(cat)
