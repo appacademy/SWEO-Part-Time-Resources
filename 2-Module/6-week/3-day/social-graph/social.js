82dcfdec5983450a7b4c4e9d3345ae19c0447491
James Thompson <thompsonjames0120@gmail.com>
2022-10-06 22:52:53 -0500

    added social

---
+++ 2-Module/6-week/3-day/social-graph/social.js
@@ -0,0 +1,35 @@
+// Implement the SocialNetwork class here
+class SocialNetwork {
+
+  constructor() {
+    this.users = {};
+    this.follows = {};
+    this.currentID = 0;
+  }
+
+  addUser(name) {
+    // Your code here
+  }
+
+  getUser(userID) {
+    // Your code here
+  }
+
+  follow(userID1, userID2) {
+    // Your code here
+  }
+
+  getFollows(userID) {
+    // Your code here
+  }
+
+  getFollowers(userID) {
+    // Your code here
+  }
+
+  getRecommendedFollows(userID, degrees) {
+    // Your code here
+  }
+}
+
+module.exports = SocialNetwork;
