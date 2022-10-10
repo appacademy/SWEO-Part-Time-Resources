68a315422d2b58f7cb9d6a5c9848782d644afd61
will <w.duffy@outlook.com>
2022-10-10 18:22:46 -0500

    T

--- 1-Module/5-week/1-day/scratch.js
+++ 1-Module/5-week/1-day/scratch.js
@@ -1,6 +1,36 @@
-let firstName = "Will";
-let lastName = "Duffy";
+// let firstName = "Will";
+// let lastName = "Duffy";
 
-let greeting = `Hello, ${firstName} ${lastName}!`;
+// let greeting = `Hello, ${firstName} ${lastName}!`;
 
-console.log(greeting);
+// console.log(greeting);
+
+
+// const nameFunc = str => {
+// 	return str
+// }
+
+// let greeting = `Hello, ${nameFunc(firstName)} ${lastName}!`;
+// console.log(greeting);
+
+//--------------------------------------------------------------------------------------
+const arr = [1, 2, 3, 4, 5];
+
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+// 	let eachNum = arr[i]
+// 	sum += eachNum
+// }
+
+// console.log(sum)
+
+
+const recurseSum = (arr) => {
+    debugger
+    if (arr.length === 1) {
+          return arr.pop();
+      }
+    return arr.pop() + recurseSum(arr);
+  };
+
+  console.log(recurseSum(arr));
