fe0cbf48f63e1d5fe89c220cd44fb60e5b1f2c37
will <w.duffy@outlook.com>
2022-10-10 20:56:24 -0500

    T

--- 1-Module/5-week/1-day/scratch.js
+++ 1-Module/5-week/1-day/scratch.js
@@ -1,10 +1,53 @@
-// let firstName = "Doctor";
-// let lastName = "Who";
 
-// let greeting = `Hello, ${firstName} ${lastName}!`;
 
-// let greeting = "Hello, " + firstName + " " + lastName + "!"
+let firstName = "Will";
+let lastName = "Duffy";
+console.log("Hello," + " " + firstName + " " + lastName + "!")
+
+// // console.log('That\'s')
+// let obj = {
+//     name: "IN THE OBJ"
+// }
+// // console.log(`that's ${firstName} \$\{\}`)
+// let greeting = `Hello, ${obj.name} ${lastName}!`;
+
 // console.log(greeting);
 
-// let sentence = `5 + 5 = ${5 + 5}`;
-// console.log(sentence);
+
+// // const nameFunc = str => {
+// // 	return str
+// // }
+
+// // let greeting = `Hello, ${nameFunc(firstName)} ${lastName}!`;
+// console.log(greeting);
+
+//--------------------------------------------------------------------------------------
+// const arr = [1, 2, 3, 4, 5];
+// const myFunc = (arr) =>{
+
+//     debugger
+//     let sum = 0;
+//     debugger
+//     for (let i = 0; i < arr.length; i++) {
+//         debugger
+//         let eachNum = arr[i]
+//         debugger
+//         sum += eachNum
+//     }
+//     return sum
+// }
+
+// console.log(myFunc(arr))
+
+
+// const recurseSum = (arr) => {
+//     debugger;
+//     if (arr.length === 1) {
+//         debugger;
+//           return arr.pop();
+//       }
+//       debugger;
+//     return arr.pop() + recurseSum(arr);
+//   };
+
+//   console.log(recurseSum(arr));
