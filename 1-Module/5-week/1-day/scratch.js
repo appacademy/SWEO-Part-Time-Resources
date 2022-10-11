6f56ae200f2f0885add40cbea270dff4daaaae5a
will <w.duffy@outlook.com>
2022-10-11 15:08:51 -0500

    T

--- 1-Module/5-week/1-day/scratch.js
+++ 1-Module/5-week/1-day/scratch.js
@@ -1,43 +1,51 @@
 
 
-let firstName = "Will";
-let lastName = "Duffy";
-console.log("Hello," + " " + firstName + " " + lastName + "!")
+// let firstName = "JIMMY";
+// let lastName = "Bob";
+
+// // console.log("Hello," + " " + firstName + " " + lastName + "!")
+// // console.log(`Hi my name is will ${newVariable}`)
+// let obj = {
+//     name: "JOHNNY"
+// }
+// let greeting = `Hello, ${obj.name} ${lastName}!`;
+
+// console.log(greeting)
 
 // // console.log('That\'s')
-// let obj = {
-//     name: "IN THE OBJ"
-// }
+
 // // console.log(`that's ${firstName} \$\{\}`)
-// let greeting = `Hello, ${obj.name} ${lastName}!`;
+
 
 // console.log(greeting);
 
 
-// // const nameFunc = str => {
-// // 	return str
-// // }
+// const nameFunc = str => {
+// 	return str
+// }
 
-// // let greeting = `Hello, ${nameFunc(firstName)} ${lastName}!`;
+// let greeting = `Hello, ${nameFunc(firstName)} ${lastName}!`;
 // console.log(greeting);
 
 //--------------------------------------------------------------------------------------
-// const arr = [1, 2, 3, 4, 5];
-// const myFunc = (arr) =>{
+const arr = [1, 2, 3, 4, 5];
 
-//     debugger
-//     let sum = 0;
-//     debugger
-//     for (let i = 0; i < arr.length; i++) {
-//         debugger
-//         let eachNum = arr[i]
-//         debugger
-//         sum += eachNum
-//     }
-//     return sum
-// }
+const myFunc = (arr) =>{
+ 
+    debugger;
+    let sum = 0;
+    debugger;
+    for (let i = 0; i < arr.length; i++) {
+        debugger;
+        let eachNum = arr[i]
+        sum += eachNum
+        debugger;
+        // console.log("SUM", sum)
+    }
+    return sum
+}
 
-// console.log(myFunc(arr))
+console.log(myFunc(arr))
 
 
 // const recurseSum = (arr) => {
