69cd8d4eb22a8059e91d10d11a0fdf9cefaf7bc8
Philip Ling <66927690+philling83@users.noreply.github.com>
2022-10-12 00:02:04 -0400

    solutions

---
+++ 3-Module/5-week/2-day/solutions/practice-for-week-09-window-object-and-debugger-long-practice/assets/js/search.js
@@ -0,0 +1,95 @@
+/***************************** HELPER FUNCTIONS *****************************/
+
+// depth-first search for first matching element
+function findFirstMatchingElement(parentEle, cb) {
+    // do a depth-first search to recursively find the first element in the tree
+    // that will return true when passed into the cb function
+    if (cb(parentEle)) {
+        return parentEle;
+    }
+
+    for (let i = 0; i < parentEle.children.length; i++) {
+        const result = findFirstMatchingElement(parentEle.children[i], cb);
+        if (result) return result;
+    }
+
+    return null;
+}
+
+// breath-first search for first matching element
+// function findFirstMatchingElement(parentEle, cb) {
+//     // do a breath-first search with a queue to find the first element in the
+//     // tree that will return true when passed into the cb function
+
+//     const queue = [];
+//     queue.push(parentEle);
+//     let times = 0;
+
+//     while (queue.length) {
+//         const currentEle = queue.shift();
+
+//         if (cb(currentEle)) return currentEle;
+
+//         for (let i = 0; i < currentEle.children.length; i++) {
+//             queue.push(currentEle.children[i]);
+//         }
+//     }
+
+//     return null;
+// }
+
+function findAllMatchingElements(parentEle, cb) {
+    // recursively find all elements in the tree that will return true when
+    // passed into the cb function (depth-first search)
+    let results = [];
+    if (cb(parentEle)) {
+        results.push(parentEle);
+    }
+
+    for (let i = 0; i < parentEle.children.length; i++) {
+        const childResults = findAllMatchingElements(parentEle.children[i], cb);
+        if (childResults.length) results = results.concat(childResults);;
+    }
+
+    return results;
+}
+
+/**************************** EXPORTED FUNCTIONS *****************************/
+
+export function findElementById(id) {
+    return findFirstMatchingElement(document.body, function(ele) {
+        return ele.id === id;
+    });
+}
+
+export function findFirstElementOfTag(tag) {
+    // Return the first occurence of an element of tag name `tag`
+
+    return findFirstMatchingElement(document.body, function(ele) {
+        return ele.tagName === tag;
+    });
+}
+
+export function findFirstElementOfClass(cls) {
+    // Return the first occurence of an element of class `cls`
+
+    return findFirstMatchingElement(document.body, function(ele) {
+        return ele.classList.contains(cls);
+    });
+}
+
+export function findElementsOfTag(tag) {
+    // Return an array of elements that have a tag name of `tag`
+
+    return findAllMatchingElements(document.body, function(ele) {
+        return ele.tagName === tag;
+    });
+}
+
+export function findElementsOfClass(cls) {
+    // Return an array of elements that have are of class `cls`
+
+    return findAllMatchingElements(document.body, function(ele) {
+        return ele.classList.contains(cls);
+    });
+}
