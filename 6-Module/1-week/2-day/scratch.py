7e7f963ac1187d9ffe7a52bd5675ae648f63a217
AlexBetita <alexbheb25@gmail.com>
2022-10-11 19:19:18 -0700

    updated lectures

---
+++ 6-Module/1-week/2-day/scratch.py
@@ -0,0 +1,16 @@
+#variables
+# let, const, var keywords
+
+# x = 'hello'
+
+# +, -, *, /, %, **, //
+
+
+# function name(){}
+# () => {}
+
+# def test(first_name):
+# 	print(first_name) # console.log
+# 	return 'bye'
+
+# print(test('bruce'))
