a6e97faddf4e15216d46d1b95dc51193060fbd87
will <w.duffy@outlook.com>
2022-10-12 20:14:33 -0500

    T

--- 1-Module/5-week/3-day/scratch.js
+++ 1-Module/5-week/3-day/scratch.js
@@ -1,33 +1,56 @@
-// const range = (start, end) => {
-//   // Base Case start === end
-//   if (start === end) return [];
-//   // Recursive Case start !== end
-//   // Recursive Step start + 1
-//   return [start, ...range(start + 1, end)];
+// const addAndMultiply = (num1, num2 = 10, num3 = 5) => {
+//   let sum = num1 + num2;
+//   let product = sum * num3;
+//   return product;
 // };
 
-// const range = (start, end, arr = []) => {
-//   if (start === end) return arr;
-//   arr.push(start);
-//   return range(start + 1, end, arr);
+// console.log(addAndMultiply(1, 2, 3)) // 9
+// console.log(addAndMultiply(1, 2)); // 15
+// console.log(addAndMultiply(1)); // 55
+// console.log(addAndMultiply()); // NaN
+
+// const printMyVals = (num = 1, str = false, arr = [10]) => {
+//   console.log(num);
+//   console.log(str);
+//   console.log(arr);
 // };
 
-const range = (start, end) => {
-  let arr = [];
-  function inner() {
-    if (start === end) return arr;
-    arr.push(start);
-    start += 1;
-    return inner();
-  }
-  return inner();
+// printMyVals(20, [100, 200]);
+//-----------------------------------------------------------------------------
+
+
+
+//! Focus on getting to this solution, the others are good too but this
+//! one is the most relevant for where we're at in the curriculum
+const rangeDefault = (start, end, finalArr = []) => {
+  // debugger;
+  // console.log(finalArr);
+  let arr = []
+  if (start >= end) return finalArr;
+
+  finalArr.push(start);
+
+   rangeDefault(start + 1, end, finalArr); // [1, 2, 3, 4]
+   return finalArr
 };
 
-console.log(range(4, 6)); // [4, 5]
+console.log(rangeDefault(1, 5)); // => [1, 2, 3, 4]
+console.log(rangeDefault(3, 4)); // => [3]
 
 
-// function sum(num1 = 3, num2 = 4) { 
-//   return num1 + num2;
-// }
+//------------------------------------------------------------------------------------
 
-// console.log(sum(2));
+const rangeClosure = (start, end) => {
+  const finalArr = [];
+
+  const innerRange = () => {
+    if (start >= end) return finalArr;
+    finalArr.push(start);
+    start += 1;
+    return innerRange();
+  };
+
+  return innerRange();
+};
+
+// console.log(rangeClosure(1, 5)); // => [1, 2, 3, 4]
