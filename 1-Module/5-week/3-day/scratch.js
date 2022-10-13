5b3f06002c900670074c3e60cff99d7e18de7f9d
will <w.duffy@outlook.com>
2022-10-12 20:13:56 -0500

    T

--- 1-Module/5-week/3-day/scratch.js
+++ 1-Module/5-week/3-day/scratch.js
@@ -1,13 +1,13 @@
-const addAndMultiply = (num1, num2 = 10, num3 = 5) => {
-  let sum = num1 + num2;
-  let product = sum * num3;
-  return product;
-};
+// const addAndMultiply = (num1, num2 = 10, num3 = 5) => {
+//   let sum = num1 + num2;
+//   let product = sum * num3;
+//   return product;
+// };
 
-console.log(addAndMultiply(1, 2, 3)) // 9
-console.log(addAndMultiply(1, 2)); // 15
-console.log(addAndMultiply(1)); // 55
-console.log(addAndMultiply()); // NaN
+// console.log(addAndMultiply(1, 2, 3)) // 9
+// console.log(addAndMultiply(1, 2)); // 15
+// console.log(addAndMultiply(1)); // 55
+// console.log(addAndMultiply()); // NaN
 
 // const printMyVals = (num = 1, str = false, arr = [10]) => {
 //   console.log(num);
@@ -18,19 +18,24 @@ console.log(addAndMultiply()); // NaN
 // printMyVals(20, [100, 200]);
 //-----------------------------------------------------------------------------
 
+
+
 //! Focus on getting to this solution, the others are good too but this
 //! one is the most relevant for where we're at in the curriculum
-// const rangeDefault = (start, end, finalArr = []) => {
-//   debugger;
-//   // console.log(finalArr);
-//   if (start >= end) return finalArr;
+const rangeDefault = (start, end, finalArr = []) => {
+  // debugger;
+  // console.log(finalArr);
+  let arr = []
+  if (start >= end) return finalArr;
 
-//   finalArr.push(start);
+  finalArr.push(start);
 
-//   return rangeDefault(start + 1, end, finalArr); // [1, 2, 3, 4]
-// };
+   rangeDefault(start + 1, end, finalArr); // [1, 2, 3, 4]
+   return finalArr
+};
 
-// console.log(rangeDefault(1, 5)); // => [1, 2, 3, 4]
+console.log(rangeDefault(1, 5)); // => [1, 2, 3, 4]
+console.log(rangeDefault(3, 4)); // => [3]
 
 
 //------------------------------------------------------------------------------------
