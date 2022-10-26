642baa3463cce52db7d1feb55504024646d85992
Philip Ling <66927690+philling83@users.noreply.github.com>
2022-10-26 00:35:41 -0400

    solutions

---
+++ 4-Module/1-week/2-day/solutions/practice-for-week-10-express-route-handlers/server/app.js
@@ -0,0 +1,40 @@
+// Phase 2
+const {
+  getAllArtists,
+  getLatestArtist,
+  getArtistByArtistId,
+  addArtist,
+  editArtistByArtistId,
+  deleteArtistByArtistId,
+  getAlbumsForLatestArtist,
+  getAlbumsByArtistId,
+  getAlbumByAlbumId,
+  addAlbumByArtistId,
+  editAlbumByAlbumId,
+  deleteAlbumByAlbumId,
+  getFilteredAlbums,
+  getSongsByArtistId,
+  getSongsByAlbumId,
+  getSongBySongId,
+  addSongByAlbumId,
+  editSongBySongId,
+  deleteSongBySongId
+} = require('./data');
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get("/artists", (req, res) => {
+  res.json(getAllArtists());
+});
+
+app.post("/artists", (req, res) => {
+  let data = req.body;
+
+  res.status(201).json(addArtist(data));
+});
+
+const port = 5000;
+app.listen(port, () => console.log('Server is listening on port', port));
