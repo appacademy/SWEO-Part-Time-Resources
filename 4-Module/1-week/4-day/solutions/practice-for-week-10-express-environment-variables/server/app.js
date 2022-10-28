573d7071c86a8cb73c22a71eb5fd194583ff7772
Philip Ling <66927690+philling83@users.noreply.github.com>
2022-10-27 20:54:07 -0400

    solutions

---
+++ 4-Module/1-week/4-day/solutions/practice-for-week-10-express-environment-variables/server/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(process.env.SECRET_MESSAGE);
+});
+
+const port = process.env.PORT;
+app.listen(port, () => console.log('Server is listening on port', port));
