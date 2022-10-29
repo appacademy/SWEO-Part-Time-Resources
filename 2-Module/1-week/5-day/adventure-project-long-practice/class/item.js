02471ac7a1b6fa02f1beaf540c8d7eed5c633039
will <w.duffy@outlook.com>
2022-10-29 17:44:34 -0500

    T

---
+++ 2-Module/1-week/5-day/adventure-project-long-practice/class/item.js
@@ -0,0 +1,14 @@
+class Item {
+  // Item Class: Item should have name and description attributes
+
+  //!!START
+    constructor(name, description) {
+        this.name = name;
+        this.description = description;
+    }
+    //!!END
+}
+
+module.exports = {
+  Item,
+};
