e01667fe8cb95e0d6f5e10c6d31539594422787a
Jorge Denis <59179145+javidenis@users.noreply.github.com>
2022-10-31 20:34:14 -0400

    added .sql file example

---
+++ 4-Module/2-week/2-day/file.sql
@@ -0,0 +1,9 @@
+CREATE TABLE pies (
+  flavor VARCHAR(255) PRIMARY KEY,
+  price FLOAT
+);
+
+INSERT INTO pies VALUES('Apple', 19.95);
+INSERT INTO pies VALUES('Caramel Apple Crumble', 20.53);
+INSERT INTO pies VALUES('Blueberry', 19.31);
+INSERT INTO pies VALUES('Blackberry', 22.86);
