13d3e91c6b44ff0a427cb18cd0926e7a72dca442
will <w.duffy@outlook.com>
2022-11-01 16:27:06 -0500

    T

---
+++ 2-Module/2-week/2-day/scratch.js
@@ -0,0 +1,49 @@
+class Person{
+    constructor(name,age){
+      this.name = name;
+      this.age = age;
+    };
+
+    ageUp(message){
+      this.age++;
+      console.log(`Happy birthday! ${this.name} is ${this.age} years old! ${message}`);
+    };
+
+    eat(food){
+      console.log(`yum i love ${food}`);
+    };
+  }
+
+  let joe = new Person('Joe', 24);
+  let aging = joe.ageUp
+
+//   aging() // TypeError: Cannot read properties of undefined (reading 'age')
+  // once we assigned that method to a variable we lost the context.
+    let boundAging = aging.bind(joe, "YAY!");
+  // notice we are using the function that wouldn't work before, can use the method as well by saying joe.ageUp.bind(joe);
+    boundAging(); // prints `Happy birthday! Joe is 25 years old! YAY!
+let boundEat = joe.eat
+boundEat("apple")
+boundEat.call(joe, 'apples');
+ boundEat.apply(joe, ['pears'])
+
+
+
+
+
+
+//ARROW FUNC
+// class Fruit {
+//     constructor(name) {
+//       this.name = name;
+//     }
+
+//     eat = () => {
+//       console.log(`${this.name} has a bite mark`);
+//     };
+//   }
+
+//   const apple = new Fruit('apple');
+//   const eat = apple.eat;
+//   eat();
+//   setTimeout(eat, 1000);
