560c1e11f4c7730ce2127d14da19b70beb6261d4
will <w.duffy@outlook.com>
2022-10-31 21:36:14 -0500

    T

--- 2-Module/2-week/1-day/scratch.js
+++ 2-Module/2-week/1-day/scratch.js
@@ -1,22 +1,47 @@
-// class Test {
-//   testFunc() {
-//     debugger;
-//     console.log(this)
-//     console.log(`this is equal to test1: ${this === test1}`);
+//---------Context: the object that `this` references (value of `this`)---------
+//1) Context is the object that `this` inside of a function belongs to,
+//or what `this` represents when the function is executed
+
+//2) Whichever or wherever the object invokes the
+//function/method (that has `this` in it), that object
+//becomes the context for `this` in that function
+
+
+
+// const helloWorld = function(){
+//   console.log("helloWorld");
+//   console.log(this); //console.log(global)
+
+// }
+
+// helloWorld(); // I'm invoking the function under the global context or the global object
+//so when invoked under the global object, `this` inside helloWorld
+//function represents the global object (context).
+//test
+// class Cat{
+//   constructor(name, age){
+//       this.name = name;
+//       this.age = age;
+//       console.log(this)
+//   }
+
+//   purr(){ //instance method
+//       console.log("meow");
+//   }
+
+//   purrMore(){
+//       this.purr(); //When we execute this method,
+// // the key word `this` inside of purrMore refers to the OBJECT that the method is executed in.
 //   }
 // }
 
-// const test1 = new Test();
-// Calling the method on the instance
-test1.testFunc(); // true
-// Calling the method outside the class
-const test1Func = test1.testFunc;
-test1Func(); // false
-// Calling the method as a callback
-setTimeout(test1.testFunc, 1000); // false
+// let tomTheCat = new Cat("tommmy", 5); //instantiated a new instance (tomTheCat)
+// // let tomTheCat2 = new Cat("NEW NAME", 5); //instantiated a new instance (tomTheCat)
+// //that is an object ob the Cat class.
+// tomTheCat.purrMore(); //what is the value of `this`
+// let newFunc = tomTheCat.purrMore()
+// console.log(newFunc)
+// setTimeout(tomTheCat.purrMore, 1000)
+//inside of that purrMore function  // what is the context? //the context: tomTheCat
 
-// Using this in Function Declaration Syntax
-function someFunc() {
-  console.log(this);
-}
-someFunc(); // Global Object
+//does tomTheCat have a purrMore method? Yes it does and does it also have a purr method? yes it does.
