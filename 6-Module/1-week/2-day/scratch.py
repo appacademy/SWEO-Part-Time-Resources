c8bbf84fe42ad2b7732890650fe29d059fc32ced
Zaviar Brown <brown.zaviar@gmail.com>
2022-11-08 15:56:02 -0800

    Basic python intro

---
+++ 6-Module/1-week/2-day/scratch.py
@@ -0,0 +1,16 @@
+# def myFunc(aNum):
+#     myVal = 3
+#     aNum //= myVal
+#     return aNum
+
+
+# print(myFunc(10))
+# print(10 ** 20000)
+# print(10.0 ** 20000)
+
+
+def myFunc():
+    return "hello"
+
+
+myValue = 10
