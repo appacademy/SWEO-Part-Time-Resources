885c0a41103fe6b2c597808ae4021f7aca6d78dd
Zaviar Brown <brown.zaviar@gmail.com>
2022-11-08 18:58:48 -0800

    Reference types review

--- 6-Module/1-week/2-day/scratch.js
+++ 6-Module/1-week/2-day/scratch.js
@@ -1,1 +1,13 @@
-console.log(10 ** 20000);
+const myArr = [1, 2, 3];
+const otherArr = [...myArr];
+
+let myNum = 5;
+let otherNum = myNum;
+
+otherArr.pop();
+otherNum += 100;
+
+console.log(myArr);
+console.log(otherArr);
+// console.log(myNum);
+// console.log(otherNum);
