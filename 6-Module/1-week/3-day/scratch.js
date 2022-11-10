63ae9a4a3ad157a357dc741470f232b47655ed80
Zaviar Brown <brown.zaviar@gmail.com>
2022-11-09 18:58:11 -0800

    Python day 2

---
+++ 6-Module/1-week/3-day/scratch.js
@@ -0,0 +1,64 @@
+// let highestNum,longestStrLen,counter = 0;
+
+// console.log(highestNum, longestStrLen, counter);
+
+// highestNum += 100;
+// longestStrLen = 'eijfiejfeifj'.length;
+// counter += 1;
+
+// console.log(highestNum, longestStrLen, counter);
+
+// const myFunc = () => 5;
+
+// console.log(typeof myFunc)
+
+// const myNum = 5
+// const myStr = '5'
+
+// // console.log(myNum === myStr)
+// // console.log(myNum == myStr)
+// console.log(myNum === Number(myStr))
+
+// let ob1 = { name: 'Z' };
+// let ob2 = { name: 'Z' };
+
+// console.log(ob1 == ob2);
+
+// const myArr = ['a', 'b', 'c'];
+
+// console.log('In');
+// for (let val in myArr) {
+//     console.log(val);
+//     // console.log(myArr[val]);
+// }
+// console.log('Of');
+
+// for (let val of myArr) {
+//     console.log(val);
+// }
+
+const myString = 'aiejfiaejfiaejfiajef'.split('');
+
+console.log(myString)
+
+
+myString.forEach(el => {
+	console.log(el)
+})
+// const myObjectString = {
+// 	"0": 'a',
+// 	"1": 'i',
+// 	"2": 'e'
+// }
+
+// for (let i = 0; i < myString.length; i++) {
+//     console.log(i);
+// }
+
+// console.log('------------------------------');
+// console.log('------------------------------');
+// console.log('------------------------------');
+
+// for (let i in myString) {
+//     console.log(i);
+// }
---
+++ 6-Module/1-week/3-day/scratch.py
@@ -0,0 +1,70 @@
+# # highest_num = longest_str_len = counter = 0
+
+# # print(highest_num, longest_str_len, counter)
+
+# # highest_num += 100
+# # longest_str_len = len('eijfiejfeifj')
+# # counter += 1
+
+# # print(highest_num, longest_str_len, counter)
+
+
+# # def myFunc():
+# #     return 5
+
+
+# # print(myFunc)
+# # print(type('edijfe'))
+# # print(type(myFunc))
+
+# # myStr = '5'
+# # myNum = 5
+
+# # print("'5' is 5? ", myStr == myNum)
+# # print("'5' is '5'? ", myStr == str(myNum))
+# # print("'5' is 5? ", myStr is myNum)
+# # print("'5' is '5'? ", myStr is str(myNum))
+
+# # myNum = myStr
+
+# # print("REASSIGN TO STRING")
+
+# # print("'5' is 5? ", myStr == myNum)
+# # print("'5' is '5'? ", myStr == str(myNum))
+# # print("'5' is '5'? ", myStr is myNum)
+# # print("'5' is '5'? ", myStr is str(myNum))
+
+# # myList = ['a', 'b', 'c']
+
+
+# # for val in myList:
+# #     print(val)
+
+
+# # for i in range(len(myList)):
+# #     print(myList[i])
+
+# # catching named errors
+# a = 100
+# # b = "5"
+# # b = 0
+# b = 5
+# try:
+#     print(a / b)
+# except ZeroDivisionError:
+#     print("heyooo")
+#     # pass
+# except (TypeError, NameError) as e:
+#     print("ERROR!", e)
+# finally:
+#     print("Finally...")
+
+
+myString = 'aiejfiejfeiajfaefliaejfleif'
+
+for i in myString:
+    print(i)
+
+
+# for aNumberVariable in range(10):
+#     print(aNumberVariable)
