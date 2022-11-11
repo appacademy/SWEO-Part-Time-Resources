511986f3bdf19353b92c8b388ab8aa01556e511f
will <w.duffy@outlook.com>
2022-11-10 23:09:35 -0600

    T

--- 2-Module/3-week/4-day/solutions.js
+++ 2-Module/3-week/4-day/solutions.js
@@ -101,3 +101,43 @@ function asciiToDecimal(blob) {
 
 let string = "ABC"
 console.log(asciiToDecimal(string))
+
+// PROJECT 3 ---------------------------------------------------------------
+
+class DynamicArray {
+
+  constructor(defaultSize=4) {
+
+    //!!START
+    this.data = new Array(defaultSize);
+    this.capacity = defaultSize;
+    this.length = 0;
+    //!!END
+
+  }
+
+  read(index) {
+
+    //!!START
+    if (index >= this.length) return undefined;
+
+    return this.data[index];
+    //!!END
+  }
+
+  unshift(val) {
+
+    //!!START
+    for (let i = this.length ; i > 0 ; i--) {
+      this.data[i] = this.data[i-1];
+    }
+
+    this.data[0] = val;
+    this.length++;
+    //!!END
+  }
+
+}
+
+
+module.exports = DynamicArray;
