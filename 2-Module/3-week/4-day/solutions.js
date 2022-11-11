165216fe11660008be6239d950b123cb6244c66c
will <w.duffy@outlook.com>
2022-11-10 19:57:20 -0600

    T

--- 2-Module/3-week/4-day/solutions.js
+++ 2-Module/3-week/4-day/solutions.js
@@ -101,4 +101,45 @@ function binaryToHexadecimal(blob) {
     return charCodes;
     //!!END
   }
-  console.log(asciiToDecimal("ABC"))
+//   console.log(asciiToDecimal("ABC"))
+
+
+  // THIRD PRACTICE ----------------------------------------------------------------------------------------------
+
+  class DynamicArray {
+
+    constructor(defaultSize=4) {
+
+      //!!START
+      this.data = new Array(defaultSize); 
+      this.capacity = defaultSize;
+      this.length = 0;
+      //!!END
+
+    }
+
+    read(index) {
+
+      //!!START
+      if (index >= this.length) return undefined;
+
+      return this.data[index];
+      //!!END
+    }
+
+    unshift(val) {
+
+      //!!START
+      for (let i = this.length ; i > 0 ; i--) {
+        this.data[i] = this.data[i-1];
+      }
+
+      this.data[0] = val;
+      this.length++;
+      //!!END
+    }
+
+  }
+
+
+  module.exports = DynamicArray;
