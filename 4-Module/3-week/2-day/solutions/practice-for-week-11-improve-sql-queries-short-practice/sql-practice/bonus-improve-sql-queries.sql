54be7bcf51cade9c076816a910b0e66ae45cd0b3
Philip Ling <66927690+philling83@users.noreply.github.com>
2022-11-10 19:23:39 -0500

    solutions

---
+++ 4-Module/3-week/2-day/solutions/practice-for-week-11-improve-sql-queries-short-practice/sql-practice/bonus-improve-sql-queries.sql
@@ -0,0 +1,90 @@
+----------
+-- Step 0 - Create a Query 
+----------
+-- Query: Find a count of `toys` records that have a price greater than
+    -- 55 and belong to a cat that has the color "Olive".
+
+    -- Your code here
+
+-- Paste your results below (as a comment):
+
+
+
+
+----------
+-- Step 1 - Analyze the Query
+----------
+-- Query:
+
+    -- Your code here
+
+-- Paste your results below (as a comment):
+
+
+-- What do your results mean?
+
+    -- Was this a SEARCH or SCAN?
+
+
+    -- What does that mean?
+
+
+
+
+----------
+-- Step 2 - Time the Query to get a baseline
+----------
+-- Query (to be used in the sqlite CLI):
+
+    -- Your code here
+
+-- Paste your results below (as a comment):
+
+
+
+
+----------
+-- Step 3 - Add an index and analyze how the query is executing
+----------
+
+-- Create index:
+
+    -- Your code here
+
+-- Analyze Query:
+    -- Your code here
+
+-- Paste your results below (as a comment):
+
+
+-- Analyze Results:
+
+    -- Is the new index being applied in this query?
+
+
+
+
+----------
+-- Step 4 - Re-time the query using the new index
+----------
+-- Query (to be used in the sqlite CLI):
+
+    -- Your code here
+
+-- Paste your results below (as a comment):
+
+
+-- Analyze Results:
+    -- Are you still getting the correct query results?
+
+
+    -- Did the execution time improve (decrease)?
+
+
+    -- Do you see any other opportunities for making this query more efficient?
+
+
+
+---------------------------------
+-- Notes From Further Exploration
+---------------------------------
