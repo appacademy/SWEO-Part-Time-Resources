78058a8ea68e31bdf2fd81c6fdba87742297b7c1
will <w.duffy@outlook.com>
2022-11-15 20:35:58 -0600

    T

--- 2-Module/4-week/2-day/scratch.js
+++ 2-Module/4-week/2-day/scratch.js
@@ -54,3 +54,71 @@ class LinkedList {
 
 
 module.exports = LinkedList;
+
+
+class DoublyLinkedListNode {
+  constructor(val) {
+    this.value = val;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    // Your code here
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  addToHead(val) {
+    // Your code here
+    let newNode = new DoublyLinkedListNode(val)
+
+    if(this.length >= 1){
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode
+    } else {
+      this.head = newNode
+      this.tail = newNode
+    }
+    this.length++
+  }
+
+  addToTail(val) {
+    // Your code here
+    let newNode = new DoublyLinkedListNode(val)
+    if(this.length >= 1){
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode
+    } else{
+      this.head = newNode
+      this.tail = newNode
+    }
+    this.length += 1
+  }
+
+  // You can use this function to help debug
+  print() {
+    let current = this.head;
+
+    while (current) {
+      process.stdout.write(`${current.value} <-> `);
+      current = current.next;
+    }
+
+    console.log("NULL");
+  }
+}
+doublyLinkedList = new DoublyLinkedList();
+
+doublyLinkedList.addToHead(1);
+doublyLinkedList.addToHead(2);
+doublyLinkedList.addToHead(3);
+doublyLinkedList.print()
+
+module.exports = DoublyLinkedList;
+
