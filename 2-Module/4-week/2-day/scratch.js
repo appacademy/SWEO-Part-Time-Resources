f0dfacef150c3445c0e7b9b04f75757f901224f7
will <w.duffy@outlook.com>
2022-11-15 18:59:07 -0600

    T

--- 2-Module/4-week/2-day/scratch.js
+++ 2-Module/4-week/2-day/scratch.js
@@ -1,21 +1,56 @@
-class Queue {
-  constructor() {
-    this.store = new Array()
-  }
-
-  size() {
-    // returns the length of the queue
-  }
-
-  enqueue(ele) {
-    //adds ele to end of queue
-  }
-
-  dequeue() {
-    //remove the first element
-  }
-
-  peek() {
-    //return the first element
+class LinkedListNode {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
   }
 }
+
+class LinkedList {
+  constructor() {
+    // Your code here
+    this.head = null;
+    this.length = 0;
+  }
+
+  addToHead(val) {
+    // Your code here
+    const newNode = new LinkedListNode(val);
+    newNode.next = this.head
+    this.head = newNode
+
+    this.length++
+  }
+
+  addToTail(val) {
+    // Your code here
+    let newNode = new LinkedListNode(val)
+    this.length++;
+
+    if(!this.head){
+      this.head = newNode;
+      return this
+    }
+    let curr = this.head
+    while(curr.next){
+      curr = curr.next
+    }
+    curr.next = newNode
+
+  }
+
+  // You can use this function to help debug
+  print() {
+    let current = this.head;
+
+    while (current) {
+      process.stdout.write(`${current.value} -> `);
+      current = current.next;
+    }
+
+    console.log("NULL");
+  }
+}
+
+
+
+module.exports = LinkedList;
