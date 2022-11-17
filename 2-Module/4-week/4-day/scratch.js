99781b1d97c865c382d30f0de16cfc18b559df4d
will <w.duffy@outlook.com>
2022-11-17 16:57:06 -0600

    T

--- 2-Module/4-week/4-day/scratch.js
+++ 2-Module/4-week/4-day/scratch.js
@@ -1,9 +1,44 @@
-const hashIt = (word) => {
-  return word
-    .split('')
-    .reduce((res, char) => Number(char.charCodeAt().toString(2)) + res, 0);
+function simpleHash(str) {
+  let hashValue = 0;
+
+  for (let i = 0 ; i < str.length ; i++) {
+    hashValue += str.charCodeAt(i);
+  }
+
+  return hashValue;
+}
+// console.log(simpleHash('listen'));
+// console.log(simpleHash('brooooo'));
+// console.log(simpleHash('whattttt'));
+// console.log(simpleHash('whattttt'));
+// console.log(simpleHash('whattttt'));
+
+
+
+const isUnique = (arr) => {
+let set = new Set(arr);
+return set.size === arr.length;
 };
 
-console.log(hashIt('listen'));
-console.log(hashIt('brooooo'));
-console.log(hashIt('silent'));
+// console.log(isUnique([1, 2, 3])); // t or f
+// console.log(isUnique([1, 2, 3, 1])); ///t or f
+
+const mySet = new Set()
+let thing = {name: 'Will'}
+
+mySet.add(1)
+mySet.add(2)
+mySet.add(3)
+mySet.add(1)
+mySet.add(thing)
+mySet.add([1,2,3,4,5,6,7,8,9])
+mySet.add([1,2,3,4,5])
+
+
+// console.log(mySet, 'before deletion')
+// mySet.delete(thing)
+// console.log(thing, 'line 40')
+// console.log(mySet, 'afeter deletion')
+for(let item of mySet){
+console.log("Iteration", item)
+}
