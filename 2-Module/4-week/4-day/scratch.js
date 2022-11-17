23d22b1fe5f9489bcca5739d04b52db8068c4d50
will <w.duffy@outlook.com>
2022-11-17 17:28:31 -0600

    T

--- 2-Module/4-week/4-day/scratch.js
+++ 2-Module/4-week/4-day/scratch.js
@@ -1,44 +1,51 @@
-  function simpleHash(str) {
-    let hashValue = 0;
+//   function simpleHash(str) {
+//     let hashValue = 0;
 
-    for (let i = 0 ; i < str.length ; i++) {
-      hashValue += str.charCodeAt(i);
-    }
+//     for (let i = 0 ; i < str.length ; i++) {
+//       hashValue += str.charCodeAt(i);
+//     }
 
-    return hashValue;
-  }
+//     return hashValue;
+//   }
 // console.log(simpleHash('listen'));
 // console.log(simpleHash('brooooo'));
 // console.log(simpleHash('whattttt'));
 // console.log(simpleHash('whattttt'));
 // console.log(simpleHash('whattttt'));
 
+// let pojo = {a:1}
 
 
-const isUnique = (arr) => {
-  let set = new Set(arr);
-  return set.size === arr.length;
-};
+
+// const isUnique = (arr) => {
+//   let set = new Set(arr);
+//   return set.size === arr.length;
+// };
 
 // console.log(isUnique([1, 2, 3])); // t or f
 // console.log(isUnique([1, 2, 3, 1])); ///t or f
 
 const mySet = new Set()
 let thing = {name: 'Will'}
+let thing2 = thing
+
+
 
 mySet.add(1)
 mySet.add(2)
 mySet.add(3)
 mySet.add(1)
 mySet.add(thing)
-mySet.add([1,2,3,4,5,6,7,8,9])
+mySet.add(thing2)
+
 mySet.add([1,2,3,4,5])
+mySet.add([1,2,3,4,5])
+console.log(mySet, 'before deletion')
 
 
-// console.log(mySet, 'before deletion')
 // mySet.delete(thing)
 // console.log(thing, 'line 40')
-// console.log(mySet, 'afeter deletion')
-for(let item of mySet){
-  console.log("Iteration", item)
-}
+// console.log(mySet, 'after deletion')
+// for(let apple of mySet){
+//   console.log("Iteration", apple)
+// }
