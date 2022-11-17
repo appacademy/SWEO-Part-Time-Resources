65a892998a698fa6f3d38fcfb8a86eff37c47862
tristan <thuckabee.work@gmail.com>
2022-11-17 17:15:50 -0600

    day 4 lecture

---
+++ 1-Module/2-week/4-day/scratch.js
@@ -0,0 +1,8 @@
+// type of variables
+// typeof
+let test = null;
+
+console.log( typeof test );
+// null v undefined;
+// null is the absence of a value
+// undefined is the value of an unassigned var
