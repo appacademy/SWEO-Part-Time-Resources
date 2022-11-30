b5afbf4056e9279d344fc64044d5a4f0bb511fad
Jacob North <yakenorth@protonmail.com>
2022-11-30 01:04:29 -0500

    5-mod week-1 day-1 practices

---
+++ 5-Module/1-week/1-day/props/src/BaseStats.js
@@ -0,0 +1,35 @@
+import './BaseStats.css';
+
+
+const BaseStats = ({ stats, clicker }) => {
+    const { hp, attack, defense, speed } = stats;
+
+    return (
+		<div className="base-stats">
+			<h1>BaseStats</h1>
+            <button className='sp-stats' onClick={clicker} >Check Special Stats</button>
+			<table>
+				<tbody>
+					<tr>
+						<td>Hit Points</td>
+						<td>{hp}</td>
+					</tr>
+					<tr>
+						<td>Attack</td>
+						<td>{attack}</td>
+					</tr>
+					<tr>
+						<td>Defense</td>
+						<td>{defense}</td>
+					</tr>
+					<tr>
+						<td>Speed</td>
+						<td>{speed}</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	);
+}
+
+export default BaseStats;
