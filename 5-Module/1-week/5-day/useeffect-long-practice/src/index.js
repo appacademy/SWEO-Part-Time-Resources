947f039bea37a0ae80b0affb78a94345efb27f69
Jacob North <yakenorth@protonmail.com>
2022-12-03 19:26:38 -0500

    mod-5 week-1 day-5 long practice solutions

---
+++ 5-Module/1-week/5-day/useeffect-long-practice/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+// To run the bonus files, comment out the preceding line and uncomment the 
+// following line.
+// import App from './bonus/App'; 
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
