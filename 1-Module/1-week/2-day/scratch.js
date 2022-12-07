bf39466b5bafecb3a59f5e6c2c83ce1cc3ff983c
“Alexi <alexi56@comcast.net>
2022-12-06 22:51:22 -0700

    tuesday instruction

---
+++ 1-Module/1-week/2-day/scratch.js
@@ -0,0 +1,39 @@
+/* console.log()
+single line //
+
+multi line /* */
+
+/* 
+String: "akjsdfkjn",
+Number: 23423423423,
+Boolean: true or false,
+Undefined: undefined
+
+let boxOfCats = true;
+
+let samTheCat = "A cat named sam";
+
+boxOfCats = samTheCat
+
+let samTheCat = "I'm a cat";
+
+const boxOfCats = samTheCat;
+
+boxOfCats = "sam"
+samTheCat = "I'm a dog";
+*/
+
+//Comparison Operators
+
+/*
+Greater than: >
+Less than <
+Greater than or equal to: >=
+Less than or equal to: <=
+Equal to ==
+Strictly Equal to ===
+Not Equal to !==
+
+*/
+
+
