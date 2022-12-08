861b2e66d82f3ccc306ec0d02273f885c928a489
“Alexi <alexi56@comcast.net>
2022-12-07 22:46:30 -0700

    Wednesday Dec 7

---
+++ 1-Module/1-week/3-day/scratch.js
@@ -0,0 +1,48 @@
+//STRINGS
+
+//You can put ANY characters into strings
+
+//console.log('Shakespeare said, "whats up"');
+
+//ConCATenation
+
+//fancy word for putting two strings together
+
+//console.log("howdy " + "ralph");
+
+//VARIABLES
+
+//Variables are used to store information
+
+/*
+let numOfCats = 43;
+
+console.log(numOfCats);
+
+numOfCats = 12;
+
+console.log(numOfCats);
+*/
+
+/*
+let num = 10;
+
+num = num + 10;
+
+num += 10
+num -= 10
+num *= 10
+num /= 10
+
+*/
+
+//LOGICAL OPERATORS
+
+// !(NOT)     && (AND)   || (OR)
+
+//console.log(!false);
+
+//console.log(3 > 2 && 4 > 2);
+
+//console.log(3 > 2 || 1 > 2);
+
