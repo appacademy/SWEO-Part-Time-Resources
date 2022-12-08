05261dcfe712931bd7cc1e034fc61e6f2165edae
“Alexi <alexi56@comcast.net>
2022-12-07 18:56:40 -0700

    Wednesday's Lecture

---
+++ 1-Module/1-week/3-day/functions.js
@@ -0,0 +1,10 @@
+//FUNCTIONS
+//A function is a REUSABLE block of code. DRY - Don't Repeat Yourself
+
+/*
+INSTEAD OF :
+console.log(3 + 2)
+console.log(4 + 5)
+console.log(3 + 2)
+console.log(5 + 9)
+*/
---
+++ 1-Module/1-week/3-day/scratch.js
@@ -0,0 +1,96 @@
+//Single line comment
+//console.log("hello world");
+
+/*
+console.log("Multi line comment")
+*/
+
+//NUMBER DATA TYPE
+
+//EX: -7, 450, 4.4532
+
+//ARITHMETIC OPERATORS
+
+// + - * /   %
+
+//console.log(10 % 3);
+
+//console.log(3 + 2);
+
+//console.log((2 + 3) * 5);
+
+//STRING DATA TYPE
+
+//-String are always wrapped in single or double quotes.
+//You can put ANY characters inside of strings
+
+// GOOD : console.log('Shakespeare said, "Lets boogie on the dance floor".');
+// BAD : console.log("'"');
+
+//CONCATENATION
+//Fancy word for putting to strings together!
+
+//console.log("Howdy " + "Ralph");
+
+//BOOLEAN DATA TYPE
+
+//TRUE OR FALSE
+
+//LOGICAL OPERATORS
+
+// ! 
+
+//console.log(!true);
+
+// &&-AND   ||-OR
+
+//console.log(3 > 2 && 1 > 2);
+
+//console.log(3 > 2 || 1 > 2);
+
+//COMPARISON OPERATORS
+
+// > , < , >= , <= , EQUALS : == , STRICTLY EQUALS : ===
+
+//console.log(3 > 2);
+//console.log(1 < 2);
+//console.log(2 >= 2);
+//console.log(2 <= 2);
+
+//console.log(3 == '3');
+//console.log(3 === '3');
+
+//VARIABLES
+
+//USED TO STORE INFORMATION
+
+/*
+let numberOfCats = 43;
+let nameOfCat = "Greg";
+let noValue;
+
+//console.log(numberOfCats);
+console.log(noValue);
+
+noValue = "Here is a value";
+
+console.log(noValue);
+*/
+
+/*
+let num = 10;
+
+console.log(num);
+
+//num = num + 10;
+num += 10;
+
+console.log(num);
+
+*/
+
+//NaN
+
+let num;
+
+console.log(num + 3);
