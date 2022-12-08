0403d76eb84ddf7bbfdc156bbc1c6de93026b2b2
Zaviar Brown <brown.zaviar@gmail.com>
2022-12-07 19:59:38 -0800

    Order up login/logout

--- 6-Module/4-week/3-day/Order_up/app/models.py
+++ 6-Module/4-week/3-day/Order_up/app/models.py
@@ -7,13 +7,12 @@ db = SQLAlchemy()
 
 class Employee(db.Model, UserMixin):
     __tablename__ = 'employees'
-
+    
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    employee_number = db.Column(
-        db.Integer, nullable=False, unique=True)
+    employee_number = db.Column(db.Integer, nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
-
+    
     @property
     def password(self):
         return self.hashed_password
@@ -24,4 +23,3 @@ class Employee(db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
-        # return check_password_hash(self.hashed_password, password)
