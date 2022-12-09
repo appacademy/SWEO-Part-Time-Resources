0159bb4664067f7e7f9d70fd174177a957a32f03
will <w.duffy@outlook.com>
2022-12-08 19:52:03 -0600

    t

---
+++ 2-Module/6-week/3-day/scratch.js
@@ -0,0 +1,12 @@
+const matrix = [
+    [ 0, 1, 0, 0, 1 ],
+    [ 1, 0, 0, 0, 1 ],
+    [ 1, 1, 0, 1, 1 ],
+    [ 0, 1, 1, 0, 0 ],
+    [ 0, 0, 0, 0, 0 ]
+];
+
+let row = 1;
+let col = 0;
+
+console.log(matrix[row][col])
