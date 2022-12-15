5b523463d8a386cac5222e6ecac5167527db622a
James Thompson <thompsonjames0120@gmail.com>
2022-12-14 20:26:36 -0600

    added solutions

---
+++ 4-Module/3-week/3-day/create-migration-short/server/db/migrations/20221214235512-create-color.js
@@ -0,0 +1,28 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     */
+    await queryInterface.createTable("Colors", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: Sequelize.STRING,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     */
+    await queryInterface.dropTable('Colors');
+  },
+};
