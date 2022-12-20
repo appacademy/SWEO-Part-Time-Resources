24c0c6b8de244c2a5719a4bee8750323534d3e71
Jesse <brooks.jesse83@gmail.com>
2022-12-19 21:36:16 -0600

    updated readme

---
+++ 6-Module/2-week/1-day/scratch.py
@@ -0,0 +1,17 @@
+toys = [
+    { 
+        "on_sale": False,
+        "price": 5,
+        "name": 'Toy Truck'
+    },
+    { 
+        "on_sale": False,
+        "price": 3,
+        "name": 'Tech Deck'
+    },
+    {
+        "on_sale": True,
+        "price": 101,
+        "name": 'Drone'
+    }
+]
