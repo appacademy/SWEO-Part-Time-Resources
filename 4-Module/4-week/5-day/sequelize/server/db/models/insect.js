7cbdc6f1709c20b65721ceb41e6f070cfeb5892f
James Thompson <thompsonjames0120@gmail.com>
2022-12-22 22:04:12 -0600

    added PA solution

---
+++ 4-Module/4-week/5-day/sequelize/server/db/models/insect.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Insect extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Insect.init({
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    fact: DataTypes.STRING,
+    territory: DataTypes.STRING,
+    millimeters: {
+      type: DataTypes.FLOAT,
+      validate: {
+        min: 0,
+      },
+    }
+  }, {
+    sequelize,
+    modelName: 'Insect',
+  });
+  return Insect;
+};
