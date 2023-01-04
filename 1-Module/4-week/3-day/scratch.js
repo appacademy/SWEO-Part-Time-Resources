6793e4671f83a9d0544e62efc28d674faad50aeb
“Alexi <alexi56@comcast.net>
2023-01-04 16:27:35 -0700

    Jan 4

---
+++ 1-Module/4-week/3-day/scratch.js
@@ -0,0 +1,12 @@
+//TERNARY OPERATORS
+//Used in place of if else statements
+
+//SYNTAX
+
+// condition ? expression (do this if true) : expression (do this if false)
+
+let fruit = "apple";
+
+//fruit === "apple" ? console.log("its an apple") : console.log("idk");
+
+fruit === "apple" ? console.log("apple") : fruit === "pear" ? console.log("pear") : console.log("idk");
