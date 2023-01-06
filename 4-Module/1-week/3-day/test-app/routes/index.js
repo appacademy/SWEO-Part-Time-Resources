08eeed3101d8ceac105d96019a6d2f8d560b625a
Watts-Blake <watts.blake52@gmail.com>
2023-01-05 19:31:17 -0800

    finished lecture project, fixed readme typo

---
+++ 4-Module/1-week/3-day/test-app/routes/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  return res.status(200).send("Welcome to our test application!");
+});
+
+module.exports = router;
