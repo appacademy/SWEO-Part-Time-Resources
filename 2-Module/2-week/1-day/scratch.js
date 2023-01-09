17cd5a15031b4feba66e71ad12eb480353c833e6
tristan <thuckabee.work@gmail.com>
2023-01-09 16:28:01 -0600

    layout

--- 2-Module/2-week/1-day/scratch.js
+++ 2-Module/2-week/1-day/scratch.js
@@ -1,22 +1,5 @@
-// class Test {
-//   testFunc() {
-//     debugger;
-//     console.log(this)
-//     console.log(`this is equal to test1: ${this === test1}`);
-//   }
-// }
+//* Context
 
-// const test1 = new Test();
-// Calling the method on the instance
-test1.testFunc(); // true
-// Calling the method outside the class
-const test1Func = test1.testFunc; 
-test1Func(); // false
-// Calling the method as a callback
-setTimeout(test1.testFunc, 1000); // false
+//* Context when using normal functions
 
-// Using this in Function Declaration Syntax
-function someFunc() {
-  console.log(this);
-}
-someFunc(); // Global Object
+//* Other Cases of Context
