9820ed922d018fe18e82168b9c971481e4cec473
tristan <thuckabee.work@gmail.com>
2023-01-09 21:46:40 -0600

    west

--- 2-Module/2-week/1-day/scratch.js
+++ 2-Module/2-week/1-day/scratch.js
@@ -1,22 +1,72 @@
-// class Test {
-//   testFunc() {
-//     debugger;
-//     console.log(this)
-//     console.log(`this is equal to test1: ${this === test1}`);
+// "use strict";
+
+//* Context - refers to 'this' when code executed
+ // this is the object invoking the function / method
+
+class Dog {
+  constructor( name, isSitting) {
+    this.name = name;
+    this.isSitting = isSitting;
+  }
+
+  stand() {
+    this.isSitting = false;
+    return this.isSitting;
+  }
+  changeName() {
+    this.name = 'Toad';
+  }
+}
+
+let bowser = new Dog('Bowser', true);
+
+bowser.stand(); //Method-Style Invocation
+console.log( bowser );
+
+class Cat {
+  purr() {
+    console.log('prrrrrrrrrrrr');
+  }
+  purrMore() {
+    this.purr();
+  }
+}
+
+let cat = new Cat()
+
+// cat.purrMore();
+// cat.purr();
+
+//* Context in general Functions
+function testMe() {
+  console.log(this);
+}
+// testMe();
+
+// class Dog {
+//   constructor( name, isSitting) {
+//     this.name = name;
+//     this.isSitting = isSitting;
+//   }
+
+//   stand() {
+//     this.isSitting = false;
+//     return this.isSitting;
+//   }
+//   changeName() {
+//     this.name = 'Toad';
 //   }
 // }
 
-// const test1 = new Test();
-// Calling the method on the instance
-test1.testFunc(); // true
-// Calling the method outside the class
-const test1Func = test1.testFunc; 
-test1Func(); // false
-// Calling the method as a callback
-setTimeout(test1.testFunc, 1000); // false
+let change = bowser.changeName;
+// change();
+// bowser.changeName();
 
-// Using this in Function Declaration Syntax
-function someFunc() {
+// purrMore();
+// global.setTimeout(cat.purrMore, 5000);
+
+//* Use Strict
+function showGlobal() {
   console.log(this);
 }
-someFunc(); // Global Object
+showGlobal();
