12ac3e7b93a648aecb36f9e88bbff31ceaa88fd8
will <w.duffy@outlook.com>
2023-01-10 20:02:03 -0600

    update

--- 3-Module/4-week/2-day/scratch.js
+++ 3-Module/4-week/2-day/scratch.js
@@ -165,3 +165,30 @@ export function deleteDog(id) {
 }
 
   //THIRD PRACTICE~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/* =============== 1. Print the status code of the response =============== */
+
+// Your code here
+
+fetch("/products")
+    .then(res => console.log(res.status))
+
+/* ====== 2. Print true if the status of the response was successful ====== */
+
+// Your code here
+fetch("/products")
+    .then(res => console.log(res.ok))
+
+
+/* =================== 3. Print the Content-Type Header =================== */
+
+// Your code here
+
+fetch("/products")
+    .then(res => console.log(res.headers.get("Content-Type")))
+
+/* ============== 4. Print the body of the response as text =============== */
+
+// Your code here
+fetch("/products")
+    .then(res => res.text())
+    .then(resBody => console.log(resBody))
