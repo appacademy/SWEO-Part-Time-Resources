bd8fef73c399f84011bb7c7d7544ee8ee6488454
J0914 <jordynsechrist@gmail.com>
2023-01-11 20:04:01 -0600

    update w1d3

---
+++ 1-Module/1-week/3-day/scratch.js
@@ -0,0 +1,75 @@
+// Booleans and Comparison Operators
+
+// let a = true;
+// let b = false;
+
+// console.log(a);
+// console.log(!a);
+// console.log(a && b);
+// console.log(a && !b);
+// console.log(a || b);
+
+// let c = 5;
+// let d = 10;
+// let e = '5';
+
+// console.log(c < d);
+// console.log(c <= d);
+// console.log(c <= 5);
+// console.log(c == e);
+// console.log(c === e);
+// console.log(c !== d);
+
+// Variables
+
+// let dog = 'Fido';
+// console.log(dog);
+
+// let x = 1
+// let y = 2
+
+// console.log(x);
+
+// x + y
+// console.log(x, y);
+
+// x += y
+// console.log(x, y)
+
+// let z;
+// z = 'zebra'
+
+// console.log(z);
+
+// Strings
+
+// let cat = 'fluffy !'
+
+// let contraction = 'can\'t'
+// contraction = "can't"
+
+// console.log(contraction);
+
+// console.log(cat.length)
+
+// console.log(cat.indexOf('f'))
+
+// let first = '123';
+// let second = '456';
+
+// console.log(first + second)
+
+// Functions
+
+function addTwo (num) {
+    // take the num and increment once
+    // add 7 to the num and reassign its value
+    // console.log num plus 2
+    num++
+    num += 7
+    console.log(num + 2);
+};
+
+// addTwo(4)
+console.log(addTwo(4));
+
