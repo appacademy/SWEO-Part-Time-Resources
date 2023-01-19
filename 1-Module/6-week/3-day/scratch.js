e87d1208fa2593e145e578f3d60b738f4a383aea
“Alexi <alexi56@comcast.net>
2023-01-18 20:49:02 -0700

    Jan 18

---
+++ 1-Module/6-week/3-day/scratch.js
@@ -0,0 +1,43 @@
+//SWITCH STATEMENTS
+//No longer commonly used.
+//Used in place of if else statements and they perform different actions on different conditions
+
+//SYNTAX
+// switch(expression) {
+//     case compare value to case:
+//         //code
+//         break;
+//     case 2:
+//         //code
+//         break;
+//     default:
+//         //default code
+// }
+
+
+// let day = "Tuesday";
+
+// switch(day) {
+//     case "Tuesday":
+//         console.log("It's tuesday");
+//         break;
+//     case "Wednesday":
+//         console.log("It's wednesday");
+//         break;
+//     default:
+//         console.log("Idk bruv")
+// }
+
+
+// let x = 5;
+
+// switch(x + 10) {
+//     case 14:
+//         console.log("OGABOOGA");
+//         break;
+//     case 15:
+//         console.log("Yeayuh");
+//         break;
+//     default:
+//         console.log("Brother")
+// }
