4f482ec319d99c7fb293b05c088e559f35c3d5ca
“Alexi <alexi56@comcast.net>
2023-01-18 20:49:26 -0700

    Jan 18

---
+++ 1-Module/6-week/3-day/scratch.js
@@ -0,0 +1,44 @@
+//SWITCH STATEMENTS (SWITCH CASES - SWITCH CASE STATEMENTS)
+//Used to be used in place of an if statement
+
+//SYNTAX
+
+// switch(expression) {
+//     case x = 7:
+//         //Code
+//         break;
+//     case x = 5:
+//         console.log(x);
+//         break;
+//     default:
+//         console.log("Didn't match a case");
+// }
+
+//The switch expression is evaluated one time
+//The value of the expression is compared with the values of each case
+//If the value matches one of the cases, it will execute that code
+//If there is no match, the default code is run
+//The BREAK statements stop the switch from continuing to check for matches
+
+// let day = "Tuesday";
+
+// switch(day) {
+//     case day = "Tuesday":
+//         console.log("It's tuesday!");
+//         break;
+//     case day = "Wednesday":
+//         console.log("today is wednesday");
+//         break;
+//     default:
+//         console.log("Idk what day it is");
+// }
+
+// let x = 5;
+
+// switch(x + 5) {
+//     case x = 10:
+//         console.log("Its ten");
+//         break;
+//     default:
+//         console.log("IDK");
+// }
