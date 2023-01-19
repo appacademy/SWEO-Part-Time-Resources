5d29d72c162596aaf42b0342505672c182f42141
tristan <thuckabee.work@gmail.com>
2023-01-18 22:18:50 -0600

    west lecture

--- 2-Module/3-week/3-day/scratch.js
+++ 2-Module/3-week/3-day/scratch.js
@@ -1,58 +1,110 @@
-const binary = '0b11001010'
+//* Timing Benchmarks
+console.time('lecture1') //start timer
+for (let i = 0; i < 10; i++) {
+  console.timeLog('lecture1') // print current time elapsed
+}
+console.time('lecture2');
+console.timeEnd('lecture1') //stops timer;
+console.timeEnd('lecture2');
 
-const binaryToDecimal = (binaryStr) => {
-  // Remove the "Ob" prepended to the str
-  const rawBinary = binaryStr.substring(2)
-  // Reverse the string for the next step
-  const reversedBinary = rawBinary.split('').reverse().join('')
+let startTime = Date.now();
+for (let i = 0; i < 10; i++) {
+  console.log('.')
+}
+let endTime = Date.now();
+console.log(endTime - startTime)
+//* Time and Space Complexity
+//O(?)
+//Constant -> stays the same
+//Linear -> Grows at a fixed rate
+//Quadratic -> exponentially grows
 
-  let sum = 0
-
-  for (let i = 0; i < reversedBinary.length; i++) {
-    // Convert string to integer
-    const currentInt = parseInt(reversedBinary[i])
-    const num = Math.pow(2, i) * currentInt
-    sum += num
+// Time Complexity
+let constant = (num1, num2) => { //O(1)
+  return num1 + num2;
+}
+let linear = (num) => { //O(n)
+  let res = [];
+  for (let i = 0; i < num; i++) {
+    res.push(i);
   }
-
-  return sum
+  for (let i = 0; i < num; i++) {
+    res.push(i);
+  }
+  return res;
 }
-
-console.log(binaryToDecimal(binary))
-
-const hexChars = {
-  A: 10,
-  B: 11,
-  C: 12,
-  D: 13,
-  E: 14,
-  F: 15,
-}
-
-const hex1 = '0xF23C'
-
-const hexToDecimal = (hexStr) => {
-  // Remove the Ox
-  const rawHex = hexStr.substring(2)
-  // Reverse remaining hex
-  const reverseHex = rawHex.split('').reverse().join('')
-
-  let sum = 0
-
-  for (let i = 0; i < reverseHex.length; i++) {
-    const currentChar = reverseHex[i]
-    if (hexChars[currentChar]) {
-      // If it is a char use the value from the hexChars object
-      let num = Math.pow(16, i) * hexChars[currentChar]
-      sum += num
-    } else {
-      // If it is a number we will convert the currentChar to a string
-      let num = Math.pow(16, i) * parseInt(currentChar)
-      sum += num
+let quadratic = (num) => { //O(n^2)
+  let res = [];
+  for (let i = 0; i < num; i++) { //n
+    for (let j = 0; j < num; j++) { //n
+      res.push([i, j]);
     }
   }
 
-  return sum
+  return res;
+}
+// Space Complexity
+function addNums(n) { //O(n), O(1)
+  let total = 0;
+
+  for (let i = 1; i <= n; i++) {
+    total += i;
+  }
+
+  return total;
+}
+function getNumList(n) { //O(n), O(n)
+  let nums = [];
+
+  for (let i = 0; i < n; i++) {
+    nums.push(i);
+  }
+
+  return nums;
+}
+//1+1+n => n+1 => n
+function getNumPairsList(n) { // O(n^2), O(n^2)
+  let pairs = [];
+
+  for (let i = 0; i < n; i++) { //n times
+    for (let j = 0; j < n; j++) {// pushing n things into pairs
+      pairs.push([i, j]);
+    }
+  }
+
+  return pairs;
+}
+// In Place Operations
+
+//Out of Place
+function increaseByOne(nums) { // O(n), O(n)
+  const increased = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    increased.push(nums[i] + 1);
+  }
+
+  return increased;
+}
+nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(increaseByOne(nums));
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(nums);
+// [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// In Place
+function increaseByOneInPlace(nums) { // O(n), O(1)
+  for (let i = 0; i < nums.length; i++) {
+    nums[i]++;
+  }
+
+  return nums;
 }
 
-console.log(hexToDecimal(hex1))
+nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(increaseByOneInPlace(nums));
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(nums);
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
