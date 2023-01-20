a90f9b1575fded8e8b473031f34e29eb58f4d935
Philip Ling <66927690+philling83@users.noreply.github.com>
2023-01-20 00:14:43 -0500

    Add solutions

---
+++ 5-Module/3-week/4-day/solutions/practice-for-week-15-react-redux-useDispatch-useSelector/src/App.js
@@ -0,0 +1,12 @@
+import ArticleList from './components/ArticleList';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <ArticleList />
+    </div>
+  );
+}
+
+export default App;
