24c2f0fa5188597b5da1e5c929a0403d44b37171
will <w.duffy@outlook.com>
2023-01-23 19:29:59 -0600

    T

---
+++ 3-Module/6-week/1-day/events-with-forms/index.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', (e) => {
+  const modal = document.getElementById('modal');
+  const modalContent = document.getElementById('modal-content');
+
+  document.getElementById('click-me').addEventListener('click', (e) => {
+    modal.style.display = 'flex';
+    modalContent.style.display = 'flex';
+    modalContent.innerHTML = `
+    <h2>Subscribe to our newsletter</h2>
+    <form id="my-form" action="/users" method="POST">
+        <label for="email">Email</label>
+        <input type="text" name="email">
+        <button>Submit</button>
+    </form>
+    `;
+
+    const form = document.getElementById('my-form');
+    form.addEventListener('submit', (e) => {
+      const inputVal = document.querySelector('input').value;
+      if (
+        !inputVal.includes('@') &&
+        (!inputVal.includes('.com') || !inputVal.includes('.io'))
+      ) {
+        e.preventDefault();
+        alert('Invalid email');
+      }
+    });
+  });
+
+  modal.addEventListener('click', (e) => {
+    modal.style.display = 'none';
+  });
+
+  modalContent.addEventListener('click', (e) => {
+    e.stopPropagation();
+  });
+
+
+});
