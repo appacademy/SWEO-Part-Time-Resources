8325449c2c69e7a36dc6a2bf35248b3fa459ffef
will <w.duffy@outlook.com>
2023-01-23 19:25:02 -0600

    T

---
+++ 3-Module/6-week/1-day/scratch.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', (e) => {
+  const blue = document.getElementById('outer');
+
+  blue.addEventListener('click', (e) => {
+    // console.log(e);
+    // if we click on the red square
+    console.log("BLUE TARGET:", e.target.className, e.target.id); // red
+    console.log("BLUR CURRTARGET:", e.currentTarget.className); // blue
+  });
+
+  const red = document.getElementById('inner');
+  red.addEventListener('click', (e) => {
+    // clicking on red no longer does anything
+    e.stopPropagation();
+
+     console.log("RED TARGET:", e.target.className, e.target.id); // red
+    console.log("RED CURRTARGET:", e.currentTarget.className); // blue
+
+  });
+
+  const check = document.getElementById('unclickable');
+  check.addEventListener('click', (e) => {
+    e.preventDefault();
+  });
+});
