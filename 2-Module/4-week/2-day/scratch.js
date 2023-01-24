8e343e7ac6c107d91291feb6d4a25bcebf347a02
tristan <thuckabee.work@gmail.com>
2023-01-24 16:44:34 -0600

    West Setup

--- 2-Module/4-week/2-day/scratch.js
+++ 2-Module/4-week/2-day/scratch.js
@@ -1,21 +1,24 @@
-class Queue {
-  constructor() {
-    this.store = new Array()
-  }
-
-  size() {
-    // returns the length of the queue
-  }
-
-  enqueue(ele) {
-    //adds ele to end of queue
-  }
-
-  dequeue() {
-    //remove the first element
-  }
-
-  peek() {
-    //return the first element
+class KeyValuePair {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
   }
 }
+// Hashing
+//SimpleHash
+
+//SHA256
+
+class HashTable {
+  constructor(size = 8) {
+    this.data = new Array(size).fill(null);
+  }
+  // Hash Modulo
+
+  // Inserting Key/Vals
+
+  // Reading Key/Vals
+}
+
+let hs = new HashTable();
+console.log(hs.data)
