ea37b7eee258b982b17ba336f2c8a4a6f428d696
Bill Adams <vikingbill@gmail.com>
2023-01-26 01:04:54 -0500

    end of w3d3

---
+++ 1-Module/3-week/3-day/lecture.js
@@ -0,0 +1,25 @@
+// // OBJECTS!!
+
+// let arr = ["an", "array", "with", "stuff", "in","it"]
+
+// let mango = 2;
+// let method = "log"
+// console.table(arr)
+// console[method](arr[mango])
+// console.log(arr instanceof Object)
+
+// let obj = {a:"a", b:22,c:"a string!", d:arr}
+
+// console.table(obj)
+// console.log(obj)
+
+let wayne = {
+    name: "Wayne",
+    hasTail: false,
+    isChonky: true,
+    fur: "black"
+}
+wayne.disposition = "cuddly and curious"
+wayne["noisy"] = false
+console.log(wayne)
+// console.log(arr[arr.length] = "newValue")
