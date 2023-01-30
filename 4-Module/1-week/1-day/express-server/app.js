6b807e397b6982487a1b8239980bfdd273d1f80c
will <w.duffy@outlook.com>
2023-01-30 17:13:23 -0600

    update

---
+++ 4-Module/1-week/1-day/express-server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  // res.send('WOw how express');
+  res.json({ msg: "Look at me I'm so cool" });
+});
+
+const port = 5000;
+
+app.listen(port, () => console.log('Heeyyooooooo', port));
