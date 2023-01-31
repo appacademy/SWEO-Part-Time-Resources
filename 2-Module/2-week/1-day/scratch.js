b77afbd6c681ceac24aa33d7388b65c79181fbeb
“Alexi <alexi56@comcast.net>
2023-01-30 21:24:41 -0700

    Jan 30

--- 2-Module/2-week/1-day/scratch.js
+++ 2-Module/2-week/1-day/scratch.js
@@ -1,22 +1,83 @@
-// class Test {
-//   testFunc() {
-//     debugger;
-//     console.log(this)
-//     console.log(`this is equal to test1: ${this === test1}`);
+
+//Context
+//Scope and Context are DIFFERENT THINGS
+
+//Scope - refers to the visibility and availablity of variables
+
+//Context - refers to value of the THIS keyword
+//Another definition - "this" refers to the object that is executing the current function/method
+
+//ALL functions have a scope AND a context
+
+//If we do not assign a context to an object, the context will be the global object
+
+//Strict mode - the string 'use strict' protects the global from being accessed or manipulated
+
+// function test() {
+//   console.log(this);
+// }
+
+// test();
+
+
+// class Dog {
+//   constructor(name) {
+//     this.name = name;
+//   }
+
+//   changeName() {
+//     this.name = "Marley";
 //   }
 // }
 
-// const test1 = new Test();
-// Calling the method on the instance
-test1.testFunc(); // true
-// Calling the method outside the class
-const test1Func = test1.testFunc; 
-test1Func(); // false
-// Calling the method as a callback
-setTimeout(test1.testFunc, 1000); // false
+// let haru = new Dog("Haru");
 
-// Using this in Function Declaration Syntax
-function someFunc() {
-  console.log(this);
+// let king = new Dog("Kingly");
+
+// let changeFunc = king.changeName;
+
+
+// console.log(changeFunc())
+
+
+// class Cat {
+//   purr() {
+//     console.log("Prrrrr");
+//   }
+
+//   purrMore() {
+//     this.purr();
+//   }
+// }
+
+// let king = new Cat();
+
+// king.purrMore()
+
+class Person {
+  constructor(firstName) {
+    this.firstName = firstName;
+  }
+
+  addProp() {
+    this.color = "purple"
+  }
 }
-someFunc(); // Global Object
+
+function printName() {
+  console.log(this.firstName)
+}
+
+const adrian = new Person('Adrian');
+
+adrian.addProp()
+
+console.log(adrian.color)
+
+
+
+function hello() {
+  console.log(`Hello ${this.firstName}`);
+}
+
+const greeting = new hello();
