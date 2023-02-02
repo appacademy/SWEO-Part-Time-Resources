f2020fb209adcd4e0c6f0da4aa14d8a6fbc516cd
James Thompson <thompsonjames0120@gmail.com>
2023-02-01 20:58:34 -0600

    added solutions

---
+++ 5-Module/1-week/3-day/react-template/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Hello from App</h1>
+  );
+}
+
+export default App;
---
+++ 5-Module/1-week/3-day/react-template/src/index.js
@@ -0,0 +1,12 @@
+  // ./src/index.js
+  import React from 'react';
+  import ReactDOM from 'react-dom';
+  import './index.css';
+  import App from './App';
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
