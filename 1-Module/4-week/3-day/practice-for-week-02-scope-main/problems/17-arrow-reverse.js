85bf5db3c733d6746da54fbcdf297eaec9dcd26d
AlexBetita <alexbheb25@gmail.com>
2023-02-01 18:57:34 -0800

    end of lecture

---
+++ 1-Module/4-week/3-day/practice-for-week-02-scope-main/problems/17-arrow-reverse.js
@@ -0,0 +1,24 @@
+/***********************************************************************
+Write a function arrowReverseString(str) that accepts a string and returns that string
+reversed.
+
+Write this function using a fat arrow function!
+
+Examples:
+let result1 = arrowReverseString("hello"); // returns "olleh"
+let result2 = arrowReverseString("garden"); // returns "nedrag"
+let result3 = arrowReverseString("potato"); // returns "otatop"
+
+
+***********************************************************************/
+
+// Your code here
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+
+try {
+  module.exports = arrowReverseString;
+} catch (e) {
+  // catch the ref err
+  module.exports = null;
+}
