de878cb7894ea90276498f20eeb004d6d4c62b33
yinyang117 <slcryan2010@yahoo.com>
2023-02-08 18:18:52 -0700

    polyas

--- 1-Module/1-week/3-day/polyas.js
+++ 1-Module/1-week/3-day/polyas.js
@@ -2,3 +2,10 @@
 Write a function called printer that intakes two strings
 and console.logs both strings on one line
 */
+
+function printer(string1, string2) {
+
+}
+
+let banana = "I'm making a program that can "
+let apple = "let me do all the things!"
