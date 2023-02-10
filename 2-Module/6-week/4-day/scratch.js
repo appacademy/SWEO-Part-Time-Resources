1d89dd157a12703be50cb32ae1b346c98fa85494
tristan <thuckabee.work@gmail.com>
2023-02-09 20:36:20 -0600

    lecture

--- 2-Module/6-week/4-day/scratch.js
+++ 2-Module/6-week/4-day/scratch.js
@@ -7,9 +7,25 @@ const matrix = [
 ];
 
 function getNeighbors(start, matrix) {
+  let neighbors = [];
+  
+  let [row, col] = start;
+  if (matrix[row][col - 1]) neighbors.push([row, col - 1]);
+  if (matrix[row][col + 1]) neighbors.push([row, col + 1]);
+  if (matrix[row - 1]) {
+    if (matrix[row - 1][col]) neighbors.push([row - 1, col]);
+    if (matrix[row - 1][col - 1]) neighbors.push([row - 1, col - 1]);
+    if (matrix[row - 1][col + 1]) neighbors.push([row - 1, col + 1]);
+  }
+  if (matrix[row + 1]) {
+    if (matrix[row + 1][col]) neighbors.push([row - 1, col]);
+    if (matrix[row + 1][col - 1]) neighbors.push([row - 1, col - 1]);
+    if (matrix[row + 1][col + 1]) neighbors.push([row - 1, col + 1]);
+  }
 
+  return neighbors;
 }
 
-getNeighbors([2,2], matrix) // returns [ [1,2], [3,2], [1,2], [3,2] ]
-getNeighbors([0,0], matrix) // returns [ [1,0], [0,1] ]
-getNeighbors([2,0], matrix) // returns [ [1,0], [3,0], [2,1] ]
+console.log(getNeighbors([2,2], matrix)) // returns [ [1,2], [3,2], [1,2], [3,2] ]
+console.log(getNeighbors([0,0], matrix)) // returns [ [1,0], [0,1] ]
+console.log(getNeighbors([2,0], matrix)) // returns [ [1,0], [3,0], [2,1] ]
