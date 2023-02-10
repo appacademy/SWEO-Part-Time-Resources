6452db360807ef5efe7bfc3444019ea6f315f001
“Alexi <alexi56@comcast.net>
2023-02-09 19:46:22 -0700

    Feb 9

---
+++ 2-Module/3-week/4-day/scratch.js
@@ -0,0 +1,26 @@
+//RAM
+//Random access memory
+//Stores processes that are currently running
+
+//When RAM runs out of memory storage to run programs
+//it pushes them into the hard drive and that is why your computer starts running slower
+
+//Arrays and Stacks
+
+//The most fundamental data structure
+//very elegant and efficient
+
+//Array indexing is the fastest way to access data
+//Arrays store their elements in memory next to each other, so there's no wasted space
+
+//Stacks
+//First in, Last out
+
+//PUSH
+//We always push items on top of the stack
+
+//POP
+//We always pop off the top
+
+//Usually under the hood we implement stacks with arrays
+
