693602385ca3ab7648bb2c036d9e3daffe75d386
tristan <thuckabee.work@gmail.com>
2023-02-09 21:29:38 -0600

    done

--- 2-Module/6-week/4-day/scratch.js
+++ 2-Module/6-week/4-day/scratch.js
@@ -7,7 +7,13 @@ const matrix = [
 ];
 
 function getNeighbors(start, matrix) {
-
+  let [row, col] = start;
+  
+  if (row > 0 && matrix[row - 1][col]) console.log('up',matrix[row - 1][col]);
+  if (row < matrix.length - 1 && matrix[row + 1][col]) console.log('down', matrix[row + 1][col]);
+  if (col > 0 && matrix[row][col - 1]) console.log('left', matrix[row][col - 1]);
+  if (col < matrix[row].length -1 && matrix[row][col + 1]) console.log('right', matrix[row][col + 1]);
+  console.log('------------')
 }
 
 getNeighbors([2,2], matrix) // returns [ [1,2], [3,2], [1,2], [3,2] ]
