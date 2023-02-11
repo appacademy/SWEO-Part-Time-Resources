043ad5e6fb2aa32332d8221357a42e395bf52a30
“Alexi <alexi56@comcast.net>
2023-02-11 11:59:47 -0700

    Feb 11

---
+++ 2-Module/3-week/4-day/scratch.js
@@ -0,0 +1,22 @@
+//RAM
+//Random access memory
+
+//IT IS A HARDWARE COMPONENT
+
+//Stores data that is currently in use
+//RAM is super fast but that makes it more expensive
+
+//If ram starts to fill up we start pushing processes into the hard drive
+
+//Hard drive
+//Slower than ram, less expesive, however the data can persist without power
+
+//Arrays and Stacks
+//The are the most fundamental data structure
+//Super elegant and very efficient
+
+//Space wise arrays are stored with every element
+//right next to eachother with no wasted space
+
+//Regarding time, array indexing is constant time so it's as fast as you can get
+
