ff0f11c5a627ad4c9d5301f649e04620a7213f07
Bill Adams <vikingbill@gmail.com>
2023-02-21 23:49:28 -0500

    end lecture

---
+++ 2-Module/1-week/2-day/dry/practice.js
@@ -0,0 +1,64 @@
+// const biggerNum = (num1, num2, operator) => eval(`${num1} ${operator} ${num2}`) ? num1 : num2;
+const biggerNum = (num1, num2, operator) => num1  > num2 ? num1 : num2;
+
+function multiplyBiggerNumByTwo(num1, num2) {
+  // let bigNum; 
+  // if (num1 > num2) {
+  //   bigNum = num1;
+  //   return bigNum * 2;
+  // } else {
+  //   bigNum = num2;
+  //   return bigNum * 2;
+  // }
+  return biggerNum(num1, num2) * 2
+  // return biggerNum(num1, num2, ">") * 2
+}
+
+function divideBiggerNumByThree(num1, num2) {
+  // let bigNum;
+  // if (num1 > num2) {
+  //   bigNum = num1;
+  //   return bigNum / 3;
+  // } else {
+  //   bigNum = num2;
+  //   return bigNum / 3;
+  // }
+  // return biggerNum(num1, num2, ">") / 3
+  return biggerNum(num1, num2) / 3
+}
+
+function eatMostTacos(sum1, sum2) {
+  // let bigNum;
+  // if (sum1 > sum2) {
+  //   bigNum = sum1;
+  //   return `I ate ${bigNum} tacos.`;
+  // } else {
+  //   bigNum = sum2;
+  //   return `I ate ${bigNum} tacos.`;
+  // }
+  // return `I ate ${biggerNum(sum1, sum2, ">")} tacos.`
+  return `I ate ${biggerNum(sum1, sum2,)} tacos.`
+}
+
+function adoptSmallerDog(weight1, weight2) {
+  // let smallDog;
+  // if (weight1 < weight2) {
+  //   smallDog = weight1;
+  //   return `I adopted a dog that weighs ${smallDog} pounds.`;
+  // } else {
+  //   smallDog = weight2;
+  //   return `I adopted a dog that weighs ${smallDog} pounds.`;
+  // }
+  return `I adopted a dog that weighs ${weight1 === biggerNum(weight1, weight2) ? weight2 : weight1} pounds.` 
+  // return `I adopted a dog that weighs ${biggerNum(weight1, weight2, "<")} pounds.` 
+}
+
+
+/**************************************************************************/
+/* DO NOT CHANGE THE CODE BELOW */
+module.exports = {
+  multiplyBiggerNumByTwo,
+  divideBiggerNumByThree,
+  eatMostTacos,
+  adoptSmallerDog
+};
---
+++ 2-Module/1-week/2-day/srp/practice.js
@@ -0,0 +1,82 @@
+/******************************** CONSTANTS *********************************/
+const applePieRecipe = [
+  { name: "pie crust", cost: 10.00, quantity: 1 },
+  { name: "sugar", cost: 3.00, quantity: 0.5 },
+  { name: "butter", cost: 1.00, quantity: 1 },
+  { name: "apples", cost: 6.00, quantity: 7 },
+  { name: "cinnamon", cost: 5.50, quantity: 1 },
+  { name: "eggs", cost: 2.00, quantity: 1 },
+];
+
+const pumpkinPieRecipe = [
+  { name: "pie crust", cost: 10.00, quantity: 1 },
+  { name: "sugar", cost: 3.00, quantity: 0.5 },
+  { name: "butter", cost: 1.00, quantity: 1 },
+  { name: "pumpkin", cost: 3.75, quantity: 2 },
+  { name: "cinnamon", cost: 5.50, quantity: 1 },
+  { name: "eggs", cost: 2.00, quantity: 1 },
+];
+
+const cherryPieRecipe = [
+  { name: "pie crust", cost: 10.00, quantity: 1 },
+  { name: "sugar", cost: 3.00, quantity: 0.5 },
+  { name: "butter", cost: 1.00, quantity: 1 },
+  { name: "cherries", cost: 12.00, quantity: 10 },
+  { name: "eggs", cost: 2.00, quantity: 1 },
+];
+
+const recipes = {
+  applePie: applePieRecipe,
+  pumpkinPie: pumpkinPieRecipe,
+  cherryPie: cherryPieRecipe
+};
+/* DO NOT CHANGE THE CODE ABOVE */
+
+/*************************** FUNCTION TO REFACTOR ****************************/
+function bakeThosePies(pieType, pieQuantity, recipe) {
+  for (let i = 0; i < pieQuantity; i++) {
+    // Print the ingredients for each ingredient in the recipe
+    let combiningMsg = `Combining ingredients for ${pieType}: `
+    combiningMsg += recipe.map(ingredient => ingredient.name).join(', ');
+    console.log(combiningMsg);
+
+    // Print the nth pie that was baked
+    console.log(`Baked pie ${i + 1}!`);
+  }
+}
+
+function determineRevenue(pieQuantity, recipe, profitMargin = 1.2) {
+  // Print the cost of each pie based on the cost of each ingredient
+  const costOfPie = recipe.reduce((prev, current) => {
+    return prev + current.cost;
+  }, recipe[0].cost);
+  console.log(`Cost per pie: ${costOfPie}`);
+
+  // Calculate the revenue of baking these pies
+  return costOfPie * pieQuantity * profitMargin;
+}
+
+function bakeAndSellPies(pieType, pieQuantity, profitMargin) {
+  // Find the recipe for the pieType specified
+  const recipe = recipes[pieType];
+
+  // BAKE EM!
+  bakeThosePies(pieType, pieQuantity, recipe);
+
+  // DETERMINE TOTAL REVENUE!
+  const revenue = determineRevenue(pieQuantity, recipe, profitMargin);
+
+  // PRINT THEM RESULTS
+  console.log(`Sold ${pieQuantity} pies for $${revenue.toFixed(2)}!`);
+}
+
+/******************************* LOCAL TESTS *******************************/
+// bakeAndSellPies("applePie", 5, 2.5);
+// bakeAndSellPies("pumpkinPie", 2);
+// bakeAndSellPies("cherryPie", 7, 1.7);
+
+/**************************************************************************/
+/* DO NOT CHANGE THE CODE BELOW */
+module.exports = {
+  bakeAndSellPies
+};
