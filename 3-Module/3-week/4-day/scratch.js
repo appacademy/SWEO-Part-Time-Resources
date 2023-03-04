00a1f3a4845bcfeee48f7581524b1fb373ed588e
tristan <thuckabee.work@gmail.com>
2023-03-04 10:59:52 -0600

    done

--- 3-Module/3-week/4-day/scratch.js
+++ 3-Module/3-week/4-day/scratch.js
@@ -1,9 +1,37 @@
 const http = require('http');
+const fs = require('fs');
 const {formatComments, parseBody} = require('./parseInput');
 
 const comments = [{'id': 1, 'content':'Nice'}, {'id': 2, 'content': 'Not Nice'}];
 const server = http.createServer((req, res) => {
+  if (req.method === 'POST' && req.url === '/') {
+    let reqBody = '';
+    req.on("data", data => {
+      reqBody += data;
+    });
+    req.on("end", () => {
+      // parse the body
+      let comment = parseBody(reqBody);
+      // add the new comment to our comments array
+      comments.push(comment);
+      // res for a redirect
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      res.end();
+    })
+  } else if (req.method === 'GET' && req.url === '/') {
+    const htmlDoc = fs.readFileSync('./index.html', 'utf-8');
+    let commentText = formatComments(comments);
+    let html = htmlDoc.replaceAll(/#{comments}/g, commentText);
   
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.end(html);
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Location could not be found...");
+  }
 });
 const port = 8080;
 server.listen(port, () => console.log(`Server is listening on port ${port}...`));
