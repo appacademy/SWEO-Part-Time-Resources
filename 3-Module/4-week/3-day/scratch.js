a8a6b9e53a2e66c27009df1cf79de784137fe187
tristan <thuckabee.work@gmail.com>
2023-03-08 17:24:06 -0600

    Wednesday Lecture

--- 3-Module/4-week/3-day/scratch.js
+++ 3-Module/4-week/3-day/scratch.js
@@ -1,3 +1,10 @@
+/*
+API - Application Programming Interface
+Abstracts away the inner workings of a package and allows you to focus on input and output.
+Web API is like a standard API except its over the internet.
+You only need to worry about the request and response but not the websites' inner workings.
+*/
+
 const http = require('http');
 const parseForm = require("./parseForm");
 
@@ -12,15 +19,17 @@ const server = http.createServer((req, res) => {
     let reqType = req.headers['content-type']
     if (reqBody && reqType === "application/json") {
       //Parse Our JSON
+      req.body = JSON.parse(reqBody);
       console.log('JSON: ', req.body);
     }
     if (reqBody && reqType === "application/x-www-form-urlencoded") {
       req.body = parseForm(reqBody);
+      // "key1=wow&key2=32&key3=%5B18%2C22%2C16%5D"
       console.log('URL ENCODED: ', req.body);
     }
 
     // Add the res.body to the res body as JSON
-    
+    res.end(JSON.stringify(req.body))
   });
 });
 
