97962fd5c8a69ccd0ec6fe00a857a4759d5dc60f
tristan <thuckabee.work@gmail.com>
2023-03-14 21:14:24 -0500

    lol

---
+++ 3-Module/4-week/2-day/parseInput.js
@@ -0,0 +1,36 @@
+function formatComments(arr) {
+  let res = '<ul>';
+  arr.forEach(obj => {
+    res += `<li>${obj.content}</li>`
+  })
+  res += '</ul>'
+
+  return res;
+}
+function parseBody(body) {
+  let res = body.split('&');
+  res = res.map(el => el.split('='));
+  res.forEach(el => {
+    el.forEach((str,i) => {
+      el[i] = str.replaceAll('+', ' ');
+    })
+  })
+  res.forEach(el => {
+    el.forEach((str,i) => {
+      el[i] = decodeURIComponent(str);
+    })
+  })
+  return res.reduce((accum, [key,val]) => {
+    accum[key] = val;
+    return accum;
+  }, {});
+}
+
+module.exports = {
+  formatComments,
+  parseBody
+}
+// export {
+//   formatComments,
+//   parseBody
+// }
