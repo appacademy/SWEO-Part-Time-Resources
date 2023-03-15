4a2fffc2172fbcaaf1dfbdef2fcb698191400b87
James Thompson <thompsonjames0120@gmail.com>
2023-03-15 16:48:30 -0500

    lecture notes

---
+++ 6-Module/1-week/2-day/scratch.py
@@ -0,0 +1,66 @@
+# print(True and False)
+# print(True or False)
+# print(True and not False) # true && !false
+
+
+
+# print(len('james')) # len similar to .length
+
+# print('james'[-4])
+
+# something = 'something'
+# something_1 = 'something 11111'
+
+# print(something[4:-2])
+
+# print(something + ' ' + something)
+
+# print((something + ' ' )* 3)
+# formatted_word = '{0} !!! {1}'.format(something, something_1)
+# print(formatted_word)
+
+# this_string = f'this is {something} and this is {something_1}'
+
+# print(this_string)
+
+# print(20 / 3)
+# print(20 // 3)
+
+# function someName(){
+#     return something
+# }
+
+# def my_func(str):
+#     if len(str) > 3:
+#         return str + 'longgggg'
+    
+#     return str + 'shorttttt'
+
+
+
+# print(my_func('james'))
+# print(my_func('ja'))
+
+# my_func = lambda str : str + '!!!'
+
+# my_dict = {'a': 'b'}
+
+# print(my_dict)
+
+
+# Write your function, here.
+# Write your function, here.
+
+
+
+# def not_equal(num1, num2):
+#     return num1 != num2
+
+# not_equal = lambda num1, num2 : num1 != num2
+
+
+# print(not_equal(0, 2))   #>  True
+# print(not_equal(2, 2))   #>  False
+# print(not_equal(2, "2")) #>  True
+
+print(12 ^ 2)
