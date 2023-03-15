800a494ccd2fab936908c9e4e8320485eb2137c5
James Thompson <thompsonjames0120@gmail.com>
2023-03-14 19:15:20 -0500

    added readme

---
+++ 6-Module/1-week/2-day/scratch.py
@@ -0,0 +1,49 @@
+# print(True and False)
+# print(True or False)
+# print(True and not False)
+
+# print('something')
+
+# print(True and not"")
+
+# print(len('james'))
+
+# print('james'[-3])
+
+# word_1 = 'hello'
+# word_2 = 'world'
+# format_sentence = '{0} small {1}!!!'.format(word_2, word_1)
+# fstring_sentence = f'{word_1} small {word_2}!'
+
+# print(fstring_sentence)
+
+# my_word = 'something'
+
+# my_word = 'something_else'
+
+# print(my_word[4:])
+
+# def my_func(str):
+#     if len(str) > 3:
+#         str += 'long'
+#         return str
+#     return str + 'short'
+
+# print(my_func('ja'))
+# print(my_func('jamesss'))
+
+# print(27 // 2)
+# print(27 / 2)
+
+# def add_one(num):
+#     num += 1
+#     return num
+
+# print(add_one(12))
+
+def not_equal(num1, num2):
+    return num1 != num2
+
+print(not_equal(0, 2))   #>  True
+print(not_equal(2, 2))   #>  False
+print(not_equal(2, "2")) #>  True
