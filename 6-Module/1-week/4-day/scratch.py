407998920e8b44f8fdf7a34cbba44ab2f355d36f
crystalchavez99 <redfire1205@hotmail.com>
2023-03-16 16:41:38 -0700

    added readme

---
+++ 6-Module/1-week/4-day/scratch.py
@@ -0,0 +1,73 @@
+# FUNCTIONS
+
+# def is_even(num):
+#     return num % 2 == 0
+
+# print(is_even(5))
+# print(is_even(2))
+
+# LAMBDA FUNCTIONS
+# is_even = lambda num: num % 2 == 0
+
+# print(is_even(8))
+
+# LISTS
+# empty = []
+# # print(empty)
+
+# fruits = ["banana", "apple", "kiwi"]
+# # print(fruits)
+
+# print(fruits[0])
+# print(fruits[1:2])
+
+# TUPLES
+# a = (1, 2,3,4,5,6,7,8)
+# # print(a)
+# b = ('a','b','c', 'd')
+# # print(b)
+# # a.append(9)
+# a += (9,)
+# print(a)
+# c = (1,)
+# print(c)
+
+# RANGE
+# number_range = range(10)
+# # print(number_range)
+# print(list(number_range))
+
+# reversed = range(51, 5, -3)
+# print(list(reversed))
+
+# items = [1,2,3]
+# for i in range(len(items)):
+#     print(i, items[i])
+
+# for i in range(1, 10, 2):
+#     print(i)
+
+# DICTIONARIES
+# a = {'one': 1, 'two': 2, 'three': 3}
+# print(a)
+# print(a.one)
+# # print(a['one'])
+# # print(a['new'])
+# print(a.get('one', None))
+# del a['three']
+# print(a)
+# a['one'] = 11
+# print(a)
+
+# SET
+# a = {}
+# print(type(a))
+# b = set()
+# print(type(b))
+
+# my_list = [5,2,3,4,1]
+# print(set(my_list))
+# my_string = "hello"
+# print(set(my_string))
+# my_tuple = (1,1,1)
+# print(set(my_tuple))
