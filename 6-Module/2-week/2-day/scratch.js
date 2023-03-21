e266af483dfcb5d3b7f3d4bcd5b39e138ad1f028
James Thompson <thompsonjames0120@gmail.com>
2023-03-21 18:04:12 -0500

    added notes

---
+++ 6-Module/2-week/2-day/scratch.js
@@ -0,0 +1,15 @@
+// const js_decorator = (func_being_decorated) => {
+//     return (another_arg) => {
+//         another_arg += 'something happened';
+//         return func_being_decorated(another_arg);
+//     };
+// };
+
+// let my_func = (arg) => {
+//     // Pretend this function does something
+//     return arg;
+// };
+
+// my_func = js_decorator(my_func);
+
+// console.log(my_func('Pretend '));
