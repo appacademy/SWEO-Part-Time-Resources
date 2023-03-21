e938d6f97c608c6790b6417f692fc52995f00f39
James Thompson <thompsonjames0120@gmail.com>
2023-03-21 17:12:53 -0500

    added notes

---
+++ 6-Module/2-week/2-day/scratch.py
@@ -0,0 +1,26 @@
+# names = ['James', 'Seika', 'Eiki']
+# pets = {
+#     'James': ['Momo', "Tenten", 'Kiki'],
+#     'Seika': ['Tora', 'Sonic', 'Dragon'],
+#     'Eiki': ['Hime', 'Koga']
+# }
+# name_iter = iter(names)
+# pet_iter = iter(pets)
+
+# print(name_iter)
+# print(pet_iter)
+
+# print(next(name_iter))
+# print(next(name_iter))
+# print(next(name_iter))
+# print(next(pet_iter))
+# print(next(pet_iter))
+
+# my_dict = {'hello': 'world'}
+# print(my_dict)
+
+# other_dict = dict(first=1, second=2, third=3) #naming with kwargs
+# print(other_dict)
+# hello = 'hello'
+# print(my_dict[hello])
+# print(my_dict.hello)
