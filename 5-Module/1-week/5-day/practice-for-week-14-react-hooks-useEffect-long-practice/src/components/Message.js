f82e24a7a60d760967cfd1502c2c26b79df2d9c5
will <w.duffy@outlook.com>
2023-04-08 16:26:15 -0500

    T

---
+++ 5-Module/1-week/5-day/practice-for-week-14-react-hooks-useEffect-long-practice/src/components/Message.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+
+function Message({ size, featherCount }) {
+
+// function Message({ size }) {
+  // console.log('Message', size);
+// }
+
+  const [sizeClass, setSizeClass] = useState('');
+  const [message, setMessage] = useState('');
+
+  useEffect(() =>{
+    if (featherCount <= 0)
+      setMessage('Oh my! Your bird is naked!');
+    else if (featherCount >= 10) {
+      setMessage('Full turkey!');
+    } else {
+      setMessage('Coming along...');
+    }
+  }, [featherCount]);
+
+  useEffect(() => {
+    console.log('Message', size);
+    let cname = '';
+    switch (size) {
+      case 'm':
+        cname = 'medium';
+        break;
+      case 'l':
+        cname = 'large';
+        break;
+      case 'xl':
+        cname = 'xlarge';
+        break;
+      default:
+        cname = 'small';
+        break;
+    }
+    setSizeClass(cname);
+  }, [size]);
+
+
+  return (
+
+    <div className={`message ${sizeClass}`}>
+      {message}
+    </div>
+
+  );
+};
+
+export default Message;
