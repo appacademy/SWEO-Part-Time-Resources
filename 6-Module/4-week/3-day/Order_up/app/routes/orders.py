a39d3a618616ac491e3ba5c53bdfc6bfe413b85e
James Thompson <thompsonjames0120@gmail.com>
2023-04-13 17:06:47 -0500

    added solution

---
+++ 6-Module/4-week/3-day/Order_up/app/routes/orders.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+from flask_login import login_required
+
+
+bp = Blueprint('orders', __name__, url_prefix='')
+
+
+@bp.route('/')
+@login_required
+def index():
+    return render_template("orders.html")
