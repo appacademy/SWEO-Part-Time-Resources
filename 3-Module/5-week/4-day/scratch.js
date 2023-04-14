d3a313ea8fc744a04265b6c329ffb155a5803b26
“Alexi <alexi56@comcast.net>
2023-04-13 21:19:45 -0600

    April 13

---
+++ 3-Module/5-week/4-day/scratch.js
@@ -0,0 +1,7 @@
+//Window.onload vs DOMContentLoaded
+
+//Window.onload waits for literally EVERYTHING to load including the DOM, but also all assets (scripts, images);
+
+//DOMContentLoaded only waits for the DOM to be loaded
+
+//Event bubbling
