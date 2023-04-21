bdc280919e5ab060a9a654d073dd604eea275342
tristan <thuckabee.work@gmail.com>
2023-04-20 20:12:20 -0500

    east lecture

--- 4-Module/3-week/3-day/db/models/user.js
+++ 4-Module/3-week/3-day/db/models/user.js
@@ -1,7 +1,7 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
