d8bd77d90642c265e2d93bf16cb7d1d17659170f
tristan <thuckabee.work@gmail.com>
2023-04-20 20:11:42 -0500

    east lecture

--- 4-Module/3-week/4-day/scratch.js
+++ 4-Module/3-week/4-day/scratch.js
@@ -8,30 +8,56 @@ app.get('/', (req, res) => {
   res.send('Server is Live');
 });
 
-//* Change a single attribute
-// const calico = await Cat.findOne({
-//   where: {color: 'Calico'}
-// });
-// calico.name = 'Toby';
-// await calico.save()
-//* changing multiple attributes
-// const calico = await Cat.findOne({
-//   where: {color: 'Calico'}
-// });
-// calico.set({
-//   name: 'Toby',
-//   color: 'Orange'
-// })
-// await calico.save();
+//*SELECT
+  app.get('/cats', async (req, res) => {
+    // SELECT * FROM Cats;
+    const cats = await Cat.findAll()
+    // SELECT name, color FROM Cats
+    cats = await Cat.findAll({
+      attributes: ['color', 'name']
+    });
+  });
+  app.get('/cats/first', async (req, res) => {
+    // SELECT * FROM Cats LIMIT 1;
+    const cats = await Cat.findOne()
+    // SELECT name, color FROM Cats LIMIT 1;
+    cats = await Cat.findOne({
+      attributes: ['color', 'name']
+    });
+  });
+  app.get('/cats/:id', async (req, res) => {
+    // SELECT * FROM Cats LIMIT 1;
+    const id = req.params.id
+    const cat = await Cat.findByPk(id);
+  });
+//*INSERT
+  app.get('/', async (req, res) => {
+    const newCat = Cat.build({name: 'Snowball', color: 'White'});
+    await newCat.save();
 
-// merge set and save
-// const calico = await Cat.findOne({
-//   where: {color: 'Calico'}
-// });
-// calico.update({
-//   name: 'Toby',
-//   color: 'Orange'
-// })
+    newCat = Cat.create({name: 'Snowball', color: 'White'});
+  })
+//*UPDATE
+app.get('/', async (req, res) => {
+  const calico = await Cat.findOne({
+    where: {color: 'Calico'}
+  });
+  // calico.name = 'Toby';
+  // calico.set({
+  //   name: 'Toby',
+  //   color: 'Orange'
+  // });
+  // await calico.save();
+
+  calico.update({
+    name: 'Toby'
+  });
+})
+//*DELETE
+app.get('/cats/delete', async (req, res) => {
+  const cat = await Cat.findOne({ where: {id : 2} });
+  await cat.destroy();
+})
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
