aee52da042ddd219f5027a6517a255b5c807cc57
tristan <thuckabee.work@gmail.com>
2023-05-03 17:10:20 -0500

    east done

---
+++ 4-Module/5-week/2-day/scratch.js
@@ -0,0 +1,18 @@
+// CORS - Cross-Origin Resource Sharing
+// Indicates which sources (origins) are allowed to make requests to a server
+
+// npm install cors
+
+const cors = require('cors');
+
+// server setup
+
+app.use( cors({ origin: ["http://website.com"] }));
+
+const allowGoogle = () => {
+  cors({ origin: ["http://google.com"]})
+}
+
+app.get('/users', allowGoogle, (req, res) => {
+
+})
