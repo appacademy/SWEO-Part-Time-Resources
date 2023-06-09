ccff8144cee75f0cf8043d4e501afea88d3f66e5
KateCiz <ellie2joyful5@gmail.com>
2023-06-08 21:58:59 -0400

    lecture notes

---
+++ 4-Module/3-week/4-day/practice-for-week-11-combining-express-sequelize-short-practice/server/app.js
@@ -0,0 +1,122 @@
+// Instantiate Express and the application - DO NOT MODIFY
+const express = require('express');
+const app = express();
+
+const { Tree } = require('../db/models');
+const { Sequelize } = require('sequelize');
+
+// Import environment variables in order to connect to database - DO NOT MODIFY
+require('dotenv').config();
+
+// Express using json - DO NOT MODIFY
+app.use(express.json());
+
+
+app.get('/trees', async (req, res, next) => {
+    // SELECT * FROM trees;
+
+    const trees = await Tree.findAll({attributes: ['location', 'age']});
+
+    res.json(trees);
+
+    // res returns this to the backend
+    // {
+    //     species: 'Apple',
+    //     location: 'Vermont',
+    //     age: 40
+    //   }
+});
+
+app.get('/trees/apple', async (req, res, next) => {
+    // SELECT * FROM trees WHERE species='Apple';
+
+    const appleTrees = await Tree.findAll({
+        where: {species: 'Apple'},
+        order: [['age', DESC]],
+        limit: 5
+    });
+
+    res.json(appleTrees);
+
+    // res returns this to the backend
+    // {
+    //     species: 'Apple',
+    //     location: 'Vermont',
+    //     age: 40
+    //   }
+});
+
+app.post('/trees', async (req, res, next) => {
+    // SELECT * FROM trees WHERE species='Apple';
+
+    // need to save for it to update the database
+    // const plumTree = await Tree.build({species: 'Plum', location: 'maryland', age: 10});
+
+    
+    // await plumTree.save();
+    
+    // saves automatically
+    const plumTree = await Tree.create({species: 'Plum', location: 'maryland', age: 10});
+    await Tree.findOne({where: {species: 'Plum'}});
+
+    plumTree.location = 'New Zealand';
+
+    await plumTree.validate();
+
+    await plumTree.save();
+
+    res.json(plumTree);
+});
+
+app.put('/trees/:tree_id', async (req, res, next) => {
+    const tree = await Tree.findOne({where: {id: `${tree_id}`}});
+    const { location, age } = req.body;
+
+    // need to save for it to update the database
+    tree.set({
+        location,
+        age
+    });
+
+    await tree.save();
+
+    // saves automatically
+    tree.update({
+        location,
+        age
+    });
+});
+
+app.put('/trees/:species', async (req, res, next) => {
+    const { location, age } = req.body;
+
+    // saves automatically
+    await Tree.update(
+        {
+        location,
+        age
+    },
+    {
+        where: { species: { [Sequelize.Op.startsWith]: 'Pl'}}
+    }
+    );
+});
+
+app.delete('/trees', async (req, res, next) => {
+
+    await Tree.destroy();
+
+});
+
+app.delete('/trees/:tree_id', async (req, res, next) => {
+    const tree = await Tree.findOne({where: {id: `${tree_id}`}});
+    await tree.destroy();
+});
+
+
+
+
+
+// Set port and listen for incoming requests - DO NOT MODIFY
+const port = 5000;
+app.listen(port, () => console.log('Server is listening on port', port));
