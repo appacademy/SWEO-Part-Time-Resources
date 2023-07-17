b9af41ae7849f7e020a29e92ff0fcf25e9185b0a
“Alexi <alexi56@comcast.net>
2023-07-17 17:17:13 -0600

    July 17

--- 4-Module/5-week/5-day/scratch.js
+++ 4-Module/5-week/5-day/scratch.js
@@ -1,1 +1,21 @@
-asds
+//Regular action creator
+
+function addFruit(fruit) {
+    return {
+        type: ADD_FRUIT,
+        fruit
+    }
+}
+
+
+//Thunky action creator
+
+function thunkBoi() {
+    return function theRealThunk(dispatch) {
+        
+    }
+}
+
+const thunkBoi = () => (dispatch) => {
+
+}
