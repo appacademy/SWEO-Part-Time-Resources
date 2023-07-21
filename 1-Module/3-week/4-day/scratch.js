1062dbe4a17b03f07e693c8c749fc1664601bf34
yinyang117 <slcryan2010@yahoo.com>
2023-07-20 20:59:49 -0600

    4 probs into scratch

--- 1-Module/3-week/4-day/scratch.js
+++ 1-Module/3-week/4-day/scratch.js
@@ -1,67 +1,116 @@
-// let company = {
-//   name: "Coffee Co.",
-//   product: "coffee",
-//   location: "Seattle",
-//   ceo: {
-//     name: "Jared",
-//     age: 34,
-//   }
+/*
+Destructuring
+*/
+
+let bigArray = ["apple", 14, 32, 100, { name: "party" }, ["pineapple"]];
+
+let [ food, num1, num2, num3, obj, bestPizzaToppings, undef ] = bigArray
+
+console.log(food)
+console.log(num1)
+console.log(num2)
+console.log(num3)
+console.log(obj)
+console.log(bestPizzaToppings)
+console.log(undef)
+
+
+// let bigObj = {
+//   food: "apple", 
+//   num1: 14, 
+//   num2: 32, 
+//   num3: 100, 
+//   littleObj: { name: "party" }, 
+//   bestPizzaToppings: ["pineapple"]
 // }
 
-// // let name = company.name;
-// // let product = company.product;
-// // let location = company.location;
+// let {food, undef, ...objCopy} =  bigObj
 
-// let location = "Chicago";
+// objCopy['oh nooo'] = 'bad'
 
-// let { product, location: city, name, ceo: { name: ceoName, age } } = company;
+// // console.log(food)
+// console.log(objCopy)
 
-// console.log(ceoName, age)
+// console.log(undef)
 
-// let arr = [1, 2, 3];
 
-// let [banana, _cat, ant] = [1, 2, 3];
 
-// console.log(banana, ant);
 
-// let mylo = 300;
-// let jm = 1001;
-
-// // let temp = mylo;
-// // mylo = jm;
-// // jm = temp;
-
-// console.log(mylo, jm);
-// [mylo, jm] = [jm, mylo];
-// console.log(mylo, jm);
-
-// function sum(message, ...nums) {
-//   console.log(nums);
-//   let sum = 0;
-//   for (let i = 0; i < nums.length; i++) {
-//     let num = nums[i];
-//     sum += num;
-//   }
-//   return message + sum;
+/*
+Spread
+*/
+// const spreadItOut = (array1, array2) => {
+// 	return [...array1, ...array2]
 // }
 
-// console.log(sum("This is the sum: ",1,2,3,4,5));
+// console.log(spreadItOut([3,5,6], [1,2,3])); // => [3,5,6,1,2,3];
+// console.log(spreadItOut([], [1,2,3])); // => [1,2,3];
+// console.log(spreadItOut(["apple", "banana"], [1,2,3])); // => ["apple", "banana", 1, 2, 3];
 
-// Spread Operator
 
-// let biff = {
-//   type: "dog",
-//   age: 10,
-// };
 
-// let buster = { ...biff };
-// buster.age = 0;
 
-// console.log(biff, buster); // Two objs with age 0
-// console.log(biff === buster);
+/*
+Break down object 
+*/
 
-// let arr1 = [1, 2, 3];
-// let arr2 = [4, 5, 6];
-// let arr3 = [...arr1, "s", ...arr2, 9, 10, 12];
-// console.log(arr3)
+// let object1 = {name: 'Rupert', age: 5, speak: 'Meow'};
+// let object2 = {location: 'NY', borough: 'Brooklyn'};
+
+// const breakDownObj = obj => {
+// 	let keys = Object.keys(obj)
+// 	let vals = Object.values(obj)
+// 	return [...keys, ...vals]	
+// }
+
+// console.log(breakDownObj(object1));          // => [ 'name', 'age', 'speak', 'Rupert', 5, 'Meow' ]
+// console.log(breakDownObj(object2));          // => [ 'location', 'borough', 'NY', 'Brooklyn' ]
+
+
+
+/*
+Spread Rest
+*/
+
+
+// const arr = [1, "two", "3"];
+
+// const obj = {
+//     red: "circle",
+//     blue: "square",
+//     green: "hexagon"
+// }
+
+// let [...arrRest] = arr
+
+// let arrSpread = [...arr]
+
+// let {...objRest} = obj
+
+// let objSpread = {...obj}
+
+// console.log(arrRest);           // [1, "two", "3"]
+// console.log(arrSpread);         // [1, "two", "3"]
+
+// console.log(objRest);           // { red: "circle", blue: "square", green: "hexagon" }
+// console.log(objSpread);         // { red: "circle", blue: "square", green: "hexagon" }
+
+
+
+/*
+Obj Clone
+*/
+
+// const obj = {
+//     red: "circle",
+//     blue: "square",
+//     green: "hexagon"
+// }
+
+// let {green, ...objClone } = { yellow:'pentagon', ...obj }
+
+// console.log(objClone);          // { red: "circle", blue: "square", yellow: "pentagon" }
+// console.log(green);             // hexagon
+// // console.log(obj)				// should be unchanged
+
 
