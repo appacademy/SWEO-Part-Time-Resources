7ee66188913f19e9d68a3d2ca080ef6ee8d73636
tristan <thuckabee.work@gmail.com>
2023-07-24 15:57:40 -0500

    monday setup

---
+++ 6-Module/2-week/1-day/scratch.py
@@ -0,0 +1,16 @@
+## Iterables
+
+lfi = ['Tristan', 'Greg', 'Toby', 'Maria', 'Nancy']
+second_lfi = ['Huckabee', 'Isales', 'McGuire', 'Takeuchi', 'Drew']
+# Filter
+filtered_lst = 0
+# Map
+mapped_lst = 0
+# Sorted
+sorted_list = 0
+# Enumerate
+enumed_lst = 0
+# Zip
+zipped_lst = 0
+
+# SET OPERATIONS
