7b4057bbb367d697150933c016e03eeb9a26e2a8
yinyang117 <slcryan2010@gmail.com>
2023-08-01 20:50:48 -0600

    recursion product examples

--- 1-Module/5-week/2-day/scratch.js
+++ 1-Module/5-week/2-day/scratch.js
@@ -1,22 +1,44 @@
-function foo() {
-    debugger;
-    let res = 2 + bar();
-    debugger;
-    return res
+/*
+Below are 3 examples of a function that uses recursion
+to solve the same problem again and again.
+Set up a number and multiply every number from 1 up to that number given
+and return an end result of the final product.
+*/
+
+let recursiveProduct = (limit) => {
+	if (limit === 1) return 1
+	return recursiveProduct(limit - 1) * limit
+} 
+
+
+console.log(recursiveProduct(1))
+console.log(recursiveProduct(2))
+console.log(recursiveProduct(3))
+console.log(recursiveProduct(4))
+
+
+let dualArgProduct = (num, product = 1) => {
+	if (num === 0) return product
+	else return dualArgProduct(num - 1, product * num) 
 }
 
-function bar() {
-    debugger;
-    let res = 6 + baz();
-    debugger;
-    return res
+
+console.log(dualArgProduct(1))
+console.log(dualArgProduct(2))
+console.log(dualArgProduct(3))
+console.log(dualArgProduct(4))
+
+
+let restedProd = (...nums) => {
+	if (nums.length === 0) return 1
+	else {
+		let num = nums.pop()
+		return num * restedProd(...nums)
+	}
 }
 
-function baz() {
-    debugger;
-    let res = 3;
-    debugger;
-    return res;
-}
 
-console.log(foo());
+console.log(restedProd(1))
+console.log(restedProd(1, 2))
+console.log(restedProd(1, 2, 3))
+console.log(restedProd(1, 2, 3, 4))
