d5b175359bca8b4cf68b6c9f4983ea027783ceee
yinyang117 <slcryan2010@gmail.com>
2023-08-01 20:52:16 -0600

    small update

--- 1-Module/5-week/2-day/rec_sum.js
+++ 1-Module/5-week/2-day/rec_sum.js
@@ -2,11 +2,8 @@
 Write a function called rec_sum that intakes a number, and adds up all
 numbers from 1 to the number and returns the sum. Must be recursive.
 We're expecting all nums to be >= 1
-
-
-
-
 */
+
 function rec_sum(num) { 
   debugger;
   // Base Case 
