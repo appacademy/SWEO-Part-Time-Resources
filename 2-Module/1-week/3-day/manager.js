a1dff533710353a88ce2689fe804211c93fce337
yinyang117 <slcryan2010@gmail.com>
2023-08-17 18:22:04 -0600

    more comments

--- 2-Module/1-week/3-day/manager.js
+++ 2-Module/1-week/3-day/manager.js
@@ -10,16 +10,16 @@ class Manager extends Employee {
 		this.employees.push(employee)
 	}
 
-	calculateBonus(mult) {
-		let bonus = (this.salary + this._totalSubSalary(this)) * mult
+	calculateBonus(mult) { // overrides Employee version with polymorphism
+		let bonus = (this.salary + this._totalSubSalary(this)) * mult // we know when this version of calcBonus is being used, it's on a Manager, so we use _totalSS to get all the employee salaries
 		return bonus
 	}
 
 	_totalSubSalary() {
 		let sal = 0
-		this.employees.forEach(e => {
-			if (e instanceof Manager) sal += e.salary + e._totalSubSalary();
-			else sal += e.salary;
+		this.employees.forEach(e => { // this is the manager object instance employee array
+			if (e instanceof Manager) sal += e.salary + e._totalSubSalary(); // if there's sub managers with sub employees, recurse and "this" changes to the sub manager in the next call stack
+			else sal += e.salary; // normal employee just needs to add salary
 		});
 		return sal
 	}
