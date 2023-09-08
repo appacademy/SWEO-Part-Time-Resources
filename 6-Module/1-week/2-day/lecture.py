ef306dfecb1ae504c1285776d7eddcf545741586
AlexBetita <alexbheb25@gmail.com>
2023-09-07 18:05:24 -0700

    update lecture.py

---
+++ 6-Module/1-week/2-day/lecture.py
@@ -0,0 +1,39 @@
+# primitive types
+# int / Number
+# float / Number
+# bool / Boolean
+# str / String
+
+x = 1
+y = 1.0
+z = True
+a = "hello"
+a = 'hello'
+a = """
+hello
+"""
+
+str_ = ''
+
+# snake_case
+# is_token = token
+
+# camelCase
+# const isToken = token
+
+# reference types
+# dictionary
+l = {"key": "value"}
+l["key"] # how to key in a value
+
+# list
+# mutable
+# takes in any data type
+o = [1,2,3,4,5]
+o[0] # 1
+o[-1] # 5
+# ranges
+# [inclusive:exclusive]
+# [from:to]
+o[1:4] # 
+# sequences
