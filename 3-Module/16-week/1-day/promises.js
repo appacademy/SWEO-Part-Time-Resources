df03fb0a8aebe15c08c93218a789c51a827bfcf4
yinyang117 <slcryan2010@gmail.com>
2023-10-16 21:43:08 -0600

    pst promises walkthrough

--- 3-Module/16-week/1-day/promises.js
+++ 3-Module/16-week/1-day/promises.js
@@ -1,31 +1,29 @@
 let newPromise = new Promise((res, rej) => {
-	setTimeout(() => {
-		if (false) res('Ive been fulfilled!');
-		else rej('Ive been rejected')
-	}, 1000);
+	if (true) { // some logic happens, and its a good result
+		console.log('Im a success!')
+		setTimeout(() => res(12345), 500)
+	} else { // Not what we wanted, rejection
+		console.log('Im a failed promise!')
+		rej('this is the rej argument?')
+	}
 })
 
-let asyncyFunc = () => {
-	newPromise
-		.then(res => {
-			console.log('.then our res', res)
-		})
-		.then(res => {
-			console.log('.then our res', res)
-		})
-		.then(res => {
-			console.log('.then our res', res)
-		})
-		.catch(rej => {
-			console.log('our rej', rej)
-		})
+let asyncFuncWrapper = async () => {
+	try {
+		console.log(newPromise)
+		await newPromise
+		console.log('still in the try')
+		console.log(newPromise)
+	} catch (error) {
+		console.log('error has been caught', error)
+	}
 }
 
-async
-try {
+newPromise
+	.then(() => {},
+	() => {})
+	.catch()
 
-} catch (e) {
-	console.log(e)
-}
 
-asyncyFunc()
+asyncFuncWrapper()
+
