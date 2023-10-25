7c3a5f475b9abacff490006ab266795ede581505
yinyang117 <slcryan2010@gmail.com>
2023-10-24 18:26:57 -0600

    html cand node collections with console.logs

---
+++ 3-Module/17-week/2-day/scratch.js
@@ -0,0 +1,14 @@
+// returns NodeList (static and live)
+const fruits = document.querySelectorAll('.fruits');
+console.log(fruits)
+
+// const fruits = document.querySelector('.fruits');
+// const childFruit = fruits.childNodes;
+// console.log(fruits)
+// console.log(childFruit)
+
+// // returns HTMLCollection (using item method)
+// const fruits = document.getElementsByClassName('fruits');
+// console.log(fruits.item(0))
+// fruits.item(0).classList.add('apples')
+// console.log(fruits.item(0))
