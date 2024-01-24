16bf384ad3aa049e9e13a624b9268c8c6ed48d93
“Alexi <alexi56@comcast.net>
2024-01-23 17:21:42 -0700

    day 1

---
+++ 1-Module/1-week/2-day/scratch.js
@@ -0,0 +1,92 @@
+//console.log(5)
+
+// Single Line Comment - ANYTHING I WRITE HERE WILL BE IGNORED BY JS
+
+/* Multi Line Comment
+ALL
+OF
+THESE 
+LINES
+WILL BE IGNORED
+*/
+
+
+//BASIC DATA TYPES
+
+//STRINGS
+// Wrapped in single or double quotes and can contain any characters
+// 'string' or "string"
+
+//console.log('Hi, my name is "Alex".')
+
+//NUMBERS
+//Numbers, decimal, whole, negative
+
+// console.log(5);
+// console.log(-25);
+// console.log(45.23457567)
+
+//BOOLEAN
+// true or false
+
+// console.log(true)
+// console.log(false)
+
+//UNDEFINED
+//The lack of a value
+//console.log(undefined)
+
+
+
+//VARIABLES - Box that we put values/data into
+
+//Instead of this:
+
+// console.log('alsjkdfsdljfdsjfhdsjfhdsfjhdskjfhdskjfhdskjfhdskjfhskjfhkjh')
+// console.log('alsjkdfsdljfdsjfhdsjfhdsfjhdskjfhdskjfhdskjfhdskjfhskjfhkjh')
+// console.log('alsjkdfsdljfdsjfhdsjfhdsfjhdskjfhdskjfhdskjfhdskjfhskjfhkjh')
+// console.log('alsjkdfsdljfdsjfhdsjfhdsfjhdskjfhdskjfhdskjfhdskjfhskjfhkjh')
+// console.log('alsjkdfsdljfdsjfhdsjfhdsfjhdskjfhdskjfhdskjfhdskjfhskjfhkjh')
+
+
+//We can do this:
+
+// //VARIABLE DECLARATION
+// let userName;
+
+// //VARIABLE ASSIGNMENT:
+// let userName2 = 'alsjkdfsdljfdsjfhdsjfhdsfjhdskjfhdskjfhdskjfhdskjfhskjfhkjh';
+
+// let undefinedVariable = undefined;
+
+// let number = 5;
+
+// let bool = true;
+
+// console.log(userName2);
+
+
+
+let userName = 'apples';
+
+console.log(userName);
+
+userName = 'King';
+
+console.log(userName)
+
+// userName = "Pear";
+
+// console.log(userName)
+
+//TWO OTHER WAYS TO DECLARE VARIABLES
+
+var userName2; //NOBODY LIKES VAR
+
+const userName3 = 'Bubbles'; //Stands for constant
+
+// userName3 = 'Puppies';
+
+// console.log(userName3)
+
+//OLD WAY: var CONSTANT
