8bb49a582b8df82ec53113e1c9f20dcce0b4ea73
“Alexi <alexi56@comcast.net>
2024-01-24 12:04:19 -0700

    day one jan

---
+++ 1-Module/1-week/2-day/scratch.js
@@ -0,0 +1,117 @@
+//console.log('Hello World!')
+
+// SINGLE LINE COMMENT - Javascript will ignore this
+
+/* 
+MULTI LINE COMMENT
+
+Javascript will ignore ALL of this
+*/
+
+//DATA TYPES
+
+//STRING - A collection of characters surrounded by single or double quotes
+
+//console.log('ALEX')
+// console.log('And then he said, "Boo"')
+// console.log("Let's go out!!")
+
+//NUMBERS - decimal, whole, negative
+
+// console.log(5);
+// console.log(-345);
+// console.log(43.678354);
+
+//BOOLEAN - true or false
+
+// console.log(true);
+// console.log(false);
+
+//UNDEFINED - Lack of being given a value
+
+//console.log(undefined)
+
+
+//VARIABLES - Kind of like a box. We use them to store information.
+
+// let boxOfCats = 'bunch of cats';
+
+// console.log(boxOfCats)
+
+let userName = 'a;skdjas;kjdsa;kfj;askjf;dkajfkdsjlfkdnskfjbakfjabdkjaksjdbaskjdbkasjdbkjasbdkjasbdjb'
+
+// console.log(userName)
+// console.log(userName)
+// console.log(userName)
+// console.log(userName)
+
+
+//VARIABLE DECLARATION - Create a variable, but DO NOT assign it a value
+let fruit;
+
+
+//VARIABLE ASSIGNMENT - When we take a variable that already exists and THEN we ASSIGN IT A VALUE
+fruit = 'apple';
+
+//VARIABLE INITIALIZATION - When we create and assign a value to a variable at the same time
+
+let fruit2 = 'pear';
+
+
+//IN JS THE EQUAL SIGN IS THE ASSIGNMENT OPERATOR
+
+// let userName2 = 'Alex';
+
+// console.log(userName2)
+
+// userName2 = 'Bryan';
+
+
+// console.log(userName2)
+
+
+
+//COMPARISON OPERATORS
+// >, <, == , === , != , !==
+
+//console.log(3 < 2)
+
+// LOOSE EQUALITY- Double equals == - DOES NOT CARE about the DATA TYPE, it only cares about the VALUE
+
+//console.log('13' == 13);
+
+// STRICT EQUALITY - triple equal === - Is only equal if the DATA TYPE AND THE VALUE are the same
+
+//console.log('13' === 13);
+// console.log(15 === 15);
+// console.log('17' === '17')
+
+// console.log('15' === '17')
+
+
+
+//ARITHMETIC OPERATORS 
+//    +, -, *, /         %
+
+//   % Modulo Operator - It gives us the remainder of a division problem
+
+// console.log(6 % 3)
+
+// console.log(4 + 5)
+
+
+//LOGICAL OPERATORS 
+//     ! (BANG) NOT   - Flips a true or false value to the opposite value  
+
+//console.log(!true)
+
+
+//         OR ||    - Will evaluate to true if one value is true
+
+console.log(4 > 5 || 7 > 5)
+
+//        AND &&   - Will only evaluate to true if ALL expressions are true
+
+console.log(4 > 5 && 7 > 5)
+
+console.log(true && false && true)
