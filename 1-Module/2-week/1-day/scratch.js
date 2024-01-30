74a83b3965a60656baae70633aad4073d41c883a
“Alexi <alexi56@comcast.net>
2024-01-29 20:52:35 -0700

    week 2 day 1

---
+++ 1-Module/2-week/1-day/scratch.js
@@ -0,0 +1,31 @@
+//SEMI COLON CHEAT SHEET
+
+//MOST OF THE TIME in JS the semi colon is optional
+
+//IN JS if you have more than one statement on ONE LINE you MUST HAVE a semicolon
+
+// let apple = 'apple'; let fruit = 'pear'; //THIS IS BAD PRACTICE
+
+// console.log(); console.log();
+
+// let fruit2 = 'lemon'
+
+// for (let i = 0; i < lasjdhljs; i++) {}
+
+// //DO NOT put semicolons after {} blocks
+// //It won't break anything, but it's weird
+
+// if (blue) {
+
+// } //Completely unnecessary
+
+
+
+let fruits = ['apple', 'banana', 'lemon', 'cherry', 'pineapple', 'orange'];
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log('OUTER LOOP ------------------')
+    for (let j = i + 1; j < fruits.length; j++) {
+        console.log(fruits[i], fruits[j])
+    }
+}
