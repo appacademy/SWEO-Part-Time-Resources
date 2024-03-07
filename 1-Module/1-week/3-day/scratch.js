8fe279bbd54f6c0a8ec42182f0f21df6777390e1
Jesse Brooks <brooks.jesse83@gmail.com>
2024-03-06 16:40:19 -0800

    lecture notes

---
+++ 1-Module/1-week/3-day/scratch.js
@@ -0,0 +1,92 @@
+// let myFirstName = 'Jesse'
+// let myFirstName = "Jesse"
+
+// let sentence = 'Jesse said: "Hello World" '
+// let sentence = "Jesse's cat is named Cow"
+
+// let anotherSentence = "   Jesse's cat said: \"meow\"   "
+
+// console.log(myFirstName.length)
+// console.log(sentence.length)
+// console.log(anotherSentence.length)
+
+// let fruit = 'appleXD145ZZZZZZZZZZZZZZZZZZZZZZZZB'
+//           01234
+// console.log(fruit[5+5*25/5-5])
+// console.log(fruit[1])
+// console.log(fruit[2])
+// console.log(fruit[3])
+// console.log(fruit[4])
+
+// console.log(fruit[5])
+
+// let x = 5
+
+// console.log(fruit[x])
+
+// console.log(fruit.length - 1)
+
+// console.log(fruit[fruit.length - 1])
+
+// console.log(5 + 5)
+// console.log("Jesse" + " " + "Brooks")
+
+// let movie = 'Dune'
+// let vegetable = 'lettuce'
+//               0123456
+
+// console.log(movie.indexOf('D'))
+// console.log(movie.indexOf('Z'))
+
+// console.log('Dune'.indexOf('D'))
+
+// console.log(vegetable.indexOf('t'))
+// console.log(vegetable.lastIndexOf('t'))
+
+// let fruit;
+
+// function daysOfTheWeekPrinter(name){
+//     console.log('Monday')
+//     console.log('Tuesday')
+//     console.log('Wednesday')
+//     console.log('Thursday')
+//     console.log('Friday')
+//     console.log('Saturday')
+//     console.log('Sunday')
+// }
+
+// console.log(daysOfTheWeekPrinter)
+
+// console.log(daysOfTheWeekPrinter('Jesse'))
+
+// function namePrinter(name, age){
+//     console.log("Hello, my name is " + name)
+//     console.log("My age is " + age)
+// }
+
+// namePrinter("Jesse", 27)
+// namePrinter("April")
+// namePrinter("Tyler")
+
+let x = 5
+
+function addOne(number){
+    number = number + 1
+    console.log(number)
+    // return number
+}
+
+// console.log(addOne(x))
+
+// console.log("X equals: " + x)
+
+
+
+x = addOne(x)
+let y = addOne(10)
+
+console.log("X equals: " + x)
+console.log("Y equals: " + y)
+
+// console.log(x)
+// console.log(y)
