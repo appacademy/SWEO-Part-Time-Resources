a0717a34627a0a352b24143aebf75cc1d21cd006
irelius <kihong711@hotmail.com>
2024-03-12 21:04:56 -0700

    correcting spelling error in readme

---
+++ 1-Module/2-week/2-day/scratch.js
@@ -0,0 +1,61 @@
+// -------------------------------- SPLICE --------------------------------
+// let myArray = ["Hello", "my", "name", "cat's", "is", "Garfield."]
+
+// myArray.splice(1, myArray.length - 1, "world")
+
+// console.log(myArray)
+
+
+// let myArray = ['apple', 'orange', 'watermelon', 'grape', 'banana']
+
+// let cutFruits = myArray.splice(1, 3) // this will delete 3 items, starting at index 1. It will then store the deleted items into the "cutFruits" variable as an array
+
+
+// console.log('myArray 2:', myArray) // Prints: ['apple', 'banana']
+// console.log(cutFruits) // Prints: ['orange', 'watermelon', 'grape']
+
+
+
+// ---------------------------- JOIN and SPLIT ----------------------------
+
+// // SPLIT
+// let string = "I dream of electric sheep"
+// let splitArray = string.split("") // this will split the string using ""
+
+// console.log(splitArray) // Prints: ???
+
+
+
+// // JOIN
+// let array = ["hello", "world", "it's", "good", "to", "see", "you"]
+
+// let joinedString = array.join(" ") // this will join the array using ","
+
+// // console.log(joinedString)
+
+// let splitAgain = joinedString.split(" ") // this will split the joinedArray for each " "
+
+// console.log(splitAgain) // Prints: ???
+
+
+// let joinAndSplit = array.join(" ").split(" ").join(" ")
+// console.log(joinAndSplit)
+
+// let joined = array.join("asdfasdgagrs")
+// console.log(joined)
+
+
+
+// // Why use split or join
+// let string = "Vankouver"
+
+// let splitArray = string.split("")
+// splitArray[3] = 'c'
+
+// let joinedString = splitArray.join("")
+// console.log(joinedString)
+
+
+// let splitArray = string.split("k")
+// let joinedString = splitArray.join("c")
+// console.log(joinedString)
