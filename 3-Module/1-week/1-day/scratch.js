e670b996e8d056535ee3af70f361300428512c5f
“Alexi <alexi56@comcast.net>
2024-04-23 21:27:24 -0600

    week 13 day 1

---
+++ 3-Module/1-week/1-day/scratch.js
@@ -0,0 +1,36 @@
+//Cypress - testing framework thats used to test anything that runs in the browser
+
+//WCAG - Web content accessibility guidelines
+
+//The four principles of WCAG
+
+//1. Perceivable - All information and UI must be presentable in ways that users
+// can perceive
+
+//2. Operable - UI components and navigation must be operable
+
+//3. Understandable - The UI and all info that it holds must be understandable
+
+//4. Robust - Content must be robust enough to be interpreted by user agents including
+// assistive technologies
+
+//BASICS OF HTML
+
+//Hypertext Markup Language
+
+//3 main components of HTML
+
+//TAGS - 
+//Anything inside of the angled brackies wont be displayed in the browser
+//Anything between the TAGS themselves will be displayed in the browser
+//<h1>        </h1>
+
+//ELEMENTS -
+//A set of tags and everything inside of those tags
+//<p> A oaisjdosioaijdo iaoisjdsijd aoidjsoid </p>
+
+//ATTRIBUTES -
+//Used to define additional information about an element
+//<img src="catmeme.com/catboi"></img>
+
+//<!--  -->
