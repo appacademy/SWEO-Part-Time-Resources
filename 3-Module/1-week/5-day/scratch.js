7627abbf1418a34a946f971ee90ad2c32757e2a3
“Alexi <alexi56@comcast.net>
2024-04-29 16:52:49 -0600

    week 14 day 1

---
+++ 3-Module/1-week/5-day/scratch.js
@@ -0,0 +1,50 @@
+//Difference between EM and REM
+
+//EM - Relative unit of measurement, specifically relative to the element its contained within
+// Element measurement
+
+/* 
+h1 {
+    font-size: 10px;
+    padding: 1em
+}
+
+div {
+    font-size: 50px;
+    padding: 1em;
+}
+1em is 100% of the font-size of THIS ELEMENT so the padding is 10px;
+*/
+
+//REM - Is also a relative unit of measurement BUT its relative to the body font size
+
+/*
+
+body {
+    font-size: 12px;
+}
+
+h1 {
+    padding: 2rem;
+}
+
+*/
+
+//Breakpoints and media queries
+//What our website should look like at certain screen sizes
+
+//Media query 
+// width and height of a device
+// orientation 
+// resolution
+
+/* 
+
+@media mediatype (expression) {
+
+}
+
+@media screen ()
+*/
+
+//screen, print, speech 
