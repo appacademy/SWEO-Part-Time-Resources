1909a1e0af579a803f6d2f0f61ce7644fb743c33
Jesse Brooks <brooks.jesse83@gmail.com>
2024-05-01 15:01:42 -0700

    lecture notes

---
+++ 2-Module/2-week/3-day/scratch.js
@@ -0,0 +1,24 @@
+// Error examples
+
+// Uncaught SyntaxError: Unexpected identifier
+// funtion broken () {
+//     console.log("I'm broke");
+// }
+
+
+// ReferenceError: pupy is not defined
+// function callPuppy() {
+//     const puppy = "puppy";
+//     console.log(pupy);
+// }
+// callPuppy();
+
+
+// TypeError: dog is not a function
+// let dog; // Remember unassigned variables are undefined!
+// dog();
+
+// When you get an error referencing a specific function you are trying to use, look up the function behavior on mdn!
+// Start learning how to read your error messages
+
+// TTD
