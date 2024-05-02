7b3efb3f2f5a6d221fc02af3ecf6474fd534b931
“Alexi <alexi56@comcast.net>
2024-05-01 20:53:41 -0600

    week 14 day 3

---
+++ 3-Module/1-week/2-day/scratch.js
@@ -0,0 +1,58 @@
+//CSS - Cascading Style Sheets
+
+//HTML - Id's vs Classes
+//ID's are meant for ONE element and no more
+//CLASSES are for grouping multiple elements
+
+//We select HTML elements that we want to style and we change properties on those
+//elements
+
+//The main selectors
+
+//Type Selector - Will style every element of a specific tag
+
+//ID selectors - will select the element with the given id
+// You append a # infront of the id you wish to select
+
+//Class selector - will select the element(s) with a given class
+// You append a . infront of the class you wish to select
+
+//Universal selector - Will select literally every single element in the HTML
+// * 
+
+//Attribute selector - will match elements based on if they have a specific attribute
+//     tagname[attribute]
+
+//Compound Selector - Smushing two selectors together to make a VERY specific selector
+// .box.yellow      .box#id
+
+//Combinators - A type of css selector that combines selectors
+
+//Descedant Selector - Selects any element (B) that descends from (A)
+// A B {}
+
+//Direct Child Selector - Selects an element (B) that is the direct child of (A)
+
+//A > B{}
+
+//Adjacent sibling selector - Selects an element (B) that is an
+// ADJACENT sibling to (A)
+
+// A + B {}
+
+
+//Pseudoclasses - Defines a state of an element and with only style
+//it if it is in that state
+
+//Specificity - Some selectors take precedence over others
+//If you were styling the same property on the same element
+//with different selectors, which style would win?
+
+// INLINE STYLING
+
+// ID's
+
+//CLASSES - Attribute selectors - pseudoclasses
+
+//TAGS 
+
