b82b5091557b96aa4b5f7763d6edad3fd88f67ff
“Alexi <alexi56@comcast.net>
2024-05-08 16:50:30 -0600

    week 15 day 3

---
+++ 3-Module/3-week/3-day/scratch.js
@@ -0,0 +1,29 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'GET' && req.url === '/cats') {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/plain");
+        return res.end("Here are cats!")
+    }
+
+    let reqBody = '';
+
+    req.on('data', (data) => {
+        reqBody += data;
+    })
+
+    req.on('end', () => {
+        console.log(reqBody);
+    })
+
+    res.statusCode = 500;
+    res.setHeader("Content-Type", 'text/plain');
+    res.write('Hello!!');
+    res.write('Whats up!');
+    res.end('Last thing!')
+});
+
+const PORT = 5000;
+
+server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
