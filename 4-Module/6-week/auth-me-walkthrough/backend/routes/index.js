20487f1130816d4d2d3dd95082a39cf1e6ace12e
jt989073 <thompsonjames0120@gmail.com>
2024-07-15 21:45:04 -0500

    added csrf restore route

--- 4-Module/6-week/auth-me-walkthrough/backend/routes/index.js
+++ 4-Module/6-week/auth-me-walkthrough/backend/routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
+// router.get('/hello/world', function(req, res) {
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World!');
+// });
+// Add a XSRF-TOKEN cookie
+router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+      'XSRF-Token': csrfToken
+    });
+  });
 
 module.exports = router;
