bdcc023b758ade96b4aefe8cc0e87d13801ff63b
jt989073 <thompsonjames0120@gmail.com>
2024-07-15 19:59:14 -0500

    added error formatting middleware

--- 4-Module/6-week/auth-me-walkthrough/backend/app.js
+++ 4-Module/6-week/auth-me-walkthrough/backend/app.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
+
 const { environment } = require('./config');
 const router = require('./routes')
 
@@ -40,5 +42,40 @@ if (!isProduction) {
   );
 
   app.use(router)
+
+  // Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = 'Validation error';
+    err.errors = errors;
+  }
+  next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
 
 module.exports = app
