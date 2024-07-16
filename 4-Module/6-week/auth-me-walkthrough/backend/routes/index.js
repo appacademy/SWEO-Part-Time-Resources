875ef6e27fb011b730d235c4a5dae3ed2926fd61
jt989073 <thompsonjames0120@gmail.com>
2024-07-15 21:53:43 -0500

    added notes

--- 4-Module/6-week/auth-me-walkthrough/backend/routes/index.js
+++ 4-Module/6-week/auth-me-walkthrough/backend/routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
 //   res.send('Hello World!');
