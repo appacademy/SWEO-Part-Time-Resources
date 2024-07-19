55ebca9c155e238adfe1a2491807f088023d41ed
jt989073 <thompsonjames0120@gmail.com>
2024-07-18 19:33:57 -0500

    added signup route to users.js

--- 4-Module/6-week/auth-me-walkthrough/backend/routes/api/users.js
+++ 4-Module/6-week/auth-me-walkthrough/backend/routes/api/users.js
@@ -1,4 +1,32 @@
-const express = require('express')
+const express = require('express');
+const bcrypt = require('bcryptjs');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
 const router = express.Router();
 
+// Sign up
+router.post(
+    '/',
+    async (req, res) => {
+      const { email, password, username } = req.body;
+      const hashedPassword = bcrypt.hashSync(password);
+      const user = await User.create({ email, username, hashedPassword });
+  
+      const safeUser = {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+      };
+  
+      await setTokenCookie(res, safeUser);
+  
+      return res.json({
+        user: safeUser
+      });
+    }
+  );
+
+  
 module.exports = router;
