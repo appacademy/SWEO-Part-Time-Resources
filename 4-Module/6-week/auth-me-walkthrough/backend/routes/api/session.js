bd14e94686ead7bde785c3a8c949f663c5505481
jt989073 <thompsonjames0120@gmail.com>
2024-07-18 19:28:26 -0500

    added logout route

--- 4-Module/6-week/auth-me-walkthrough/backend/routes/api/session.js
+++ 4-Module/6-week/auth-me-walkthrough/backend/routes/api/session.js
@@ -40,4 +40,13 @@ router.post("/", async (req, res, next) => {
   });
 });
 
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
+
 module.exports = router;
