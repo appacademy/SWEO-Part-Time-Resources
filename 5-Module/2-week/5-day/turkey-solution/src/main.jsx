758976c8422bd7aa702bb863dd9bbef49a4a7b95
jt989073 <thompsonjames0120@gmail.com>
2024-08-16 18:10:36 -0500

    added solutions

---
+++ 5-Module/2-week/5-day/turkey-solution/src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+// To run the bonus files, comment out the preceding line and uncomment the
+// following line.
+// import App from './bonus/App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
