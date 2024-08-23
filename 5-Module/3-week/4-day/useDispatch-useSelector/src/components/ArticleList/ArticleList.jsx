486929dd31a6dc716e5ee451aaeb781ddf9fdc46
jt989073 <thompsonjames0120@gmail.com>
2024-08-22 19:56:30 -0500

    useSelector pract

---
+++ 5-Module/3-week/4-day/useDispatch-useSelector/src/components/ArticleList/ArticleList.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loadArticles } from "../../store";
+import { NavLink } from "react-router-dom";
+
+const ArticleList = () => {
+  const dispatch = useDispatch();
+  const articles = useSelector((state) => state.articles.entries);
+
+  useEffect(() => {
+    dispatch(loadArticles());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h1>Article List</h1>
+      <ol>
+        {articles &&
+          articles.map((article) => (
+            <li key={article.id}>
+              <NavLink to={`${article.id}`}>{article.title}</NavLink>
+            </li>
+          ))}
+      </ol>
+    </div>
+  );
+};
+
+export default ArticleList;
---
+++ 5-Module/3-week/4-day/useDispatch-useSelector/src/store/index.js
@@ -0,0 +1,6 @@
+import configureStore from './store';
+import { loadArticles } from './articleReducer';
+
+export {loadArticles}
+
+export default configureStore;
