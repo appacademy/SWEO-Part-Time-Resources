6370c4288d3847eae0b1c339e39225ea9bc2ce31
jt989073 <thompsonjames0120@gmail.com>
2024-09-17 20:00:26 -0500

    python day uno

---
+++ 6-Module/1-week/2-day/scratch.py
@@ -0,0 +1,34 @@
+# console.log('whatever') // js version
+# print('whatever')
+
+# def my_func(word_1, word_2):
+#     print(f"hey my name is {word_1}, nice to meet you {word_2}")
+    
+# my_func('james', 'roland')
+
+
+
+# questions = 20
+# num_correct = 10
+# score = f'You got {num_correct/questions:%}'
+# print(score)
+# score = f'You got {num_correct/questions:.1%}'
+# print(score)
+
+# print(22/7:%)
+# num_1 = 22
+# num_2 = 7
+# print(f"{num_1/num_2:%}")
+
+
+#Py
+# print(20 / 6) # 3.33333
+# print(23 // 6) # 3 integer division
+
+# num_1 = 3
+
+# num_1 += 1
+
+# print(num_1)
+
+print(20 % 6)
